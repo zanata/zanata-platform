ce292b8494a6174a3e321c0d734d28e956c0d6cb
Asgeir Frimannsson <asgeirf@redhat.com>
2009-05-05 16:58:53 +1000

    added admin action to import existing java locales

--- flies-web/src/hot/org/fedorahosted/flies/core/action/LocaleList.java
+++ flies-web/src/hot/org/fedorahosted/flies/core/action/LocaleList.java
@@ -1,8 +1,14 @@
 package org.fedorahosted.flies.core.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.framework.EntityQuery;
+import org.apache.commons.lang.StringUtils;
 import org.fedorahosted.flies.core.model.FliesLocale;
+
+import com.ibm.icu.util.ULocale;
 
 @Name("localeList")
 public class LocaleList extends EntityQuery<FliesLocale> {
@@ -10,4 +16,21 @@ public class LocaleList extends EntityQuery<FliesLocale> {
 		setEjbql("select locale from FliesLocale locale");
 	}
 
+	
+	public void importJavaLocales(){
+		ULocale [] locales = ULocale.getAvailableLocales();
+		List<String> addedLocales = new ArrayList<String>();
+		for(ULocale locale : locales){
+			FliesLocale fliesLocale = getEntityManager().find(FliesLocale.class, FliesLocale.getFliesId(locale));
+			if(fliesLocale == null){
+				fliesLocale = new FliesLocale(locale);
+				getEntityManager().persist(fliesLocale);
+				addedLocales.add(fliesLocale.getId());
+			}
+		}
+		if(addedLocales.size() > 0)
+			getFacesMessages().add("Added {0} locales: {1}", addedLocales.size(), StringUtils.join(addedLocales, ", "));
+		else
+			getFacesMessages().add("No locales added");
+	}
 }
