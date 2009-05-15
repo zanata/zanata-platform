3ea037c3f69bd0bf0dd58fcf6f5d5fb876058d47
Asgeir Frimannsson <asgeirf@redhat.com>
2009-05-15 17:41:05 +1000

    cleaning up community conversations

--- flies-web/src/hot/org/fedorahosted/flies/core/action/CommunityHome.java
+++ flies-web/src/hot/org/fedorahosted/flies/core/action/CommunityHome.java
@@ -4,36 +4,31 @@ import org.fedorahosted.flies.core.model.Community;
 import org.fedorahosted.flies.core.model.Person;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.web.RequestParameter;
+import org.jboss.seam.core.Conversation;
 import org.jboss.seam.framework.EntityHome;
 
 @Name("communityHome")
 @Scope(ScopeType.CONVERSATION)
 public class CommunityHome extends EntityHome<Community>{
 
-	@RequestParameter("communityId")
-	private Long communityId;
-
-	@Override
-	public Object getId() {
-		if(communityId != null)
-			return communityId;
-		return super.getId();
-	}
-	
-	@Override
-	@Begin
-	public void create() {
-		super.create();
-	}
-
 	@Override
 	protected Community createInstance() {
 		Community instance = super.createInstance();
 		instance.setOwner(getEntityManager().find(Person.class, 1l));
 		return instance;
+	}
+
+	@Begin(join = true)
+	public void validateSuppliedId(){
+		getInstance(); // this will raise an EntityNotFound exception
+					   // when id is invalid and conversation will not
+		               // start
+		Conversation c = Conversation.instance();
+		c.setDescription(getInstance().getName());
 	}
 	
 	public void cancel(){}
