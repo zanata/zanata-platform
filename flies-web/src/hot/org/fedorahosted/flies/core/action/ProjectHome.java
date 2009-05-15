8f07d1056a2903b7c1968a2565f9549625b3ff6d
Asgeir Frimannsson <asgeirf@redhat.com>
2009-05-15 11:35:49 +1000

    clean up project conversations and navigation

--- flies-web/src/hot/org/fedorahosted/flies/core/action/ProjectHome.java
+++ flies-web/src/hot/org/fedorahosted/flies/core/action/ProjectHome.java
@@ -1,36 +1,24 @@
 package org.fedorahosted.flies.core.action;
 
-import javax.persistence.EntityNotFoundException;
-import javax.persistence.NoResultException;
-
-import org.jboss.seam.ScopeType;
-import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Begin;
-import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.annotations.web.RequestParameter;
-import org.jboss.seam.framework.EntityHome;
-
 import org.fedorahosted.flies.core.model.Project;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.core.Conversation;
+import org.jboss.seam.framework.EntityHome;
 
 @Name("projectHome")
 @Scope(ScopeType.CONVERSATION)
 public class ProjectHome extends EntityHome<Project> {
 
-	@Override
-	@Begin
-	public void create() {
-		super.create();
-	}
-	
-	public String validateEntityFound() {
-		try {
-			this.getInstance();
-		} catch (EntityNotFoundException e) {
-			// TODO this exception will be caught earlier by Seam and a it will redirect to the error page.
-			return "invalid";
-		}
-
-		return this.isManaged() ? "valid" : "invalid";
+	@Begin(join = true)
+	public void validateSuppliedId(){
+		getInstance(); // this will raise an EntityNotFound exception
+					   // when id is invalid and conversation will not
+		               // start
+		Conversation c = Conversation.instance();
+		c.setDescription(getInstance().getName());
 	}
 
 	public void cancel(){}
