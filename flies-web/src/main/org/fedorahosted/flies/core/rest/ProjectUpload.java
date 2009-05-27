23ce1be14d24d40fc93903eebfa84f9b4aa69120
James Ni <kent.neo@gmail.com>
2009-05-27 18:36:07 +0800

    Add file uploading using atom with RESTEasy. Add a method to get one json file for multi projects in ProjectResource.java

---
+++ flies-web/src/main/org/fedorahosted/flies/core/rest/ProjectUpload.java
@@ -0,0 +1,29 @@
+package org.fedorahosted.flies.core.rest;
+
+import org.jboss.resteasy.plugins.providers.atom.Content;
+import org.jboss.resteasy.plugins.providers.atom.Entry;
+
+import java.io.*;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.lang.String;
+
+@Path("atom")
+public class ProjectUpload
+{
+   @POST
+   @Path("entry")
+   @Produces("application/atom+xml")
+   public String postProject(Entry entry) throws IOException
+   {
+      Content content = entry.getContent();
+      String cont = content.getText();
+      File file = new File("/home/jamesni/Desktop", "test.po");
+      PrintWriter out = new PrintWriter(new FileWriter(file));
+      out.print(cont);
+      out.close();
+      return "success";
+   }
+}
+
