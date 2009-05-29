a961bd9bfc28beebbe3a012be1646d2fdc820d01
Asgeir Frimannsson <asgeirf@redhat.com>
2009-05-29 17:37:43 +1000

    started test suite for repository

---
+++ flies-web-test/src/test/org/fedorahosted/flies/repository/model/DocumentTests.java
@@ -0,0 +1,47 @@
+package org.fedorahosted.flies.repository.model;
+
+
+
+import org.dbunit.operation.DatabaseOperation;
+import org.fedorahosted.flies.core.model.Person;
+import org.fedorahosted.flies.core.model.Project;
+import org.fedorahosted.flies.core.model.ProjectSeries;
+import org.fedorahosted.flies.core.model.ProjectTarget;
+import org.jboss.seam.mock.DBUnitSeamTest;
+import org.testng.annotations.Test;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Date;
+import static org.testng.Assert.*;
+
+public class DocumentTests extends DBUnitSeamTest {
+
+    protected void prepareDBUnitOperations() {
+        beforeTestOperations.add(
+                new DataSetOperation("org/fedorahosted/flies/repository/model/ProjectsBaseData.dbunit.xml", DatabaseOperation.CLEAN_INSERT)
+        );
+    }
+
+    @Test
+    public void traverseProjectGraph() throws Exception {
+        new FacesRequest() {
+
+            protected void invokeApplication() throws Exception {
+                EntityManager em = (EntityManager) getInstance("entityManager");
+                Project project = em.find(Project.class, 1l);
+                assertNotNull(project, "Failed to find project by Id");
+                
+                List<ProjectSeries> projectSeries =  project.getProjectSeries();
+                assertEquals(projectSeries.size(), 1, "Project should have n series");
+                
+                List<ProjectTarget> projectTargets = project.getProjectTargets();
+                assertEquals(projectTargets.size(), 1, "Project should have n targets");
+                
+                ProjectTarget target = projectTargets.get(0);
+                assertEquals(target.getId(), Long.valueOf(1l), "expected target with id 1");
+                
+            }
+        }.run();
+    }
+}
