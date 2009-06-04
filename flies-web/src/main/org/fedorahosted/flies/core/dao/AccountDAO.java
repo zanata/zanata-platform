fc3e4524c523403ff726464439c26dffebf6dba8
James Ni <kent.neo@gmail.com>
2009-06-04 23:40:26 +0800

    Add createApiKey method. Using "random salt"+username and md5 to generate api key.

--- flies-web/src/main/org/fedorahosted/flies/core/dao/AccountDAO.java
+++ flies-web/src/main/org/fedorahosted/flies/core/dao/AccountDAO.java
@@ -10,6 +10,10 @@ import org.hibernate.criterion.Restrictions;
 import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.util.Hex;
+
+import java.security.MessageDigest;
+import java.security.SecureRandom;
 
 @Name("accountDAO")
 @AutoCreate
@@ -33,5 +37,40 @@ public class AccountDAO {
                        .set("apiKey", apikey))
                      .uniqueResult();
         }
- 
+
+        public void createApiKey(Account account){
+                String username = account.getUsername();
+                String apikey = encryptUserName(username);
+                account.setApiKey(apikey);
+        }
+
+        public static String encryptUserName(String username){
+                try {
+                	byte[] salt = new byte[16];
+                	SecureRandom.getInstance("SHA1PRNG").nextBytes(salt);
+
+        		MessageDigest md5 = MessageDigest.getInstance("MD5");
+        		byte[] name = username.getBytes("UTF-8");
+                
+                	// add salt
+                	byte[] salted = new byte[name.length + salt.length];
+                	System.arraycopy(name, 0, salted, 0, name.length);
+                	System.arraycopy(salt, 0, salted, name.length, salt.length);
+                
+                	// generate md5 digest 
+                	md5.reset();
+                	byte[] digest = md5.digest(salted);
+
+                	// Appends salt bytes to md5 digest.
+                	byte[] salteddigest = new byte[digest.length + salt.length];  
+                	System.arraycopy(digest, 0, salteddigest, 0, digest.length);  
+                	System.arraycopy(salt, 0, salteddigest, digest.length, salt.length);  
+                
+                	return new String(Hex.encodeHex(salteddigest));
+                } catch (Exception exc) {
+            		throw new RuntimeException(exc);
+        	}
+
+        }
+
 }
