71d9859ddf20fbbfd96702c5d29ab57bb97211b3
Asgeir Frimannsson <asgeirf@redhat.com>
2009-06-05 10:36:42 +1000

    create a valid md5 key

--- flies-web/src/main/org/fedorahosted/flies/core/dao/AccountDAO.java
+++ flies-web/src/main/org/fedorahosted/flies/core/dao/AccountDAO.java
@@ -30,46 +30,39 @@ public class AccountDAO {
 		    .uniqueResult();
 	}
 
-        public Account getByApiKey(String apikey){
-                Session session = (Session) entityManager.getDelegate();
-                return (Account) session.createCriteria(Account.class)
-                       .add( Restrictions.naturalId()
-                       .set("apiKey", apikey))
-                     .uniqueResult();
-        }
+	public Account getByApiKey(String apikey) {
+		Session session = (Session) entityManager.getDelegate();
+		return (Account) session.createCriteria(Account.class).add(
+				Restrictions.naturalId().set("apiKey", apikey)).uniqueResult();
+	}
 
-        public void createApiKey(Account account){
-                String username = account.getUsername();
-                String apikey = encryptUserName(username);
-                account.setApiKey(apikey);
-        }
+	public void createApiKey(Account account) {
+		String username = account.getUsername();
+		String apikey = createSaltedApiKey(username);
+		account.setApiKey(apikey);
+	}
 
-        public static String encryptUserName(String username){
-                try {
-                	byte[] salt = new byte[16];
-                	SecureRandom.getInstance("SHA1PRNG").nextBytes(salt);
-        		MessageDigest md5 = MessageDigest.getInstance("MD5");
-        		byte[] name = username.getBytes("UTF-8");
-                
-                	// add salt
-                	byte[] salted = new byte[name.length + salt.length];
-                	System.arraycopy(name, 0, salted, 0, name.length);
-                	System.arraycopy(salt, 0, salted, name.length, salt.length);
-                
-                	// generate md5 digest 
-                	md5.reset();
-                	byte[] digest = md5.digest(salted);
+	public static String createSaltedApiKey(String username) {
+		try {
+			byte[] salt = new byte[16];
+			SecureRandom.getInstance("SHA1PRNG").nextBytes(salt);
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			byte[] name = username.getBytes("UTF-8");
 
-                	// Appends salt bytes to md5 digest.
-                	byte[] salteddigest = new byte[digest.length + salt.length];  
-                	System.arraycopy(digest, 0, salteddigest, 0, digest.length);  
-                	System.arraycopy(salt, 0, salteddigest, digest.length, salt.length);  
-                
-                	return new String(Hex.encodeHex(salteddigest));
-                } catch (Exception exc) {
-            		throw new RuntimeException(exc);
-        	}
+			// add salt
+			byte[] salted = new byte[name.length + salt.length];
+			System.arraycopy(name, 0, salted, 0, name.length);
+			System.arraycopy(salt, 0, salted, name.length, salt.length);
 
-        }
+			// generate md5 digest
+			md5.reset();
+			byte[] digest = md5.digest(salted);
 
+			return new String(Hex.encodeHex(digest));
+			
+		} catch (Exception exc) {
+			throw new RuntimeException(exc);
+		}
+
+	}
 }
