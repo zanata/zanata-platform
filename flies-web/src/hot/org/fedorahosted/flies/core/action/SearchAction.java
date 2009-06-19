9a2fd0222b036aeecf8bcea0b3ae444353355280
James Ni <kent.neo@gmail.com>
2009-06-19 18:25:26 +0800

    Add count of search results and simplify the SearchAction class.

--- flies-web/src/hot/org/fedorahosted/flies/core/action/SearchAction.java
+++ flies-web/src/hot/org/fedorahosted/flies/core/action/SearchAction.java
@@ -1,66 +1,41 @@
 package org.fedorahosted.flies.core.action;
 
-import java.util.List;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.annotations.datamodel.DataModel;
 import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Out;
-
-import org.fedorahosted.flies.core.model.Project;
-import org.fedorahosted.flies.core.model.Community;
-import org.fedorahosted.flies.core.model.Tribe;
 
 @Name("searchaction")
 @Scope(ScopeType.EVENT)
 public class SearchAction {
-	//@In
-	//private EntityManager entityManager;
-	
 	@In 
 	ProjectSearch projectSearch;
 	
 	@In 
 	CommunitySearch communitySearch;
-	
-	//@In 
-	//private TribeSearch tribeSearch;
-	
+
     private String searchQuery;
-	
-    @Out(required= false)
-    private List<Project> projects;
-    
-    @Out(required= false)
-    private List<Community> communities;
-    
-    @Out(required= false)
-    private List<Tribe> tribes;
-    
+    private String activeView;
+       
     public String getSearchQuery() {
         return searchQuery;
     }
     
     public void setSearchQuery(String searchQuery) {
-        this.searchQuery = searchQuery;
+    	projectSearch.setSearchQuery(searchQuery);
+    	communitySearch.setSearchQuery(searchQuery);
+    }
+    
+    public String getActiveView() {
+    	return activeView;
+    }
+    
+    public void setActiveView(String view) {
+    	this.activeView = view;
     }
 	
-    public String search() {
-        projectSearch.setSearchQuery(searchQuery);
+    public void search() {
         projectSearch.doSearch();
-        projects = projectSearch.getSearchResults();
-        
-        communitySearch.setSearchQuery(searchQuery);
         communitySearch.doSearch();
-        communities = communitySearch.getSearchResults();
-        
-        //tribeSearch.setSearchQuery(searchQuery);
-        //tribeSearch.doSearch();
-        
-        return "browse";
     }
-    
-    
-
 }
