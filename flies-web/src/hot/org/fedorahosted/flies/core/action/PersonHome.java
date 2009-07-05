d5640ccd18e2a73ac227271db7dab8d309ac035c
Asgeir Frimannsson <asgeirf@redhat.com>
2009-07-06 08:25:22 +1000

    Allow user to view and reset API key through the profile page

--- flies-web/src/hot/org/fedorahosted/flies/core/action/PersonHome.java
+++ flies-web/src/hot/org/fedorahosted/flies/core/action/PersonHome.java
@@ -1,12 +1,15 @@
 package org.fedorahosted.flies.core.action;
 
+import org.fedorahosted.flies.core.dao.AccountDAO;
 import org.fedorahosted.flies.core.model.Account;
 import org.fedorahosted.flies.core.model.Person;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.framework.EntityHome;
+import org.jboss.seam.log.Log;
 import org.jboss.seam.security.management.JpaIdentityStore;
 
 @Name("personHome")
@@ -16,6 +19,12 @@ public class PersonHome extends EntityHome<Person>{
 	@In(required=false, value=JpaIdentityStore.AUTHENTICATED_USER) 
 	Account authenticatedAccount;
 	
+	@In
+	AccountDAO accountDAO;
+	
+	@Logger
+	Log log;
+	
 	@Override
 	public Object getId() {
 		Object id = super.getId();
@@ -23,5 +32,11 @@ public class PersonHome extends EntityHome<Person>{
 			return authenticatedAccount.getPerson().getId();
 		}
 		return id;
+	}
+	
+	public void regenerateApiKey(){
+		accountDAO.createApiKey(getInstance().getAccount());
+		getEntityManager().merge(getInstance().getAccount());
+		log.info("Reset API key for {0}", getInstance().getAccount().getUsername());
 	}
 }
