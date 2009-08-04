cd70278636eb029da79a3b05028674097fc639f4
Asgeir Frimannsson <asgeirf@redhat.com>
2009-08-04 17:51:18 +1000

    improved Translation Table navigation

--- flies-web/src/hot/org/fedorahosted/flies/webtrans/action/TextFlowTargetDataModel.java
+++ flies-web/src/hot/org/fedorahosted/flies/webtrans/action/TextFlowTargetDataModel.java
@@ -1,5 +1,9 @@
 package org.fedorahosted.flies.webtrans.action;
 
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
 import net.openl10n.packaging.jpa.document.HTextFlowTarget;
 
 import org.hibernate.Session;
@@ -12,10 +16,35 @@ import org.jboss.seam.annotations.Scope;
 @Name("flies.tftDataModel")
 @AutoCreate
 @Scope(ScopeType.CONVERSATION)
-public class TextFlowTargetDataModel extends BaseModifiableHibernateDataModel<HTextFlowTarget> {
+public class TextFlowTargetDataModel extends
+		BaseModifiableHibernateDataModel<HTextFlowTarget> {
 
-	@In(value="#{entityManager.delegate}")
+	@In(value = "#{entityManager.delegate}")
 	private Session hibernateSession;
+
+	private Converter rowKeyConverter = new Converter() {
+
+		@Override
+		public String getAsString(FacesContext context, UIComponent component,
+				Object value) {
+			if (value == null) {
+				return null;
+			}
+			if (value instanceof String) {
+				return (String) value;
+			}
+			return String.valueOf(value);
+		}
+
+		@Override
+		public Object getAsObject(FacesContext context, UIComponent component,
+				String value) {
+			if (value == null) {
+				return null;
+			}
+			return hibernateSession.get(HTextFlowTarget.class, Long.valueOf(value));
+		}
+	};
 
 	public TextFlowTargetDataModel() {
 		super(HTextFlowTarget.class);
@@ -24,6 +53,10 @@ public class TextFlowTargetDataModel extends BaseModifiableHibernateDataModel<HT
 	@Override
 	protected Session getSession() {
 		return hibernateSession;
+	}
+
+	public Converter getRowKeyConverter() {
+		return rowKeyConverter;
 	}
 
 }
