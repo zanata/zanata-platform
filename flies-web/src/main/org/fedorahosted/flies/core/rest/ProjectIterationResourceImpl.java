f0eb5390f88ed5141852a5eb26ec65e22bd6e215
Asgeir Frimannsson <asgeirf@redhat.com>
2009-08-25 13:52:44 +1000

    split extensions up into a string-set

--- flies-web/src/main/org/fedorahosted/flies/core/rest/ProjectIterationResourceImpl.java
+++ flies-web/src/main/org/fedorahosted/flies/core/rest/ProjectIterationResourceImpl.java
@@ -1,5 +1,7 @@
 package org.fedorahosted.flies.core.rest;
 
+import java.util.Set;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +10,12 @@ import net.openl10n.api.ContentType;
 import net.openl10n.api.rest.document.Document;
 import net.openl10n.api.rest.project.Project;
 
+import org.apache.commons.lang.StringUtils;
 import org.fedorahosted.flies.core.model.ProjectIteration;
 import org.jboss.seam.Component;
 import org.jboss.seam.annotations.Name;
+
+import com.google.common.collect.ImmutableSet;
 
 @Name("projectIterationResource")
 public class ProjectIterationResourceImpl implements ProjectIterationResource{
@@ -22,8 +27,9 @@ public class ProjectIterationResourceImpl implements ProjectIterationResource{
 	}
 
 	@Override
-	public Project get(String extensions) {
+	public Project get(String ext) {
 		Project p = new Project("id", "name", "summary");
+		Set<String> extensions = ImmutableSet.of( StringUtils.split(ext, ',') );
 		if(extensions != null && extensions.contains("docs")){
 			p.getDocuments().add( new Document("/path/to/doc.txt", ContentType.TextPlain ) );
 		}
