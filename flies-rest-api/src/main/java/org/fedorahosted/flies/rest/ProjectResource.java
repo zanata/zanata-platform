90b6bad198c6df34b65d39f44133b7c725062cd9
Asgeir Frimannsson <asgeirf@redhat.com>
2009-08-26 17:41:10 +1000

    include flies-rest-api in deployed jars

--- flies-rest-api/src/main/java/org/fedorahosted/flies/rest/ProjectResource.java
+++ flies-rest-api/src/main/java/org/fedorahosted/flies/rest/ProjectResource.java
@@ -9,9 +9,23 @@ import net.openl10n.api.rest.project.ProjectRefs;
 
 public interface ProjectResource {
 
+	/**
+	 * This method will pass control on to the specific type of project
+	 * being queried for. e.g. for an IterationProject, it will pass control
+	 * on to IterationProjectResource
+	 *  
+	 * @param projectSlug textual id of project
+	 * 
+	 * @return a SubResource based on the type of project 
+	 */
 	@Path("/{projectSlug}")
 	public Object getProject(@PathParam("projectSlug") String projectSlug);
-	
+
+	/**
+	 * Retrieve a list of projects
+	 *  
+	 * @return list of Project references
+	 */
 	@GET
 	@Produces("application/xml")
 	public ProjectRefs get();
