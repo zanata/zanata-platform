2c3c02cde93ce3d3dbb9a13baf42e3a0834066bc
Asgeir Frimannsson <asgeirf@redhat.com>
2009-08-31 14:03:47 +1000

    change namespaces

--- flies-rest-api/src/main/java/org/fedorahosted/flies/Namespaces.java
+++ flies-rest-api/src/main/java/org/fedorahosted/flies/Namespaces.java
@@ -2,10 +2,10 @@ package org.fedorahosted.flies;
 
 public final class Namespaces {
 
-	public static final String DOCUMENT = "urn:openl10n:document:1.0";
+	public static final String DOCUMENT = "http://flies.fedorahosted.org/api/document";
 	public static final String DOCUMENT_JSON = "doc";
 	
-	public static final String PROJECT = "urn:openl10n:project:1.0";
+	public static final String PROJECT = "http://flies.fedorahosted.org/api/project";
 	public static final String PROJECT_JSON = "proj";
 	
 	public static final String XML = "http://www.w3.org/XML/1998/namespace";
