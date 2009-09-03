9a6c1ef3ac55168f6f2b05a69de910429e0a55c2
Sean Flanigan <sflaniga@redhat.com>
2009-09-03 16:49:53 +1000

    Changed ant tasks to use Documents instead of ProjectIteration, renamed to suit
    --HG--
    rename : flies-clients/flies-client-ant/src/main/java/org/fedorahosted/flies/client/ant/properties/Project2PropsTask.java => flies-clients/flies-client-ant/src/main/java/org/fedorahosted/flies/client/ant/properties/Docs2PropsTask.java
    rename : flies-clients/flies-client-ant/src/main/java/org/fedorahosted/flies/client/ant/properties/Props2ProjectTask.java => flies-clients/flies-client-ant/src/main/java/org/fedorahosted/flies/client/ant/properties/Props2DocsTask.java

--- flies-clients/flies-client-ant/src/test/java/org/fedorahosted/flies/client/ant/properties/BuildTest.java
+++ flies-clients/flies-client-ant/src/test/java/org/fedorahosted/flies/client/ant/properties/BuildTest.java
@@ -1,7 +1,11 @@
 package org.fedorahosted.flies.client.ant.properties;
 
+import java.io.File;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
 import org.apache.tools.ant.BuildFileTest;
-import org.fedorahosted.flies.client.ant.properties.Props2ProjectTask;
 
 @SuppressWarnings("nls")
 public class BuildTest extends BuildFileTest {
@@ -10,42 +14,46 @@ public class BuildTest extends BuildFileTest {
      * build.xml
      */
     @SuppressWarnings("unchecked")
-    static Class[] testedClasses = { Props2ProjectTask.class };
+    static Class[] testedClasses = { 
+	Props2DocsTask.class, Docs2PropsTask.class };
 
     public BuildTest(String name) {
 	super(name);
     }
 
     @Override
+    protected void runTest() throws Throwable {
+	try {
+	    executeTarget(getName());
+	} finally {
+	    System.out.print(getLog());
+	    System.out.print(getOutput());
+	    System.err.print(getError());
+	}
+    }
+    
+    @Override
     protected void setUp() throws Exception {
 	// work around maven bug: http://jira.codehaus.org/browse/SUREFIRE-184
 	System.getProperties().remove("basedir");
 	configureProject("src/test/resources/org/fedorahosted/flies/client/ant/properties/build.xml");
     }
-
-    @Override
-    protected void tearDown() throws Exception {
-	super.tearDown();
+    
+    public static Test suite() {
+	TestSuite suite = new TestSuite(BuildTest.class.getName());
+	suite.addTest(new BuildTest("test1"));
+	suite.addTest(new BuildTest("test2"));
+	suite.addTest(new BuildTest("test3"));
+	return suite;
     }
 
-    public void test1() throws Exception {
-	executeTarget(getName());
-	System.out.print(getLog());
-	System.out.print(getOutput());
-	System.err.print(getError());
-    }
+//    @Override
+//    protected void tearDown() throws Exception {
+//	String outDir = getProject().getProperty("out.dir");
+//	if (outDir != null) {
+//	    TestUtil.delete(new File(outDir));
+//	}
+//	super.tearDown();
+//    }
 
-    public void test2() throws Exception {
-	executeTarget(getName());
-	System.out.print(getLog());
-	System.out.print(getOutput());
-	System.err.print(getError());
-    }
-
-    public void test3() throws Exception {
-	executeTarget(getName());
-	System.out.print(getLog());
-	System.out.print(getOutput());
-	System.err.print(getError());
-    }
 }
