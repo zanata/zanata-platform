ff2a5dfbea8a256c8f75034b376426e89cd16aa6
James Ni <kent.neo@gmail.com>
2009-09-08 17:14:53 +0800

    Add integration tests for retrieving Projects as XML and JSON

--- flies-war/src/test/java/org/fedorahosted/flies/core/rest/ProjectServiceSeamTest.java
+++ flies-war/src/test/java/org/fedorahosted/flies/core/rest/ProjectServiceSeamTest.java
@@ -1,9 +1,17 @@
 package org.fedorahosted.flies.core.rest;
 
+import java.io.ByteArrayInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import org.fedorahosted.flies.rest.MediaTypes;
+import org.fedorahosted.flies.rest.dto.ProjectRef;
+import org.fedorahosted.flies.rest.dto.ProjectRefs;
 import org.jboss.seam.mock.EnhancedMockHttpServletRequest;
 import org.jboss.seam.mock.EnhancedMockHttpServletResponse;
 import org.jboss.seam.mock.ResourceRequestEnvironment;
@@ -11,7 +19,11 @@ import org.jboss.seam.mock.SeamTest;
 import org.jboss.seam.mock.ResourceRequestEnvironment.Method;
 import org.jboss.seam.mock.ResourceRequestEnvironment.ResourceRequest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.testng.Assert.*;
 
 @Test(groups={"seam-tests"})
 public class ProjectServiceSeamTest extends SeamTest {
@@ -25,27 +37,56 @@ public class ProjectServiceSeamTest extends SeamTest {
 			public Map<String, Object> getDefaultHeaders() {
 				return new HashMap<String, Object>() {
 					{
-						put("Accept", MediaTypes.APPLICATION_FLIES_PROJECTS_XML);
+						put("X-Auth-Token", "bob");
 					}
 				};
 			}
 		};
 	}
 
-	@Test
-	public void retrieveListOfProjects() throws Exception {
-
+	public void retrieveListOfProjectsAsJson() throws Exception {
 		new ResourceRequest(sharedEnvironment, Method.GET, "/restv1/projects") {
 			@Override
 			protected void prepareRequest(EnhancedMockHttpServletRequest request) {
-				//request.addHeader("Accept-Language", "en_US, de");
-				request.addHeader("X-Auth-Token", "bob");
+				request.addHeader("Accept", MediaType.APPLICATION_JSON);
 			}
 
 			@Override
 			protected void onResponse(EnhancedMockHttpServletResponse response) {
-				assert response.getStatus() == 200;
-				// assert response.getContentAsString().equals("foobar");
+				assertThat( response.getStatus(), is(200));
+			}
+		};
+	}
+	public void retrieveListOfProjectsAsXml() throws Exception {
+		new ResourceRequest(sharedEnvironment, Method.GET, "/restv1/projects") {
+			@Override
+			protected void prepareRequest(EnhancedMockHttpServletRequest request) {
+				request.addHeader("Accept", MediaTypes.APPLICATION_FLIES_PROJECTS_XML);
+			}
+
+			@Override
+			protected void onResponse(EnhancedMockHttpServletResponse response) {
+				assertThat( response.getStatus(), is(200));
+				
+				// check that the content we get back is XML
+				byte [] xmlVal = response.getContentAsByteArray();
+				JAXBContext context;
+				ProjectRefs projectRefs = null;
+				try {
+					context = JAXBContext.newInstance(ProjectRefs.class);
+					Unmarshaller unmarshaller = context.createUnmarshaller();
+					projectRefs = (ProjectRefs) unmarshaller.unmarshal(new ByteArrayInputStream(xmlVal));
+					
+				} catch (JAXBException e) {
+					fail("Failed to initialize Jaxb", e);
+				}
+				
+				assertThat( projectRefs, notNullValue() );
+				
+				assertThat( projectRefs.getProjects().size(), is(1) );
+				
+				ProjectRef projectRef = projectRefs.getProjects().get(0);
+				assertThat( projectRef.getName(), is("Sample Project"));
 			}
 
 		}.run();
