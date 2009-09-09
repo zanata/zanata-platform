24ede1924f96f6d0179be28fa66b26e842a39bc1
Asgeir Frimannsson <asgeirf@redhat.com>
2009-09-09 12:57:39 +1000

    split REST tests into http-based and client-based tests

---
+++ flies-war/src/test/java/org/fedorahosted/flies/core/rest/ProjectServiceRawSeamTest.java
@@ -0,0 +1,97 @@
+package org.fedorahosted.flies.core.rest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.testng.Assert.fail;
+
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.fedorahosted.flies.rest.MediaTypes;
+import org.fedorahosted.flies.rest.dto.ProjectRef;
+import org.fedorahosted.flies.rest.dto.ProjectRefs;
+import org.jboss.seam.mock.EnhancedMockHttpServletRequest;
+import org.jboss.seam.mock.EnhancedMockHttpServletResponse;
+import org.jboss.seam.mock.ResourceRequestEnvironment;
+import org.jboss.seam.mock.SeamTest;
+import org.jboss.seam.mock.ResourceRequestEnvironment.Method;
+import org.jboss.seam.mock.ResourceRequestEnvironment.ResourceRequest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Test(groups={"seam-tests"})
+public class ProjectServiceRawSeamTest extends SeamTest{
+
+	ResourceRequestEnvironment sharedEnvironment;
+	
+	@BeforeClass
+	public void prepareSharedEnvironment() throws Exception {
+		sharedEnvironment = new ResourceRequestEnvironment(this) {
+			@Override
+			public Map<String, Object> getDefaultHeaders() {
+				return new HashMap<String, Object>() {
+					{
+						put("X-Auth-Token", "bob");
+					}
+				};
+			}
+		};
+	}
+	
+	public void retrieveListOfProjectsAsJson() throws Exception {
+		new ResourceRequest(sharedEnvironment, Method.GET, "/restv1/projects") {
+			@Override
+			protected void prepareRequest(EnhancedMockHttpServletRequest request) {
+				request.addHeader("Accept", MediaType.APPLICATION_JSON);
+			}
+
+			@Override
+			protected void onResponse(EnhancedMockHttpServletResponse response) {
+				assertThat( response.getStatus(), is(200));
+			}
+		};
+	}
+	public void retrieveListOfProjectsAsXml() throws Exception {
+		new ResourceRequest(sharedEnvironment, Method.GET, "/restv1/projects") {
+			@Override
+			protected void prepareRequest(EnhancedMockHttpServletRequest request) {
+				request.addHeader("Accept", MediaTypes.APPLICATION_FLIES_PROJECTS_XML);
+			}
+
+			@Override
+			protected void onResponse(EnhancedMockHttpServletResponse response) {
+				assertThat( response.getStatus(), is(200));
+				
+				// check that the content we get back is XML
+				byte [] xmlVal = response.getContentAsByteArray();
+				JAXBContext context;
+				ProjectRefs projectRefs = null;
+				try {
+					context = JAXBContext.newInstance(ProjectRefs.class);
+					Unmarshaller unmarshaller = context.createUnmarshaller();
+					projectRefs = (ProjectRefs) unmarshaller.unmarshal(new ByteArrayInputStream(xmlVal));
+					
+				} catch (JAXBException e) {
+					fail("Failed to initialize Jaxb", e);
+				}
+				
+				assertThat( projectRefs, notNullValue() );
+				
+				assertThat( projectRefs.getProjects().size(), is(1) );
+				
+				ProjectRef projectRef = projectRefs.getProjects().get(0);
+				assertThat( projectRef.getName(), is("Sample Project"));
+			}
+
+		}.run();
+	}
+	
+	
+}
--- flies-war/src/test/java/org/fedorahosted/flies/core/rest/ProjectServiceSeamTest.java
+++ flies-war/src/test/java/org/fedorahosted/flies/core/rest/ProjectServiceSeamTest.java
@@ -1,57 +1,33 @@
 package org.fedorahosted.flies.core.rest;
 
-import java.io.ByteArrayInputStream;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.testng.Assert.fail;
+
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.ws.rs.core.MediaType;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-
-import org.fedorahosted.flies.rest.MediaTypes;
 import org.fedorahosted.flies.rest.client.ProjectResource;
 import org.fedorahosted.flies.rest.dto.Project;
-import org.fedorahosted.flies.rest.dto.ProjectRef;
 import org.fedorahosted.flies.rest.dto.ProjectRefs;
 import org.jboss.resteasy.client.ClientRequestFactory;
 import org.jboss.resteasy.client.ClientResponse;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
-import org.jboss.seam.mock.EnhancedMockHttpServletRequest;
-import org.jboss.seam.mock.EnhancedMockHttpServletResponse;
-import org.jboss.seam.mock.ResourceRequestEnvironment;
 import org.jboss.seam.mock.SeamTest;
-import org.jboss.seam.mock.ResourceRequestEnvironment.Method;
-import org.jboss.seam.mock.ResourceRequestEnvironment.ResourceRequest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.*;
-import static org.testng.Assert.*;
 
 @Test(groups={"seam-tests"})
 public class ProjectServiceSeamTest extends SeamTest {
 
-	ResourceRequestEnvironment sharedEnvironment;
 	ClientRequestFactory clientRequestFactory;
 	ProjectResource projectService;
 	
 	@BeforeClass
-	public void prepareSharedEnvironment() throws Exception {
-		sharedEnvironment = new ResourceRequestEnvironment(this) {
-			@Override
-			public Map<String, Object> getDefaultHeaders() {
-				return new HashMap<String, Object>() {
-					{
-						put("X-Auth-Token", "bob");
-					}
-				};
-			}
-		};
-		
+	public void prepareRestEasyClientFramework() throws Exception {
+
 		ResteasyProviderFactory instance = ResteasyProviderFactory.getInstance();
 		RegisterBuiltin.register(instance);
 
@@ -62,56 +38,8 @@ public class ProjectServiceSeamTest extends SeamTest {
 		projectService = clientRequestFactory.createProxy(ProjectResource.class);
 	
 	}
-
-	public void retrieveListOfProjectsAsJson() throws Exception {
-		new ResourceRequest(sharedEnvironment, Method.GET, "/restv1/projects") {
-			@Override
-			protected void prepareRequest(EnhancedMockHttpServletRequest request) {
-				request.addHeader("Accept", MediaType.APPLICATION_JSON);
-			}
-
-			@Override
-			protected void onResponse(EnhancedMockHttpServletResponse response) {
-				assertThat( response.getStatus(), is(200));
-			}
-		};
-	}
-	public void retrieveListOfProjectsAsXml() throws Exception {
-		new ResourceRequest(sharedEnvironment, Method.GET, "/restv1/projects") {
-			@Override
-			protected void prepareRequest(EnhancedMockHttpServletRequest request) {
-				request.addHeader("Accept", MediaTypes.APPLICATION_FLIES_PROJECTS_XML);
-			}
-
-			@Override
-			protected void onResponse(EnhancedMockHttpServletResponse response) {
-				assertThat( response.getStatus(), is(200));
-				
-				// check that the content we get back is XML
-				byte [] xmlVal = response.getContentAsByteArray();
-				JAXBContext context;
-				ProjectRefs projectRefs = null;
-				try {
-					context = JAXBContext.newInstance(ProjectRefs.class);
-					Unmarshaller unmarshaller = context.createUnmarshaller();
-					projectRefs = (ProjectRefs) unmarshaller.unmarshal(new ByteArrayInputStream(xmlVal));
-					
-				} catch (JAXBException e) {
-					fail("Failed to initialize Jaxb", e);
-				}
-				
-				assertThat( projectRefs, notNullValue() );
-				
-				assertThat( projectRefs.getProjects().size(), is(1) );
-				
-				ProjectRef projectRef = projectRefs.getProjects().get(0);
-				assertThat( projectRef.getName(), is("Sample Project"));
-			}
-
-		}.run();
-	}
 	
-	public void retrieveProjectRefsUsingClientFramework() throws Exception{
+	public void retrieveListofProjects() throws Exception{
 
 		ClientResponse<ProjectRefs> response = projectService.getProjects();
 		
@@ -133,4 +61,15 @@ public class ProjectServiceSeamTest extends SeamTest {
 		assertThat( response.getStatus(), lessThan(400) );
 	}
 	
+	public void createNewProject(){
+		fail("Not implemented");
+	}
+	
+	public void deleteExistingProject(){
+		fail("Not implemented");
+	}
+	
+	public void deleteNonExistingProject(){
+		fail("Not implemented");
+	}
 }
