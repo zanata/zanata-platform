f6498cb054f181c5c19ab80d4555d8b8676e2823
Sean Flanigan <sflaniga@redhat.com>
2009-09-11 15:01:59 +1000

    ant tasks and their integration with rest client classes

--- flies-clients/flies-client-ant/src/main/java/org/fedorahosted/flies/client/ant/properties/Utility.java
+++ flies-clients/flies-client-ant/src/main/java/org/fedorahosted/flies/client/ant/properties/Utility.java
@@ -8,6 +8,16 @@ import org.apache.tools.ant.Project;
 
 class Utility {
 
+    /**
+     * Converts a string to a URL.  If it doesn't form a valid URL as is, it
+     * will be treated as a file and converted to a file: URL.  Relative file
+     * paths will be interpreted, relative to the Ant project basedir,
+     * and converted to absolute form.
+     * @param src a URL or a file path
+     * @param project Ant Project for relative file paths
+     * @return URL-equivalent of src
+     * @throws MalformedURLException
+     */
     public static URL createURL(String src, Project project) throws MalformedURLException {
         URL srcURL;
         try {
