e9ff2fc2b681d5ca1c8ec045b79994693103cc55
Asgeir Frimannsson <asgeirf@redhat.com>
2009-09-15 14:31:11 +1000

    add example of hot deployed REST class

---
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/service/ProjectsServiceAction.java
@@ -0,0 +1,9 @@
+package org.fedorahosted.flies.rest.service;
+
+import org.fedorahosted.flies.rest.dto.ProjectRefs;
+
+public interface ProjectsServiceAction {
+	
+	public ProjectRefs get();
+
+}
---
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/service/ProjectsServiceActionImpl.java
@@ -0,0 +1,38 @@
+package org.fedorahosted.flies.rest.service;
+
+import java.util.List;
+
+import org.fedorahosted.flies.core.model.HProject;
+import org.fedorahosted.flies.rest.dto.Project;
+import org.fedorahosted.flies.rest.dto.ProjectRef;
+import org.fedorahosted.flies.rest.dto.ProjectRefs;
+import org.hibernate.Session;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name("projectsServiceAction")
+@Scope(ScopeType.STATELESS)
+@AutoCreate
+public class ProjectsServiceActionImpl implements ProjectsServiceAction{
+
+	@In
+	Session session;
+
+	public ProjectRefs get() {
+		ProjectRefs projectRefs = new ProjectRefs();
+		
+		List<HProject> projects = session.createQuery("from HProject p").list();
+		
+		for(HProject hProject : projects){
+			Project project = 
+				new Project(hProject.getSlug(), hProject.getName(), hProject.getDescription());
+			projectRefs.getProjects().add( new ProjectRef( project ));
+		}
+		
+		return projectRefs;
+
+	}
+}
