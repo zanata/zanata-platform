e7dc79d3b3d9928876b9ea035d21786b2bbe4849
Asgeir Frimannsson <asgeirf@redhat.com>
2009-09-16 13:00:57 +1000

    initial work on DocumentService

--- flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentService.java
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentService.java
@@ -20,11 +20,13 @@ import org.fedorahosted.flies.core.dao.ProjectDAO;
 import org.fedorahosted.flies.core.dao.ProjectIterationDAO;
 import org.fedorahosted.flies.core.model.HProjectIteration;
 import org.fedorahosted.flies.repository.model.HDocument;
+import org.fedorahosted.flies.repository.model.HProjectContainer;
 import org.fedorahosted.flies.repository.model.HResource;
 import org.fedorahosted.flies.rest.MediaTypes;
 import org.fedorahosted.flies.rest.dto.Document;
 import org.fedorahosted.flies.rest.dto.Resource;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
 import org.jboss.resteasy.spi.NotFoundException;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
@@ -68,7 +70,7 @@ public class DocumentService {
 			throw new WebApplicationException(Status.NOT_FOUND);
 		}
 		
-		return null;
+		return new Document(hDoc.getDocId(), hDoc.getContentType());
 	}
 
 	@PUT
@@ -81,23 +83,42 @@ public class DocumentService {
 		if(hProjectIteration == null)
 			throw new NotFoundException("Project Iteration not found");
 		
-		HDocument hDoc = new HDocument(document);
-		
-		// TODO check if it's a update or create operation
-		
-		hProjectIteration.getContainer().getDocuments().add(hDoc);
-		try{
-			session.flush();
-			for(Resource res : document.getResources()) {
-				HResource hRes = HDocument.create(res);
-				hRes.setDocument(hDoc);
-				hDoc.getResourceTree().add(hRes);
-				session.flush();
-			}
-			return Response.created( new URI("/d/"+hDoc.getDocId())).build();
+		String hDocId = convertToRealDocumentId(documentId);
+
+		if(!document.getId().equals(hDocId)){
+			Response.notAcceptable(null).build();
 		}
-		catch(Exception e){
-			return Response.notAcceptable(null).build();
+
+		HDocument hDoc = documentDAO.getByDocId(hProjectIteration.getContainer(), hDocId);
+		
+		if(hDoc == null) { // it's a create operation
+			hDoc = new HDocument(document);
+			hProjectIteration.getContainer().getDocuments().add(hDoc);
+			try{
+				session.flush();
+				for(Resource res : document.getResources()) {
+					HResource hRes = HDocument.create(res);
+					hRes.setDocument(hDoc);
+					hDoc.getResourceTree().add(hRes);
+					session.flush();
+				}
+				return Response.created( new URI("/d/"+hDoc.getDocId())).build();
+			}
+			catch(Exception e){
+				return Response.notAcceptable(null).build();
+			}
+		}
+		else{ // it's an update operation
+			if(!document.getName().equals(hDoc.getName()) ){
+				hDoc.setName(document.getName());
+			}
+			try{
+				session.flush();
+				return Response.ok().build();
+			}
+			catch(Exception e){
+				return Response.notAcceptable(null).build();
+			}
 		}
 		
 	}
---
+++ flies-war/src/test/java/org/fedorahosted/flies/rest/service/DocumentServiceSeamTest.java
@@ -0,0 +1,66 @@
+package org.fedorahosted.flies.rest.service;
+
+import java.net.URI;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.dbunit.operation.DatabaseOperation;
+import org.fedorahosted.flies.ContentType;
+import org.fedorahosted.flies.rest.ApiKeyHeaderDecorator;
+import org.fedorahosted.flies.rest.client.IDocumentResource;
+import org.fedorahosted.flies.rest.dto.Document;
+import org.jboss.resteasy.client.ClientRequestFactory;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.jboss.seam.mock.DBUnitSeamTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+
+@Test(groups={"seam-tests"},suiteName="DocumentService")
+public class DocumentServiceSeamTest extends DBUnitSeamTest{
+	
+	ClientRequestFactory clientRequestFactory;
+	URI baseUri = URI.create("/restv1/");
+	IDocumentResource documentResource;
+	
+	@BeforeClass
+	public void prepareRestEasyClientFramework() throws Exception {
+
+		ResteasyProviderFactory instance = ResteasyProviderFactory.getInstance();
+		RegisterBuiltin.register(instance);
+
+		clientRequestFactory = 
+			new ClientRequestFactory(
+					new SeamMockClientExecutor(this), baseUri);
+		
+		clientRequestFactory.getPrefixInterceptors().registerInterceptor(new ApiKeyHeaderDecorator("admin", "12345678901234567890123456789012"));
+
+		documentResource = clientRequestFactory.createProxy(IDocumentResource.class, baseUri.resolve("projects/p/sample-project/iterations/i/1.0/documents/d/my,fancy,document.txt"));
+		
+	}
+	
+	@Override
+    protected void prepareDBUnitOperations() {
+        beforeTestOperations.add(
+                new DataSetOperation("org/fedorahosted/flies/test/model/ProjectData.dbunit.xml", DatabaseOperation.CLEAN_INSERT)
+        );
+    }
+
+
+	public void getDocumentThatDoesntExist(){
+		assertThat ( documentResource.get(null).getResponseStatus(), is(Status.NOT_FOUND) ); 
+	}
+	
+	public void putNewDocument() {
+		Document doc = new Document("my/fancy/document.txt", ContentType.TextPlain);
+		Response response = documentResource.put(doc);
+		assertThat( response.getStatus(), is(Status.CREATED.getStatusCode()) );
+		assertThat ( documentResource.get(null).getResponseStatus(), is(Status.OK) ); 
+	}
+	
+}
