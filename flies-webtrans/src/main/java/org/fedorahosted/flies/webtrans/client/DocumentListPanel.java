4067f471a28856729876abfe8d4516d840bdf46f
Asgeir Frimannsson <asgeirf@redhat.com>
2009-10-01 12:19:12 +1000

    start of DocumentListPanel

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/DocumentListPanel.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/DocumentListPanel.java
@@ -2,9 +2,10 @@ package org.fedorahosted.flies.webtrans.client;
 
 import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
-import com.extjs.gxt.ui.client.widget.layout.LayoutData;
+import com.extjs.gxt.ui.client.widget.TabItem;
+import com.extjs.gxt.ui.client.widget.TabPanel;
+import com.extjs.gxt.ui.client.widget.TabPanel.TabPosition;
 import com.extjs.gxt.ui.client.widget.layout.RowLayout;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 
 public class DocumentListPanel extends ContentPanel {
@@ -12,8 +13,19 @@ public class DocumentListPanel extends ContentPanel {
 	public DocumentListPanel() {
 		setHeading("Documents");
 		setLayout(new RowLayout(Orientation.VERTICAL));
-		add(new Label("hello world"));
-		add(new Button("gwt button"));
-		add(new com.extjs.gxt.ui.client.widget.button.Button("gxt button"));
+
+		TabPanel innerTab = new TabPanel();
+		innerTab.setPlain(false);
+		innerTab.setTabPosition(TabPosition.BOTTOM);
+
+		TabItem treeTab = new TabItem("Tree");
+		treeTab.add( new Label("replace this with a TreePanel"));
+		innerTab.add(treeTab);
+		TabItem listTab = new TabItem("List");
+		listTab.addText("Just a plain old List");
+		innerTab.add(listTab);
+
+		add(innerTab);
 	}
+
 }
