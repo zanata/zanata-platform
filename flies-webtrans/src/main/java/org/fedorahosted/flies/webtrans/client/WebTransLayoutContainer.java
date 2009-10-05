756b0b0f8e0915472351e6ca88fd9052fbad30c7
Sean Flanigan <sflaniga@redhat.com>
2009-10-05 13:52:36 +1000

    Changed flies-webtrans to use gwt-codemirror-0.5-SNAPSHOT, added a couple of editor instances to the WebTransLayoutContainer

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WebTransLayoutContainer.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WebTransLayoutContainer.java
@@ -3,24 +3,25 @@ package org.fedorahosted.flies.webtrans.client;
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
 import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.Style.Scroll;
-import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.RowData;
 import com.extjs.gxt.ui.client.widget.layout.RowLayout;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
+import com.weborient.codemirror.client.CodeMirrorConfiguration;
+import com.weborient.codemirror.client.CodeMirrorEditorWidget;
 
 public class WebTransLayoutContainer extends LayoutContainer {
 
 	ContentPanel west;
 	ContentPanel east;
 	ContentPanel center;
+	private CodeMirrorEditorWidget sourceWidget;
+	private CodeMirrorEditorWidget targetWidget;
 
 	public WebTransLayoutContainer() {
 		final BorderLayout layout = new BorderLayout();
@@ -37,9 +38,13 @@ public class WebTransLayoutContainer extends LayoutContainer {
 		center = new ContentPanel();
 		center.setHeaderVisible(false);
 		center.setScrollMode(Scroll.AUTOX);
+		sourceWidget = getEditorWidget(true);
+		center.add(sourceWidget);
 
 		east = new ContentPanel();
 		east.setHeaderVisible(false);
+		targetWidget = getEditorWidget(false);
+		east.add(targetWidget);
 
 		BorderLayoutData westData = new BorderLayoutData(LayoutRegion.WEST, 200);
 		westData.setSplit(true);
@@ -61,4 +66,30 @@ public class WebTransLayoutContainer extends LayoutContainer {
 		add(east, eastData);
 	}
 
+	
+	CodeMirrorEditorWidget getEditorWidget(boolean readOnly) {
+        
+        // use the configuration class in order 
+        // to override the default widget configuraions
+
+        CodeMirrorConfiguration configuration = new CodeMirrorConfiguration();
+        configuration.setTagSelectorLabel("Templates: ");
+        configuration.setListBoxPreSets("<html></html>", "<div></div>");
+        configuration.setLineNumbers(false);
+        configuration.setTextWrapping(true);
+        configuration.setReadOnly(readOnly);
+   
+        // pass the configuration object to the widget
+
+        return new CodeMirrorEditorWidget(configuration);
+   }
+
+	// this should be called after container is added to RootPanel
+	public void init() {
+		// NB: These editor widget methods currently can't be called until
+		// the JSNI is initialized by onLoad() 
+		targetWidget.setText("Target text");
+		sourceWidget.setText("This is the source text (text flow)");
+	}	
+	
 }
