b3829a1e8a3096aebc56400d51aef2e020ee5a0a
Sean Flanigan <sflaniga@redhat.com>
2009-10-06 16:51:02 +1000

    Added multiple instances of HighlightingLabel, disabled the toolbar on the editor

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WebTransLayoutContainer.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WebTransLayoutContainer.java
@@ -1,7 +1,6 @@
 package org.fedorahosted.flies.webtrans.client;
 
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
-import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.Style.Scroll;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.util.Padding;
@@ -11,22 +10,25 @@ import com.extjs.gxt.ui.client.widget.Text;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.FillLayout;
-import com.extjs.gxt.ui.client.widget.layout.RowData;
-import com.extjs.gxt.ui.client.widget.layout.RowLayout;
 import com.extjs.gxt.ui.client.widget.layout.VBoxLayout;
 import com.extjs.gxt.ui.client.widget.layout.VBoxLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.VBoxLayout.VBoxLayoutAlign;
-import com.google.gwt.user.client.Command;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.weborient.codemirror.client.CodeMirrorConfiguration;
 import com.weborient.codemirror.client.CodeMirrorEditorWidget;
+import com.weborient.codemirror.client.HighlightingLabel;
+import com.weborient.codemirror.client.SyntaxLanguage;
 
 public class WebTransLayoutContainer extends LayoutContainer {
 
 	ContentPanel west;
 	ContentPanel east;
 	ContentPanel center;
-	private CodeMirrorEditorWidget sourceWidget;
-	private CodeMirrorEditorWidget targetWidget;
 
 	public WebTransLayoutContainer() {
 		final BorderLayout layout = new BorderLayout();
@@ -54,15 +56,74 @@ public class WebTransLayoutContainer extends LayoutContainer {
 		center.setScrollMode(Scroll.AUTOY);
 		east = new ContentPanel();
 		east.setHeaderVisible(false);
+		Panel langBar = new HorizontalPanel();
+		langBar.add(new Label("Highlighting: "));
+		RadioButton noneButton = new RadioButton("langBar", "None");
+		noneButton.setValue(true, false);
+		langBar.add(noneButton);
+		RadioButton jsButton = new RadioButton("langBar", "JavaScript");
+		langBar.add(jsButton);
+		RadioButton xmlButton = new RadioButton("langBar", "XML");
+		langBar.add(xmlButton);
+		RadioButton mixedButton = new RadioButton("langBar", "HTML");
+		langBar.add(mixedButton);
+		center.add(langBar);
 		
-		sourceWidget = getEditorWidget(true);
-		sourceWidget.setText("Original/source text (text flow)");
-		center.add(sourceWidget);
+		for (int i=0; i<5; i++) {
+			
+			String sourceText = "Original/source text (\"text flow\"/<text-flow>) #"+i;
+			final HighlightingLabel sourceWidget = new HighlightingLabel(sourceText);
+					
+			noneButton.addClickHandler(new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent ce) {
+					sourceWidget.setLanguage(SyntaxLanguage.NONE);
+				}});
+			jsButton.addClickHandler(new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent ce) {
+					sourceWidget.setLanguage(SyntaxLanguage.JAVASCRIPT);
+				}});
+			xmlButton.addClickHandler(new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent ce) {
+					sourceWidget.setLanguage(SyntaxLanguage.XML);
+				}});
+			mixedButton.addClickHandler(new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent ce) {
+					sourceWidget.setLanguage(SyntaxLanguage.MIXED);
+				}});
+			
+			center.add(sourceWidget);
+		}
 
+		final CodeMirrorEditorWidget targetWidget;
 		targetWidget = getEditorWidget(false);
-		targetWidget.setText("Translation text (text flow target)");
-		east.add(targetWidget);
+		targetWidget.setText("Translation text (\"text flow target\"/<text-flow-target>)");
+		noneButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent ce) {
+				targetWidget.setLanguage(SyntaxLanguage.NONE);
+			}});
+		jsButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent ce) {
+				targetWidget.setLanguage(SyntaxLanguage.JAVASCRIPT);
+			}});
+		xmlButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent ce) {
+				targetWidget.setLanguage(SyntaxLanguage.XML);
+			}});
+		mixedButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent ce) {
+				targetWidget.setLanguage(SyntaxLanguage.MIXED);
+			}});
 
+		center.add(targetWidget);
+		
 		BorderLayoutData westData = new BorderLayoutData(LayoutRegion.WEST, 200);
 		westData.setSplit(true);
 		westData.setMinSize(1);
@@ -83,18 +144,16 @@ public class WebTransLayoutContainer extends LayoutContainer {
 		add(east, eastData);
 	}
 
-	
 	CodeMirrorEditorWidget getEditorWidget(boolean readOnly) {
         
         // use the configuration class in order 
         // to override the default widget configuraions
 
         CodeMirrorConfiguration configuration = new CodeMirrorConfiguration();
-        configuration.setTagSelectorLabel("Templates: ");
-        configuration.setListBoxPreSets("<html></html>", "<div></div>");
         configuration.setLineNumbers(false);
         configuration.setTextWrapping(true);
         configuration.setReadOnly(readOnly);
+        configuration.setToolbar(false);
    
         // pass the configuration object to the widget
 
