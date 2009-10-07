ed6b6cdb43289c84e5e8eb0cfe8ac875ba66366e
Asgeir Frimannsson <asgeirf@redhat.com>
2009-10-07 17:54:59 +1000

    added images for Translators Tree

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WorkspaceUsersView.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WorkspaceUsersView.java
@@ -1,48 +1,71 @@
 package org.fedorahosted.flies.webtrans.client;
 
+import org.fedorahosted.flies.webtrans.client.WestNavigationView.Images;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DecoratedStackPanel;
 import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.ImageBundle;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.StackPanel;
 import com.google.gwt.user.client.ui.Tree;
+import com.google.gwt.user.client.ui.TreeImages;
 import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalSplitPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.ImageBundle.Resource;
 
-public class WorkspaceUsersView extends Composite implements WorkspaceUsersPresenter.Display {
+public class WorkspaceUsersView extends Composite implements
+		WorkspaceUsersPresenter.Display {
 
 	private final StackPanel panel;
-	
+
+	public interface Images extends ImageBundle, TreeImages {
+
+		@Resource("org/fedorahosted/flies/webtrans/images/silk/group.png")
+		AbstractImagePrototype treeOpen();
+
+		@Resource("org/fedorahosted/flies/webtrans/images/silk/group.png")
+		AbstractImagePrototype treeClosed();
+
+		@Resource("org/fedorahosted/flies/webtrans/images/silk/user.png")
+		AbstractImagePrototype treeLeaf();
+
+	}
+
+	private static Images images = (Images) GWT.create(Images.class);
+
 	public WorkspaceUsersView() {
 		panel = new DecoratedStackPanel();
-		initWidget( panel );
+		initWidget(panel);
 		panel.add(getChatAllPanel(), "Translators");
 	}
-	
+
 	private static Tree createLocaleTranslatorsTree() {
-		Tree tree = new Tree();
+		Tree tree = new Tree(images);
 		tree.setWidth("100%");
 		addLocaleData(tree, "German");
 		return tree;
 	}
-	
+
 	private static TreeItem createTranslator(String name) {
 		TreeItem item = new TreeItem();
 		item.setText(name);
 		return item;
 	}
-	
+
 	private static void addLocaleData(Tree tree, String locale) {
 		TreeItem item = new TreeItem();
 		item.setText(locale);
 		tree.addItem(item);
-		item.addItem( createTranslator("Bob") );
-		item.addItem( createTranslator("Jane") );
+		item.addItem(createTranslator("Bob"));
+		item.addItem(createTranslator("Jane"));
 	}
 
 	public static Widget getChatAllPanel() {
-		
+
 		VerticalSplitPanel vSplit = new VerticalSplitPanel();
 		vSplit.setBottomWidget(new Label("Chat"));
 		vSplit.setWidth("200px");
@@ -50,7 +73,7 @@ public class WorkspaceUsersView extends Composite implements WorkspaceUsersPrese
 		vSplit.setTopWidget(createLocaleTranslatorsTree());
 		return vSplit;
 	}
-	
+
 	@Override
 	public Widget asWidget() {
 		return this;
@@ -59,13 +82,13 @@ public class WorkspaceUsersView extends Composite implements WorkspaceUsersPrese
 	@Override
 	public void startProcessing() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void stopProcessing() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
