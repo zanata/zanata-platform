fe3f467fc4591aa44f2d76dc35a34f20d3e63dc4
Sean Flanigan <sflaniga@redhat.com>
2009-10-07 18:14:09 +1000

    Turned SyntaxLanguageWidget into a MenuBar

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/SyntaxLanguageWidget.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/SyntaxLanguageWidget.java
@@ -3,13 +3,10 @@ package org.fedorahosted.flies.webtrans.client;
 import java.util.Collection;
 import java.util.HashSet;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.Panel;
-import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
 import com.weborient.codemirror.client.SyntaxLanguage;
 import com.weborient.codemirror.client.SyntaxObservable;
 import com.weborient.codemirror.client.SyntaxSelection;
@@ -23,58 +20,43 @@ public class SyntaxLanguageWidget extends Composite implements SyntaxObservable 
 
 	public SyntaxLanguageWidget(SyntaxLanguage syntax) {
 		this.syntax = syntax;
-		Panel syntaxBar = new HorizontalPanel();
-		syntaxBar.add(new Label("Highlighting: "));
-		String groupName = "syntaxBar"+System.identityHashCode(this);
-		RadioButton noneButton = new RadioButton(groupName, "None");
-		noneButton.setValue(true, false);
-		syntaxBar.add(noneButton);
-		RadioButton jsButton = new RadioButton(groupName, "JavaScript");
-		syntaxBar.add(jsButton);
-		RadioButton xmlButton = new RadioButton(groupName, "XML");
-		syntaxBar.add(xmlButton);
-		RadioButton mixedButton = new RadioButton(groupName, "HTML");
-		syntaxBar.add(mixedButton);
 		
-		switch(syntax) {
-		case JAVASCRIPT:
-			jsButton.setValue(true, false);
-			break;
-		case MIXED:
-			mixedButton.setValue(true, false);
-			break;
-		case NONE:
-			noneButton.setValue(true, false);
-			break;
-		case XML:
-			xmlButton.setValue(true, false);
-			break;
-		default:
-			throw new IllegalArgumentException();
-		}
-		
-		noneButton.addClickHandler(new ClickHandler() {
+//	    MenuBar menu = new MenuBar();
+//	    menu.setAutoOpen(true);
+//	    menu.setWidth("500px");
+//	    menu.setAnimationEnabled(true);
+//	    MenuBar syntaxMenu = new MenuBar(true);
+//	    menu.addItem(new MenuItem("Highlighting", syntaxMenu));
+		MenuBar syntaxMenu = new MenuBar();
+	    
+		syntaxMenu.addItem(new MenuItem("None", new Command() {
 			@Override
-			public void onClick(ClickEvent ce) {
+			public void execute() {
 				setSyntax(SyntaxLanguage.NONE);
-			}});
-		jsButton.addClickHandler(new ClickHandler() {
+			}
+		}));
+		syntaxMenu.addItem(new MenuItem("JavaScript", new Command() {
 			@Override
-			public void onClick(ClickEvent ce) {
+			public void execute() {
 				setSyntax(SyntaxLanguage.JAVASCRIPT);
-			}});
-		xmlButton.addClickHandler(new ClickHandler() {
+			}
+		}));
+		syntaxMenu.addItem(new MenuItem("XML", new Command() {
 			@Override
-			public void onClick(ClickEvent ce) {
+			public void execute() {
 				setSyntax(SyntaxLanguage.XML);
-			}});
-		mixedButton.addClickHandler(new ClickHandler() {
+			}
+		}));
+		syntaxMenu.addItem(new MenuItem("HTML", new Command() {
 			@Override
-			public void onClick(ClickEvent ce) {
+			public void execute() {
 				setSyntax(SyntaxLanguage.MIXED);
-			}});
+			}
+		}));
 		
-		initWidget(syntaxBar);
+//		initWidget(menu);
+		initWidget(syntaxMenu);
+
 	}
 	
 	void setSyntax(SyntaxLanguage syntax) {
