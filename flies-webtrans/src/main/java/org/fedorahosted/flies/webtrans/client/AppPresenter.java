f6764c84dc0fdd6d0664b3257350529076d22774
Sean Flanigan <sflaniga@redhat.com>
2009-10-09 17:59:18 +1000

    Added a footer to make DockPanel issues more apparent

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/AppPresenter.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/AppPresenter.java
@@ -1,14 +1,16 @@
 package org.fedorahosted.flies.webtrans.client;
 
-import org.fedorahosted.flies.webtrans.client.Application.WindowResizeEvent;
-
 import net.customware.gwt.presenter.client.EventBus;
 
+import org.fedorahosted.flies.webtrans.client.Application.WindowResizeEvent;
 
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 
@@ -32,6 +34,9 @@ public class AppPresenter {
 		container.clear();
 		
 		final DockPanel dockPanel = new DockPanel();
+		final Label appFooter = new HTML("<span style=\"float: left\">Flies page footer goes here</span><span style=\"float: right\">Flies page footer goes here</span>");
+		appFooter.setHeight("1em");
+		dockPanel.add(appFooter, DockPanel.SOUTH);
 		Widget center = transUnitListPresenter.getDisplay().asWidget();
 		Widget west = westNavigationPresenter.getDisplay().asWidget();
 		dockPanel.add(center, DockPanel.CENTER );
