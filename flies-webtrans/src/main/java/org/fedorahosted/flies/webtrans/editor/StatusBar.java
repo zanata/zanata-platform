8bc5b2f428f3cf2f7bd8e5e5b4ea6194068fbfeb
James Ni <kent.neo@gmail.com>
2009-10-14 13:48:37 +0800

    Show the popup on hover and show it above the status bar.

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/editor/StatusBar.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/editor/StatusBar.java
@@ -3,6 +3,12 @@ package org.fedorahosted.flies.webtrans.editor;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.dom.client.HasMouseOutHandlers;
+import com.google.gwt.event.dom.client.HasMouseOverHandlers;
+import com.google.gwt.event.dom.client.MouseOutEvent;
+import com.google.gwt.event.dom.client.MouseOutHandler;
+import com.google.gwt.event.dom.client.MouseOverEvent;
+import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
@@ -12,13 +18,16 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.widgetideas.client.ProgressBar;
 
-public class StatusBar extends Composite implements ClickHandler,HasClickHandlers {
+public class StatusBar extends Composite implements MouseOverHandler, MouseOutHandler, HasMouseOverHandlers, HasMouseOutHandlers {
+	
+	private final static int popupoffset = 35;
 	private final ProgressBar bar;
+	private PopupWindow popupWindow;
 	
 	private static class PopupWindow extends DecoratedPopupPanel {
 		    public PopupWindow() {
 		      super(true);
-		      setWidget(new Label("Click outside of this popup to close it"));
+		      setWidget(new Label("Status of Translation Unit"));
 		    }
 	}
 
@@ -32,24 +41,36 @@ public class StatusBar extends Composite implements ClickHandler,HasClickHandler
 		bar.setWidth("200px");
 		
 		panel.add(bar);
-		
-		addClickHandler(this);
+		addMouseOverHandler(this);
+		addMouseOutHandler(this);
 	}
 	
 	@Override
-	public void onClick(ClickEvent event) {
+	public void onMouseOver(MouseOverEvent event) {
 		// TODO Auto-generated method stub
-		PopupWindow popupWindow = new PopupWindow();
+		popupWindow = new PopupWindow();
 		int top = bar.getAbsoluteTop();
 	    int left = bar.getAbsoluteLeft();
-		popupWindow.setPopupPosition(left, top-bar.getOffsetHeight());
+		popupWindow.setPopupPosition(left, top-popupoffset);
 		popupWindow.show();
 	}
 
 	@Override
-	public HandlerRegistration addClickHandler(ClickHandler handler) {
+	public HandlerRegistration addMouseOverHandler(MouseOverHandler handler) {
 		// TODO Auto-generated method stub
-		return addDomHandler(handler, ClickEvent.getType());
+		return addDomHandler(handler, MouseOverEvent.getType());
+	}
+
+	@Override
+	public void onMouseOut(MouseOutEvent event) {
+		// TODO Auto-generated method stub
+		popupWindow.hide();
+	}
+
+	@Override
+	public HandlerRegistration addMouseOutHandler(MouseOutHandler handler) {
+		// TODO Auto-generated method stub
+		return addDomHandler(handler, MouseOutEvent.getType());
 	}
 
 }
