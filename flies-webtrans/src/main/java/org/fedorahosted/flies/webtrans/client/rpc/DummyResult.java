fbbd7be35292ce58ef884b9442e578a768d2f723
Sean Flanigan <sflaniga@redhat.com>
2009-10-19 17:18:37 +1000

    Removed obsolete dummy action classes

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/rpc/DummyResult.java
+++
@@ -1,9 +0,0 @@
-package org.fedorahosted.flies.webtrans.client.rpc;
-
-import net.customware.gwt.dispatch.shared.Result;
-
-public class DummyResult implements Result{
-
-	private static final long serialVersionUID = 7127547817403181729L;
-
-}
