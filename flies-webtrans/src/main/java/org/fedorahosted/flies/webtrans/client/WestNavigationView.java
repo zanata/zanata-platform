991eb8e16c6617472a9254c816dfe639533e4c06
Asgeir Frimannsson <asgeirf@redhat.com>
2009-10-21 01:42:09 +1000

    change left panel to a flow-panel for now

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WestNavigationView.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/WestNavigationView.java
@@ -2,6 +2,7 @@ package org.fedorahosted.flies.webtrans.client;
 
 import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -9,14 +10,11 @@ import com.google.gwt.user.client.ui.Widget;
 public class WestNavigationView extends Composite implements
 		WestNavigationPresenter.Display {
 
-	final VerticalPanel panel;
+	final FlowPanel panel;
 	
 	public WestNavigationView() {
 		Log.info("setting up LeftNavigationView");
-		panel = new VerticalPanel();
-		panel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-		panel.setVerticalAlignment(VerticalPanel.ALIGN_TOP);
-		panel.setSpacing(5);
+		panel = new FlowPanel();
 		panel.setStylePrimaryName("LeftContentNavBar");
 		initWidget(panel);
 		panel.setWidth("220px");
