733cd73009893c1a4d45e926cd035f91a43b0fa1
Caius "kaio" Chance <cchance@redhat.com>
2009-10-23 10:07:05 +1000

    removed duplicated HasNodeMouseOverHandlers.java

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/HasNodeMouseOverHandlers.java
+++
@@ -1,9 +0,0 @@
-package org.fedorahosted.flies.webtrans.client;
-
-import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.event.shared.HasHandlers;
-
-public interface HasNodeMouseOverHandlers extends HasHandlers {
-	HandlerRegistration addDocumentSelectionHandler(DocumentSelectionHandler handler);
-}
-
