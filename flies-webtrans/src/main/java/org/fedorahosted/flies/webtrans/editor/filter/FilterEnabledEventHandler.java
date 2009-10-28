6bfd173145e8194971c420be1099bb3c5f4332aa
Caius "kaio" Chance <cchance@redhat.com>
2009-10-28 12:39:01 +1000

    add events and event handlers to translation unit

---
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/editor/filter/FilterEnabledEventHandler.java
@@ -0,0 +1,9 @@
+package org.fedorahosted.flies.webtrans.editor.filter;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface FilterEnabledEventHandler extends EventHandler {
+	
+	void onFilterEnabled(FilterEnabledEvent event);
+
+}
