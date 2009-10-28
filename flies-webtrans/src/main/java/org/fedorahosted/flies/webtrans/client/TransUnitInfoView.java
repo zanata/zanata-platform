184e0d5807bff09601cd0393aee2fa33e8e4673e
Caius "kaio" Chance <cchance@redhat.com>
2009-10-28 11:45:49 +1000

    create ui of translation unit panel

--- flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/TransUnitInfoView.java
+++ flies-webtrans/src/main/java/org/fedorahosted/flies/webtrans/client/TransUnitInfoView.java
@@ -1,14 +1,20 @@
 package org.fedorahosted.flies.webtrans.client;
 
 import org.fedorahosted.flies.webtrans.client.ui.CaptionPanel;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Label;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.widgetideas.client.ProgressBar;
 
 public class TransUnitInfoView extends CaptionPanel implements TransUnitInfoPresenter.Display {
 
+	private static TextBox filter1Label, filter2Label;
+	private static RadioButton filterAndTypeRadio, filterOrTypeRadio;
+	private static Button filterEnableButton, filterDisableButton;
+	
 	public TransUnitInfoView() {
 		setTitle("Translation Unit Info");
 		setBody(getStatusPanel());
@@ -20,11 +26,32 @@ public class TransUnitInfoView extends CaptionPanel implements TransUnitInfoPres
 		vpanel.setHeight("100px");
 		vpanel.setHorizontalAlignment(VerticalPanel.ALIGN_LEFT);
 		vpanel.setVerticalAlignment(VerticalPanel.ALIGN_TOP);
+		
+		filter1Label = new TextBox();
+		filter2Label = new TextBox();
+		
+		HorizontalPanel filterTypeRadioBar = new HorizontalPanel();
+		filterAndTypeRadio = new RadioButton(null);
+		filterAndTypeRadio.setText("And");
+		filterOrTypeRadio = new RadioButton(null);
+		filterOrTypeRadio.setText("Or");
+		filterTypeRadioBar.add(filterAndTypeRadio);
+		filterTypeRadioBar.add(filterOrTypeRadio);
+		
+		HorizontalPanel filterButtonBar = new HorizontalPanel();
+		filterEnableButton = new Button("Filter");
+		filterDisableButton = new Button("Reset");
+		filterButtonBar.add(filterEnableButton);
+		filterButtonBar.add(filterDisableButton);
 				
 		//ProgressBar bar = new ProgressBar(0.0, 2000.0,0.0);
 		//bar.setProgress(1500.0);
 		//bar.setTitle("Complete");
-		vpanel.add(new Label("Translation Unit Info"));
+		filterAndTypeRadio.setValue(true);
+		vpanel.add(filter1Label);
+		vpanel.add(filterTypeRadioBar);
+		vpanel.add(filter2Label);
+		vpanel.add(filterButtonBar);
 		//vpanel.add(bar);
 		return vpanel;
 	}
