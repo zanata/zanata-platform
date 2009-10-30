e01ab2509dc0341e44381df2f738c3c2a198bd34
Sean Flanigan <sflaniga@redhat.com>
2009-10-30 18:11:46 +1000

    Moved implementation of DocumentService into DocumentServiceActionImpl to allow hot deployment

--- flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentService.java
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentService.java
@@ -1,40 +1,19 @@
 package org.fedorahosted.flies.rest.service;
 
 import java.net.URISyntaxException;
-import java.util.Set;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.core.Response.Status;
 
-import org.fedorahosted.flies.LocaleId;
-import org.fedorahosted.flies.core.dao.DocumentDAO;
-import org.fedorahosted.flies.core.dao.ProjectContainerDAO;
-import org.fedorahosted.flies.repository.model.HDocument;
-import org.fedorahosted.flies.repository.model.HProjectContainer;
-import org.fedorahosted.flies.rest.MediaTypes;
 import org.fedorahosted.flies.rest.client.ContentQualifier;
-import org.fedorahosted.flies.rest.dto.Container;
 import org.fedorahosted.flies.rest.dto.Document;
 import org.fedorahosted.flies.rest.dto.DocumentResource;
 import org.fedorahosted.flies.rest.dto.ResourceList;
-import org.hibernate.Session;
 import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.security.Restrict;
-import org.jboss.seam.log.Log;
 
 @Name("documentService")
 @Path("/projects/p/{projectSlug}/iterations/i/{iterationSlug}/documents/d/{documentId}")
@@ -49,190 +28,60 @@ public class DocumentService implements DocumentServiceAction {
 	@PathParam("documentId")
 	private String documentId;
 	
-    @In 
-    private DocumentConverter documentConverter;
-
-    @In
-    private DocumentDAO documentDAO;
-	
-	@In
-	private ProjectContainerDAO projectContainerDAO;
-	
-	@In
-	private Session session;
-	
 	@Context
 	private UriInfo uri;
 	
-    @Logger 
-    private Log log;
-    
+	@In("DocumentServiceActionImpl")
+	private DocumentServiceAction impl;
+
 	
-    private HProjectContainer projectContainer;
+	public String getDocumentId() {
+		return documentId;
+	}
 	
-	private String getIterationSlug() {
+	public String getIterationSlug() {
 		return iterationSlug;
 	}
 
-	private String getProjectSlug() {
+	public String getProjectSlug() {
 		return projectSlug;
 	}
-
-	private HProjectContainer getContainer() {
-		if (projectContainer != null)
-			return projectContainer;
-		projectContainer = projectContainerDAO.getBySlug(
-				getProjectSlug(), 
-				getIterationSlug());
-		return projectContainer;
-	}
-
-	private Response containerNotFound() {
-		return Response.status(Status.NOT_FOUND).entity("Project Container not found").build();
-	}
-    
-	/* (non-Javadoc)
-	 * @see org.fedorahosted.flies.rest.service.DocumentServiceAction#get(org.fedorahosted.flies.rest.client.ContentQualifier)
-	 */
-	@GET
-	@Produces({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaTypes.APPLICATION_FLIES_DOCUMENT_JSON, 
-				MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public Response get(
-			@QueryParam("resources") @DefaultValue("") ContentQualifier resources) {
-		
-		HProjectContainer hProjectContainer = getContainer();
-		if(hProjectContainer == null)
-			return containerNotFound();
-		String docId = URIHelper.convertFromDocumentURIId(documentId);
-		
-		HDocument hDoc = documentDAO.getByDocId(hProjectContainer, docId);
-		
-		if(hDoc == null) {
-			return Response.status(Status.NOT_FOUND).entity("Document not found").build();
-		}
-		
-		Set<LocaleId> requestedLanguages = resources.getLanguages();
-		if (resources.isAll()) {
-			requestedLanguages = documentDAO.getTargetLocales(hDoc); 
-		}
-		
-		int requestedLevels = resources.isNone() ? 0 : Integer.MAX_VALUE;
-		Document doc = hDoc.toDocument(requestedLevels); 
-		documentConverter.addLinks(doc,uri.getRequestUri(), 
-				uri.getBaseUri().resolve(URIHelper.getIteration(projectSlug, iterationSlug)));
-		
-		return Response.ok().entity(doc).tag("v-" + doc.getRevision()).build();
-	}
 	
-	/* (non-Javadoc)
-	 * @see org.fedorahosted.flies.rest.service.DocumentServiceAction#put(org.fedorahosted.flies.rest.dto.Document)
-	 */
-	@PUT
-	@Consumes({ MediaTypes.APPLICATION_FLIES_DOCUMENT_XML, MediaTypes.APPLICATION_FLIES_DOCUMENT_JSON,
-				MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Restrict("#{identity.loggedIn}")
+	public UriInfo getUri() {
+		return uri;
+	}
+
+
+	public Response get(ContentQualifier resources) {
+		return impl.get(resources);
+	}
+
+	public Response getContent(ContentQualifier qualifier, int levels) {
+		return impl.getContent(qualifier, levels);
+	}
+
+	public Response getContentByResourceId(ContentQualifier qualifier,
+			String resourceId, int levels) {
+		return impl.getContentByResourceId(qualifier, resourceId, levels);
+	}
+
+	public Response postContent(ResourceList content, ContentQualifier qualifier) {
+		return impl.postContent(content, qualifier);
+	}
+
+	public Response postContentByResourceId(DocumentResource resource,
+			ContentQualifier qualifier, String resourceId) {
+		return impl.postContentByResourceId(resource, qualifier, resourceId);
+	}
+
 	public Response put(Document document) throws URISyntaxException {
-		
-		String hDocId = URIHelper.convertFromDocumentURIId(documentId);
-
-		if(!document.getId().equals(hDocId)){
-			return Response.status(Status.BAD_REQUEST).entity("Invalid document Id").build();
-		}
-
-		HProjectContainer hProjectContainer = getContainer();
-		if(hProjectContainer == null)
-			return containerNotFound();
-
-		HDocument hDoc = documentDAO.getByDocId(hProjectContainer, hDocId);
-		
-		if(hDoc == null) { // it's a create operation
-			hDoc = documentConverter.create(document, hProjectContainer);
-			hProjectContainer.getDocuments().put(hDoc.getDocId(), hDoc);
-			session.save(hDoc);
-			try{
-				session.flush();
-				return Response.created( uri.getBaseUri().resolve(URIHelper.getDocument(projectSlug, iterationSlug, documentId))).build();
-			}
-			catch(Exception e){
-				log.error("Invalid document content", e);
-				// TODO validation on the input data
-				// this could also be a server error
-				return Response.status(Status.BAD_REQUEST).entity("Invalid document content").build();
-			}
-		}
-		else{ // it's an update operation
-			documentConverter.merge(document, hDoc);
-			session.save(hDoc);
-			session.flush();
-			return Response.status(205).build();
-		}
-		
-	}
-	
-	// FIXME implement DELETE
-	
-	/* (non-Javadoc)
-	 * @see org.fedorahosted.flies.rest.service.DocumentServiceAction#getContent(org.fedorahosted.flies.rest.client.ContentQualifier, int)
-	 */
-	@GET
-	@Path("content/{qualifier}")
-	public Response getContent(
-			@PathParam("qualifier") ContentQualifier qualifier,
-			@QueryParam("levels") @DefaultValue("1") int levels){
-		ResourceList resources = new ResourceList();
-		return Response.ok().entity(resources).build();
+		return impl.put(document);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.fedorahosted.flies.rest.service.DocumentServiceAction#postContent(org.fedorahosted.flies.rest.dto.ResourceList, org.fedorahosted.flies.rest.client.ContentQualifier)
-	 */
-	@POST
-	@Path("content/{qualifier}")
-	@Restrict("#{identity.loggedIn}")
-	public Response postContent(
-			ResourceList content,
-			@PathParam("qualifier") ContentQualifier qualifier) {
-		
-		return Response.ok().build();
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.fedorahosted.flies.rest.service.DocumentServiceAction#putContent(org.fedorahosted.flies.rest.dto.ResourceList, org.fedorahosted.flies.rest.client.ContentQualifier)
-	 */
-	@PUT
-	@Path("content/{qualifier}")
-	public Response putContent(
-			ResourceList content,
-			@PathParam("qualifier") ContentQualifier qualifier) {
-		
-		return Response.ok().build();
+	public Response putContent(ResourceList content, ContentQualifier qualifier) {
+		return impl.putContent(content, qualifier);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.fedorahosted.flies.rest.service.DocumentServiceAction#postContentByResourceId(org.fedorahosted.flies.rest.dto.DocumentResource, org.fedorahosted.flies.rest.client.ContentQualifier, java.lang.String)
-	 */
-	@POST
-	@Path("content/{qualifier}/{resourceId}")
-	@Restrict("#{identity.loggedIn}")
-	public Response postContentByResourceId(
-			DocumentResource resource,
-			@PathParam("qualifier") ContentQualifier qualifier,
-			@PathParam("resourceId") String resourceId) {
-		
-		return Response.ok().build();
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.fedorahosted.flies.rest.service.DocumentServiceAction#getContentByResourceId(org.fedorahosted.flies.rest.client.ContentQualifier, java.lang.String, int)
-	 */
-	@GET
-	@Path("content/{qualifier}/{resourceId}")
-	public Response getContentByResourceId(
-			@PathParam("qualifier") ContentQualifier qualifier,
-			@PathParam("resourceId") String resourceId,
-			@QueryParam("levels") @DefaultValue("1") int levels){
-		return Response.ok().entity(new Container("id")).build();
-	}
-	
+
 
 }
--- flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentServiceActionImpl.java
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/service/DocumentServiceActionImpl.java
@@ -1,58 +1,188 @@
 package org.fedorahosted.flies.rest.service;
 
 import java.net.URISyntaxException;
+import java.util.Set;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.Response.Status;
 
+import org.fedorahosted.flies.LocaleId;
+import org.fedorahosted.flies.core.dao.DocumentDAO;
+import org.fedorahosted.flies.core.dao.ProjectContainerDAO;
+import org.fedorahosted.flies.repository.model.HDocument;
+import org.fedorahosted.flies.repository.model.HProjectContainer;
 import org.fedorahosted.flies.rest.client.ContentQualifier;
+import org.fedorahosted.flies.rest.dto.Container;
 import org.fedorahosted.flies.rest.dto.Document;
 import org.fedorahosted.flies.rest.dto.DocumentResource;
 import org.fedorahosted.flies.rest.dto.ResourceList;
+import org.hibernate.Session;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
 
+@AutoCreate
+@Scope(ScopeType.STATELESS)
+@Name("DocumentServiceActionImpl")
 public class DocumentServiceActionImpl implements DocumentServiceAction {
 
-	@Override
-	public Response get(ContentQualifier resources) {
-		// TODO Auto-generated method stub
-		return null;
+	@Logger 
+    private Log log;
+
+    // To access properties projectSlug, iterationSlug and request
+    @In 
+    private DocumentService documentService; 
+    
+    @In 
+    private DocumentConverter documentConverter;
+
+    @In
+    private DocumentDAO documentDAO;
+	
+	@In
+	private ProjectContainerDAO projectContainerDAO;
+	
+	@In
+	private Session session;
+	
+    private HProjectContainer projectContainer;
+	
+    private String getDocumentId() {
+    	return documentService.getDocumentId();
+    }
+    
+	private String getIterationSlug() {
+		return documentService.getIterationSlug();
 	}
 
-	@Override
-	public Response getContent(ContentQualifier qualifier, int levels) {
-		// TODO Auto-generated method stub
-		return null;
+	private String getProjectSlug() {
+		return documentService.getProjectSlug();
 	}
 
-	@Override
-	public Response getContentByResourceId(ContentQualifier qualifier,
-			String resourceId, int levels) {
-		// TODO Auto-generated method stub
-		return null;
+	public UriInfo getUri() {
+		return documentService.getUri();
+	}
+	
+	private HProjectContainer getContainer() {
+		if (projectContainer != null)
+			return projectContainer;
+		projectContainer = projectContainerDAO.getBySlug(
+				getProjectSlug(), 
+				getIterationSlug());
+		return projectContainer;
 	}
 
-	@Override
-	public Response postContent(ResourceList content, ContentQualifier qualifier) {
-		// TODO Auto-generated method stub
-		return null;
+	private Response containerNotFound() {
+		return Response.status(Status.NOT_FOUND).entity("Project Container not found").build();
 	}
-
-	@Override
-	public Response postContentByResourceId(DocumentResource resource,
-			ContentQualifier qualifier, String resourceId) {
-		// TODO Auto-generated method stub
-		return null;
+    
+	public Response get(
+			ContentQualifier resources) {
+		
+		HProjectContainer hProjectContainer = getContainer();
+		if(hProjectContainer == null)
+			return containerNotFound();
+		String docId = URIHelper.convertFromDocumentURIId(getDocumentId());
+		
+		HDocument hDoc = documentDAO.getByDocId(hProjectContainer, docId);
+		
+		if(hDoc == null) {
+			return Response.status(Status.NOT_FOUND).entity("Document not found").build();
+		}
+		
+		Set<LocaleId> requestedLanguages = resources.getLanguages();
+		if (resources.isAll()) {
+			requestedLanguages = documentDAO.getTargetLocales(hDoc); 
+		}
+		
+		int requestedLevels = resources.isNone() ? 0 : Integer.MAX_VALUE;
+		Document doc = hDoc.toDocument(requestedLevels); 
+		documentConverter.addLinks(doc, getUri().getRequestUri(), 
+				getUri().getBaseUri().resolve(URIHelper.getIteration(getProjectSlug(), getIterationSlug())));
+		
+		return Response.ok().entity(doc).tag("v-" + doc.getRevision()).build();
 	}
-
-	@Override
+	
 	public Response put(Document document) throws URISyntaxException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String hDocId = URIHelper.convertFromDocumentURIId(getDocumentId());
+
+		if(!document.getId().equals(hDocId)){
+			return Response.status(Status.BAD_REQUEST).entity("Invalid document Id").build();
+		}
+
+		HProjectContainer hProjectContainer = getContainer();
+		if(hProjectContainer == null)
+			return containerNotFound();
+
+		HDocument hDoc = documentDAO.getByDocId(hProjectContainer, hDocId);
+		
+		if(hDoc == null) { // it's a create operation
+			hDoc = documentConverter.create(document, hProjectContainer);
+			hProjectContainer.getDocuments().put(hDoc.getDocId(), hDoc);
+			session.save(hDoc);
+			try{
+				session.flush();
+				return Response.created(getUri().getBaseUri().resolve(
+						URIHelper.getDocument(
+								getProjectSlug(), getIterationSlug(), getDocumentId()))).build();
+			}
+			catch(Exception e){
+				log.error("Invalid document content", e);
+				// TODO validation on the input data
+				// this could also be a server error
+				return Response.status(Status.BAD_REQUEST).entity("Invalid document content").build();
+			}
+		}
+		else{ // it's an update operation
+			documentConverter.merge(document, hDoc);
+			session.save(hDoc);
+			session.flush();
+			return Response.status(205).build();
+		}
+		
+	}
+	
+	// FIXME implement DELETE
+	
+	public Response getContent(
+			ContentQualifier qualifier,
+			int levels){
+		ResourceList resources = new ResourceList();
+		return Response.ok().entity(resources).build();
 	}
 
-	@Override
-	public Response putContent(ResourceList content, ContentQualifier qualifier) {
-		// TODO Auto-generated method stub
-		return null;
+	public Response postContent(
+			ResourceList content,
+			ContentQualifier qualifier) {
+		
+		return Response.ok().build();
+	}
+	
+	public Response putContent(
+			ResourceList content,
+			ContentQualifier qualifier) {
+		
+		return Response.ok().build();
 	}
 
+	public Response postContentByResourceId(
+			DocumentResource resource,
+			ContentQualifier qualifier,
+			String resourceId) {
+		
+		return Response.ok().build();
+	}
+	
+	public Response getContentByResourceId(
+			ContentQualifier qualifier,
+			String resourceId,
+			int levels){
+		return Response.ok().entity(new Container("id")).build();
+	}
 }
