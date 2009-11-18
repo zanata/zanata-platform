c3627c7a9b19df638469e2f8feef7525ea13f8b5
Sean Flanigan <sflaniga@redhat.com>
2009-11-18 16:06:21 +1000

    Avoids overwriting database parameters if set by TestNG

--- flies-war/src/test/java/org/fedorahosted/flies/rest/service/FliesDBUnitSeamTest.java
+++ flies-war/src/test/java/org/fedorahosted/flies/rest/service/FliesDBUnitSeamTest.java
@@ -5,10 +5,35 @@ import org.testng.annotations.BeforeClass;
 
 public abstract class FliesDBUnitSeamTest extends DBUnitSeamTest {
 
+	private boolean databaseSet;
+	private boolean jndiNameSet;
+	private boolean binaryDirSet;
+
 	@BeforeClass
 	public void setDatabaseParameters() throws Exception {
-		setDatabase("hsql");
-		setDatasourceJndiName("java:/fliesDatasource");
-		setBinaryDir("");
+		if(!binaryDirSet)
+			setBinaryDir("");
+		if (!databaseSet)
+			setDatabase("hsql");
+		if (jndiNameSet)
+			setDatasourceJndiName("java:/fliesDatasource");
+	}
+	
+	@Override
+	public void setBinaryDir(String binaryDir) {
+		super.setBinaryDir(binaryDir);
+		binaryDirSet = true;
+	}
+	
+	@Override
+	public void setDatabase(String database) {
+		super.setDatabase(database);
+		databaseSet = true;
+	}
+	
+	@Override
+	public void setDatasourceJndiName(String datasourceJndiName) {
+		super.setDatasourceJndiName(datasourceJndiName);
+		jndiNameSet = true;
 	}
 }
