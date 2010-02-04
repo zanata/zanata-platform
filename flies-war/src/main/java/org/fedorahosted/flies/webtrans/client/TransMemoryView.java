5f6cdae22c9d301092d9564cd44af53547769109
Sean Flanigan <sflaniga@redhat.com>
2010-02-04 19:57:24 +1000

    Enter key now triggers search button

--- flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/TransMemoryView.java
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/TransMemoryView.java
@@ -1,5 +1,8 @@
 package org.fedorahosted.flies.webtrans.client;
 
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -15,6 +18,15 @@ public class TransMemoryView extends FlowPanel implements TransMemoryPresenter.D
 	public TransMemoryView() {
 		tmTextBox = new TextBox();
 		searchButton = new Button("Search");
+		
+		tmTextBox.addKeyUpHandler(new KeyUpHandler() {
+			@Override
+			public void onKeyUp(KeyUpEvent event) {
+				if( event.getNativeKeyCode() == KeyCodes.KEY_ENTER ) {
+					searchButton.click();
+				}
+			}
+		});
 	
 		add(tmTextBox);
 		add(searchButton);
