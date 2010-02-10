b641580bdfcd1074bcdc89a07339660609653878
James Ni <kent.neo@gmail.com>
2010-02-10 20:25:51 +0800

    Remove the VisibilityEvent Handler from TableEditorPresenter

--- flies-war/src/main/java/org/fedorahosted/flies/webtrans/editor/table/TableEditorPresenter.java
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/editor/table/TableEditorPresenter.java
@@ -24,8 +24,6 @@ import org.fedorahosted.flies.webtrans.client.DocumentSelectionHandler;
 import org.fedorahosted.flies.webtrans.client.NavTransUnitEvent;
 import org.fedorahosted.flies.webtrans.client.NavTransUnitHandler;
 import org.fedorahosted.flies.webtrans.client.NotificationEvent;
-import org.fedorahosted.flies.webtrans.client.VisibilityEvent;
-import org.fedorahosted.flies.webtrans.client.VisibilityHandler;
 import org.fedorahosted.flies.webtrans.client.WorkspaceContext;
 import org.fedorahosted.flies.webtrans.client.NotificationEvent.Severity;
 import org.fedorahosted.flies.webtrans.client.auth.Identity;
@@ -64,7 +62,6 @@ public class TableEditorPresenter extends DocumentEditorPresenter<TableEditorPre
 	
 	public static final Place PLACE = new Place("TableEditor");
 	private int latestStatusCountOffset = -1;
-	private boolean transMemoryTabVisible = false;
 	
 	public interface Display extends WidgetDisplay, HasPageNavigation {
 		HasSelectionHandlers<TransUnit> getSelectionHandlers();
@@ -113,18 +110,8 @@ public class TableEditorPresenter extends DocumentEditorPresenter<TableEditorPre
 				if(event.getSelectedItem() != currentSelection) {
 					currentSelection = event.getSelectedItem();
 					//startEditing(currentSelection);
-					if(transMemoryTabVisible) {
-						//Start search the Translation Memory
-					}
 					eventBus.fireEvent(event);
 				}
-			}
-		}));
-		
-		registerHandler(eventBus.addHandler(VisibilityEvent.getType(), new VisibilityHandler() {
-			@Override
-			public void onVisibilityChange(VisibilityEvent tabSelectionEvent) {
-				 //transMemoryTabVisible = tabSelectionEvent.isTranslationMemoryVisible();
 			}
 		}));
 		
