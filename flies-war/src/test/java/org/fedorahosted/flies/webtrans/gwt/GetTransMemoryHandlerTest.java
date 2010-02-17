ecec6a7bff46c710f2b9ad9902e124d419ac0de3
Sean Flanigan <sflaniga@redhat.com>
2010-02-17 14:53:23 +1000

    Swapped 'actual' and 'expected' in GetTransMemoryHandlerTest

--- flies-war/src/test/java/org/fedorahosted/flies/webtrans/gwt/GetTransMemoryHandlerTest.java
+++ flies-war/src/test/java/org/fedorahosted/flies/webtrans/gwt/GetTransMemoryHandlerTest.java
@@ -11,16 +11,22 @@ import static org.fedorahosted.flies.webtrans.gwt.GetTransMemoryHandler.toHQLQue
 public class GetTransMemoryHandlerTest {
 
 	public void testHQLQuery() {
-		assertThat("%plaintext%", is(toHQLQuery("plaintext")));
-		assertThat("%like special characters ~% ~_ ~~ plus \\%", 
-			is(toHQLQuery(
-				"LIKE special characters % _ ~ plus \\")));
+		assertThat(
+			toHQLQuery("plaintext"), 
+			is("%plaintext%"));
+		assertThat(
+			toHQLQuery("LIKE special characters % _ ~ plus \\"), 
+			is("%like special characters ~% ~_ ~~ plus \\%"));
 	}
 	
 	public void testLuceneQuery(){
-		assertThat("plaintext", is(toLuceneQuery("plaintext")));
-		assertThat("lucene special characters \\+ \\- \\&& \\|| \\! \\( \\) \\{ \\} \\[ \\] \\^ \\\" \\~ \\* \\? \\: \\\\ plus % _", 
-			is(toLuceneQuery(
-				"lucene special characters + - && || ! ( ) { } [ ] ^ \" ~ * ? : \\ plus % _")));
+		assertThat(
+			toLuceneQuery("plaintext"), 
+			is("plaintext"));
+		assertThat(
+			toLuceneQuery("lucene special characters + - && || ! ( ) " +
+					"{ } [ ] ^ \" ~ * ? : \\ plus % _"), 
+			is("lucene special characters \\+ \\- \\&& \\|| \\! \\( \\) " +
+					"\\{ \\} \\[ \\] \\^ \\\" \\~ \\* \\? \\: \\\\ plus % _"));
 	}
 }
