0e2a4d041551d16e938746b0be69fc6d37ea6920
Sean Flanigan <sflaniga@redhat.com>
2010-02-25 13:33:24 +1000

    Removed LIKE HQL query for EXACT searches, replaced with a Lucene phrase query.

--- flies-war/src/test/java/org/fedorahosted/flies/webtrans/gwt/GetTransMemoryHandlerTest.java
+++ flies-war/src/test/java/org/fedorahosted/flies/webtrans/gwt/GetTransMemoryHandlerTest.java
@@ -6,20 +6,10 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.fedorahosted.flies.webtrans.gwt.GetTransMemoryHandler.toFuzzyLuceneQuery;
 import static org.fedorahosted.flies.webtrans.gwt.GetTransMemoryHandler.toLuceneQuery;
-import static org.fedorahosted.flies.webtrans.gwt.GetTransMemoryHandler.toHQLQuery;
 
 @Test(groups={"unit-tests"})
 public class GetTransMemoryHandlerTest {
 
-	public void testHQLQuery() {
-		assertThat(
-			toHQLQuery("plaintext"), 
-			is("%plaintext%"));
-		assertThat(
-			toHQLQuery("LIKE special characters % _ ~ plus \\"), 
-			is("%like special characters ~% ~_ ~~ plus \\%"));
-	}
-	
 	// list of special chars taken from
 	// http://lucene.apache.org/java/2_4_1/queryparsersyntax.html#Escaping%20Special%20Characters
 
