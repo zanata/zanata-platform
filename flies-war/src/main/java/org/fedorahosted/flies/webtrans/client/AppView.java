75e53e30c2215b487b1000c4f4e7df0568772b98
Asgeir Frimannsson <asgeirf@redhat.com>
2010-03-08 10:36:52 +1000

    added ui fields for page-level elements and minor style changes

--- flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/AppView.java
+++ flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/AppView.java
@@ -2,9 +2,12 @@ package org.fedorahosted.flies.webtrans.client;
 
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.AnchorElement;
+import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -18,10 +21,19 @@ public class AppView extends Composite implements AppPresenter.Display {
 	private static AppViewUiBinder uiBinder = GWT.create(AppViewUiBinder.class);
 	
 	@UiField
+	AnchorElement signOutLink, leaveLink, helpLink;
+	
+	@UiField
+	SpanElement user, workspaceName, workspaceLocale;
+	
+	@UiField
 	FlowPanel editor;
 	
 	public AppView() {
 		initWidget(uiBinder.createAndBindUi(this));
+		user.setInnerText("Bob the Builder");
+		workspaceName.setInnerText("Deployment Guide (F13)");
+		workspaceLocale.setInnerText("German");
 	}
 	
 	@Override
