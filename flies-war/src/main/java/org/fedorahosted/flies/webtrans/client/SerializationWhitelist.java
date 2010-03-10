58f63b62f839613f90e5bad957e4807fad0ff08e
Asgeir Frimannsson <asgeirf@redhat.com>
2010-03-10 10:52:31 +1000

    remove classes not in use

--- flies-war/src/main/java/org/fedorahosted/flies/webtrans/client/SerializationWhitelist.java
+++
@@ -1,7 +0,0 @@
-package org.fedorahosted.flies.webtrans.client;
-
-import com.google.gwt.user.client.rpc.IsSerializable;
-
-public class SerializationWhitelist implements IsSerializable {
-//	InvocationTargetException dummy1;
-}
