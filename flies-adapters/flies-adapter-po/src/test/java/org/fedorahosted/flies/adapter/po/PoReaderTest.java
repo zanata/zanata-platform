6d446286686a9b550872e9b8991b39312bd908ac
Caius "kaio" Chance <cchance@redhat.com>
2010-03-15 17:01:22 +1000

    Moved po/pot samples into publican filestruct. Modified unit test of poreader testing on publican real po/pot files.
    --HG--
    rename : flies-adapters/flies-adapter-po/src/test/resources/Accounts_And_Subscriptions.po => flies-adapters/flies-adapter-po/src/test/resources/ja-JP/Accounts_And_Subscriptions.po
    rename : flies-adapters/flies-adapter-po/src/test/resources/Accounts_And_Subscriptions.pot => flies-adapters/flies-adapter-po/src/test/resources/pot/Accounts_And_Subscriptions.pot

--- flies-adapters/flies-adapter-po/src/test/java/org/fedorahosted/flies/adapter/po/PoReaderTest.java
+++ flies-adapters/flies-adapter-po/src/test/java/org/fedorahosted/flies/adapter/po/PoReaderTest.java
@@ -4,7 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 import junit.framework.Assert;
 
@@ -12,22 +14,24 @@ import org.fedorahosted.flies.common.LocaleId;
 import org.fedorahosted.flies.rest.dto.Document;
 import org.fedorahosted.flies.rest.dto.DocumentResource;
 import org.fedorahosted.flies.rest.dto.TextFlow;
+import org.fedorahosted.flies.rest.dto.TextFlowTargets;
+import org.fedorahosted.flies.rest.dto.po.PoHeader;
+import org.fedorahosted.flies.rest.dto.po.PotEntryData;
 import org.junit.Test;
 import org.xml.sax.InputSource;
 
 public class PoReaderTest {
 	
-	LocaleId de = new LocaleId("de");
-	LocaleId fr = new LocaleId("fr");
+	LocaleId ja = new LocaleId("ja-JP");
 
 	@Test
 	public void extractTemplateThenAdd2Targets() throws IOException, JAXBException {
-		String testDir = "src/test/resources/extractTemplateThenAdd2Targets";
+		String testDir = "src/test/resources/";
 
 		Document doc = new Document("doc1", "doc1.pot", "/", PoReader.PO_CONTENT_TYPE);
 		
 		InputSource inputSource = new InputSource(
-				new File(testDir, "pot/file.pot").toURI().toString()
+				new File(testDir, "pot/Accounts_And_Subscriptions.pot").toURI().toString()
 		);
 		inputSource.setEncoding("utf8");
 		
@@ -35,38 +39,27 @@ public class PoReaderTest {
 
 		System.out.println("parsing template");
 		poReader.extractTemplate(doc, inputSource);
-		String [] locales = new String[]{"de", "fr"};
+		String [] locales = new String[]{"ja-JP"};
 		for (String locale : locales){
 			inputSource = new InputSource(
-					new File(testDir, locale+"/file.po").toURI().toString()
+					new File(testDir, locale+"/Accounts_And_Subscriptions.po").toURI().toString()
 			);
 			inputSource.setEncoding("utf8");
 			System.out.println("extracting target: " + locale);
 			poReader.extractTarget(doc, inputSource, new LocaleId(locale));
 		}
 		
-//		System.out.println("marshalling");
-//		JAXBContext jaxbContext = JAXBContext.newInstance(Document.class, PotEntryData.class, PoHeader.class, TextFlowTargets.class);
-//		Marshaller m = jaxbContext.createMarshaller();
-//		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-//		m.marshal(doc, System.out);
+		System.out.println("marshalling");
+		JAXBContext jaxbContext = JAXBContext.newInstance(Document.class, PotEntryData.class, PoHeader.class, TextFlowTargets.class);
+		Marshaller m = jaxbContext.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		m.marshal(doc, System.out);
 		
 		List<DocumentResource> resources = doc.getResources();
 		
 		TextFlow tf1 = (TextFlow) resources.get(0);
-		Assert.assertEquals("a", tf1.getContent());
-		Assert.assertEquals("one", tf1.getTarget(de).getContent());
-		Assert.assertEquals("1", tf1.getTarget(fr).getContent());
-		
-		TextFlow tf2 = (TextFlow) resources.get(1);
-		Assert.assertEquals("b", tf2.getContent());
-		Assert.assertEquals("two", tf2.getTarget(de).getContent());
-		Assert.assertNull(tf2.getTarget(fr));
-
-		TextFlow tf3 = (TextFlow) resources.get(2);
-		Assert.assertEquals("c", tf3.getContent());
-		Assert.assertNull(tf3.getTarget(de));
-		Assert.assertNull(tf3.getTarget(fr));
+		Assert.assertEquals("Accounts and Subscriptions", tf1.getContent());
+		Assert.assertEquals("アカウントと登録", tf1.getTarget(ja).getContent());
 		
 		// TODO test PO headers and attributes
 	}
