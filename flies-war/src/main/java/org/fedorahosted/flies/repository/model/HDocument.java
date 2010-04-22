ec9e76dd3f8e5537b88fa46a1686051a7b9ad080
Sean Flanigan <sflaniga@redhat.com>
2010-04-22 17:58:33 +1000

    Explicitly check HTextFlows with Hibernate validator

--- flies-war/src/main/java/org/fedorahosted/flies/repository/model/HDocument.java
+++ flies-war/src/main/java/org/fedorahosted/flies/repository/model/HDocument.java
@@ -211,7 +211,7 @@ public class HDocument extends AbstractFliesEntity{
 		this.allResources = resources;
 	}
 	
-	@OneToMany(cascade=CascadeType.ALL/*, mappedBy="document"*/)
+	@OneToMany(cascade=CascadeType.ALL, mappedBy="document")
 	@Where(clause="obsolete=0")
 	@IndexColumn(name="pos", base=0, nullable=true)// see http://opensource.atlassian.com/projects/hibernate/browse/HHH-4390?focusedCommentId=30964#action_30964
 	@JoinColumn(name="document_id",nullable=true)
