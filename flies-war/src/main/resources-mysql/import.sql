a28e0c4dc0bed54cf28d1389d66c80cae9971c38
Asgeir Frimannsson <asgeirf@redhat.com>
2010-05-20 16:05:35 +1000

    added mysql versions of triggers

---
+++ flies-war/src/main/resources-mysql/import.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+DROP TRIGGER /*!50033 IF EXISTS */ `HDocument_Update` $$
+CREATE TRIGGER `HDocument_Update` BEFORE UPDATE on `HDocument` FOR EACH ROW BEGIN IF NEW.revision != OLD.revision THEN INSERT INTO HDocumentHistory(document_id,revision,contentType,docId,locale,name,path,lastChanged,last_modified_by_id,obsolete) VALUES (OLD.id,OLD.revision,OLD.contentType,OLD.docId,OLD.locale,OLD.name,OLD.path,OLD.lastChanged,OLD.last_modified_by_id,OLD.obsolete); END IF; END$$
+DROP TRIGGER /*!50033 IF EXISTS */ `HTextFlow_Update` $$
+CREATE TRIGGER `HTextFlow_Update` BEFORE UPDATE on `HTextFlow` FOR EACH ROW BEGIN IF NEW.revision != OLD.revision THEN INSERT INTO HTextFlowHistory(tf_id,revision,content, obsolete, pos) VALUES (OLD.id,OLD.revision,OLD.content,OLD.obsolete,OLD.pos); END IF; END$$
+DROP TRIGGER /*!50033 IF EXISTS */ `HTextFlowTarget_Update` $$
+CREATE TRIGGER `HTextFlowTarget_Update` BEFORE UPDATE on `HTextFlowTarget` FOR EACH ROW BEGIN IF NEW.versionNum != OLD.versionNum THEN INSERT INTO HTextFlowTarget(target_id,versionNum,content, lastChanged, last_modified_by_id, state, tf_revision) VALUES (OLD.id,OLD.versionNum,OLD.content,OLD.lastChanged,OLD.last_modified_by_id,OLD.state,OLD.tf_revision); END IF; END$$
+DELIMITER ;
+
