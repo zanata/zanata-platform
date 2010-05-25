190acd4c10f06ec07a5ba5b41eadb0cfd64eb6bb
Asgeir Frimannsson <asgeirf@redhat.com>
2010-05-25 13:49:26 +1000

    let the exception mapper be responsible for logging HibernateExceptions

--- flies-war/src/main/java/org/fedorahosted/flies/rest/HibernateExceptionMapper.java
+++ flies-war/src/main/java/org/fedorahosted/flies/rest/HibernateExceptionMapper.java
@@ -6,13 +6,18 @@ import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 import org.hibernate.HibernateException;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.log.Logging;
 
 @Provider
 public class HibernateExceptionMapper implements
 		ExceptionMapper<HibernateException> {
 
+	Log log = Logging.getLog(HibernateExceptionMapper.class);
+	
 	@Override
 	public Response toResponse(HibernateException exception) {
+		log.error("Hibernate Exception in REST request", exception);
 		return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 	}
 
