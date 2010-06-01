2a40a20fb46397b786b3c5576e72ea076c913202
Asgeir Frimannsson <asgeirf@redhat.com>
2010-06-01 18:35:12 +1000

    added missing classes

---
+++ flies-common-api/src/main/java/org/fedorahosted/flies/rest/dto/v1/TranslationResource.java
@@ -0,0 +1,37 @@
+package org.fedorahosted.flies.rest.dto.v1;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.fedorahosted.flies.common.Namespaces;
+
+@XmlType(name="translationResourceType", namespace=Namespaces.FLIES, propOrder={"extensions", "textFlowTargets"})
+@XmlRootElement(name="translation-resource", namespace=Namespaces.FLIES)
+public class TranslationResource implements Serializable {
+
+	private ExtensionSet extensions;
+	private List<TextFlowTargetWithId> textFlowTargets;
+
+	@XmlElementWrapper(name="extensions", namespace=Namespaces.FLIES, required=false)
+	@XmlAnyElement(lax=true)
+	public ExtensionSet getExtensions() {
+		if(extensions == null)
+			extensions = new ExtensionSet();
+		return extensions;
+	}
+
+	@XmlElementWrapper(name="targets", namespace=Namespaces.FLIES, required=false)
+	public List<TextFlowTargetWithId> getTextFlowTargets() {
+		if(textFlowTargets == null) {
+			textFlowTargets = new ArrayList<TextFlowTargetWithId>();
+		}
+		return textFlowTargets;
+	}
+	
+}
---
+++ flies-common-api/src/main/java/org/fedorahosted/flies/rest/dto/v1/ext/PoTargetHeader.java
@@ -0,0 +1,56 @@
+package org.fedorahosted.flies.rest.dto.v1.ext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.fedorahosted.flies.common.LocaleId;
+import org.fedorahosted.flies.rest.dto.po.HeaderEntry;
+import org.fedorahosted.flies.rest.dto.v1.Extension;
+
+@XmlType(name="poTargetHeader", namespace=PoHeader.NAMESPACE, propOrder={"comment", "entries"})
+@XmlRootElement(name="po-target-header", namespace=PoHeader.NAMESPACE)
+public class PoTargetHeader extends Extension {
+
+	public static final String ID = "gettext-target-header";
+	public static final String VERSION = PoHeader.VERSION;
+	public static final String NAMESPACE = PoHeader.NAMESPACE;
+	
+	private String comment;
+	private List<HeaderEntry> entries;
+	
+	public PoTargetHeader() {
+		super(ID,VERSION);
+	}
+	
+	public PoTargetHeader(String comment, HeaderEntry ... entries) {
+		super(ID,VERSION);
+		setComment(comment);
+		for (int i = 0; i < entries.length; i++) {
+			getEntries().add(entries[i]);
+		}
+	}
+	
+	@XmlElement(name="comment", namespace=PoHeader.NAMESPACE, required=true)
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	@XmlElementWrapper(name="entries", namespace=PoHeader.NAMESPACE, required=true)
+	@XmlElement(name="entry", namespace=PoHeader.NAMESPACE)
+	public List<HeaderEntry> getEntries() {
+		if(entries == null)
+			entries = new ArrayList<HeaderEntry>();
+		return entries;
+	}	
+	
+}
