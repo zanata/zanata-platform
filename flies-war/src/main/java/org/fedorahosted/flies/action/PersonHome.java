7b339af6088380ceba373c0bfe4eee9a4ebfba00
Caius "kaio" Chance <cchance@redhat.com>
2010-06-02 13:13:14 +1000

    Revert PersonHome when editprofile is now using #{personHome.instance.*}

--- flies-war/src/main/java/org/fedorahosted/flies/action/PersonHome.java
+++ flies-war/src/main/java/org/fedorahosted/flies/action/PersonHome.java
@@ -8,11 +8,8 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
-import org.jboss.seam.faces.FacesMessages;
 import org.jboss.seam.framework.EntityHome;
-import org.jboss.seam.international.StatusMessages;
 import org.jboss.seam.log.Log;
-import org.jboss.seam.security.management.IdentityManager;
 import org.jboss.seam.security.management.JpaIdentityStore;
 
 @Name("personHome")
@@ -28,9 +25,6 @@ public class PersonHome extends EntityHome<HPerson>{
 	@Logger
 	Log log;
 	
-	private String name;
-	private String email;
-	
 	@Override
 	public Object getId() {
 		Object id = super.getId();
@@ -44,35 +38,5 @@ public class PersonHome extends EntityHome<HPerson>{
 		accountDAO.createApiKey(getInstance().getAccount());
 		getEntityManager().merge(getInstance().getAccount());
 		log.info("Reset API key for {0}", getInstance().getAccount().getUsername());
-	}
-	
-	@Override
-	public String update(){
-		if (getName() == null || "".equals(getName())
-				|| getEmail() == null || "".equals(getEmail())) {
-			StatusMessages.instance().addToControl("emptyField", "Please fill out all required fields.");
-            return "failure";
-		} else {
-			getInstance().setName(getName());
-			getInstance().setEmail(getEmail());
-			FacesMessages.instance().add("Profile is updated.");
-		}
-		return "success";
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getEmail() {
-		return email;
 	}
 }
