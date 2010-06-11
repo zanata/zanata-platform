4432f52a6ee4ebe7ac1299e87c4df4c7dce26bdd
Sean Flanigan <sflaniga@redhat.com>
2010-06-11 12:23:20 +1000

    Implement workaround for JBSEAM-4503
    Update issue 85
    Implement workaround for JBSEAM-4503

---
+++ flies-war/src/main/java/org/fedorahosted/flies/security/FixedClientUidSelector.java
@@ -0,0 +1,52 @@
+package org.fedorahosted.flies.security;
+
+import javax.faces.context.FacesContext;
+
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Install;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.ui.ClientUidSelector;
+import org.jboss.seam.util.RandomStringUtils;
+
+@Name("org.jboss.seam.ui.clientUidSelector")
+@Install(precedence = Install.DEPLOYMENT)
+/**
+ * Workaround for https://jira.jboss.org/browse/JBSEAM-4503
+ * supplied by "wolfgang geck" in 
+ * http://seamframework.org/Community/SeamTokenTagProblemCSRF#comment110227
+ */
+public class FixedClientUidSelector extends ClientUidSelector {
+
+	private static final long serialVersionUID = -4923235748771706010L;
+	private String clientUid;
+
+	@Create
+	public void onCreate() {
+		setCookiePath(FacesContext.getCurrentInstance().getExternalContext()
+				.getRequestContextPath());
+		setCookieMaxAge(-1);
+		setCookieEnabled(true);
+		clientUid = getCookieValue();
+	}
+
+	public void seed() {
+		if (!isSet()) {
+			clientUid = RandomStringUtils.random(50, true, true); // Fixed
+			setCookieValueIfEnabled(clientUid);
+		}
+	}
+
+	public boolean isSet() {
+		return clientUid != null;
+	}
+
+	public String getClientUid() {
+		return clientUid;
+	}
+
+	@Override
+	protected String getCookieName() {
+		return "javax.faces.ClientToken";
+	}
+
+}
