62d1453ccd74b6a42d079a00a335681a919512b6
Asgeir Frimannsson <asgeirf@redhat.com>
2010-07-16 22:46:01 +1000

    update sync gwt messages script to autocreate missing directories and also sync UiBinder generated messages

--- flies-war/sync_gwt_messages.sh
+++ flies-war/sync_gwt_messages.sh
@@ -4,6 +4,9 @@ EXTRA_DIR=target/extra/webtrans/
 TARGET_DIR=src/main/resources/
 
 SUFFIX='_default.properties'
+
+echo "Processing Messages";
+
 for i in `ls $EXTRA_DIR*$SUFFIX`; 
 do
   # remove EXTRA_DIR prefix
@@ -14,7 +17,28 @@ do
 
   NEW_FILE=$TARGET_DIR${NEW_FILE//./\/}.properties
   echo "syncing $NEW_FILE";
+  mkdir -p `dirname $NEW_FILE`;
   cp $i $NEW_FILE;
 done
+
+echo "Processing UiBinder Messages";
+
+SUFFIX='.properties'
+
+for i in `ls $EXTRA_DIR*UiBinderImplGenMessages$SUFFIX`; 
+do
+  # remove EXTRA_DIR prefix
+  NEW_FILE=${i#$EXTRA_DIR}
+
+  # remove SUFFIX
+  NEW_FILE=${NEW_FILE%$SUFFIX}
+
+  NEW_FILE=$TARGET_DIR${NEW_FILE//./\/}.properties
+  echo "syncing $NEW_FILE";
+  mkdir -p `dirname $NEW_FILE`;
+  cp $i $NEW_FILE;
+done
+
+
 
 
