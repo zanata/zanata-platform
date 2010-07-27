adbb85269d8d706d13d4e8db3773502022b57b53
Sean Flanigan <sflaniga@redhat.com>
2010-07-27 14:13:59 +1000

    Added test class which escaped earlier check-in

---
+++ flies-war/src/test/java/org/fedorahosted/flies/dao/TribeDAOTest.java
@@ -0,0 +1,51 @@
+package org.fedorahosted.flies.dao;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.dbunit.operation.DatabaseOperation;
+import org.fedorahosted.flies.FliesDbunitJpaTest;
+import org.fedorahosted.flies.model.HTribe;
+import org.hibernate.Session;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Test(groups = { "jpa-tests" })
+public class TribeDAOTest extends FliesDbunitJpaTest
+{
+
+   private TribeDAO dao;
+
+   @Override
+   protected void prepareDBUnitOperations()
+   {
+      beforeTestOperations.add(new DataSetOperation("META-INF/testdata/AccountData.dbunit.xml",
+            DatabaseOperation.CLEAN_INSERT));
+      beforeTestOperations.add(new DataSetOperation("META-INF/testdata/TribesData.dbunit.xml",
+            DatabaseOperation.CLEAN_INSERT));
+   }
+
+   @BeforeMethod(firstTimeOnly = true)
+   public void setup()
+   {
+      dao = new TribeDAO((Session) getEm().getDelegate());
+   }
+
+   @Test
+   public void getValidTribeByLocale()
+   {
+      HTribe tribe = dao.getByLocale("de-DE");
+      assertThat(tribe, notNullValue());
+      assertThat(tribe.getLocale().getId(), is("de-DE"));
+   }
+
+   @Test
+   public void getValidTribeById()
+   {
+      HTribe tribe = dao.findById(1l, false);
+      assertThat(tribe, notNullValue());
+      assertThat(tribe.getLocale().getId(), is("as-IN"));
+   }
+
+}
