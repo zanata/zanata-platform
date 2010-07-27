d5cd67012289c544ec8242ddc588037ae53ce0d4
Sean Flanigan <sflaniga@redhat.com>
2010-07-27 14:15:13 +1000

    Added namespace for Flies client configuration

--- flies-common-api/src/main/java/org/fedorahosted/flies/common/Namespaces.java
+++ flies-common-api/src/main/java/org/fedorahosted/flies/common/Namespaces.java
@@ -4,6 +4,7 @@ public final class Namespaces
 {
 
    public static final String FLIES = "http://flies.fedorahosted.org/api/v1/";
+   public static final String FLIES_CONFIG = "http://flies.fedorahosted.org/config/v1/";
    public static final String XML = "http://www.w3.org/XML/1998/namespace";
 
    private Namespaces()
