97ccb2db2b5494b1d382b71382bac2f75ea36513
Sean Flanigan <sflaniga@redhat.com>
2010-08-12 16:17:24 +1000

    Added parameters to PutUserMojo

--- flies-clients/flies-maven-plugin/src/main/java/org/fedorahosted/flies/maven/PutUserMojo.java
+++ flies-clients/flies-maven-plugin/src/main/java/org/fedorahosted/flies/maven/PutUserMojo.java
@@ -12,9 +12,120 @@ import org.fedorahosted.flies.client.commands.PutUserCommand;
 public class PutUserMojo extends ConfigurableMojo<PutUserCommand>
 {
 
+   /**
+    * Full name of the user
+    * 
+    * @parameter expression="${flies.user.name}"
+    * @required
+    */
+   @SuppressWarnings("unused")
+   private String name;
+
+   /**
+    * Email address of the user
+    * 
+    * @parameter expression="${flies.user.email}"
+    * @required
+    */
+   @SuppressWarnings("unused")
+   private String email;
+
+   /**
+    * Login/username of the user
+    * 
+    * @parameter expression="${flies.user.username}"
+    * @required
+    */
+   @SuppressWarnings("unused")
+   private String userUsername;
+
+   /**
+    * User password hash
+    * 
+    * @parameter expression="${flies.user.passwordhash}"
+    * @required
+    */
+   @SuppressWarnings("unused")
+   private String passwordHash;
+
+   /**
+    * User's api key (empty for none)
+    * 
+    * @parameter expression="${flies.user.key}"
+    * @required
+    */
+   @SuppressWarnings("unused")
+   private String userKey;
+
+   /**
+    * Security roles for the user
+    * 
+    * @parameter expression="${flies.user.roles}"
+    * @required
+    */
+   @SuppressWarnings("unused")
+   private String roles;
+
+   /**
+    * Language teams for the user
+    * 
+    * @parameter expression="${flies.user.langs}"
+    * @required
+    */
+   @SuppressWarnings("unused")
+   private String langs;
+
+   /**
+    * Whether the account should be disabled
+    * 
+    * @parameter expression="${flies.user.disabled}"
+    * @required
+    */
+   @SuppressWarnings("unused")
+   private boolean disabled;
+
    public PutUserMojo() throws Exception
    {
       super(new PutUserCommand());
    }
 
+   public void setName(String name)
+   {
+      getCommand().setName(name);
+   }
+
+   public void setEmail(String email)
+   {
+      getCommand().setEmail(email);
+   }
+
+   public void setUserUsername(String username)
+   {
+      getCommand().setUserUsername(username);
+   }
+
+   public void setPasswordHash(String passwordHash)
+   {
+      getCommand().setPasswordHash(passwordHash);
+   }
+
+   public void setUserKey(String userKey)
+   {
+      getCommand().setUserKey(userKey);
+   }
+
+   public void setLangs(String langs)
+   {
+      getCommand().setLangs(langs);
+   }
+
+   public void setRoles(String roles)
+   {
+      getCommand().setRoles(roles);
+   }
+
+   public void setDisabled(boolean disabled)
+   {
+      getCommand().setDisabled(disabled);
+   }
 }
