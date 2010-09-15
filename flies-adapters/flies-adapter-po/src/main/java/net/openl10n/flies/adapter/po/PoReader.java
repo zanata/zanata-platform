2d4fd4375e0135275713954a0b100366c1d94e3b
Sean Flanigan <sflaniga@redhat.com>
2010-09-15 16:56:46 +1000

    copied PoReader to PoReader2 and adapted for new DTO classes, changed PoReader to use PoReader2's static methods
    --HG--
    rename : flies-adapters/flies-adapter-po/src/main/java/net/openl10n/flies/adapter/po/PoReader.java => flies-adapters/flies-adapter-po/src/main/java/net/openl10n/flies/adapter/po/PoReader2.java

--- flies-adapters/flies-adapter-po/src/main/java/net/openl10n/flies/adapter/po/PoReader.java
+++ flies-adapters/flies-adapter-po/src/main/java/net/openl10n/flies/adapter/po/PoReader.java
@@ -1,14 +1,9 @@
 package net.openl10n.flies.adapter.po;
 
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import net.openl10n.flies.common.ContentState;
 import net.openl10n.flies.common.ContentType;
 import net.openl10n.flies.common.LocaleId;
 import net.openl10n.flies.resources.LocaleInputSourcePair;
@@ -22,7 +17,6 @@ import net.openl10n.flies.rest.dto.po.PoTargetHeader;
 import net.openl10n.flies.rest.dto.po.PoTargetHeaders;
 import net.openl10n.flies.rest.dto.po.PotEntryData;
 
-import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang.StringUtils;
 import org.fedorahosted.tennera.jgettext.HeaderFields;
 import org.fedorahosted.tennera.jgettext.Message;
@@ -39,9 +33,9 @@ public class PoReader
 
    public static final LocaleId PO_SOURCE_LANGUAGE = LocaleId.EN_US;
 
-   public static final ImmutableSet<String> POT_HEADER_FIELDS = ImmutableSet.of(HeaderFields.KEY_ProjectIdVersion, HeaderFields.KEY_ReportMsgidBugsTo, HeaderFields.KEY_PotCreationDate, HeaderFields.KEY_MimeVersion, HeaderFields.KEY_ContentType, HeaderFields.KEY_ContentTransferEncoding);
+   public static final ImmutableSet<String> POT_HEADER_FIELDS = PoReader2.POT_HEADER_FIELDS;
 
-   public static final ImmutableSet<String> PO_HEADER_FIELDS = ImmutableSet.of(HeaderFields.KEY_PoRevisionDate, HeaderFields.KEY_LastTranslator, HeaderFields.KEY_LanguageTeam, HeaderFields.KEY_Language, "Plural-Forms", "X-Generator");
+   public static final ImmutableSet<String> PO_HEADER_FIELDS = PoReader2.PO_HEADER_FIELDS;
 
    public PoReader()
    {
@@ -49,7 +43,7 @@ public class PoReader
 
    public void extractTarget(Document document, InputSource inputSource, LocaleId targetLocaleId)
    {
-      MessageStreamParser messageParser = createParser(inputSource);
+      MessageStreamParser messageParser = PoReader2.createParser(inputSource);
 
       List<TextFlow> resources = document.getTextFlows();
       Map<String, TextFlow> resourceMap = new HashMap<String, TextFlow>(resources.size());
@@ -81,17 +75,17 @@ public class PoReader
          }
          else
          {
-            TextFlow tf = (TextFlow) resourceMap.get(message.getMsgid());
+            TextFlow tf = resourceMap.get(message.getMsgid());
             if (tf != null)
             {
-               String id = createId(message);
+               String id = PoReader2.createId(message);
 
                matchIdOrFail(tf.getId(), id);
 
                // add the target content (msgstr)
                TextFlowTarget tfTarget = new TextFlowTarget(tf, targetLocaleId);
                tfTarget.setContent(message.getMsgstr());
-               tfTarget.setState(getContentState(message));
+               tfTarget.setState(PoReader2.getContentState(message));
                tf.addTarget(tfTarget);
 
                // add the PO comment
@@ -160,7 +154,7 @@ public class PoReader
 
    public void extractTemplate(Document document, InputSource inputSource, LocaleId sourceLocaleId)
    {
-      MessageStreamParser messageParser = createParser(inputSource);
+      MessageStreamParser messageParser = PoReader2.createParser(inputSource);
 
       document.setLang(sourceLocaleId);
       document.setContentType(PO_CONTENT_TYPE);
@@ -193,7 +187,7 @@ public class PoReader
          }
          else
          {
-            String id = createId(message);
+            String id = PoReader2.createId(message);
             // add the content (msgid)
             TextFlow tf = new TextFlow(id, sourceLocaleId);
             tf.setContent(message.getMsgid());
@@ -227,152 +221,6 @@ public class PoReader
    public void extractTemplate(Document document, LocaleInputSourcePair localeInputSourcePair)
    {
       extractTemplate(document, localeInputSourcePair.getInputSource(), localeInputSourcePair.getLocaleId());
-   }
-
-   public void extract(Document document, InputSource inputSource, LocaleId targetLocaleId, LocaleId sourceLocaleId)
-   {
-      MessageStreamParser messageParser = createParser(inputSource);
-
-      document.setLang(sourceLocaleId);
-      document.setContentType(PO_CONTENT_TYPE);
-
-      List<TextFlow> resources = document.getTextFlows();
-
-      boolean headerFound = false;
-      while (messageParser.hasNext())
-      {
-         Message message = messageParser.next();
-
-         if (message.isHeader())
-         {
-            if (headerFound)
-               throw new IllegalStateException("found a second header!");
-            headerFound = true;
-
-            // store POT data
-            PoHeader potHeader = new PoHeader();
-            extractPotHeader(message, potHeader);
-            document.getExtensions(true).add(potHeader);
-
-            // store PO data
-            PoHeader poHeader = new PoHeader();
-            extractPoHeader(message, poHeader);
-            document.getExtensions(true).add(poHeader);
-
-         }
-         else if (message.isObsolete())
-         {
-            // append obsolete
-         }
-         else if (message.isPlural())
-         {
-            // skip for now
-         }
-         else
-         {
-            String id = createId(message);
-            // add the content (msgid)
-            TextFlow tf = new TextFlow(id, sourceLocaleId);
-            tf.setContent(message.getMsgid());
-            resources.add(tf);
-
-            // add the entry header POT fields
-            tf.getExtensions().add(createFromMessage(id, message));
-
-            // add the target content (msgstr)
-            TextFlowTarget tfTarget = new TextFlowTarget(tf, targetLocaleId);
-            tfTarget.setContent(message.getMsgstr());
-            tfTarget.setState(getContentState(message));
-            tf.addTarget(tfTarget);
-
-            // add the PO comment
-            tfTarget.getExtensions().add(new SimpleComment(StringUtils.join(message.getComments(), "\n")));
-
-         }
-
-      }
-
-   }
-
-   public void extract(Document document, InputSource inputSource, LocaleId targetLocaleId)
-   {
-      extract(document, inputSource, targetLocaleId, PO_SOURCE_LANGUAGE);
-   }
-
-   public void extract(Document document, LocaleInputSourcePair localeInputSourcePair)
-   {
-      extract(document, localeInputSourcePair.getInputSource(), localeInputSourcePair.getLocaleId());
-   }
-
-   public void extract(Document document, LocaleInputSourcePair localeInputSourcePair, LocaleId sourceLocaleId)
-   {
-      extract(document, localeInputSourcePair.getInputSource(), localeInputSourcePair.getLocaleId(), sourceLocaleId);
-   }
-
-   private static MessageStreamParser createParser(InputSource inputSource)
-   {
-      MessageStreamParser messageParser;
-      if (inputSource.getCharacterStream() != null)
-         messageParser = new MessageStreamParser(inputSource.getCharacterStream());
-      else if (inputSource.getByteStream() != null)
-      {
-         if (inputSource.getEncoding() != null)
-            messageParser = new MessageStreamParser(inputSource.getByteStream(), Charset.forName(inputSource.getEncoding()));
-         else
-            messageParser = new MessageStreamParser(inputSource.getByteStream(), Charset.forName("UTF-8"));
-      }
-      else if (inputSource.getSystemId() != null)
-      {
-         try
-         {
-            URL url = new URL(inputSource.getSystemId());
-
-            if (inputSource.getEncoding() != null)
-               messageParser = new MessageStreamParser(url.openStream(), Charset.forName(inputSource.getEncoding()));
-            else
-               messageParser = new MessageStreamParser(url.openStream(), Charset.forName("UTF-8"));
-         }
-         catch (IOException e)
-         {
-            // TODO throw stronger typed exception
-            throw new RuntimeException("failed to get input from url in inputSource", e);
-         }
-      }
-      else
-         // TODO throw stronger typed exception
-         throw new RuntimeException("not a valid inputSource");
-
-      return messageParser;
-   }
-
-   private static ContentState getContentState(Message message)
-   {
-      if (message.getMsgstr() == null || message.getMsgstr().isEmpty())
-         return ContentState.New;
-      else if (message.isFuzzy())
-         return ContentState.NeedReview;
-      else
-         return ContentState.Approved;
-   }
-
-   private static String createId(Message message)
-   {
-      String hashBase = message.getMsgctxt() == null ? message.getMsgid() : message.getMsgctxt() + "\u0000" + message.getMsgid();
-      return generateHash(hashBase);
-   }
-
-   public static String generateHash(String key)
-   {
-      try
-      {
-         MessageDigest md5 = MessageDigest.getInstance("MD5");
-         md5.reset();
-         return new String(Hex.encodeHex(md5.digest(key.getBytes("UTF-8"))));
-      }
-      catch (Exception exc)
-      {
-         throw new RuntimeException(exc);
-      }
    }
 
    private static void matchIdOrFail(String id, String id2)
---
+++ flies-adapters/flies-adapter-po/src/main/java/net/openl10n/flies/adapter/po/PoReader2.java
@@ -0,0 +1,285 @@
+package net.openl10n.flies.adapter.po;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import net.openl10n.flies.common.ContentState;
+import net.openl10n.flies.common.ContentType;
+import net.openl10n.flies.common.LocaleId;
+import net.openl10n.flies.rest.dto.extensions.PoHeader;
+import net.openl10n.flies.rest.dto.extensions.PoTargetHeader;
+import net.openl10n.flies.rest.dto.extensions.PotEntryHeader;
+import net.openl10n.flies.rest.dto.extensions.SimpleComment;
+import net.openl10n.flies.rest.dto.po.HeaderEntry;
+import net.openl10n.flies.rest.dto.resource.Resource;
+import net.openl10n.flies.rest.dto.resource.TextFlow;
+import net.openl10n.flies.rest.dto.resource.TextFlowTarget;
+import net.openl10n.flies.rest.dto.resource.TranslationsResource;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang.StringUtils;
+import org.fedorahosted.tennera.jgettext.HeaderFields;
+import org.fedorahosted.tennera.jgettext.Message;
+import org.fedorahosted.tennera.jgettext.catalog.parse.MessageStreamParser;
+import org.xml.sax.InputSource;
+
+import com.google.common.collect.ImmutableSet;
+
+public class PoReader2
+{
+
+   public static final ContentType PO_CONTENT_TYPE = new ContentType("application/x-gettext");
+
+   public static final LocaleId PO_SOURCE_LANGUAGE = LocaleId.EN_US;
+
+   public static final ImmutableSet<String> POT_HEADER_FIELDS = ImmutableSet.of(HeaderFields.KEY_ProjectIdVersion, HeaderFields.KEY_ReportMsgidBugsTo, HeaderFields.KEY_PotCreationDate, HeaderFields.KEY_MimeVersion, HeaderFields.KEY_ContentType, HeaderFields.KEY_ContentTransferEncoding);
+
+   public static final ImmutableSet<String> PO_HEADER_FIELDS = ImmutableSet.of(HeaderFields.KEY_PoRevisionDate, HeaderFields.KEY_LastTranslator, HeaderFields.KEY_LanguageTeam, HeaderFields.KEY_Language, "Plural-Forms", "X-Generator");
+
+   public PoReader2()
+   {
+   }
+
+   public void extractTarget(Resource srcDoc, TranslationsResource document, InputSource inputSource, LocaleId targetLocaleId)
+   {
+      MessageStreamParser messageParser = createParser(inputSource);
+
+      List<TextFlow> resources = srcDoc.getTextFlows();
+      Set<String> textFlowIds = new HashSet<String>();
+      for (TextFlow res : resources)
+      {
+         textFlowIds.add(res.getId());
+      }
+
+      boolean headerFound = false;
+      while (messageParser.hasNext())
+      {
+         Message message = messageParser.next();
+
+         if (message.isHeader())
+         {
+            if (headerFound)
+               throw new IllegalStateException("found a second header!");
+            headerFound = true;
+
+            // add target header data
+            PoTargetHeader poHeader = new PoTargetHeader();
+            extractPoHeader(message, poHeader);
+            document.getExtensions(true).add(poHeader);
+         }
+         else if (message.isObsolete())
+         {
+            // TODO append obsolete
+         }
+         else if (message.isPlural())
+         {
+            // TODO skip for now
+         }
+         else
+         {
+            String id = createId(message);
+            if (!textFlowIds.contains(id))
+            {
+               // TODO append obsolete
+            }
+            else
+            {
+               // add the target content (msgstr)
+               TextFlowTarget tfTarget = new TextFlowTarget();
+               tfTarget.setResId(id);
+
+               tfTarget.setContent(message.getMsgstr());
+               tfTarget.setState(getContentState(message));
+
+               // add the PO comment
+               tfTarget.getExtensions().add(new SimpleComment<TextFlowTarget>(StringUtils.join(message.getComments(), "\n")));
+               document.getTextFlowTargets(true).add(tfTarget);
+            }
+         }
+      }
+
+   }
+
+   private static void extractPotHeader(Message message, PoHeader potHeader)
+   {
+      potHeader.setComment(StringUtils.join(message.getComments(), "\n"));
+
+      HeaderFields hf = HeaderFields.wrap(message);
+      for (String key : hf.getKeys())
+      {
+         String val = hf.getValue(key);
+         if (POT_HEADER_FIELDS.contains(key))
+         {
+            potHeader.getEntries().add(new HeaderEntry(key, val));
+         }
+         // we add any custom fields to the PO only, not the POT
+         // TODO this should be configurable
+      }
+   }
+
+   private static void extractPoHeader(Message message, PoTargetHeader poHeader)
+   {
+      poHeader.setComment(StringUtils.join(message.getComments(), "\n"));
+
+      HeaderFields hf = HeaderFields.wrap(message);
+      for (String key : hf.getKeys())
+      {
+         String val = hf.getValue(key);
+         if (PO_HEADER_FIELDS.contains(key))
+         {
+            poHeader.getEntries().add(new HeaderEntry(key, val));
+         }
+         else if (!POT_HEADER_FIELDS.contains(key))
+         {
+            // we add any custom fields to the PO only, not the POT
+            // TODO this should be configurable
+            poHeader.getEntries().add(new HeaderEntry(key, val));
+         }
+      }
+   }
+
+   public void extractTemplate(Resource document, InputSource inputSource, LocaleId sourceLocaleId)
+   {
+      MessageStreamParser messageParser = createParser(inputSource);
+
+      document.setLang(sourceLocaleId);
+      document.setContentType(PO_CONTENT_TYPE);
+      List<TextFlow> resources = document.getTextFlows();
+
+      boolean headerFound = false;
+      while (messageParser.hasNext())
+      {
+         Message message = messageParser.next();
+
+         if (message.isHeader())
+         {
+            if (headerFound)
+               throw new IllegalStateException("found a second header!");
+            headerFound = true;
+
+            // store POT data
+            PoHeader potHeader = new PoHeader();
+            extractPotHeader(message, potHeader);
+            document.getExtensions(true).add(potHeader);
+
+         }
+         else if (message.isObsolete())
+         {
+            // TODO append obsolete
+         }
+         else if (message.isPlural())
+         {
+            // TODO skip for now
+         }
+         else
+         {
+            String id = createId(message);
+            // add the content (msgid)
+            TextFlow tf = new TextFlow(id, sourceLocaleId);
+            tf.setContent(message.getMsgid());
+            resources.add(tf);
+
+            // add the entry header POT fields
+            tf.getExtensions().add(createPotEntryHeader(message));
+            tf.getExtensions().add(createSimpleComment(message));
+         }
+
+      }
+   }
+
+   private static PotEntryHeader createPotEntryHeader(Message message)
+   {
+      PotEntryHeader data = new PotEntryHeader();
+      if (message.getMsgctxt() != null)
+      {
+         data.setContext(message.getMsgctxt());
+      }
+      data.getFlags().addAll(message.getFormats());
+      data.getReferences().addAll(message.getSourceReferences());
+      return data;
+   }
+
+   private static SimpleComment<TextFlow> createSimpleComment(Message message)
+   {
+      String comment = StringUtils.join(message.getExtractedComments(), "\n");
+      SimpleComment<TextFlow> result = new SimpleComment<TextFlow>(comment);
+      return result;
+   }
+
+   public void extractTemplate(Resource document, InputSource inputSource)
+   {
+      extractTemplate(document, inputSource, PO_SOURCE_LANGUAGE);
+   }
+
+   static MessageStreamParser createParser(InputSource inputSource)
+   {
+      MessageStreamParser messageParser;
+      if (inputSource.getCharacterStream() != null)
+         messageParser = new MessageStreamParser(inputSource.getCharacterStream());
+      else if (inputSource.getByteStream() != null)
+      {
+         if (inputSource.getEncoding() != null)
+            messageParser = new MessageStreamParser(inputSource.getByteStream(), Charset.forName(inputSource.getEncoding()));
+         else
+            messageParser = new MessageStreamParser(inputSource.getByteStream(), Charset.forName("UTF-8"));
+      }
+      else if (inputSource.getSystemId() != null)
+      {
+         try
+         {
+            URL url = new URL(inputSource.getSystemId());
+
+            if (inputSource.getEncoding() != null)
+               messageParser = new MessageStreamParser(url.openStream(), Charset.forName(inputSource.getEncoding()));
+            else
+               messageParser = new MessageStreamParser(url.openStream(), Charset.forName("UTF-8"));
+         }
+         catch (IOException e)
+         {
+            // TODO throw stronger typed exception
+            throw new RuntimeException("failed to get input from url in inputSource", e);
+         }
+      }
+      else
+         // TODO throw stronger typed exception
+         throw new RuntimeException("not a valid inputSource");
+
+      return messageParser;
+   }
+
+   static ContentState getContentState(Message message)
+   {
+      if (message.getMsgstr() == null || message.getMsgstr().isEmpty())
+         return ContentState.New;
+      else if (message.isFuzzy())
+         return ContentState.NeedReview;
+      else
+         return ContentState.Approved;
+   }
+
+   static String createId(Message message)
+   {
+      String hashBase = message.getMsgctxt() == null ? message.getMsgid() : message.getMsgctxt() + "\u0000" + message.getMsgid();
+      return generateHash(hashBase);
+   }
+
+   public static String generateHash(String key)
+   {
+      try
+      {
+         MessageDigest md5 = MessageDigest.getInstance("MD5");
+         md5.reset();
+         return new String(Hex.encodeHex(md5.digest(key.getBytes("UTF-8"))));
+      }
+      catch (Exception exc)
+      {
+         throw new RuntimeException(exc);
+      }
+   }
+
+}
