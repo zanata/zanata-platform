e0b24ae29aacfbbde7838477194e643403597b82
Sean Flanigan <sflaniga@redhat.com>
2010-09-16 14:50:39 +1000

    updated the list of JAXB classes in GenerateSchema

--- flies-common-api/src/main/java/net/openl10n/flies/rest/GenerateSchema.java
+++ flies-common-api/src/main/java/net/openl10n/flies/rest/GenerateSchema.java
@@ -9,9 +9,19 @@ import javax.xml.bind.SchemaOutputResolver;
 import javax.xml.transform.Result;
 import javax.xml.transform.stream.StreamResult;
 
+import net.openl10n.flies.rest.dto.Account;
+import net.openl10n.flies.rest.dto.Link;
+import net.openl10n.flies.rest.dto.Links;
+import net.openl10n.flies.rest.dto.Person;
 import net.openl10n.flies.rest.dto.Project;
 import net.openl10n.flies.rest.dto.ProjectIteration;
 import net.openl10n.flies.rest.dto.ProjectList;
+import net.openl10n.flies.rest.dto.ProjectType;
+import net.openl10n.flies.rest.dto.extensions.PoHeader;
+import net.openl10n.flies.rest.dto.extensions.PoTargetHeader;
+import net.openl10n.flies.rest.dto.extensions.PotEntryHeader;
+import net.openl10n.flies.rest.dto.extensions.SimpleComment;
+import net.openl10n.flies.rest.dto.po.HeaderEntry;
 import net.openl10n.flies.rest.dto.resource.Resource;
 import net.openl10n.flies.rest.dto.resource.ResourceMeta;
 import net.openl10n.flies.rest.dto.resource.TextFlow;
@@ -23,7 +33,7 @@ public class GenerateSchema
 
    public static void main(String[] args) throws IOException, JAXBException
    {
-      Class<?>[] classes = new Class<?>[] { ProjectList.class, Project.class, ProjectIteration.class, Resource.class, ResourceMeta.class, TranslationsResource.class, TextFlow.class, TextFlowTarget.class };
+      Class<?>[] classes = new Class<?>[] { Account.class, HeaderEntry.class, Link.class, Links.class, Person.class, PoHeader.class, PoTargetHeader.class, PotEntryHeader.class, Project.class, ProjectIteration.class, ProjectList.class, ProjectType.class, Resource.class, ResourceMeta.class, SimpleComment.class, TextFlow.class, TextFlowTarget.class, TranslationsResource.class };
       JAXBContext context = JAXBContext.newInstance(classes);
 
       generateSchemaToStdout(context);
