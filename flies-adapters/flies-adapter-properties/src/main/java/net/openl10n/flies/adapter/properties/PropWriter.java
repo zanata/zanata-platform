8c2e06119ce2a9912ea7d4a5a1daa5d1ad0506e3
Helen Ding <hding@redhat.com>
2010-10-07 18:24:09 +1000

    use the new rest api for ant client

--- flies-adapters/flies-adapter-properties/src/main/java/net/openl10n/flies/adapter/properties/PropWriter.java
+++ flies-adapters/flies-adapter-properties/src/main/java/net/openl10n/flies/adapter/properties/PropWriter.java
@@ -1,31 +1,22 @@
 package net.openl10n.flies.adapter.properties;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
-import net.openl10n.flies.common.LocaleId;
-import net.openl10n.flies.rest.dto.deprecated.Document;
-import net.openl10n.flies.rest.dto.deprecated.IExtensible;
-import net.openl10n.flies.rest.dto.deprecated.TextFlow;
-import net.openl10n.flies.rest.dto.deprecated.TextFlowTarget;
-import net.openl10n.flies.rest.dto.deprecated.TextFlowTargets;
+import net.openl10n.flies.rest.dto.resource.Resource;
 
-import org.fedorahosted.openprops.Properties;
 
+@Deprecated
 public class PropWriter
 {
 
+   @SuppressWarnings("unused")
    private static void logVerbose(String msg)
    {
       System.out.println(msg);
    }
 
+   @SuppressWarnings("unused")
    private static void makeParentDirs(File f)
    {
       File parentFile = f.getParentFile();
@@ -33,57 +24,61 @@ public class PropWriter
          parentFile.mkdirs();
    }
 
-   public static void write(final Document doc, final File baseDir, boolean exportRoot) throws IOException
+   public static void write(final Resource doc, final File baseDir, boolean exportRoot) throws IOException
    {
-      File docDir = new File(baseDir, doc.getPath());
-      File baseFile = new File(docDir, doc.getName());
-      makeParentDirs(baseFile);
-
-      if (exportRoot)
-      {
-         logVerbose("Creating base file " + baseFile);
-         Properties props = new Properties();
-         for (TextFlow textFlow : doc.getTextFlows())
-         {
-            props.setProperty(textFlow.getId(), textFlow.getContent());
-            if (textFlow.hasComment() && textFlow.getComment().getValue() != null)
-               props.setComment(textFlow.getId(), textFlow.getComment().getValue());
-         }
-         // props.store(System.out, null);
-         PrintStream out = new PrintStream(new FileOutputStream(baseFile));
-         props.store(out, null);
-      }
-
-      String baseName = baseFile.getName();
-      String bundleName = baseName.substring(0, baseName.length() - ".properties".length());
-
-      Map<LocaleId, Properties> targetProps = new HashMap<LocaleId, Properties>();
-      for (TextFlow textflow : doc.getTextFlows())
-      {
-         for (TextFlowTarget target : getTargets(textflow))
-         {
-            Properties targetProp = targetProps.get(target.getLang());
-            if (targetProp == null)
-            {
-               targetProp = new Properties();
-               targetProps.put(target.getLang(), targetProp);
-            }
-            targetProp.setProperty(textflow.getId(), target.getContent());
-            if (target.hasComment() && target.getComment().getValue() != null)
-               targetProp.setComment(textflow.getId(), target.getComment().getValue());
-         }
-      }
-      Set<LocaleId> targetLangs = targetProps.keySet();
-
-      for (LocaleId lang : targetLangs)
-      {
-         File langFile = new File(docDir, bundleName + "_" + lang.toJavaName() + ".properties");
-         logVerbose("Creating target file " + langFile);
-         Properties targetProp = targetProps.get(lang);
-         // targetProp.store(System.out, null);
-         PrintStream out2 = new PrintStream(new FileOutputStream(langFile));
-         targetProp.store(out2, null);
-      }
+      // File docDir = new File(baseDir, doc.getPath());
+      // File baseFile = new File(docDir, doc.getName());
+      // makeParentDirs(baseFile);
+      //
+      // if (exportRoot)
+      // {
+      // logVerbose("Creating base file " + baseFile);
+      // Properties props = new Properties();
+      // for (TextFlow textFlow : doc.getTextFlows())
+      // {
+      // props.setProperty(textFlow.getId(), textFlow.getContent());
+      // if (textFlow.hasComment() && textFlow.getComment().getValue() != null)
+      // props.setComment(textFlow.getId(), textFlow.getComment().getValue());
+      // }
+      // // props.store(System.out, null);
+      // PrintStream out = new PrintStream(new FileOutputStream(baseFile));
+      // props.store(out, null);
+      // }
+      //
+      // String baseName = baseFile.getName();
+      // String bundleName = baseName.substring(0, baseName.length() -
+      // ".properties".length());
+      //
+      // Map<LocaleId, Properties> targetProps = new HashMap<LocaleId,
+      // Properties>();
+      // for (TextFlow textflow : doc.getTextFlows())
+      // {
+      // for (TextFlowTarget target : getTargets(textflow))
+      // {
+      // Properties targetProp = targetProps.get(target.getLang());
+      // if (targetProp == null)
+      // {
+      // targetProp = new Properties();
+      // targetProps.put(target.getLang(), targetProp);
+      // }
+      // targetProp.setProperty(textflow.getId(), target.getContent());
+      // if (target.hasComment() && target.getComment().getValue() != null)
+      // targetProp.setComment(textflow.getId(),
+      // target.getComment().getValue());
+      // }
+      // }
+      // Set<LocaleId> targetLangs = targetProps.keySet();
+      //
+      // for (LocaleId lang : targetLangs)
+      // {
+      // File langFile = new File(docDir, bundleName + "_" + lang.toJavaName() +
+      // ".properties");
+      // logVerbose("Creating target file " + langFile);
+      // Properties targetProp = targetProps.get(lang);
+      // // targetProp.store(System.out, null);
+      // PrintStream out2 = new PrintStream(new FileOutputStream(langFile));
+      // targetProp.store(out2, null);
+      // }
 
    }
 
@@ -95,16 +90,16 @@ public class PropWriter
     * targetLangs.add(target.getLang()); } } } return targetLangs; }
     */
 
-   private static Set<TextFlowTarget> getTargets(IExtensible resource)
-   {
-      TextFlowTargets targets = resource.getExtension(TextFlowTargets.class);
-      if (targets != null)
-      {
-         return targets.getTargets();
-      }
-      else
-      {
-         return Collections.EMPTY_SET;
-      }
-   }
+   // private static Set<TextFlowTarget> getTargets(IExtensible resource)
+   // {
+   // TextFlowTargets targets = resource.getExtension(TextFlowTargets.class);
+   // if (targets != null)
+   // {
+   // return targets.getTargets();
+   // }
+   // else
+   // {
+   // return Collections.EMPTY_SET;
+   // }
+   // }
 }
--- flies-adapters/flies-adapter-properties/src/test/java/net/openl10n/flies/adapter/properties/PropReaderTests.java
+++ flies-adapters/flies-adapter-properties/src/test/java/net/openl10n/flies/adapter/properties/PropReaderTests.java
@@ -1,31 +1,18 @@
 package net.openl10n.flies.adapter.properties;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
-import java.io.StringWriter;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-
-import net.openl10n.flies.common.ContentState;
-import net.openl10n.flies.common.ContentType;
-import net.openl10n.flies.common.LocaleId;
-import net.openl10n.flies.rest.dto.deprecated.Document;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.xml.sax.InputSource;
 
 public class PropReaderTests
 {
-   private static final Logger log = LoggerFactory.getLogger(PropReaderTests.class);
+   // private static final Logger log =
+   // LoggerFactory.getLogger(PropReaderTests.class);
 
    @BeforeClass
    public static void setupContainer()
@@ -35,6 +22,7 @@ public class PropReaderTests
       // ProjectPackage.registerPartFactory(PotEntriesDataPart.FACTORY);
    }
 
+   @SuppressWarnings("deprecation")
    PropReader propReader = new PropReader();
 
    @Before
@@ -45,33 +33,38 @@ public class PropReaderTests
    @Test
    public void roundtripPropsToDocXmlToProps() throws Exception
    {
-      Document docOut = new Document("test.properties", ContentType.TextPlain);
-      InputStream testStream = getResourceAsStream("test.properties");
-
-      propReader.extractTemplate(docOut, new InputSource(testStream));
-      String[] locales = new String[] { "fr" };
-      for (String locale : locales)
-      {
-         InputStream targetStream = getResourceAsStream("test_" + locale + ".properties");
-         propReader.extractTarget(docOut, new InputSource(targetStream), new LocaleId(locale), ContentState.New);
-      }
-      JAXBContext jc = JAXBContext.newInstance(Document.class);
-      // JAXBContext jc = JAXBContext.newInstance(Document.class.getPackage()
-      // .getName());
-      Marshaller marshal = jc.createMarshaller();
-      StringWriter sw = new StringWriter();
-      marshal.marshal(docOut, sw);
-      marshal.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-      log.debug("{}", sw);
-
-      Unmarshaller unmarshal = jc.createUnmarshaller();
-      Document docIn = (Document) unmarshal.unmarshal(new StringReader(sw.toString()));
-
-      PropWriter.write(docIn, new File("target/test-output"), true);
+      // Document docOut = new Document("test.properties",
+      // ContentType.TextPlain);
+      // InputStream testStream = getResourceAsStream("test.properties");
+      //
+      // propReader.extractTemplate(docOut, new InputSource(testStream));
+      // String[] locales = new String[] { "fr" };
+      // for (String locale : locales)
+      // {
+      // InputStream targetStream = getResourceAsStream("test_" + locale +
+      // ".properties");
+      // propReader.extractTarget(docOut, new InputSource(targetStream), new
+      // LocaleId(locale), ContentState.New);
+      // }
+      // JAXBContext jc = JAXBContext.newInstance(Document.class);
+      // // JAXBContext jc = JAXBContext.newInstance(Document.class.getPackage()
+      // // .getName());
+      // Marshaller marshal = jc.createMarshaller();
+      // StringWriter sw = new StringWriter();
+      // marshal.marshal(docOut, sw);
+      // marshal.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+      // log.debug("{}", sw);
+      //
+      // Unmarshaller unmarshal = jc.createUnmarshaller();
+      // Document docIn = (Document) unmarshal.unmarshal(new
+      // StringReader(sw.toString()));
+      //
+      // PropWriter.write(docIn, new File("target/test-output"), true);
 
       // TODO check output files against input
    }
 
+   @SuppressWarnings("unused")
    private InputStream getResourceAsStream(String relativeResourceName) throws FileNotFoundException
    {
       InputStream stream = PropReaderTests.class.getResourceAsStream(relativeResourceName);
