29194652d494d17d55e8e2c6463b36ca8559b60a
Sean Flanigan <sflaniga@redhat.com>
2010-10-22 13:23:11 +1000

    added missing hashCode method to go with the equals method

--- flies-common-api/src/main/java/net/openl10n/flies/rest/dto/extensions/gettext/HeaderEntry.java
+++ flies-common-api/src/main/java/net/openl10n/flies/rest/dto/extensions/gettext/HeaderEntry.java
@@ -55,6 +55,16 @@ public class HeaderEntry
    }
 
    @Override
+   public int hashCode()
+   {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((key == null) ? 0 : key.hashCode());
+      result = prime * result + ((value == null) ? 0 : value.hashCode());
+      return result;
+   }
+
+   @Override
    public boolean equals(Object obj)
    {
       if (this == obj)
