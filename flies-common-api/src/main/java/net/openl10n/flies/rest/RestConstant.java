67847442e2cec29403ba87002c4cffa9715035ec
Helen Ding <hding@redhat.com>
2010-11-16 14:36:21 +1000

    exclude snapshot for build time check

--- flies-common-api/src/main/java/net/openl10n/flies/rest/RestConstant.java
+++ flies-common-api/src/main/java/net/openl10n/flies/rest/RestConstant.java
@@ -5,4 +5,6 @@ public class RestConstant
    public final static String HEADER_VERSION_NO = "X-Version-No";
    public final static String HEADER_USERNAME = "X-Auth-User";
    public final static String HEADER_API_KEY = "X-Auth-Token";
+
+   public static final String SNAPSHOT_VERSION = "SNAPSHOT";
 }
