473e289cb223eec4b7d6d8a55ebf017eb1f7e137
Sean Flanigan <sflaniga@redhat.com>
2011-01-28 14:49:02 +1000

    changed various places to use flies-common-api version instead of general project version; copied VersionUtility into flies.util (due to GWT problems)
    --HG--
    rename : client/flies-client-commands/src/main/java/net/openl10n/flies/client/VersionUtility.java => server/flies-war/src/main/java/net/openl10n/flies/util/VersionUtility.java

--- server/flies-war/src/main/java/net/openl10n/flies/rest/service/VersionService.java
+++ server/flies-war/src/main/java/net/openl10n/flies/rest/service/VersionService.java
@@ -5,12 +5,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import net.openl10n.flies.FliesInit;
 import net.openl10n.flies.rest.MediaTypes;
 import net.openl10n.flies.rest.dto.VersionInfo;
-import net.openl10n.flies.rest.service.VersionResource;
+import net.openl10n.flies.util.VersionUtility;
 
-import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 
 
@@ -19,19 +17,19 @@ import org.jboss.seam.annotations.Name;
 public class VersionService implements VersionResource
 {
    public static final String SERVICE_PATH = "/version";
-   @In
-   private FliesInit fliesInit;
-
-   public VersionService(FliesInit fliesInit)
-   {
-      this.fliesInit = fliesInit;
-   }
+   
+   private final VersionInfo version;
 
    public VersionService()
    {
-
+      this(VersionUtility.getAPIVersionInfo());
    }
 
+   VersionService(VersionInfo ver)
+   {
+      this.version = ver;
+   }
+   
    /**
     * Retrieve VersionInfo
     * 
@@ -42,7 +40,6 @@ public class VersionService implements VersionResource
    @Produces({ MediaTypes.APPLICATION_FLIES_VERSION_JSON, MediaTypes.APPLICATION_FLIES_VERSION_XML })
    public VersionInfo get()
    {
-      VersionInfo entity = new VersionInfo(fliesInit.getVersion(), fliesInit.getBuildTimestamp());
-      return entity;
+      return new VersionInfo(version);
    }
 }
