c4c158289d2104c07e01d5942bfc67bc30e8ab19
Helen Ding <hding@redhat.com>
2011-02-01 15:14:52 +1000

    issue 227: Rename GetTransUnits to GetTransUnitList
    --HG--
    rename : server/flies-war/src/main/java/net/openl10n/flies/webtrans/shared/rpc/GetTransUnits.java => server/flies-war/src/main/java/net/openl10n/flies/webtrans/shared/rpc/GetTransUnitList.java
    rename : server/flies-war/src/main/java/net/openl10n/flies/webtrans/shared/rpc/GetTransUnitsResult.java => server/flies-war/src/main/java/net/openl10n/flies/webtrans/shared/rpc/GetTransUnitListResult.java

--- server/flies-war/src/main/java/net/openl10n/flies/webtrans/shared/rpc/GetTransUnitList.java
+++ server/flies-war/src/main/java/net/openl10n/flies/webtrans/shared/rpc/GetTransUnitList.java
@@ -2,24 +2,26 @@ package net.openl10n.flies.webtrans.shared.rpc;
 
 import net.openl10n.flies.webtrans.shared.model.DocumentId;
 
-public class GetTransUnits extends AbstractWorkspaceAction<GetTransUnitsResult>
+public class GetTransUnitList extends AbstractWorkspaceAction<GetTransUnitListResult>
 {
 
    private static final long serialVersionUID = 1L;
    private int offset;
    private int count;
    private DocumentId documentId;
+   private String phrase;
 
    @SuppressWarnings("unused")
-   private GetTransUnits()
+   private GetTransUnitList()
    {
    }
 
-   public GetTransUnits(DocumentId id, int offset, int count)
+   public GetTransUnitList(DocumentId id, int offset, int count, String phrase)
    {
       this.documentId = id;
       this.offset = offset;
       this.count = count;
+      this.phrase = phrase;
    }
 
    public int getOffset()
@@ -35,6 +37,11 @@ public class GetTransUnits extends AbstractWorkspaceAction<GetTransUnitsResult>
    public DocumentId getDocumentId()
    {
       return documentId;
+   }
+
+   public String getPhrase()
+   {
+      return this.phrase;
    }
 
 }
