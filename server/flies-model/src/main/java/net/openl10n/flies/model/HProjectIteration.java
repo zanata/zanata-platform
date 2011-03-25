ec743d241d18577a1aed245240869e808e852566
Sean Flanigan <sflaniga@redhat.com>
2011-03-25 13:24:46 +1000

    changed overrideLocales field to be boolean

--- server/flies-model/src/main/java/net/openl10n/flies/model/HProjectIteration.java
+++ server/flies-model/src/main/java/net/openl10n/flies/model/HProjectIteration.java
@@ -62,7 +62,7 @@ public class HProjectIteration extends AbstractSlugEntity
    private Map<String, HDocument> documents;
    private Map<String, HDocument> allDocuments;
 
-   private Boolean overrideLocales = false;
+   private boolean overrideLocales = false;
    private Set<HLocale> customizedLocales;
 
 
