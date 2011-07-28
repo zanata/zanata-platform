1ea5f60f3cb1a980e9049a0eea76980bc8294ca4
David Mason <dr.d.mason@gmail.com>
2011-07-28 15:16:04 +1000

    change 16x16 package to valid package name _16x16

--- server/zanata-war/src/main/java/org/zanata/webtrans/client/Resources.java
+++ server/zanata-war/src/main/java/org/zanata/webtrans/client/Resources.java
@@ -33,10 +33,10 @@ public interface Resources extends ClientBundle
    @Source("org/zanata/webtrans/WebTransStyles.css")
    WebTransStyles style();
 
-   @Source("org/zanata/webtrans/images/crystal_project/16x16/actions/info.png")
+   @Source("org/zanata/webtrans/images/crystal_project/_16x16/actions/info.png")
    ImageResource infoButton();
 
-   @Source("org/zanata/webtrans/images/crystal_project/16x16/actions/search.png")
+   @Source("org/zanata/webtrans/images/crystal_project/_16x16/actions/search.png")
    ImageResource searchButton();
 
    @Source("org/zanata/webtrans/images/banner_bg.png")
