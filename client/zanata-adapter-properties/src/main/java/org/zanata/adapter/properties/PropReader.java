983ea97ac142fccf44afc901056933fbb8f3eec9
David Mason <dr.d.mason@gmail.com>
2011-08-26 19:32:16 +1000

    allow '# NON-TRANSLATABLE' comments in properties files

--- client/zanata-adapter-properties/src/main/java/org/zanata/adapter/properties/PropReader.java
+++ client/zanata-adapter-properties/src/main/java/org/zanata/adapter/properties/PropReader.java
@@ -2,6 +2,7 @@ package org.zanata.adapter.properties;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 
 import org.fedorahosted.openprops.Properties;
@@ -23,6 +24,7 @@ public class PropReader
 {
 
    public static final String PROP_CONTENT_TYPE = "text/plain";
+   private static final String NEWLINE_REGEX = "(\r\n|\r|\n)";
 
    // pre: template already extracted
    public void extractTarget(TranslationsResource doc, InputStream in, LocaleId localeId, ContentState contentState) throws IOException
@@ -46,26 +48,103 @@ public class PropReader
       }
    }
 
+   /**
+    * Reads properties from a given {@link InputStream} and adds them to the
+    * given {@link Resource}.
+    * 
+    * @param doc the resource to add properties textflows to
+    * @param in the input stream to read the properties from
+    * @throws IOException
+    */
    // TODO allowing Readers (via InputSource) might be a bad idea
+   // TODO add documentation on exceptions thrown
    public void extractTemplate(Resource doc, InputStream in) throws IOException
    {
       List<TextFlow> resources = doc.getTextFlows();
       Properties props = loadProps(in);
+      int nonTranslatableCount = 0;
       for (String key : props.stringPropertyNames())
       {
-         String val = props.getProperty(key);
-         String id = getID(key, val);
-         TextFlow textFlow = new TextFlow(id);
-         textFlow.setContent(val);
-         String comment = props.getComment(key);
-         if (comment != null && comment.length() != 0)
+         String comment = null;
+         String rawComment = props.getRawComment(key);
+         if (rawComment != null && rawComment.length() != 0)
          {
-            SimpleComment simpleComment = textFlow.getExtensions(true).findOrAddByType(SimpleComment.class);
-            simpleComment.setValue(comment);
+            StringBuilder sb = new StringBuilder(rawComment.length());
+            nonTranslatableCount = processCommentForNonTranslatable(nonTranslatableCount, rawComment, sb);
+            comment = sb.toString();
          }
-         // textFlow.setLang(LocaleId.EN);
-         resources.add(textFlow);
+         if (nonTranslatableCount == 0)
+         {
+            String val = props.getProperty(key);
+            String id = getID(key, val);
+            TextFlow textFlow = new TextFlow(id);
+            textFlow.setContent(val);
+            if (comment != null && comment.length() != 0)
+            {
+               SimpleComment simpleComment = textFlow.getExtensions(true).findOrAddByType(SimpleComment.class);
+               simpleComment.setValue(comment);
+            }
+            // textFlow.setLang(LocaleId.EN);
+            resources.add(textFlow);
+         }
       }
+   }
+
+   /**
+    * Processes a full comment for non-translatable sections, writing
+    * translatable sections to a given string buffer.
+    * 
+    * @param comment comment to process, may have multiple lines
+    * @param sb string buffer to output comments in translatable blocks
+    * @return adjusted non-translateable count, a value > 0 indicates that the
+    *         current section is non-translatable
+    * @throws Exception
+    */
+   private int processCommentForNonTranslatable(int nonTranslatableCount, String comment, StringBuilder sb) throws InvalidPropertiesFormatException
+   {
+      int nonTranslatable = nonTranslatableCount;
+      String[] lines = comment.split(NEWLINE_REGEX);
+
+      int lineNonTranslatable;
+      for (String line : lines)
+      {
+         lineNonTranslatable = checkNonTranslatable(line);
+         nonTranslatable += lineNonTranslatable;
+         if (nonTranslatable < 0)
+         {
+            // TODO probably want a different exception here
+            throw new InvalidPropertiesFormatException("Found '# END NON-TRANSLATABLE' " + "without matching '# START NON-TRANSLATABLE'");
+         }
+         if (nonTranslatable == 0 && lineNonTranslatable == 0)
+         {
+            sb.append(Properties.cookCommentLine(line));
+            // TODO if not last line
+            sb.append('\n');
+         }
+      }
+      
+      return nonTranslatable;
+   }
+
+   /**
+    * Checks a comment for START and END of NON-TRANSLATABLE sections within a
+    * single line of a comment.
+    * 
+    * @param line a single line of a comment
+    * @return 0 if no NON-TRANSLATABLE comment is found, +1 for start, -1 for
+    *         end
+    */
+   private int checkNonTranslatable(String line)
+   {
+      if (line.startsWith("# START NON-TRANSLATABLE"))
+      {
+         return 1;
+      }
+      if (line.startsWith("# END NON-TRANSLATABLE"))
+      {
+         return -1;
+      }
+      return 0;
    }
 
    private String getID(String key, String val)
