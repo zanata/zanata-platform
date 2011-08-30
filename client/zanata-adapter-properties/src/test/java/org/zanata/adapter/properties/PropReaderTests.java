f252c1887fce8daf0531365b8a72907a58b6b73e
David Mason <dr.d.mason@gmail.com>
2011-08-30 11:35:38 +1000

    automate some tests for PropWriter

--- client/zanata-adapter-properties/src/test/java/org/zanata/adapter/properties/PropReaderTests.java
+++ client/zanata-adapter-properties/src/test/java/org/zanata/adapter/properties/PropReaderTests.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.net.MalformedURLException;
 import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 
@@ -15,7 +16,7 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
-import org.apache.commons.lang.NotImplementedException;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import static org.testng.Assert.*;
@@ -28,6 +29,8 @@ import org.zanata.rest.dto.resource.TranslationsResource;
 public class PropReaderTests
 {
    private static final Logger log = LoggerFactory.getLogger(PropReaderTests.class);
+   private static final String TEST_OUTPUT_DIR_STRING = "target/test-output";
+   private static final File TEST_OUTPUT_DIR = new File(TEST_OUTPUT_DIR_STRING);
 
    @SuppressWarnings("deprecation")
    PropReader propReader;
@@ -41,8 +44,10 @@ public class PropReaderTests
    @Test
    public void roundtripSrcPropsToDocXmlToProps() throws Exception
    {
+      String docName = "test.properties";
+
       Resource srcDoc = new Resource("test");
-      InputStream testStream = getResourceAsStream("test.properties");
+      InputStream testStream = getResourceAsStream(docName);
 
       propReader.extractTemplate(srcDoc, testStream);
       JAXBContext jc = JAXBContext.newInstance(Resource.class);
@@ -55,16 +60,17 @@ public class PropReaderTests
       Unmarshaller unmarshal = jc.createUnmarshaller();
       Resource docIn = (Resource) unmarshal.unmarshal(new StringReader(sw.toString()));
 
-      PropWriter.write(docIn, new File("target/test-output"));
+      PropWriter.write(docIn, TEST_OUTPUT_DIR);
 
-      // TODO check output files against input
+      assertInputAndOutputDocContentSame(docName);
    }
 
    @Test
    public void roundtripTransPropsToDocXmlToProps() throws Exception
    {
       String locale = "fr";
-      InputStream targetStream = getResourceAsStream("test_fr.properties");
+      String docName = "test_fr.properties";
+      InputStream targetStream = getResourceAsStream(docName);
       TranslationsResource transDoc = new TranslationsResource();
       propReader.extractTarget(transDoc, targetStream, new LocaleId(locale), ContentState.New);
 
@@ -78,9 +84,33 @@ public class PropReaderTests
       Unmarshaller unmarshal = jc.createUnmarshaller();
       TranslationsResource docIn = (TranslationsResource) unmarshal.unmarshal(new StringReader(sw.toString()));
 
-      PropWriter.write(docIn, new File("target/test-output"), "test", locale);
+      PropWriter.write(docIn, TEST_OUTPUT_DIR, "test", locale);
+      
+      assertInputAndOutputDocContentSame(docName);
+   }
 
-      // TODO check output files against input
+   /**
+    * Asserts that the content of the input document and output document are
+    * identical. Assumes filename is the same for both documents and that the
+    * output file is in TEST_OUTPUT_DIR.
+    * 
+    * @param docName the name for both input and output files
+    * @throws FileNotFoundException
+    * @throws IOException
+    * @throws MalformedURLException
+    */
+   private void assertInputAndOutputDocContentSame(String docName) throws FileNotFoundException, IOException, MalformedURLException
+   {
+      File newFile = new File(TEST_OUTPUT_DIR.getPath() + File.separator + docName);
+      InputStream newStream = newFile.toURI().toURL().openStream();
+      InputStream origStream = getResourceAsStream(docName);
+
+      String origContent = IOUtils.toString(origStream);
+      String newContent = IOUtils.toString(newStream);
+
+      // note: this does not allow for differences in whitespace, so if tests
+      // start failing this should be updated to use a less strict comparison
+      assertEquals(newContent, origContent, "output file should be the same as the input file");
    }
 
    private InputStream getResourceAsStream(String relativeResourceName) throws FileNotFoundException
