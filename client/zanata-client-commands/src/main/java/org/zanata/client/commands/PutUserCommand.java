83b90a34257313b64dbb25d4d139aacee3c5ab8b
Sean Flanigan <sflaniga@redhat.com>
2011-10-03 18:05:09 +1000

    Clean up generic types in ConfigurableCommand impls

--- client/zanata-client-commands/src/main/java/org/zanata/client/commands/PutUserCommand.java
+++ client/zanata-client-commands/src/main/java/org/zanata/client/commands/PutUserCommand.java
@@ -15,33 +15,31 @@ import org.zanata.rest.dto.Account;
  * @author Sean Flanigan <sflaniga@redhat.com>
  *
  */
-public class PutUserCommand extends ConfigurableCommand
+public class PutUserCommand extends ConfigurableCommand<PutUserOptions>
 {
    private static final Logger log = LoggerFactory.getLogger(PutUserCommand.class);
-   private final PutUserOptions opts;
 
    public PutUserCommand(PutUserOptions opts)
    {
       super(opts);
-      this.opts = opts;
    }
 
    public void run() throws Exception
    {
       Account account = new Account();
-      account.setEmail(opts.getUserEmail());
-      account.setName(opts.getUserName());
-      account.setUsername(opts.getUserUsername());
-      account.setPasswordHash(opts.getUserPasswordHash());
-      account.setApiKey(opts.getUserKey());
-      account.setEnabled(!opts.isUserDisabled());
-      account.setRoles(new HashSet<String>(Arrays.asList(opts.getUserRoles().split(","))));
-      account.setTribes(new HashSet<String>(Arrays.asList(opts.getUserLangs().split(","))));
+      account.setEmail(getOpts().getUserEmail());
+      account.setName(getOpts().getUserName());
+      account.setUsername(getOpts().getUserUsername());
+      account.setPasswordHash(getOpts().getUserPasswordHash());
+      account.setApiKey(getOpts().getUserKey());
+      account.setEnabled(!getOpts().isUserDisabled());
+      account.setRoles(new HashSet<String>(Arrays.asList(getOpts().getUserRoles().split(","))));
+      account.setTribes(new HashSet<String>(Arrays.asList(getOpts().getUserLangs().split(","))));
 
       log.debug("{}", account);
 
-      IAccountResource iterResource = getRequestFactory().getAccount(opts.getUserUsername());
-      URI uri = getRequestFactory().getAccountURI(opts.getUserUsername());
+      IAccountResource iterResource = getRequestFactory().getAccount(getOpts().getUserUsername());
+      URI uri = getRequestFactory().getAccountURI(getOpts().getUserUsername());
       ClientResponse<?> response = iterResource.put(account);
       ClientUtility.checkResult(response, uri);
    }
