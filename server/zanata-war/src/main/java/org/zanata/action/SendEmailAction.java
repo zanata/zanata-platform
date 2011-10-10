506a0c55db83cf884edc58bb251ebb87b7f606f6
David Mason <dr.d.mason@gmail.com>
2011-10-10 16:22:58 +1000

    allow users to request language team membership

--- server/zanata-war/src/main/java/org/zanata/action/SendEmailAction.java
+++ server/zanata-war/src/main/java/org/zanata/action/SendEmailAction.java
@@ -66,9 +66,11 @@ public class SendEmailAction implements Serializable
 
    private static final String EMAIL_TYPE_CONTACT_ADMIN = "contact_admin";
    private static final String EMAIL_TYPE_CONTACT_COORDINATOR = "contact_coordinator";
+   private static final String EMAIL_TYPE_REQUEST_TO_JOIN = "request_to_join_language_team";
 
    private static final String ADMIN_EMAIL_TEMPLATE = "/help/email_admin.xhtml";
    private static final String COORDINATOR_EMAIL_TEMPLATE = "/language/email_coordinator.xhtml";
+   private static final String REQUEST_TO_JOIN_EMAIL_TEMPLATE = "/language/email_request_to_join.xhtml";
 
    @In
    private ApplicationConfiguration applicationConfiguration;
@@ -272,6 +274,11 @@ public class SendEmailAction implements Serializable
          else if (emailType != null && emailType.equals(EMAIL_TYPE_CONTACT_COORDINATOR))
          {
             sendToLanguageCoordinators(COORDINATOR_EMAIL_TEMPLATE);
+            return "success";
+         }
+         else if (emailType != null && emailType.equals(EMAIL_TYPE_REQUEST_TO_JOIN))
+         {
+            sendToLanguageCoordinators(REQUEST_TO_JOIN_EMAIL_TEMPLATE);
             return "success";
          }
          else
