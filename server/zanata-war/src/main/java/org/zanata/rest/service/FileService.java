e5d79114637015e947f8bf376d723616df3f783e
Carlos A. Munoz <camunoz@redhat.com>
2011-10-13 12:08:45 +1000

    Add File Resource and Service to manage file downloads.

---
+++ server/zanata-war/src/main/java/org/zanata/rest/service/FileService.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2010, Red Hat, Inc. and individual contributors as indicated by the
+ * @author tags. See the copyright.txt file in the distribution for a full
+ * listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.zanata.rest.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.seam.annotations.Name;
+
+@Name("fileService")
+@Path(FileService.SERVICE_PATH)
+@Produces( { MediaType.APPLICATION_OCTET_STREAM })
+@Consumes( { MediaType.APPLICATION_OCTET_STREAM })
+public class FileService implements TranslationFileResource
+{
+   public static final String SERVICE_PATH = ProjectIterationService.SERVICE_PATH + "/file";
+   
+   @Override
+   @GET
+   @Path(FILE_NAME_TEMPLATE)
+   // /file/{docId}
+   public InputStream downloadTranslationFile(@PathParam("docId") String docId)
+   {
+      try
+      {
+         File f = File.createTempFile(docId, ".txt");
+         return new FileInputStream(f);
+      }
+      catch (FileNotFoundException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      catch (IOException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return null;
+   }
+}
