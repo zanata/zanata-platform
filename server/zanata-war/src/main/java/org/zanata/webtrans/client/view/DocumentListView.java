23123a160dd40a4b837f88d4a777d0abf3c7e317
David Mason <dr.d.mason@gmail.com>
2011-12-01 15:13:32 +1000

    refactor document list classes closer to MVP

--- server/zanata-war/src/main/java/org/zanata/webtrans/client/view/DocumentListView.java
+++ server/zanata-war/src/main/java/org/zanata/webtrans/client/view/DocumentListView.java
@@ -20,25 +20,18 @@
  */
 package org.zanata.webtrans.client.view;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import net.customware.gwt.presenter.client.EventBus;
 
-import org.zanata.webtrans.client.editor.filter.ContentFilter;
 import org.zanata.webtrans.client.presenter.DocumentListPresenter;
 import org.zanata.webtrans.client.resources.Resources;
 import org.zanata.webtrans.client.resources.UiMessages;
 import org.zanata.webtrans.client.resources.WebTransMessages;
-import org.zanata.webtrans.client.events.TransUnitUpdatedEventHandler;
 import org.zanata.webtrans.client.rpc.CachingDispatchAsync;
 import org.zanata.webtrans.client.ui.ClearableTextBox;
 import org.zanata.webtrans.client.ui.DocumentListTable;
 import org.zanata.webtrans.client.ui.DocumentNode;
-import org.zanata.webtrans.shared.model.DocumentId;
 import org.zanata.webtrans.shared.model.DocumentInfo;
 
-import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.logical.shared.HasSelectionHandlers;
 import com.google.gwt.event.logical.shared.SelectionEvent;
@@ -52,6 +45,7 @@ import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.HasData;
 import com.google.gwt.view.client.ListDataProvider;
 import com.google.inject.Inject;
 
@@ -73,34 +67,17 @@ public class DocumentListView extends Composite implements DocumentListPresenter
    @UiField(provided = true)
    final CellTable<DocumentNode> documentListTable;
 
-   final Resources resources;
-
    private ListDataProvider<DocumentNode> dataProvider;
-
-   private ContentFilter<DocumentInfo> filter;
-
-   private DocumentNode currentSelection;
-
-   private HashMap<DocumentId, DocumentNode> nodes;
-
-   final WebTransMessages messages;
-
-   final CachingDispatchAsync dispatcher;
-
-   final EventBus eventBus;
 
    @Inject
    public DocumentListView(Resources resources, WebTransMessages messages, UiMessages uiMessages, final CachingDispatchAsync dispatcher, EventBus eventBus)
    {
-      this.resources = resources;
-      this.messages = messages;
-      this.dispatcher = dispatcher;
-      this.eventBus = eventBus;
 
       filterTextBox = new ClearableTextBox(resources, uiMessages);
-      nodes = new HashMap<DocumentId, DocumentNode>();
+      // TODO set this from the presenter if possible
       dataProvider = new ListDataProvider<DocumentNode>();
       documentListTable = DocumentListTable.initDocumentListTable(this, resources, messages, dataProvider);
+
       initWidget(uiBinder.createAndBindUi(this));
    }
 
@@ -111,91 +88,9 @@ public class DocumentListView extends Composite implements DocumentListPresenter
    }
 
    @Override
-   public void setList(ArrayList<DocumentInfo> sortedList)
+   public HasData<DocumentNode> getDocumentListTable()
    {
-      dataProvider.getList().clear();
-      nodes = new HashMap<DocumentId, DocumentNode>(sortedList.size());
-      int counter = 0;
-      long start = System.currentTimeMillis();
-      for (DocumentInfo doc : sortedList)
-      {
-         Log.info("Loading document: " + ++counter + " ");
-         DocumentNode node = new DocumentNode(messages, doc, eventBus, dataProvider);
-         if (filter != null)
-         {
-            node.setVisible(filter.accept(doc));
-         }
-         if (node.isVisible())
-         {
-            dataProvider.getList().add(node);
-         }
-         nodes.put(doc.getId(), node);
-      }
-      Log.info("Time to create DocumentNodes: " + String.valueOf(System.currentTimeMillis() - start) + "ms");
-      documentListTable.setPageSize(dataProvider.getList().size());
-      dataProvider.addDataDisplay(documentListTable);
-   }
-
-   @Override
-   public TransUnitUpdatedEventHandler getDocumentNode(DocumentId docId)
-   {
-      return nodes.get(docId);
-   }
-
-   @Override
-   public void clearSelection()
-   {
-      if (currentSelection == null)
-      {
-         return;
-      }
-      currentSelection = null;
-   }
-
-   @Override
-   public void setSelection(final DocumentId documentId)
-   {
-      if (currentSelection != null && currentSelection.getDocInfo().getId() == documentId)
-      {
-         return;
-      }
-      clearSelection();
-      DocumentNode node = nodes.get(documentId);
-      if (node != null)
-      {
-         currentSelection = node;
-         // required to have document selected in doclist when loading from
-         // bookmarked history token
-         documentListTable.getSelectionModel().setSelected(node, true);
-      }
-   }
-
-   @Override
-   public void setFilter(ContentFilter<DocumentInfo> filter)
-   {
-      this.filter = filter;
-      dataProvider.getList().clear();
-      for (DocumentNode docNode : nodes.values())
-      {
-         docNode.setVisible(filter.accept(docNode.getDocInfo()));
-         if (docNode.isVisible())
-         {
-            dataProvider.getList().add(docNode);
-         }
-      }
-      dataProvider.refresh();
-   }
-
-   @Override
-   public void removeFilter()
-   {
-      dataProvider.getList().clear();
-      for (DocumentNode docNode : nodes.values())
-      {
-         docNode.setVisible(true);
-         dataProvider.getList().add(docNode);
-      }
-      dataProvider.refresh();
+      return documentListTable;
    }
 
    @Override
@@ -216,10 +111,16 @@ public class DocumentListView extends Composite implements DocumentListPresenter
       return addHandler(handler, SelectionEvent.getType());
    }
 
+
    @Override
-   public DocumentInfo getDocumentInfo(DocumentId docId)
+   public void setPageSize(int pageSize)
    {
-      DocumentNode node = nodes.get(docId);
-      return (node == null ? null : node.getDocInfo());
+      documentListTable.setPageSize(pageSize);
+   }
+
+   @Override
+   public ListDataProvider<DocumentNode> getDataProvider()
+   {
+      return dataProvider;
    }
 }
