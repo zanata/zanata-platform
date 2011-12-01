3fae6d904bf284ebc7201bbcff4eba6e81bfc28c
Carlos A. Munoz <camunoz@redhat.com>
2011-12-01 15:32:44 +1000

    Change ExtensionSet implementation.
    Replace the HashSet based implementation of the ExtensionSet for a HashMap based one.

--- zanata-common-api/src/main/java/org/zanata/rest/dto/resource/ExtensionSet.java
+++ zanata-common-api/src/main/java/org/zanata/rest/dto/resource/ExtensionSet.java
@@ -1,27 +1,47 @@
 package org.zanata.rest.dto.resource;
 
-import java.util.HashSet;
+import java.util.AbstractCollection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.zanata.rest.dto.DTOUtil;
 import org.zanata.rest.dto.ExtensionValue;
 
 
 @XmlRootElement(name = "extension-set")
-public class ExtensionSet<T extends ExtensionValue> extends HashSet<T>
+public class ExtensionSet<T extends ExtensionValue> extends AbstractCollection<T>
 {
 
-   private static final long serialVersionUID = 1L;
-
+   private Map<Class<?>, T> extensions = new LinkedHashMap<Class<?>, T>();
+   
+   @Override
+   public Iterator<T> iterator()
+   {
+      return extensions.values().iterator();
+   }
+   
+   @Override
+   public int size()
+   {
+      return extensions.size();
+   }
+   
+   @Override
+   public boolean add(T e) 
+   {
+      this.extensions.put(e.getClass(), e);
+      return true;
+   };
+   
+   @SuppressWarnings("unchecked")
    public <E extends T> E findByType(Class<E> clz)
    {
-      for (T e : this)
-      {
-         if (clz.isInstance(e))
-            return (E) clz.cast(e);
-      }
-      return null;
+      return (E)this.extensions.get(clz);
    }
 
    @Override
@@ -46,6 +66,39 @@ public class ExtensionSet<T extends ExtensionValue> extends HashSet<T>
          }
       }
       return ext;
+   }
+   
+   @Override
+   public boolean equals(Object obj)
+   {
+      if( obj == null )
+      {
+         return false;
+      }
+      else if( !(obj instanceof ExtensionSet) )
+      {
+         return false;
+      }
+      else
+      {
+         @SuppressWarnings("rawtypes")
+         ExtensionSet other = (ExtensionSet)obj;
+         
+         return new EqualsBuilder()
+                    .append(this.extensions, other.extensions)
+                    .isEquals();
+      }
+   }
+   
+   @Override
+   public int hashCode()
+   {
+      HashCodeBuilder hcBuilder = new HashCodeBuilder(15, 67);
+      for( T t : this )
+      {
+         hcBuilder.append(t);
+      }
+      return hcBuilder.toHashCode();
    }
 
 }
