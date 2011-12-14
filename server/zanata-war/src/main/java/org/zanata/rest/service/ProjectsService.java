a926e71aa05146692caa113b259a157b7a4876a2
Carlos A. Munoz <camunoz@redhat.com>
2011-12-14 14:26:28 +1000

    Refactor Projects Rest API.
    Client Rest interface now extends the server one.
    Client and Server interfaces now return a Response object.

--- server/zanata-war/src/main/java/org/zanata/rest/service/ProjectsService.java
+++ server/zanata-war/src/main/java/org/zanata/rest/service/ProjectsService.java
@@ -1,5 +1,26 @@
+/*
+ * Copyright 2010, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.zanata.rest.service;
 
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +30,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.hibernate.Session;
 import org.jboss.resteasy.annotations.providers.jaxb.Wrapped;
+import org.jboss.resteasy.util.GenericType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
@@ -24,6 +48,7 @@ import org.zanata.rest.MediaTypes;
 import org.zanata.rest.dto.Link;
 import org.zanata.rest.dto.Project;
 import org.zanata.rest.dto.ProjectType;
+import org.zanata.rest.dto.resource.ResourceMeta;
 
 @Name("projectsService")
 @Path("/projects")
@@ -45,7 +70,7 @@ public class ProjectsService implements ProjectsResource
    @GET
    @Produces( { MediaTypes.APPLICATION_ZANATA_PROJECTS_XML, MediaTypes.APPLICATION_ZANATA_PROJECTS_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
    @Wrapped(element = "projects", namespace = Namespaces.ZANATA_API)
-   public List<Project> get()
+   public Response get()
    {
       @SuppressWarnings("unchecked")
       List<HProject> projects = session.createQuery("from HProject p").list();
@@ -59,7 +84,12 @@ public class ProjectsService implements ProjectsResource
          projectRefs.add(project);
       }
 
-      return projectRefs;
+      Type genericType = new GenericType<List<Project>>()
+      {
+      }.getGenericType();
+      Object entity = new GenericEntity<List<Project>>(projectRefs, genericType);
+      
+      return Response.ok(entity).build();
    }
 
 }
