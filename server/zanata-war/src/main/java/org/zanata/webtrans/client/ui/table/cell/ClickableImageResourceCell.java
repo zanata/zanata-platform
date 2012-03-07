a6ee721161a71443d50a71660a761f75822d492c
Alex Eng <aeng@redhat.com>
2012-03-07 13:17:58 +1000

    Add copyrights statement

--- server/zanata-war/src/main/java/org/zanata/webtrans/client/ui/table/cell/ClickableImageResourceCell.java
+++ server/zanata-war/src/main/java/org/zanata/webtrans/client/ui/table/cell/ClickableImageResourceCell.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2012, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.zanata.webtrans.client.ui.table.cell;
 
 import java.util.HashSet;
@@ -9,6 +29,11 @@ import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.resources.client.ImageResource;
 
+/**
+ * 
+ * @author Alex Eng <a href="mailto:aeng@redhat.com">aeng@redhat.com</a>
+ * 
+ **/
 public class ClickableImageResourceCell extends ImageResourceCell
 {
    public ClickableImageResourceCell()
