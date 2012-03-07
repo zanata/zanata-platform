0350197529e1359e7e74e4e34fa572d4062c1da9
Alex Eng <aeng@redhat.com>
2012-03-07 14:24:42 +1000

    Reorganising documentlistTable

---
+++ server/zanata-war/src/main/java/org/zanata/webtrans/client/ui/table/cell/TransUnitCountGraphCell.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2012, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.zanata.webtrans.client.ui.table.cell;
+
+import org.zanata.webtrans.client.ui.TransUnitCountGraph;
+
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.ValueUpdater;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+
+/**
+ * 
+ * @author Alex Eng <a href="mailto:aeng@redhat.com">aeng@redhat.com</a>
+ * 
+ **/
+public class TransUnitCountGraphCell extends AbstractCell<TransUnitCountGraph>
+{
+   public TransUnitCountGraphCell()
+   {
+      super("mouseover", "mouseout");
+   }
+
+   @Override
+   public void render(com.google.gwt.cell.client.Cell.Context context, TransUnitCountGraph value, SafeHtmlBuilder sb)
+   {
+      sb.appendHtmlConstant(value.getElement().getString());
+   }
+
+   @Override
+   public void onBrowserEvent(Context context, Element parent, TransUnitCountGraph value, NativeEvent event, ValueUpdater<TransUnitCountGraph> valueUpdater)
+   {
+      if (event.getType().equalsIgnoreCase("mouseover"))
+      {
+         value.onMouseOver(parent.getFirstChildElement());
+      }
+      else if (event.getType().equalsIgnoreCase("mouseout"))
+      {
+         value.onMouseOut();
+      }
+   }
+
+}
