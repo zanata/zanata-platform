80882fad43ea570a5e1811eae3f6005e7f65b4be
David Mason <dr.d.mason@gmail.com>
2012-03-13 11:46:32 +1000

    rename printf validation tests to match class under test

--- server/zanata-war/src/test/java/org/zanata/webtrans/shared/validation/PrintfVariablesValidationTest.java
+++ server/zanata-war/src/test/java/org/zanata/webtrans/shared/validation/PrintfVariablesValidationTest.java
@@ -46,14 +46,14 @@ import org.zanata.webtrans.shared.validation.action.PrintfVariablesValidation;
  *
  **/
 @Test(groups = { "unit-tests" })
-public class VariablesValidationTests
+public class PrintfVariablesValidationTest
 {
    private static final String MOCK_VARIABLES_VALIDATOR_NAME = "test variable validator name";
    private static final String MOCK_VARIABLES_VALIDATOR_DESCRIPTION = "test variable validator description";
    private static final String MOCK_VARIABLES_ADDED_MESSAGE = "test variables added message";
    private static final String MOCK_VARIABLES_MISSING_MESSAGE = "test variables missing message";
 
-   private PrintfVariablesValidation variablesValidation;
+   private PrintfVariablesValidation printfVariablesValidation;
 
    private ValidationMessages mockMessages;
    private Capture<List<String>> capturedVarsAdded;
@@ -77,7 +77,7 @@ public class VariablesValidationTests
    @BeforeMethod
    public void init()
    {
-      variablesValidation = null;
+      printfVariablesValidation = null;
 
       capturedVarsAdded.reset();
       capturedVarsMissing.reset();
@@ -86,27 +86,27 @@ public class VariablesValidationTests
    @Test
    public void idIsSet()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
-      assertThat(variablesValidation.getId(), is(MOCK_VARIABLES_VALIDATOR_NAME));
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
+      assertThat(printfVariablesValidation.getId(), is(MOCK_VARIABLES_VALIDATOR_NAME));
    }
 
    @Test
    public void descriptionIsSet()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
-      assertThat(variablesValidation.getDescription(), is(MOCK_VARIABLES_VALIDATOR_DESCRIPTION));
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
+      assertThat(printfVariablesValidation.getDescription(), is(MOCK_VARIABLES_VALIDATOR_DESCRIPTION));
    }
 
    @Test
    public void noErrorForMatchingVars()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
       String source = "Testing string with variable %1v and %2v";
       String target = "%2v and %1v included, order not relevant";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(false));
-      assertThat(variablesValidation.getError().size(), is(0));
+      assertThat(printfVariablesValidation.hasError(), is(false));
+      assertThat(printfVariablesValidation.getError().size(), is(0));
 
       assertThat(capturedVarsAdded.hasCaptured(), is(false));
       assertThat(capturedVarsMissing.hasCaptured(), is(false));
@@ -115,14 +115,14 @@ public class VariablesValidationTests
    @Test
    public void missingVarInTarget()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
       String source = "Testing string with variable %1v";
       String target = "Testing string with no variables";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItem(MOCK_VARIABLES_MISSING_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(1));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItem(MOCK_VARIABLES_MISSING_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(1));
 
       assertThat(capturedVarsMissing.getValue(), hasItem("%1v"));
       assertThat(capturedVarsMissing.getValue().size(), is(1));
@@ -132,14 +132,14 @@ public class VariablesValidationTests
    @Test
    public void missingVarsThroughoutTarget()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
       String source = "%a variables in all parts %b of the string %c";
       String target = "Testing string with no variables";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItem(MOCK_VARIABLES_MISSING_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(1));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItem(MOCK_VARIABLES_MISSING_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(1));
 
       assertThat(capturedVarsMissing.getValue(), hasItems("%a", "%b", "%c"));
       assertThat(capturedVarsMissing.getValue().size(), is(3));
@@ -149,14 +149,14 @@ public class VariablesValidationTests
    @Test
    public void addedVarInTarget()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
       String source = "Testing string with no variables";
       String target = "Testing string with variable %2$#x";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItem(MOCK_VARIABLES_ADDED_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(1));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItem(MOCK_VARIABLES_ADDED_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(1));
 
       assertThat(capturedVarsAdded.getValue(), hasItem("%2$#x"));
       assertThat(capturedVarsAdded.getValue().size(), is(1));
@@ -166,14 +166,14 @@ public class VariablesValidationTests
    @Test
    public void addedVarsThroughoutTarget()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
       String source = "Testing string with no variables";
       String target = "%1$-0lls variables in all parts %2$-0hs of the string %3$-0ls";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItem(MOCK_VARIABLES_ADDED_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(1));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItem(MOCK_VARIABLES_ADDED_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(1));
 
       assertThat(capturedVarsAdded.getValue(), hasItems("%1$-0lls", "%2$-0hs", "%3$-0ls"));
       assertThat(capturedVarsAdded.getValue().size(), is(3));
@@ -183,14 +183,14 @@ public class VariablesValidationTests
    @Test
    public void bothAddedAndMissingVars()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
       String source = "String with %x and %y only, not z";
       String target = "String with %y and %z, not x";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItems(MOCK_VARIABLES_ADDED_MESSAGE, MOCK_VARIABLES_MISSING_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(2));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItems(MOCK_VARIABLES_ADDED_MESSAGE, MOCK_VARIABLES_MISSING_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(2));
 
       assertThat(capturedVarsAdded.getValue(), hasItem("%z"));
       assertThat(capturedVarsAdded.getValue().size(), is(1));
@@ -202,14 +202,14 @@ public class VariablesValidationTests
    @Test
    public void substringVariablesDontMatch()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
       String source = "%ll";
       String target = "%l %ll";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItem(MOCK_VARIABLES_ADDED_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(1));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItem(MOCK_VARIABLES_ADDED_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(1));
 
       assertThat(capturedVarsAdded.getValue(), allOf(hasItem("%l"), not(hasItem("%ll"))));
       assertThat(capturedVarsAdded.getValue().size(), is(1));
@@ -220,15 +220,15 @@ public class VariablesValidationTests
    @Test
    public void superstringVariablesDontMatch()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
 
       String source = "%l %ll";
       String target = "%ll";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItem(MOCK_VARIABLES_MISSING_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(1));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItem(MOCK_VARIABLES_MISSING_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(1));
 
       assertThat(capturedVarsMissing.getValue(), allOf(hasItem("%l"), not(hasItem("%ll"))));
       assertThat(capturedVarsAdded.hasCaptured(), is(false));
@@ -238,15 +238,15 @@ public class VariablesValidationTests
    @Test
    public void superstringVariablesDontMatch2()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
 
       String source = "%z";
       String target = "%zz";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItems(MOCK_VARIABLES_MISSING_MESSAGE, MOCK_VARIABLES_ADDED_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(2));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItems(MOCK_VARIABLES_MISSING_MESSAGE, MOCK_VARIABLES_ADDED_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(2));
 
       assertThat(capturedVarsMissing.getValue(), allOf(hasItem("%z"), not(hasItem("%zz"))));
       assertThat(capturedVarsAdded.getValue(), allOf(hasItem("%zz"), not(hasItem("%z"))));
@@ -255,15 +255,15 @@ public class VariablesValidationTests
    @Test
    public void checkWithRealWorldExamples()
    {
-      variablesValidation = new PrintfVariablesValidation(mockMessages);
+      printfVariablesValidation = new PrintfVariablesValidation(mockMessages);
       // examples from strings in translate.zanata.org
       String source = "%s %d %-25s %r";
       String target = "no variables";
-      variablesValidation.validate(source, target);
+      printfVariablesValidation.validate(source, target);
 
-      assertThat(variablesValidation.hasError(), is(true));
-      assertThat(variablesValidation.getError(), hasItems(MOCK_VARIABLES_MISSING_MESSAGE));
-      assertThat(variablesValidation.getError().size(), is(1));
+      assertThat(printfVariablesValidation.hasError(), is(true));
+      assertThat(printfVariablesValidation.getError(), hasItems(MOCK_VARIABLES_MISSING_MESSAGE));
+      assertThat(printfVariablesValidation.getError().size(), is(1));
 
       assertThat(capturedVarsMissing.getValue(), hasItems("%s", "%d", "%-25s", "%r"));
    }
