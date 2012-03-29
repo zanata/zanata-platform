c0ee26df5ad6791ea19c2f339d878f77458950ac
Carlos A. Munoz <camunoz@redhat.com>
2012-03-29 13:16:19 +1000

    Add test for TextFlowTargetDAO.
    In this revision this test only covers the query that is the base for copy trans.

--- server/zanata-war/src/test/resources/import.sql
+++ server/zanata-war/src/test/resources/import.sql
@@ -1,6 +1,7 @@
 -- executed by hibernate on startup.
 -- http://relation.to/Bloggers/RotterdamJBugAndHibernatesImportsql
 -- This script is for unit tests only. 
--- It should run the same tasks as the one in src/main/resources/db/h2|mysql/create_trigger.sql
+-- It should run a superset of the tasks in src/main/resources/db/h2|mysql/create_trigger.sql
 
 CREATE TRIGGER HDocument_Update AFTER UPDATE ON HDocument FOR EACH ROW CALL "org.zanata.H2DocumentHistoryTrigger";
+CREATE ALIAS MD5 FOR "org.zanata.util.HashUtil.generateHash";
