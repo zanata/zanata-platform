81f0584869c51e59d30d48c03673a41e77b87985
Alex Eng <aeng@redhat.com>
2012-04-24 10:45:56 +1000

    Remove classes file generated via eclipse

--- server/zanata-war/src/main/webapp/WEB-INF/classes/db/h2/create_trigger.sql
+++
@@ -1,8 +0,0 @@
-/*
- * This file creates triggers for the H2 database. Please see src/test/resources/import.sql for a similar script that 
- * should do the same for unit tests.
- */
-DROP TRIGGER IF EXISTS `HDocument_Update`;
-CREATE TRIGGER HDocument_Update AFTER UPDATE ON HDocument FOR EACH ROW CALL "org.zanata.H2DocumentHistoryTrigger";
-DROP TRIGGER IF EXISTS `HTextFlow_Update`;
-DROP TRIGGER IF EXISTS `HTextFlowTarget_Update`;
