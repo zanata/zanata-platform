cbabdde2d62a5d459b9081235290439c24c5faa6
Patrick Huang <pahuang@redhat.com>
2012-04-27 09:29:27 +1000

    test method dependency

--- functional-test/src/test/java/org/zanata/feature/CreateSampleProjectTest.java
+++ functional-test/src/test/java/org/zanata/feature/CreateSampleProjectTest.java
@@ -66,8 +66,7 @@ public class CreateSampleProjectTest
       workFlow.addLanguageAndJoin("zh");
    }
 
-//   @Test(dependsOnMethods = { "canCreateProjectAndVersion", "canAddLanguage" }, invocationTimeOut = 50000)
-   @Test(invocationTimeOut = 50000)
+   @Test(dependsOnMethods = { "canCreateProjectAndVersion", "canAddLanguage" }, invocationTimeOut = 50000)
    public void canPush() throws IOException
    {
       ClientPushWorkFlow clientPushWorkFlow = new ClientPushWorkFlow();
