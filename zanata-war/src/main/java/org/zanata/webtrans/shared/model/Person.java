8959e89a61f4d3968a1aa0c19dcf628c1c321022
Patrick Huang <pahuang@redhat.com>
2012-05-09 14:15:14 +1000

    remove lombok annotation from gwt code but keep plugin in pom

--- zanata-war/src/main/java/org/zanata/webtrans/shared/model/Person.java
+++ zanata-war/src/main/java/org/zanata/webtrans/shared/model/Person.java
@@ -2,16 +2,10 @@ package org.zanata.webtrans.shared.model;
 
 import java.io.Serializable;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@Getter
-@EqualsAndHashCode(of = {"id", "avatarUrl"})
 public class Person implements HasIdentifier<PersonId>, Serializable
 {
    private static final long serialVersionUID = 510785473431813586L;
@@ -20,18 +14,59 @@ public class Person implements HasIdentifier<PersonId>, Serializable
    private String name;
    private String avatarUrl;
 
+   // for GWT
+   @SuppressWarnings("unused")
+   private Person()
+   {
+   }
+
    public Person(PersonId id, String name, String avatarUrl)
    {
       Preconditions.checkNotNull(id, "id cannot be null");
       Preconditions.checkNotNull(name, "name cannot be null");
-
       this.id = id;
       this.name = name;
       this.avatarUrl = avatarUrl;
    }
 
+   public PersonId getId()
+   {
+      return id;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
    public String getAvatarUrl()
    {
       return Strings.nullToEmpty(avatarUrl);
+   }
+
+   @Override
+   public int hashCode()
+   {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((avatarUrl == null) ? 0 : avatarUrl.hashCode());
+      result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (this == obj)
+      {
+         return true;
+      }
+      if (obj == null || getClass() != obj.getClass())
+      {
+         return false;
+      }
+
+      Person other = (Person) obj;
+      return Objects.equal(avatarUrl, other.avatarUrl) && Objects.equal(id, other.id);
    }
 }
