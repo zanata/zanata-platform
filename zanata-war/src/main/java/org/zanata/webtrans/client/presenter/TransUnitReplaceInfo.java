2a2c4f21325378fa60d452e545b958d810223d4c
David Mason <dr.d.mason@gmail.com>
2012-05-25 16:35:09 +1000

    add separate preview state for replace info model, prevent no-op rpc calls, add copyright notices

--- zanata-war/src/main/java/org/zanata/webtrans/client/presenter/TransUnitReplaceInfo.java
+++ zanata-war/src/main/java/org/zanata/webtrans/client/presenter/TransUnitReplaceInfo.java
@@ -1,16 +1,44 @@
+/*
+ * Copyright 2012, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.zanata.webtrans.client.presenter;
 
 import java.util.Comparator;
 
+import org.zanata.webtrans.client.ui.SearchResultsDocumentTable;
 import org.zanata.webtrans.shared.model.TransUnit;
 import org.zanata.webtrans.shared.model.TransUnitUpdateInfo;
 import org.zanata.webtrans.shared.model.TransUnitUpdatePreview;
 
+/**
+ * Data model used by {@link SearchResultsDocumentTable}.
+ * 
+ * @author David Mason, <a href="mailto:damason@redhat.com">damason@redhat.com</a>
+ *
+ */
 public class TransUnitReplaceInfo
 {
    private static Comparator<TransUnitReplaceInfo> comparator;
 
-   private ReplacementState state;
+   private ReplacementState replacementState;
+   private PreviewState previewState;
    private Long docId;
    private TransUnit tu;
    private TransUnitUpdatePreview preview;
@@ -22,7 +50,8 @@ public class TransUnitReplaceInfo
       this.tu = tu;
       preview = null;
       replaceInfo = null;
-      state = ReplacementState.Replaceable;
+      replacementState = ReplacementState.NotReplaced;
+      previewState = PreviewState.NotFetched;
    }
 
    public TransUnit getTransUnit()
@@ -55,14 +84,24 @@ public class TransUnitReplaceInfo
       this.replaceInfo = replaceInfo;
    }
 
-   public ReplacementState getState()
+   public ReplacementState getReplaceState()
    {
-      return state;
+      return replacementState;
    }
 
-   public void setState(ReplacementState state)
+   public void setReplaceState(ReplacementState state)
    {
-      this.state = state;
+      this.replacementState = state;
+   }
+
+   public PreviewState getPreviewState()
+   {
+      return previewState;
+   }
+
+   public void setPreviewState(PreviewState previewState)
+   {
+      this.previewState = previewState;
    }
 
    public Long getDocId()
@@ -71,7 +110,7 @@ public class TransUnitReplaceInfo
    }
 
 
-   public static Comparator<TransUnitReplaceInfo> getComparator()
+   public static Comparator<TransUnitReplaceInfo> getRowComparator()
    {
       if (comparator == null)
       {
