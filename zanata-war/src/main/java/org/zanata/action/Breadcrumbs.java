aaa5bbcca6cbfd31494b951e3e7e751c1d2ba7b5
Alex Eng <aeng@redhat.com>
2012-08-07 11:55:03 +1000

    Implement breadcrumbs on projects pages, https://bugzilla.redhat.com/show_bug.cgi?id=844822

---
+++ zanata-war/src/main/java/org/zanata/action/Breadcrumbs.java
@@ -0,0 +1,67 @@
+package org.zanata.action;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+
+@Name("breadcrumbs")
+@AutoCreate
+@Scope(ScopeType.PAGE)
+public class Breadcrumbs implements Serializable
+{
+   private static final long serialVersionUID = 1L;
+   
+   @Logger
+   Log log;
+
+   private List<Breadcrumb> locations;
+
+
+   public List<Breadcrumb> getLocations()
+   {
+      if (locations == null)
+      {
+         locations = new ArrayList<Breadcrumb>();
+      }
+      return locations;
+   }
+
+   public void addLocation(String location, String display)
+   {
+      getLocations().add(new Breadcrumb(location, display));
+   }
+
+   public void addLocation(String location, String display, int index)
+   {
+      getLocations().add(index, new Breadcrumb(location, display));
+   }
+   
+   public class Breadcrumb
+   {
+      private String location;
+      private String display;
+      
+      public Breadcrumb(String location, String display)
+      {
+         this.location = location;
+         this.display = display;
+      }
+
+      public String getLocation()
+      {
+         return location;
+      }
+
+      public String getDisplay()
+      {
+         return display;
+      }
+   }
+}
