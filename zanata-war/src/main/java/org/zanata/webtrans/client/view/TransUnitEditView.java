6c638a0d693b97b600ce08ef3b698a29b1f97a92
Patrick Huang <pahuang@redhat.com>
2012-08-13 12:24:46 +1000

    rhbz844820 - table style

--- zanata-war/src/main/java/org/zanata/webtrans/client/view/TransUnitEditView.java
+++ zanata-war/src/main/java/org/zanata/webtrans/client/view/TransUnitEditView.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.zanata.webtrans.client.editor.table.SourceContentsDisplay;
 import org.zanata.webtrans.client.editor.table.TargetContentsDisplay;
 import org.zanata.webtrans.client.ui.FilterViewConfirmationDisplay;
+
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTMLTable;
@@ -22,17 +27,22 @@ import com.google.inject.Singleton;
 @Singleton
 public class TransUnitEditView extends Composite implements TransUnitEditDisplay
 {
+   private static TransUnitEditViewUiBinder uiBinder = GWT.create(TransUnitEditViewUiBinder.class);
+
    private Grid transUnitTable = new Grid(0, 2);
    private ScrollPanel container= new ScrollPanel(transUnitTable);
    private final FilterViewConfirmationDisplay filterViewConfirmationDisplay;
    private Listener listener;
 
+   @UiField
+   Styles style;
+
    @Inject
-   public TransUnitEditView(FilterViewConfirmationDisplay filterViewConfirmationDisplay
-   )
+   public TransUnitEditView(FilterViewConfirmationDisplay filterViewConfirmationDisplay)
    {
       this.filterViewConfirmationDisplay = filterViewConfirmationDisplay;
       transUnitTable.setWidth("100%");
+      transUnitTable.setStyleName(style.table());
       transUnitTable.addClickHandler(new ClickHandler()
       {
          @Override
@@ -45,6 +55,7 @@ public class TransUnitEditView extends Composite implements TransUnitEditDisplay
             }
          }
       });
+      initWidget(uiBinder.createAndBindUi(this));
    }
 
    private void selectRow(int rowIndex)
@@ -80,11 +91,32 @@ public class TransUnitEditView extends Composite implements TransUnitEditDisplay
          TargetContentsDisplay targetDisplay = targetDisplays.get(i);
          transUnitTable.setWidget(i, 0, sourceDisplay);
          transUnitTable.setWidget(i, 1, targetDisplay);
-         transUnitTable.getCellFormatter().setVerticalAlignment(i, 0, HasVerticalAlignment.ALIGN_TOP);
-         transUnitTable.getCellFormatter().setVerticalAlignment(i, 1, HasVerticalAlignment.ALIGN_TOP);
+         HTMLTable.CellFormatter cellFormatter = transUnitTable.getCellFormatter();
+         cellFormatter.setVerticalAlignment(i, 0, HasVerticalAlignment.ALIGN_TOP);
+         cellFormatter.setVerticalAlignment(i, 1, HasVerticalAlignment.ALIGN_TOP);
+         cellFormatter.setStyleName(i, 0, style.cellFormat());
+         cellFormatter.setStyleName(i, 1, style.cellFormat());
+
       }
       transUnitTable.getColumnFormatter().setWidth(0, "50%");
       transUnitTable.getColumnFormatter().setWidth(1, "50%");
+      applyRowStyle();
+   }
+
+   private void applyRowStyle()
+   {
+      HTMLTable.RowFormatter rowFormatter = transUnitTable.getRowFormatter();
+      for (int i = 0; i < transUnitTable.getRowCount(); i++)
+      {
+         if ((i % 2) == 0)
+         {
+            rowFormatter.setStyleName(i, style.evenRow());
+         }
+         else
+         {
+            rowFormatter.setStyleName(i, style.oddRow());
+         }
+      }
    }
 
    @Override
@@ -99,5 +131,18 @@ public class TransUnitEditView extends Composite implements TransUnitEditDisplay
       return container;
    }
 
+   interface Styles extends CssResource
+   {
+      String oddRow();
 
+      String evenRow();
+
+      String cellFormat();
+
+      String table();
+   }
+
+   interface TransUnitEditViewUiBinder extends UiBinder<Widget, TransUnitEditView>
+   {
+   }
 }
