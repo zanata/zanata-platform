aa84dbcb91bf599decad7c10e4b4d30ede3b92fb
Patrick Huang <pahuang@redhat.com>
2012-09-18 13:22:18 +1000

    rhbz844820 - translator list won't cause editor row to grow and fix long word break in target

---
+++ zanata-war/src/main/java/org/zanata/webtrans/client/ui/TranslatorListWidget.java
@@ -0,0 +1,86 @@
+package org.zanata.webtrans.client.ui;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Widget;
+
+public class TranslatorListWidget extends Composite
+{
+   private static TranslatorListWidgetUiBinder ourUiBinder = GWT.create(TranslatorListWidgetUiBinder.class);
+   @UiField
+   Styles style;
+   @UiField
+   HTMLPanel container;
+
+
+   public TranslatorListWidget()
+   {
+      initWidget(ourUiBinder.createAndBindUi(this));
+
+   }
+
+   public void addTranslator(String name, String color)
+   {
+      Label nameLabel = new Label(name);
+      nameLabel.setStyleName(style.userLabel());
+
+      nameLabel.getElement().getStyle().setProperty("backgroundColor", color);
+      nameLabel.getElement().getStyle().setProperty("borderColor", color);
+      nameLabel.getElement().getStyle().setProperty("borderWidth", "1px");
+      nameLabel.getElement().getStyle().setProperty("borderStyle", "solid");
+
+      container.add(nameLabel);
+   }
+
+   public void clearTranslatorList()
+   {
+      container.clear();
+   }
+
+   public void removeTranslator(String name, String color)
+   {
+
+      for (int i = 0; i < container.getWidgetCount(); i++)
+      {
+         Label translatorLabel = (Label) container.getWidget(i);
+
+         if (translatorLabel.getText().equals(name) && removeFormat(translatorLabel.getElement().getStyle().getProperty("backgroundColor")).equals(removeFormat(color)))
+         {
+            container.remove(i);
+         }
+      }
+   }
+
+   /**
+    * Color string return from userSessionService rgb(xx,xx,xx), Color string
+    * return from browser is formatted rgb(xx, xx, xx). Method needed to
+    * unformat all color
+    *
+    * @param color color
+    */
+   private String removeFormat(String color)
+   {
+      return color.replace(" ", "");
+   }
+
+   interface TranslatorListWidgetUiBinder extends UiBinder<Widget, TranslatorListWidget>
+   {
+   }
+
+   interface Styles extends CssResource
+   {
+
+      String translatorList();
+
+      String userLabel();
+   }
+}
