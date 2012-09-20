744561b76797db9395f1bd134abd3134dad902a2
Carlos A. Munoz <camunoz@redhat.com>
2012-09-20 15:31:03 +1000

    rhbz858136 - Add copy trans resource and DTO.
    These new objects define the Copy Trans REST API.

---
+++ zanata-common-api/src/main/java/org/zanata/rest/dto/CopyTransStatus.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2010, Red Hat, Inc. and individual contributors as indicated by the
+ * @author tags. See the copyright.txt file in the distribution for a full
+ * listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.zanata.rest.dto;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * Copy Trans process status.
+ *
+ * @author Carlos Munoz <a href="mailto:camunoz@redhat.com">camunoz@redhat.com</a>
+ */
+@XmlRootElement(name = "copyTransStatus")
+@XmlType(name = "copyTransStatusType")
+public class CopyTransStatus
+{
+   private int percentageComplete;
+
+   private boolean inProgress;
+
+   @XmlElement(required = true)
+   public int getPercentageComplete()
+   {
+      return percentageComplete;
+   }
+
+   public void setPercentageComplete(int percentageComplete)
+   {
+      this.percentageComplete = percentageComplete;
+   }
+
+   @XmlElement(required = true)
+   public boolean isInProgress()
+   {
+      return inProgress;
+   }
+
+   public void setInProgress(boolean inProgress)
+   {
+      this.inProgress = inProgress;
+   }
+}
---
+++ zanata-common-api/src/main/java/org/zanata/rest/service/CopyTransResource.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2010, Red Hat, Inc. and individual contributors as indicated by the
+ * @author tags. See the copyright.txt file in the distribution for a full
+ * listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.zanata.rest.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import org.zanata.rest.dto.CopyTransStatus;
+
+/**
+ * @author Carlos Munoz <a href="mailto:camunoz@redhat.com">camunoz@redhat.com</a>
+ */
+@Path("/copytrans")
+public interface CopyTransResource
+{
+   @POST
+   @Path("/proj/{projectSlug}/iter/{iterationSlug}/doc/{docId}")
+   // /copytrans/proj/{projectSlug}/iter/{iterationSlug}/doc/{docId}
+   public void startCopyTrans( @PathParam("projectSlug") String projectSlug,
+                               @PathParam("iterationSlug") String iterationSlug,
+                               @PathParam("docId") String docId );
+
+   @GET
+   @Path("/proj/{projectSlug}/iter/{iterationSlug}/doc/{docId}")
+   // /copytrans/proj/{projectSlug}/iter/{iterationSlug}/doc/{docId}
+   public CopyTransStatus getCopyTransStatus( @PathParam("projectSlug") String projectSlug,
+                                              @PathParam("iterationSlug") String iterationSlug,
+                                              @PathParam("docId") String docId );
+}
