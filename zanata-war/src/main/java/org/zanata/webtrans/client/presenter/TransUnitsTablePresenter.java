f7d5a5b2afa189ec8d6c4276d14f8585644c09af
Patrick Huang <pahuang@redhat.com>
2012-11-16 13:54:31 +1000

    rhbz872384 - will save pending change before switching editor

--- zanata-war/src/main/java/org/zanata/webtrans/client/presenter/TransUnitsTablePresenter.java
+++ zanata-war/src/main/java/org/zanata/webtrans/client/presenter/TransUnitsTablePresenter.java
@@ -238,6 +238,7 @@ public class TransUnitsTablePresenter extends WidgetPresenter<TransUnitsTableDis
       }
       else if (event == RefreshPageEvent.REDRAW_PAGE_EVENT)
       {
+         targetContentsPresenter.savePendingChangesIfApplicable();
          List<TransUnit> currentPageValues = navigationService.getCurrentPageValues();
          sourceContentsPresenter.showData(currentPageValues);
          targetContentsPresenter.showData(currentPageValues);
