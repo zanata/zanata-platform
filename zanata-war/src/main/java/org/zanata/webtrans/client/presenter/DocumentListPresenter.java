b03684ed86c8069ef19425a6f2e493a54f2ff486
Patrick Huang <pahuang@redhat.com>
2012-12-06 13:59:55 +1000

    move inner class out

--- zanata-war/src/main/java/org/zanata/webtrans/client/presenter/DocumentListPresenter.java
+++ zanata-war/src/main/java/org/zanata/webtrans/client/presenter/DocumentListPresenter.java
@@ -22,7 +22,6 @@ package org.zanata.webtrans.client.presenter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 
 import net.customware.gwt.presenter.client.EventBus;
 import net.customware.gwt.presenter.client.widget.WidgetPresenter;
@@ -208,80 +207,6 @@ public class DocumentListPresenter extends WidgetPresenter<DocumentListDisplay> 
    public void setStatsFilter(String option)
    {
       display.setStatsFilter(option);
-   }
-
-   /**
-    * Filters documents by their full path + name, with substring and exact
-    * modes.
-    * 
-    * If there is no pattern set, this filter will accept all documents.
-    * 
-    * @author David Mason, damason@redhat.com
-    * 
-    */
-   public final class PathDocumentFilter
-   {
-      private static final String DOCUMENT_FILTER_LIST_DELIMITER = ",";
-
-      private HashSet<String> patterns = new HashSet<String>();
-      private boolean isFullText = false;
-      private boolean caseSensitive = false;
-
-      public boolean accept(DocumentInfo value)
-      {
-         if (patterns.isEmpty())
-         {
-            return true;
-         }
-         String fullPath = value.getPath() + value.getName();
-         if (!caseSensitive)
-         {
-            fullPath = fullPath.toLowerCase();
-         }
-         for (String pattern : patterns)
-         {
-            if (!caseSensitive)
-            {
-               pattern = pattern.toLowerCase();
-            }
-            if (isFullText)
-            {
-               if (fullPath.equals(pattern))
-               {
-                  return true;
-               }
-            }
-            else if (fullPath.contains(pattern))
-            {
-               return true;
-            }
-         }
-         return false; // didn't match any patterns
-      }
-
-      public void setPattern(String pattern)
-      {
-         patterns.clear();
-         String[] patternCandidates = pattern.split(DOCUMENT_FILTER_LIST_DELIMITER);
-         for (String candidate : patternCandidates)
-         {
-            candidate = candidate.trim();
-            if (candidate.length() != 0)
-            {
-               patterns.add(candidate);
-            }
-         }
-      }
-
-      public void setFullText(boolean fullText)
-      {
-         isFullText = fullText;
-      }
-
-      public void setCaseSensitive(boolean caseSensitive)
-      {
-         this.caseSensitive = caseSensitive;
-      }
    }
 
    public void updateFilterAndRun(String docFilterText, boolean docFilterExact, boolean docFilterCaseSensitive)
