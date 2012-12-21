c476833365ee4bae873aedb398c65e9dfae24e31
Carlos A. Munoz <camunoz@redhat.com>
2012-12-21 14:02:01 +1000

    rhbz888090 - Add utility method to calculate a file's MD5 checksum.

--- zanata-common-util/src/main/java/org/zanata/util/HashUtil.java
+++ zanata-common-util/src/main/java/org/zanata/util/HashUtil.java
@@ -1,9 +1,13 @@
 package org.zanata.util;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
+import com.google.common.io.Files;
 
 public class HashUtil
 {
@@ -25,5 +29,30 @@ public class HashUtil
    public static String md5Hex(String message)
    {
       return DigestUtils.md5Hex(message);
+   }
+
+   /**
+    * Generates the MD5 checksum from the contents of a file.
+    *
+    * @param f The file to calculate the checksum for.
+    * @return The MD5 checksum for f.
+    * @throws java.io.FileNotFoundException If the given file does not exist.
+    */
+   public static final String getMD5Checksum(File f)
+   {
+      try
+      {
+         MessageDigest md5 = MessageDigest.getInstance("MD5");
+         byte[] digest = Files.getDigest(f, md5);
+         return new String(Hex.encodeHex(digest));
+      }
+      catch (NoSuchAlgorithmException e)
+      {
+         throw new RuntimeException(e);
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException(e);
+      }
    }
 }
---
+++ zanata-common-util/src/test/java/org/zanata/util/HashUtilTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2010, Red Hat, Inc. and individual contributors as indicated by the
+ * @author tags. See the copyright.txt file in the distribution for a full
+ * listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.zanata.util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.security.MessageDigest;
+
+import org.apache.commons.codec.binary.Hex;
+import org.testng.annotations.Test;
+import org.zanata.common.io.DigestWriter;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * @author Carlos Munoz <a href="mailto:camunoz@redhat.com">camunoz@redhat.com</a>
+ */
+public class HashUtilTest
+{
+   @Test
+   public void fileUtilHashSameAsDigestWriter() throws Exception
+   {
+      File tmpFile = File.createTempFile("Test", "-tmp");
+      tmpFile.deleteOnExit();
+
+      MessageDigest md5 = MessageDigest.getInstance("MD5");
+      FileWriter fw = new FileWriter( tmpFile );
+      DigestWriter dw = new DigestWriter( fw, md5 );
+
+      dw.write( "This is a new Temporary file" );
+      dw.flush();
+      dw.close();
+
+      String digestWriterHash = new String(Hex.encodeHex(md5.digest()));
+      String hashUtilHash = HashUtil.getMD5Checksum( tmpFile );
+
+      assertThat(digestWriterHash, equalTo(hashUtilHash));
+   }
+}
