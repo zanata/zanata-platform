3053f8871f0b9ae361f306d20e59255dec5dcb97
David Mason <dr.d.mason@gmail.com>
2013-01-29 17:26:48 +1000

    remove unused imports

--- zanata-adapter-xliff/src/main/java/org/zanata/adapter/xliff/XliffWriter.java
+++ zanata-adapter-xliff/src/main/java/org/zanata/adapter/xliff/XliffWriter.java
@@ -2,7 +2,6 @@ package org.zanata.adapter.xliff;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
