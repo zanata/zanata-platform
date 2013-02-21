41b534d0db75c19dbd475aec8d30b2f79a74ccf7
Alex Eng <aeng@redhat.com>
2013-02-21 12:52:03 +1000

    Change activation link expired messages: https://bugzilla.redhat.com/show_bug.cgi?id=893811#c2

---
+++ zanata-war/src/main/java/org/zanata/exception/ActivationLinkExpiredException.java
@@ -0,0 +1,17 @@
+package org.zanata.exception;
+
+public class ActivationLinkExpiredException extends RuntimeException
+{
+   private static final long serialVersionUID = 1L;
+
+   public ActivationLinkExpiredException()
+   {
+      super();
+   }
+
+   public ActivationLinkExpiredException(String message)
+   {
+      super(message);
+   }
+
+}
