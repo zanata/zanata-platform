103be236f2ca9f98ec0650bccbaeb95152074e05
David Mason <dr.d.mason@gmail.com>
2013-05-13 18:27:55 +1000

    rhbz961564 gut attention key presenter pending change of behaviour

--- zanata-war/src/main/java/org/zanata/webtrans/client/presenter/AttentionKeyShortcutPresenter.java
+++ zanata-war/src/main/java/org/zanata/webtrans/client/presenter/AttentionKeyShortcutPresenter.java
@@ -20,21 +20,16 @@
  */
 package org.zanata.webtrans.client.presenter;
 
-import java.util.Map;
-import java.util.Set;
-
-import org.zanata.webtrans.client.keys.EventWrapper;
-import org.zanata.webtrans.client.keys.KeyShortcut;
-import org.zanata.webtrans.client.keys.Keys;
-import org.zanata.webtrans.client.view.AttentionKeyShortcutDisplay;
-
-import com.google.gwt.dom.client.NativeEvent;
-import com.google.inject.Inject;
-
 import net.customware.gwt.presenter.client.EventBus;
 import net.customware.gwt.presenter.client.widget.WidgetPresenter;
 
+import org.zanata.webtrans.client.view.AttentionKeyShortcutDisplay;
+
+import com.google.inject.Inject;
+
 /**
+ * Responsible for getting the necessary model data to show when attention mode is
+ * active in response to relevant events.
  * 
  * @author David Mason, <a href="mailto:damason@redhat.com">damason@redhat.com</a>
  *
@@ -42,34 +37,16 @@ import net.customware.gwt.presenter.client.widget.WidgetPresenter;
 public class AttentionKeyShortcutPresenter extends WidgetPresenter<AttentionKeyShortcutDisplay>
 {
 
-   private final EventWrapper event;
-
-   // hold a map of shortcuts similar to KeyShortcutPresenter, accessible by KeyShortcutPresenter
-   private Map<Keys, Set<KeyShortcut>> shortcutMap;
-
-
-   // hold awareness of the current shortcut key (KeyShortcutPresenter can look it up from here)
-   // (Note: this will come from user config, this is just the definitive place that looks it up from there)
-
-   // rely on KeyShortcutPresenter to invoke event handling at an appropriate time
 
    @Inject
-   public AttentionKeyShortcutPresenter(AttentionKeyShortcutDisplay display,
-                                        EventBus eventBus,
-                                        final EventWrapper event)
+   public AttentionKeyShortcutPresenter(AttentionKeyShortcutDisplay display, EventBus eventBus)
    {
       super(display, eventBus);
-      this.event = event;
    }
 
    @Override
    protected void onBind()
    {
-
-      // TODO look up attention key setting from user settings
-
-      // TODO register attention shortcut
-      // Note: keep the registration handle so it can be unregistered if the user changes the setting.
    }
 
    @Override
@@ -84,19 +61,6 @@ public class AttentionKeyShortcutPresenter extends WidgetPresenter<AttentionKeyS
    {
       // TODO Auto-generated method stub
 
-   }
-
-   public boolean isAttentionMode()
-   {
-      // TODO Auto-generated method stub
-      return false;
-   }
-
-   public void processKeyEvent(NativeEvent evt)
-   {
-      // TODO Auto-generated method stub
-
-      Keys pressedKeys = event.createKeys(evt);
    }
 
 }
