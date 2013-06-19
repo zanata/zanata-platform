9ca2a0478f2be0d903ea0434f269803670164ef2
Carlos A. Munoz <camunoz@redhat.com>
2013-06-19 16:45:39 +1000

    Add correct compatibility tests for the Stats service.
    Both raw and java client tests are included.

---
+++ zanata-war/src/test/java/org/zanata/rest/compat/v2_3_0/StatisticsCompatibilityITCase.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2010, Red Hat, Inc. and individual contributors as indicated by the
+ * @author tags. See the copyright.txt file in the distribution for a full
+ * listing of individual contributors.
+ * 
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.zanata.rest.compat.v2_3_0;
+
+import org.dbunit.operation.DatabaseOperation;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.junit.Test;
+import org.zanata.RestTest;
+import org.zanata.provider.DBUnitProvider;
+import org.zanata.v2_3_0.rest.dto.stats.ContainerTranslationStatistics;
+import org.zanata.v2_3_0.rest.dto.stats.TranslationStatistics;
+import org.zanata.v2_3_0.rest.service.StatisticsResource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+/**
+ * @author Carlos Munoz <a href="mailto:camunoz@redhat.com">camunoz@redhat.com</a>
+ */
+public class StatisticsCompatibilityITCase extends RestTest
+{
+
+   @Override
+   protected void prepareDBUnitOperations()
+   {
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/ClearAllTables.dbunit.xml", DatabaseOperation.DELETE_ALL));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/AccountData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/ProjectsData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/LocalesData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/DocumentsData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/TextFlowTestData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+
+      addAfterTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/HistoryTestData.dbunit.xml", DatabaseOperation.DELETE_ALL));
+   }
+
+   @Test
+   @RunAsClient
+   public void getStatisticsForIteration() throws Exception
+   {
+      StatisticsResource statsResource = super.createProxy(createClientProxyFactory(TRANSLATOR, TRANSLATOR_KEY),
+            StatisticsResource.class, "/");
+
+      ContainerTranslationStatistics stats = statsResource.getStatistics("sample-project", "1.0", true, true, null);
+
+      int wordStatCount = 0;
+      int mssgStatCount = 0;
+
+      assertThat(stats.getStats().size(), greaterThan(0));
+      assertThat(stats.getDetailedStats().size(), greaterThan(0)); // Has document stats
+      for(TranslationStatistics langStats : stats.getStats())
+      {
+         assertThat(langStats.getTotal(),
+               equalTo(langStats.getNeedReview() + langStats.getUntranslated() + langStats.getTranslated()));
+         //MatcherAssert.assertThat( 100,
+         //   Matchers.equalTo( asStats.getPercentNeedReview() + asStats.getPercentUntranslated() + asStats.getPercentTranslated() ));
+         wordStatCount += (langStats.getUnit() == TranslationStatistics.StatUnit.WORD ? 1 : 0);
+         mssgStatCount += (langStats.getUnit() == TranslationStatistics.StatUnit.MESSAGE ? 1 : 0);
+      }
+
+      assertThat("Word and Message level stat count should match", wordStatCount == mssgStatCount);
+   }
+
+   @Test
+   @RunAsClient
+   public void getStatisticsForDocument() throws Exception
+   {
+      StatisticsResource statsResource = super.createProxy(createClientProxyFactory(TRANSLATOR, TRANSLATOR_KEY),
+            StatisticsResource.class, "/");
+
+      ContainerTranslationStatistics stats = statsResource.getStatistics("sample-project", "1.0", "my/path/document.txt", true, null );
+
+      int wordStatCount = 0;
+      int mssgStatCount = 0;
+
+      assertThat(stats.getStats().size(), greaterThan(0));
+      for(TranslationStatistics langStats : stats.getStats())
+      {
+         assertThat(langStats.getTotal(),
+               equalTo(langStats.getNeedReview() + langStats.getUntranslated() + langStats.getTranslated()));
+         //MatcherAssert.assertThat( 100,
+         //   Matchers.equalTo( asStats.getPercentNeedReview() + asStats.getPercentUntranslated() + asStats.getPercentTranslated() ));
+         wordStatCount += (langStats.getUnit() == TranslationStatistics.StatUnit.WORD ? 1 : 0);
+         mssgStatCount += (langStats.getUnit() == TranslationStatistics.StatUnit.MESSAGE ? 1 : 0);
+      }
+
+      assertThat("Word and Message level stat count should match", wordStatCount == mssgStatCount);
+   }
+
+   @Test
+   @RunAsClient
+   public void getStatisticsForIterationAndLocale() throws Exception
+   {
+      StatisticsResource statsResource = super.createProxy(createClientProxyFactory(TRANSLATOR, TRANSLATOR_KEY),
+            StatisticsResource.class, "/");
+
+      ContainerTranslationStatistics stats = statsResource.getStatistics("sample-project", "1.0", true, false, new String[]{"as"});
+
+      assertThat(stats.getStats().size(), is(1)); // Just one locale and no word level stats
+      assertThat(stats.getDetailedStats().size(), greaterThan(0)); // Has document stats
+      for(TranslationStatistics langStats : stats.getStats())
+      {
+         assertThat(langStats.getTotal(),
+               equalTo(langStats.getNeedReview() + langStats.getUntranslated() + langStats.getTranslated()));
+         //MatcherAssert.assertThat( 100,
+         //   Matchers.equalTo( asStats.getPercentNeedReview() + asStats.getPercentUntranslated() + asStats.getPercentTranslated() ));
+         assertThat("Shouldn't have returned word level stats", langStats.getUnit(), not(TranslationStatistics.StatUnit.WORD));
+      }
+   }
+
+   @Test
+   @RunAsClient
+   public void getStatisticsForDocumentAndLocale() throws Exception
+   {
+      StatisticsResource statsResource = super.createProxy(createClientProxyFactory(TRANSLATOR, TRANSLATOR_KEY),
+            StatisticsResource.class, "/");
+
+      ContainerTranslationStatistics stats = statsResource.getStatistics("sample-project", "1.0", "my/path/document.txt", true, new String[]{"as"} );
+
+      assertThat(stats.getStats().size(), is(2)); // Just one locale
+      for(TranslationStatistics langStats : stats.getStats())
+      {
+         assertThat(langStats.getTotal(),
+               equalTo(langStats.getNeedReview() + langStats.getUntranslated() + langStats.getTranslated()));
+         //MatcherAssert.assertThat( 100,
+         //   Matchers.equalTo( asStats.getPercentNeedReview() + asStats.getPercentUntranslated() + asStats.getPercentTranslated() ));
+      }
+   }
+}
--- zanata-war/src/test/java/org/zanata/rest/compat/v2_3_0/StatisticsRawCompatibilityITCase.java
+++ zanata-war/src/test/java/org/zanata/rest/compat/v2_3_0/StatisticsRawCompatibilityITCase.java
@@ -20,9 +20,139 @@
  */
 package org.zanata.rest.compat.v2_3_0;
 
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+
+import org.dbunit.operation.DatabaseOperation;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import org.junit.Test;
+import org.zanata.RestTest;
+import org.zanata.provider.DBUnitProvider;
+import org.zanata.rest.ResourceRequest;
+import org.zanata.v2_3_0.rest.dto.stats.ContainerTranslationStatistics;
+
+import static org.zanata.util.RawRestTestUtils.assertJaxbUnmarshal;
+import static org.zanata.util.RawRestTestUtils.assertJsonUnmarshal;
+
 /**
  * @author Carlos Munoz <a href="mailto:camunoz@redhat.com">camunoz@redhat.com</a>
  */
-public class StatisticsRawCompatibilityITCase
+public class StatisticsRawCompatibilityITCase extends RestTest
 {
+   @Override
+   protected void prepareDBUnitOperations()
+   {
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/ClearAllTables.dbunit.xml", DatabaseOperation.DELETE_ALL));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/AccountData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/ProjectsData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/LocalesData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/DocumentsData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+      addBeforeTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/TextFlowTestData.dbunit.xml", DatabaseOperation.CLEAN_INSERT));
+
+      addAfterTestOperation(new DBUnitProvider.DataSetOperation("org/zanata/test/model/HistoryTestData.dbunit.xml", DatabaseOperation.DELETE_ALL));
+   }
+
+   @Test
+   @RunAsClient
+   public void getStatisticsForIterationXml() throws Exception
+   {
+      new ResourceRequest(getRestEndpointUrl("/stats/proj/{projectId}/iter/{iterationId}"), "GET")
+      {
+
+         @Override
+         protected void prepareRequest(ClientRequest request)
+         {
+            request.header(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML)
+                   .queryParameter("detail", true)
+                   .queryParameter("word", true)
+                   .pathParameter("projectId", "sample-project")
+                   .pathParameter("iterationId", "1.0");;
+         }
+
+         @Override
+         protected void onResponse(ClientResponse response)
+         {
+            assertJaxbUnmarshal(response, ContainerTranslationStatistics.class);
+         }
+      }.run();
+   }
+
+   @Test
+   @RunAsClient
+   public void getStatisticsForIterationJson() throws Exception
+   {
+      new ResourceRequest(getRestEndpointUrl("/stats/proj/{projectId}/iter/{iterationId}"), "GET")
+      {
+
+         @Override
+         protected void prepareRequest(ClientRequest request)
+         {
+            request.header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                  .queryParameter("detail", true)
+                  .queryParameter("word", true)
+                  .pathParameter("projectId", "sample-project")
+                  .pathParameter("iterationId", "1.0");
+         }
+
+         @Override
+         protected void onResponse(ClientResponse response)
+         {
+            assertJsonUnmarshal(response, ContainerTranslationStatistics.class);
+         }
+      }.run();
+   }
+
+   @Test
+   @RunAsClient
+   public void getStatisticsForDocumentXml() throws Exception
+   {
+      new ResourceRequest(getRestEndpointUrl("/stats/proj/{projectId}/iter/{iterationId}/doc/{docId}"), "GET")
+      {
+
+         @Override
+         protected void prepareRequest(ClientRequest request)
+         {
+            request.header(HttpHeaders.ACCEPT, MediaType.APPLICATION_XML)
+                  .queryParameter("detail", true)
+                  .queryParameter("word", true)
+                  .pathParameter("projectId", "sample-project")
+                  .pathParameter("iterationId", "1.0")
+                  .pathParameter("docId", "my/path/document.txt");
+         }
+
+         @Override
+         protected void onResponse(ClientResponse response)
+         {
+            assertJaxbUnmarshal(response, ContainerTranslationStatistics.class);
+         }
+      }.run();
+   }
+
+   @Test
+   @RunAsClient
+   public void getStatisticsForDocumentJson() throws Exception
+   {
+      new ResourceRequest(getRestEndpointUrl("/stats/proj/{projectId}/iter/{iterationId}/doc/{docId}"), "GET")
+      {
+
+         @Override
+         protected void prepareRequest(ClientRequest request)
+         {
+            request.header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                  .queryParameter("detail", true)
+                  .queryParameter("word", true)
+                  .pathParameter("projectId", "sample-project")
+                  .pathParameter("iterationId", "1.0")
+                  .pathParameter("docId", "my/path/document.txt");
+         }
+
+         @Override
+         protected void onResponse(ClientResponse response)
+         {
+            assertJsonUnmarshal(response, ContainerTranslationStatistics.class);
+         }
+      }.run();
+   }
 }
