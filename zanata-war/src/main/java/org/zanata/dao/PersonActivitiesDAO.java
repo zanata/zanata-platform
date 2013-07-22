e5845e0b54f341fcb67a815d3c877aefa10c7e94
Alex Eng <aeng@redhat.com>
2013-07-22 19:05:21 +1000

    Work in progress: user activities

--- zanata-war/src/main/java/org/zanata/dao/PersonActivitiesDAO.java
+++ zanata-war/src/main/java/org/zanata/dao/PersonActivitiesDAO.java
@@ -1,4 +1,26 @@
+/*
+ * Copyright 2013, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.zanata.dao;
+
+import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -7,10 +29,11 @@ import org.jboss.seam.annotations.AutoCreate;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 import org.zanata.common.UserActionType;
-import org.zanata.model.HPerson;
 import org.zanata.model.HPersonActivities;
-import org.zanata.model.HProjectIteration;
 
+/**
+ * @author Alex Eng <a href="mailto:aeng@redhat.com">aeng@redhat.com</a>
+ */
 @Name("personActivitiesDAO")
 @AutoCreate
 @Scope(ScopeType.STATELESS)
@@ -28,21 +51,31 @@ public class PersonActivitiesDAO extends AbstractDAOImpl<HPersonActivities, Long
       super(HPersonActivities.class, session);
    }
 
-   public void insertOrUpdateActivities(HPerson person, HProjectIteration version, UserActionType action)
+   @SuppressWarnings("unchecked")
+   public HPersonActivities findUserActivity(Long personId, Long versionId, UserActionType action)
    {
       Query query = getSession().createQuery("FROM HPersonActivities WHERE person.id = :personId "
             + "AND projectIteration.id = :versionId "
             + "AND action = :action");
-      query.setParameter("personId", person.getId());
-      query.setParameter("versionId", version.getId());
+      query.setParameter("personId", personId);
+      query.setParameter("versionId", versionId);
       query.setParameter("action", action);
-      query.setCacheable(false);
-      query.setComment("PersonActivitiesDAO.insertOrUpdateActivities");
+      query.setCacheable(true);
+      query.setComment("PersonActivitiesDAO.findUserActivity");
+      return (HPersonActivities)query.uniqueResult();
    }
-   
-   
-   public HPersonActivities findByActivities(HPerson person, HProjectIteration version, UserActionType action)
+
+   @SuppressWarnings("unchecked")
+   public List<HPersonActivities> findAllUserActivities(Long personId, Long versionId, int offset, int count)
    {
-      
+      Query query = getSession().createQuery("FROM HPersonActivities WHERE person.id = :personId "
+            + "AND projectIteration.id = :versionId");
+      query.setParameter("personId", personId);
+      query.setParameter("versionId", versionId);
+      query.setMaxResults(count);
+      query.setFirstResult(offset);
+      query.setCacheable(true);
+      query.setComment("PersonActivitiesDAO.findAllUserActivities");
+      return query.list();
    }
 }
