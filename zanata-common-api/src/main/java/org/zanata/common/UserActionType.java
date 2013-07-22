e74415e3e76f2fff8836c95d6c409f05f400aa63
Alex Eng <aeng@redhat.com>
2013-07-23 09:24:08 +1000

    Add upload source and upload translation doc action

--- zanata-common-api/src/main/java/org/zanata/common/UserActionType.java
+++ zanata-common-api/src/main/java/org/zanata/common/UserActionType.java
@@ -5,5 +5,5 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(name = "userActionType")
 public enum UserActionType
 {
-   UPDATE_TRANSLATION, REVIEWED_TRANSLATION, UPLOAD_DOCUMENT;
+   UPDATE_TRANSLATION, REVIEWED_TRANSLATION, UPLOAD_SOURCE_DOCUMENT, UPLOAD_TRANSLATION_DOCUMENT;
 }
