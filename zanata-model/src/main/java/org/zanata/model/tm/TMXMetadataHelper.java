a6fcb0faba0b9eb1340c1b15cebfb739f401285b
Sean Flanigan <sflaniga@redhat.com>
2013-08-07 16:19:42 +1000

    Remove reference to javaName

--- zanata-model/src/main/java/org/zanata/model/tm/TMXMetadataHelper.java
+++ zanata-model/src/main/java/org/zanata/model/tm/TMXMetadataHelper.java
@@ -282,7 +282,7 @@ public class TMXMetadataHelper
       if (lang.contains("_"))
       {
          log.debug("Converting invalid locale to BCP47: {}", lang);
-         return LocaleId.fromJavaName(lang).getId();
+         lang = lang.replace('-', '_');
       }
       return new LocaleId(lang).getId();
    }
