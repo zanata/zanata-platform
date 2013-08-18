297f7608816413bc6b6cfc27d6f1dff49e67905c
Damian Jansen <djansen@redhat.com>
2013-08-19 09:54:03 +1000

    Update database table count and data generation script
    Table count is now 81.
    Two Transmemory _METADATA tables have been dropped.

--- functional-test/src/test/java/org/zanata/util/DatabaseHelper.java
+++ functional-test/src/test/java/org/zanata/util/DatabaseHelper.java
@@ -37,7 +37,7 @@ public class DatabaseHelper
    private static Statement statement;
    private static DatabaseHelper DB;
    private String backupPath;
-   private static int CURRENT_TABLE_COUNT = 76;
+   private static int CURRENT_TABLE_COUNT = 81;
    private FluentWait<Statement> wait;
 
    private DatabaseHelper()
