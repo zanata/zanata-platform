4914522d153510243d83b41bdf72001ea22896df
Patrick Huang <pahuang@redhat.com>
2013-09-17 14:37:55 +1000

    rhbz1008253 - intercept ESC so that firefox won't close connection

--- zanata-war/src/main/java/org/zanata/webtrans/client/ui/ShortcutContextAwareDialogBox.java
+++ zanata-war/src/main/java/org/zanata/webtrans/client/ui/ShortcutContextAwareDialogBox.java
@@ -1,7 +1,12 @@
 package org.zanata.webtrans.client.ui;
 
+import org.zanata.webtrans.client.events.KeyShortcutEvent;
+import org.zanata.webtrans.client.events.KeyShortcutEventHandler;
+import org.zanata.webtrans.client.keys.KeyShortcut;
+import org.zanata.webtrans.client.keys.Keys;
 import org.zanata.webtrans.client.keys.ShortcutContext;
 import org.zanata.webtrans.client.presenter.KeyShortcutPresenter;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.user.client.ui.DialogBox;
 
 /**
@@ -17,6 +22,22 @@ public class ShortcutContextAwareDialogBox extends DialogBox
       super(autoHide, modal);
       this.modalContext = modalContext;
       this.keyShortcutPresenter = keyShortcutPresenter;
+      // intercept esc key so that Firefox won't close event service connection
+      KeyShortcut hideSelfShortcut = KeyShortcut.Builder.builder()
+            .addKey(new Keys(Keys.NO_MODIFIER, KeyCodes.KEY_ESCAPE))
+            .setContext(modalContext)
+            .setKeyEvent(KeyShortcut.KeyEvent.KEY_DOWN)
+            .setPreventDefault(true)
+            .setStopPropagation(true)
+            .setHandler(new KeyShortcutEventHandler()
+            {
+               @Override
+               public void onKeyShortcut(KeyShortcutEvent event)
+               {
+                  hide();
+               }
+            }).build();
+      keyShortcutPresenter.register(hideSelfShortcut);
    }
 
    @Override
