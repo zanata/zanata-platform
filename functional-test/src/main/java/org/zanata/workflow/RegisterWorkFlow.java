56da60288b3f6cc04bfedb525852962ffa0958db
Damian Jansen <djansen@redhat.com>
2013-10-23 08:10:16 +1000

    Use the old Google site when necessary.
    Occasionally the old Google account site presents itself when logging in,
    so if this occurs just enter the email as normal.

--- functional-test/src/main/java/org/zanata/workflow/RegisterWorkFlow.java
+++ functional-test/src/main/java/org/zanata/workflow/RegisterWorkFlow.java
@@ -36,10 +36,15 @@ public class RegisterWorkFlow extends AbstractWebWorkFlow {
                 .goToHome()
                 .clickSignInLink()
                 .selectGoogleOpenID();
-
-        // If Google has remembered us, skip entering the email
-        // If Google has remembered someone else, change the user
-        if (googleAccountPage.hasRememberedAuthentication()) {
+        /*
+            There is the chance that Google presents the old page. It seems
+            to be random. Just enter the email in this case.
+            Otherwise, If Google has remembered us, skip entering the email.
+            If Google has remembered someone else, change the user.
+        */
+        if (googleAccountPage.isTheOldGoogleSite()) {
+            googleAccountPage = googleAccountPage.enterGoogleEmail(email);
+        } else if (googleAccountPage.hasRememberedAuthentication()) {
             if (!googleAccountPage.rememberedUser().equals(email)) {
                 googleAccountPage = googleAccountPage.removeSavedAuthentication();
             }
