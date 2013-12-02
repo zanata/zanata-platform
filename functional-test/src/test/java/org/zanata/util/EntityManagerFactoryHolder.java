e08d9c91e0dc14612d275920eec70994f20516d2
Patrick Huang <pahuang@redhat.com>
2013-12-03 09:50:43 +1000

    reduce copy of persistenc.xml

--- functional-test/src/test/java/org/zanata/util/EntityManagerFactoryHolder.java
+++ functional-test/src/test/java/org/zanata/util/EntityManagerFactoryHolder.java
@@ -1,16 +1,64 @@
 package org.zanata.util;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Properties;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EntityManagerFactoryHolder {
     private static final EntityManagerFactoryHolder HOLDER =
             new EntityManagerFactoryHolder();
     private static EntityManagerFactory emFactory;
+    private final Properties overrideProps;
+
+    private EntityManagerFactoryHolder() {
+        copyPersistenceXML();
+        overrideProps = new Properties();
+        overrideProps.put("hibernate.hbm2ddl.auto", "");
+        overrideProps
+                .put("hibernate.connection.provider_class",
+                        "org.hibernate.service.jdbc.connections.internal.DriverManagerConnectionProviderImpl");
+        overrideProps.put("hibernate.connection.url",
+                PropertiesHolder.getProperty("hibernate.connection.url"));
+        overrideProps.put("hibernate.search.default.indexBase",
+                PropertiesHolder
+                        .getProperty("hibernate.search.default.indexBase"));
+    }
+
+    /**
+     * This is a temporary workaround to reduce another copy of persistence.xml.
+     * It rely on zanata-war being built (so that the persistenc.xml appears in
+     * target/test-classes/META-INF)
+     */
+    private static void copyPersistenceXML() {
+        URL resource =
+                Thread.currentThread().getContextClassLoader().getResource(".");
+        File currentDir = new File(resource.getPath());
+        String current = currentDir.getAbsolutePath();
+        String testPersistenceXML =
+                current.replace("functional-test/target/test-classes",
+                        "zanata-war/target/test-classes/META-INF/persistence.xml");
+
+        File xml = new File(testPersistenceXML);
+        Preconditions.checkState(
+                xml.exists() && xml.getName().equals("persistence.xml"),
+                "%s does not exists. You will need to build zanata-war first.",
+                xml.getAbsolutePath());
+
+        File destDir = new File(currentDir, "META-INF");
+        destDir.mkdirs();
+        try {
+            Files.copy(xml, new File(destDir, "persistence.xml"));
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+    }
 
     public static EntityManagerFactoryHolder holder() {
         return HOLDER;
@@ -20,8 +68,9 @@ public class EntityManagerFactoryHolder {
         if (emFactory == null) {
             synchronized (HOLDER) {
                 if (emFactory == null) {
-                    emFactory = Persistence.createEntityManagerFactory(
-                                    "zanataTestDataDatasourcePU", null);
+                    emFactory =
+                            Persistence.createEntityManagerFactory(
+                                    "zanataTestDatasourcePU", overrideProps);
                 }
             }
         }
