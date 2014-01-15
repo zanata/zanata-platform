21b779c6a6ad0a4600a41b620bd3d4f2d9020aa7
Patrick Huang <pahuang@redhat.com>
2014-01-15 14:46:46 +1000

    split new user and edit profile action

---
+++ zanata-war/src/main/java/org/zanata/action/AbstractProfileAction.java
@@ -0,0 +1,107 @@
+package org.zanata.action;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.faces.Renderer;
+import org.jboss.seam.security.management.JpaIdentityStore;
+import org.zanata.dao.AccountDAO;
+import org.zanata.dao.PersonDAO;
+import org.zanata.model.HAccount;
+import org.zanata.model.HPerson;
+import org.zanata.security.ZanataIdentity;
+
+/**
+ * @author Patrick Huang
+ *         <a href="mailto:pahuang@redhat.com">pahuang@redhat.com</a>
+ */
+public abstract class AbstractProfileAction {
+    protected String name;
+    protected String email;
+    protected String username;
+    protected boolean valid;
+    private String activationKey;
+
+    @In(create = true)
+    protected Renderer renderer;
+
+    @In
+    ZanataIdentity identity;
+
+    @In(required = false, value = JpaIdentityStore.AUTHENTICATED_USER)
+    HAccount authenticatedAccount;
+
+    @In
+    PersonDAO personDAO;
+
+    @In
+    AccountDAO accountDAO;
+
+    protected void validateEmail(String email) {
+        HPerson person = personDAO.findByEmail(email);
+
+        if (person != null && !person.getAccount().equals(authenticatedAccount)) {
+            valid = false;
+            FacesMessages.instance().addToControl("email",
+                    "This email address is already taken");
+        }
+    }
+
+    protected void validateUsername(String username) {
+        HAccount account = accountDAO.getByUsername(username);
+
+        if (account != null && !account.equals(authenticatedAccount)) {
+            valid = false;
+            FacesMessages.instance().addToControl("username",
+                    "This username is already taken");
+        }
+    }
+
+    @NotEmpty
+    @Size(min = 2, max = 80)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.validateEmail(email);
+        this.email = email;
+    }
+
+    @NotEmpty
+    @Size(min = 3, max = 20)
+    @Pattern(regexp = "^[a-z\\d_]{3,20}$",
+            message = "{validation.username.constraints}")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+        validateUsername(username);
+    }
+
+    public String getActivationKey() {
+        return activationKey;
+    }
+
+    public void setActivationKey(String keyHash) {
+        this.activationKey = keyHash;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+}
---
+++ zanata-war/src/main/java/org/zanata/action/NewProfileAction.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2010, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.zanata.action;
+
+import java.io.Serializable;
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Transactional;
+import org.jboss.seam.faces.FacesMessages;
+import org.zanata.ApplicationConfiguration;
+import org.zanata.security.AuthenticationType;
+import org.zanata.security.ZanataOpenId;
+import org.zanata.service.RegisterService;
+
+/**
+ * This action handles new user profile creation.
+ *
+ */
+@Name("newProfileAction")
+@Scope(ScopeType.PAGE)
+@Slf4j
+public class NewProfileAction extends AbstractProfileAction implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @In
+    private ZanataOpenId zanataOpenId;
+
+    @In
+    RegisterService registerServiceImpl;
+
+    @In
+    ApplicationConfiguration applicationConfiguration;
+
+    @Create
+    public void onCreate() {
+        if (identity.getCredentials().getAuthType() != AuthenticationType.OPENID) {
+            // Open id user names are url's so they don't make good defaults
+            username = identity.getCredentials().getUsername();
+        }
+        String domain = applicationConfiguration.getDomainName();
+        if (domain == null) {
+            email = "";
+        } else {
+            if (applicationConfiguration.isOpenIdAuth()) {
+                email = zanataOpenId.getAuthResult().getEmail();
+            } else {
+                email = identity.getCredentials().getUsername() + "@" + domain;
+            }
+        }
+    }
+
+    @Transactional
+    public void createUser() {
+        this.valid = true;
+        validateEmail(this.email);
+        validateUsername(username);
+
+        if (!this.isValid()) {
+            return;
+        }
+
+        String key;
+        AuthenticationType authType = identity.getCredentials().getAuthType();
+        if (authType == AuthenticationType.KERBEROS
+                || authType == AuthenticationType.JAAS) {
+            key = registerServiceImpl.register(
+                    this.username, this.username, this.email);
+        } else {
+            key = registerServiceImpl.register(this.username, zanataOpenId
+                            .getAuthResult().getAuthenticatedId(),
+                            AuthenticationType.OPENID, this.name, this.email);
+        }
+        setActivationKey(key);
+        renderer.render("/WEB-INF/facelets/email/email_activation.xhtml");
+        identity.unAuthenticate();
+        FacesMessages
+                .instance()
+                .add("You will soon receive an email with a link to activate your account.");
+    }
+
+    public void cancel() {
+        identity.logout();
+    }
+
+}
