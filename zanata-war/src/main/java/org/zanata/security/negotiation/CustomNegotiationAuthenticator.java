2d7aa5744fcaa85ee5d87c5aee98ca169ac674c5
Carlos A. Munoz <camunoz@redhat.com>
2014-02-03 10:07:10 +1000

    Rename NegotiationAuthenticator class.
    This to provide a name that is different from its super class.
    Remove some checkstyle failures on new lines.

--- zanata-war/src/main/java/org/zanata/security/negotiation/CustomNegotiationAuthenticator.java
+++ zanata-war/src/main/java/org/zanata/security/negotiation/CustomNegotiationAuthenticator.java
@@ -2,17 +2,17 @@
  * Copyright 2014, Red Hat, Inc. and individual contributors as indicated by the
  * @author tags. See the copyright.txt file in the distribution for a full
  * listing of individual contributors.
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -20,11 +20,13 @@
  */
 package org.zanata.security.negotiation;
 
-import org.apache.catalina.connector.Request;
-import org.apache.catalina.deploy.LoginConfig;
+import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.deploy.LoginConfig;
+import org.jboss.security.negotiation.NegotiationAuthenticator;
 
 /**
  * Override JBoss' Negotiation authenticator to customize some of its
@@ -33,8 +35,7 @@ import java.io.IOException;
  * @author Carlos Munoz <a
  *         href="mailto:camunoz@redhat.com">camunoz@redhat.com</a>
  */
-public class NegotiationAuthenticator extends
-        org.jboss.security.negotiation.NegotiationAuthenticator {
+public class CustomNegotiationAuthenticator extends NegotiationAuthenticator {
 
     @Override
     public boolean authenticate(Request request, HttpServletResponse response,
