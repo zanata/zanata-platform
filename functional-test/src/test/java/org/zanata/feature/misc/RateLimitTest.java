222c71fee023d082623cd8f9235e4a081c371b61
Patrick Huang <pahuang@redhat.com>
2014-02-26 15:23:06 +1000

    rhbz988202 - server configuration REST service and more tests

--- functional-test/src/test/java/org/zanata/feature/misc/RateLimitTest.java
+++ functional-test/src/test/java/org/zanata/feature/misc/RateLimitTest.java
@@ -1,5 +1,6 @@
 package org.zanata.feature.misc;
 
+import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -7,15 +8,21 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
+import javax.ws.rs.core.Response;
 
 import org.hamcrest.Matchers;
-import org.junit.ClassRule;
+import org.jboss.resteasy.client.core.BaseClientResponse;
+import org.jboss.resteasy.util.GenericType;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.zanata.feature.DetailedTest;
 import org.zanata.page.administration.AdministrationPage;
 import org.zanata.page.administration.ServerConfigurationPage;
+import org.zanata.rest.client.IServerConfigurationResource;
+import org.zanata.rest.dto.Configuration;
 import org.zanata.util.AddUsersRule;
+import org.zanata.util.RetryRule;
 import org.zanata.util.ZanataRestCaller;
 import org.zanata.workflow.BasicWorkFlow;
 import org.zanata.workflow.LoginWorkFlow;
@@ -26,6 +33,10 @@ import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.zanata.model.HApplicationConfiguration.KEY_ADMIN_EMAIL;
+import static org.zanata.model.HApplicationConfiguration.KEY_RATE_LIMIT_PER_SECOND;
+import static org.zanata.util.ZanataRestCaller.checkStatusAndReleaseConnection;
+import static org.zanata.util.ZanataRestCaller.getStatusAndReleaseConnection;
 
 /**
  * @author Patrick Huang <a
@@ -34,15 +45,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @Category(DetailedTest.class)
 @Slf4j
 public class RateLimitTest {
-    @ClassRule
-    public static AddUsersRule addUsersRule = new AddUsersRule();
+    @Rule
+    public AddUsersRule addUsersRule = new AddUsersRule();
+
+    @Rule
+    public RetryRule retryRule = new RetryRule(3);
 
     private static final String TRANSLATOR = "translator";
     private static final String TRANSLATOR_API =
             "d83882201764f7d339e97c4b087f0806";
 
     @Test
-    public void canConfigureRateLimit() {
+    public void canConfigureRateLimitByWebUI() {
         new LoginWorkFlow().signIn("admin", "admin");
         BasicWorkFlow basicWorkFlow = new BasicWorkFlow();
         ServerConfigurationPage serverConfigPage =
@@ -64,14 +78,87 @@ public class RateLimitTest {
     }
 
     @Test
+    public void canCallServerConfigurationRestService() {
+        IServerConfigurationResource resource =
+                getServerConfigurationResource();
+
+        // can put
+        Response putResponse = resource.put(KEY_RATE_LIMIT_PER_SECOND, "1");
+
+        assertThat(getStatusAndReleaseConnection(putResponse), Matchers.is(201));
+
+        // can get single configuration
+        Response getResponse = resource.get(KEY_RATE_LIMIT_PER_SECOND);
+
+        assertThat(getResponse.getStatus(), Matchers.is(200));
+        Configuration rateLimitConfig =
+                ((BaseClientResponse<Configuration>) getResponse)
+                        .getEntity(Configuration.class);
+        assertThat(rateLimitConfig.getKey(),
+                Matchers.equalTo(KEY_RATE_LIMIT_PER_SECOND));
+        assertThat(rateLimitConfig.getValue(), Matchers.equalTo("1"));
+
+        // can get all configurations
+        Response getAllResponse = resource.get();
+        BaseClientResponse baseClientResponse =
+                (BaseClientResponse) getAllResponse;
+        Type genericType = new GenericType<List<Configuration>>() {
+        }.getGenericType();
+
+        List<Configuration> configurations =
+                (List<Configuration>) baseClientResponse.getEntity(List.class,
+                        genericType);
+        log.info("result {}", configurations);
+
+        assertThat(getStatusAndReleaseConnection(getAllResponse),
+                Matchers.is(200));
+        assertThat(configurations, Matchers.hasItem(rateLimitConfig));
+    }
+
+    private static IServerConfigurationResource
+            getServerConfigurationResource() {
+        return new ZanataRestCaller().getZanataProxyFactory().createProxy(
+                IServerConfigurationResource.class);
+    }
+
+    @Test
+    public void serverConfigurationRestServiceOnlyAvailableToAdmin() {
+        // invoke service as translator user
+        IServerConfigurationResource resource =
+                new ZanataRestCaller(TRANSLATOR, TRANSLATOR_API)
+                        .getZanataProxyFactory().createProxy(
+                                IServerConfigurationResource.class);
+
+        // all request should be rejected
+        Response response = resource.get();
+        assertThat(getStatusAndReleaseConnection(response), Matchers.is(401));
+
+        Response response1 = resource.get(KEY_ADMIN_EMAIL);
+        assertThat(getStatusAndReleaseConnection(response1), Matchers.is(401));
+
+        Response response2 = resource.put(KEY_ADMIN_EMAIL, "admin@email.com");
+        assertThat(getStatusAndReleaseConnection(response2), Matchers.is(401));
+    }
+
+    @Test
+    public void canOnlyDealWithKnownConfiguration() {
+        IServerConfigurationResource resource =
+                getServerConfigurationResource();
+
+        Response putResponse = resource.put("arbitrary", "value");
+        assertThat(getStatusAndReleaseConnection(putResponse), Matchers.is(400));
+
+        Response getResponse = resource.get("arbitrary");
+        assertThat(getStatusAndReleaseConnection(getResponse), Matchers.is(404));
+    }
+
+    @Test
     public void canRateLimitRestRequestsPerAPIKey() throws InterruptedException {
-        new LoginWorkFlow().signIn("admin", "admin");
-        BasicWorkFlow basicWorkFlow = new BasicWorkFlow();
-        ServerConfigurationPage serverConfigPage =
-                basicWorkFlow.goToPage("admin/server_configuration",
-                        ServerConfigurationPage.class);
-        // adjust rate limit per second
-        serverConfigPage.inputRateLimit(3).save();
+        IServerConfigurationResource resource =
+                getServerConfigurationResource();
+
+        Response putResponse = resource.put(KEY_RATE_LIMIT_PER_SECOND, "3");
+        checkStatusAndReleaseConnection(putResponse);
 
         // prepare to fire multiple REST requests
         final AtomicInteger atomicInteger = new AtomicInteger(1);
@@ -121,8 +208,53 @@ public class RateLimitTest {
 
         // 1 request from translator should get 503 and fail
         log.info("result: {}", result);
-        assertThat(result, Matchers.containsInAnyOrder(201, 201, 201, 201, 201,
-                201, 403));
+        assertThat(result,
+                Matchers.containsInAnyOrder(201, 201, 201, 201, 201, 201, 403));
+    }
+
+    @Test
+    public void rateLimitChangeTakesEffectImmediately()
+            throws InterruptedException {
+        // we start allowing 10 request per second
+        checkStatusAndReleaseConnection(getServerConfigurationResource().put(
+                KEY_RATE_LIMIT_PER_SECOND, "10"));
+
+        // prepare to fire multiple REST requests
+        final AtomicInteger atomicInteger = new AtomicInteger(1);
+        // translator creates the project/version
+        final String projectSlug = "project";
+        final String iterationSlug = "version";
+        new ZanataRestCaller().createProjectAndVersion(projectSlug,
+                iterationSlug, "gettext");
+
+        // requests from admin user
+        final int threads = 2;
+        Callable<Integer> translatorTask = new Callable<Integer>() {
+
+            @Override
+            public Integer call() {
+                return invokeRestService(new ZanataRestCaller(), projectSlug,
+                        iterationSlug, atomicInteger);
+            }
+        };
+        // two tasks should work fine
+        List<Callable<Integer>> tasks =
+                Collections.nCopies(threads, translatorTask);
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(threads);
+        List<Integer> result = getResultStatusCodes(
+                executorService.invokeAll(tasks));
+        assertThat(result, Matchers.contains(201, 201));
+
+        // we now change rate limit to 1
+        checkStatusAndReleaseConnection(getServerConfigurationResource()
+                .put(KEY_RATE_LIMIT_PER_SECOND, "1"));
+
+        // new requests is rate limited
+        List<Integer> resultAfter = getResultStatusCodes(
+                executorService.invokeAll(tasks));
+        log.info("result: {}", resultAfter);
+        assertThat(resultAfter, Matchers.containsInAnyOrder(201, 403));
     }
 
     private static Integer invokeRestService(ZanataRestCaller restCaller,
@@ -140,15 +272,15 @@ public class RateLimitTest {
         }
     }
 
-    private static List<Integer> getResultStatusCodes(List<Future<Integer>> futures) {
+    private static List<Integer> getResultStatusCodes(
+            List<Future<Integer>> futures) {
         return Lists.transform(futures,
                 new Function<Future<Integer>, Integer>() {
                     @Override
                     public Integer apply(Future<Integer> input) {
                         try {
                             return input.get();
-                        }
-                        catch (Exception e) {
+                        } catch (Exception e) {
                             throw Throwables.propagate(e);
                         }
                     }
---
+++ zanata-war/src/main/java/org/zanata/rest/service/ServerConfigurationService.java
@@ -0,0 +1,144 @@
+package org.zanata.rest.service;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.util.List;
+import javax.annotation.Nonnull;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.jboss.resteasy.util.GenericType;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Transactional;
+import org.jboss.seam.annotations.security.Restrict;
+import org.zanata.dao.ApplicationConfigurationDAO;
+import org.zanata.model.HApplicationConfiguration;
+import org.zanata.rest.MediaTypes;
+import org.zanata.rest.dto.Configuration;
+import org.zanata.rest.dto.Link;
+import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Lists;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Patrick Huang <a
+ *         href="mailto:pahuang@redhat.com">pahuang@redhat.com</a>
+ */
+@Name("serverConfigurationResource")
+@Path(ServerConfigurationResource.SERVICE_PATH)
+@Transactional
+@Restrict("#{s:hasRole('admin')}")
+@Slf4j
+public class ServerConfigurationService implements ServerConfigurationResource {
+
+    private static List<String> availableKeys;
+
+    @Context
+    private UriInfo uriInfo;
+    /** Type of media requested. */
+    @HeaderParam("Accept")
+    @DefaultValue(MediaType.APPLICATION_XML)
+    @Context
+    private MediaType accept;
+    @In
+    private ApplicationConfigurationDAO applicationConfigurationDAO;
+
+    @Override
+    public Response get() {
+        List<HApplicationConfiguration> all =
+                applicationConfigurationDAO.findAll();
+        List<Configuration> allConfig =
+                Lists.transform(all, new ToConfigurationFunction(accept));
+        Type genericType = new GenericType<List<Configuration>>() {
+        }.getGenericType();
+        Object entity =
+                new GenericEntity<List<Configuration>>(allConfig, genericType);
+        return Response.ok().entity(entity).build();
+
+    }
+
+    @Override
+    public Response get(@Nonnull String configKey) {
+        HApplicationConfiguration config =
+                applicationConfigurationDAO.findByKey(configKey);
+        if (config == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        Configuration configuration =
+                new ToConfigurationFunction(accept).apply(config);
+        return Response.ok().entity(configuration).build();
+    }
+
+    @Override
+    public Response put(@Nonnull String configKey, String configValue) {
+        if (!isConfigKeyValid(configKey)) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("config key not supported: " + configKey).build();
+        }
+        HApplicationConfiguration configuration =
+                applicationConfigurationDAO.findByKey(configKey);
+        if (configuration == null) {
+            HApplicationConfiguration newConfig =
+                    new HApplicationConfiguration(configKey, configValue);
+            applicationConfigurationDAO.makePersistent(newConfig);
+            return Response.created(URI.create("c/" + configKey)).build();
+        } else {
+            configuration.setValue(configValue);
+            return Response.ok().build();
+        }
+    }
+
+    private boolean isConfigKeyValid(String configKey) {
+        return getAvailableKeys().contains(configKey);
+    }
+
+    private static List<String> getAvailableKeys() {
+        if (availableKeys != null) {
+            return availableKeys;
+        }
+        final HApplicationConfiguration dummy = new HApplicationConfiguration();
+        List<Field> availableConfigKeys =
+                Lists.newArrayList(HApplicationConfiguration.class.getFields());
+        availableKeys = Lists.transform(availableConfigKeys,
+                new Function<Field, String>() {
+                    @Override
+                    public String apply(Field input) {
+                        try {
+                            input.setAccessible(true);
+                            return (String) input.get(dummy);
+                        } catch (IllegalAccessException e) {
+                            throw Throwables.propagate(e);
+                        }
+                    }
+                });
+        return availableKeys;
+    }
+
+    @RequiredArgsConstructor
+    private class ToConfigurationFunction implements
+            Function<HApplicationConfiguration, Configuration> {
+
+        private final MediaType accept;
+
+        @Override
+        public Configuration apply(HApplicationConfiguration input) {
+            Configuration config =
+                    new Configuration(input.getKey(), input.getValue());
+            config.getLinks(true).add(
+                    new Link(URI.create("c/" + input.getKey()), "self",
+                            MediaTypes.createFormatSpecificType(
+                                    MediaType.APPLICATION_XML, accept)));
+            return config;
+        }
+    }
+}
