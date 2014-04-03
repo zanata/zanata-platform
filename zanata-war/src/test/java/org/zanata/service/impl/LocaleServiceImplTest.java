8c77f8c79a2abda7d5f3e4d0b68ad202238994a5
Alex Eng <aeng@redhat.com>
2014-04-04 09:22:41 +1000

    Fix and move getVersionSupportedLocale test to service class

--- zanata-war/src/test/java/org/zanata/service/impl/LocaleServiceImplTest.java
+++ zanata-war/src/test/java/org/zanata/service/impl/LocaleServiceImplTest.java
@@ -21,8 +21,11 @@
 package org.zanata.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.hamcrest.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
@@ -30,26 +33,73 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.zanata.common.LocaleId;
 import org.zanata.dao.LocaleDAO;
+import org.zanata.dao.ProjectDAO;
+import org.zanata.dao.ProjectIterationDAO;
 import org.zanata.model.HLocale;
+import org.zanata.model.HProject;
+import org.zanata.model.HProjectIteration;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.when;
 
 @Test(groups = { "business-tests" })
 public class LocaleServiceImplTest {
     private LocaleServiceImpl testLocaleServiceImpl;
+
     @Mock
-    private LocaleDAO mockDAO;
+    private ProjectIterationDAO projectIterationDAO;
+
+    @Mock
+    private ProjectDAO projectDAO;
+
+    @Mock
+    private LocaleDAO localeDAO;
+
+    @Mock
+    private HProject hProject;
+
+    @Mock
+    private HProjectIteration hProjectIteration;
+
+    private String projectSlug = "project";
+    private String versionSlug = "master";
 
     @BeforeMethod(firstTimeOnly = true)
     public void setup() {
         MockitoAnnotations.initMocks(this);
+
         this.testLocaleServiceImpl = new LocaleServiceImpl();
-        this.testLocaleServiceImpl.setLocaleDAO(mockDAO);
+        this.testLocaleServiceImpl.setProjectIterationDAO(projectIterationDAO);
+        this.testLocaleServiceImpl.setProjectDAO(projectDAO);
+        this.testLocaleServiceImpl.setLocaleDAO(localeDAO);
+
+        Set<HLocale> iterationLocales = new HashSet<HLocale>();
+        iterationLocales.add(new HLocale(LocaleId.EN_US));
+        iterationLocales.add(new HLocale(LocaleId.DE));
+
+        when(hProjectIteration.getId()).thenReturn(1L);
+        when(hProjectIteration.getCustomizedLocales()).thenReturn(
+                iterationLocales);
+        when(projectIterationDAO.getBySlug(projectSlug, versionSlug))
+                .thenReturn(hProjectIteration);
+
+        Set<HLocale> projectLocales = new HashSet<HLocale>();
+        projectLocales.add(new HLocale(LocaleId.ES));
+        when(hProject.getId()).thenReturn(1L);
+        when(hProject.getCustomizedLocales()).thenReturn(projectLocales);
+        when(projectDAO.getBySlug(projectSlug)).thenReturn(hProject);
+
+        List<HLocale> defaultLocales = new ArrayList<HLocale>();
+        defaultLocales.add(new HLocale(LocaleId.FR));
+        defaultLocales.add(new HLocale(LocaleId.EN));
+        defaultLocales.add(new HLocale(LocaleId.DE));
+        when(localeDAO.findAllActiveAndEnabledByDefault()).thenReturn(
+                defaultLocales);
     }
 
     @Test
     public void testGetAllJavaLanguages() throws Exception {
-        List<LocaleId> loc = this.testLocaleServiceImpl.getAllJavaLanguages();
+        List<LocaleId> loc = testLocaleServiceImpl.getAllJavaLanguages();
         StringBuilder st = new StringBuilder("");
         for (LocaleId localeId : loc) {
             st.append(localeId.getId() + ",");
@@ -67,12 +117,56 @@ public class LocaleServiceImplTest {
         List<HLocale> lan = new ArrayList<HLocale>();
         lan.add(new HLocale(new LocaleId("as-IN")));
         lan.add(new HLocale(new LocaleId("pt-BR")));
-        when(mockDAO.findAll()).thenReturn(lan);
-        List<HLocale> sup = this.testLocaleServiceImpl.getAllLocales();
+        when(localeDAO.findAll()).thenReturn(lan);
+        List<HLocale> sup = testLocaleServiceImpl.getAllLocales();
         Assert.assertEquals(sup.size(), 2);
         String loc1 = sup.get(0).getLocaleId().getId();
         Assert.assertEquals(loc1, "as-IN");
         String loc2 = sup.get(1).getLocaleId().getId();
         Assert.assertEquals(loc2, "pt-BR");
     }
+
+    @Test
+    public void testVersionLocale() {
+        when(hProject.isOverrideLocales()).thenReturn(true);
+        when(hProjectIteration.isOverrideLocales()).thenReturn(true);
+
+        List<HLocale> result =
+                testLocaleServiceImpl.getSupportedLanguageByProjectIteration(
+                        projectSlug, versionSlug);
+
+        assertThat(result, Matchers.hasSize(2));
+        assertThat(result.get(0).getLocaleId(),
+                Matchers.equalTo(LocaleId.EN_US));
+        assertThat(result.get(1).getLocaleId(), Matchers.equalTo(LocaleId.DE));
+    }
+
+    @Test
+    public void testVersionLocaleWithOverriddenProjectLocales() {
+        when(hProject.isOverrideLocales()).thenReturn(true);
+
+        List<HLocale> result =
+                testLocaleServiceImpl.getSupportedLanguageByProjectIteration(
+                        projectSlug, versionSlug);
+
+        assertThat(result, Matchers.hasSize(1));
+        assertThat(result.get(0).getLocaleId(), Matchers.equalTo(LocaleId.ES));
+    }
+
+    @Test
+    public void testVersionLocaleWithNoOverridden() throws Exception {
+        when(hProjectIteration.isOverrideLocales()).thenReturn(false);
+        when(hProject.isOverrideLocales()).thenReturn(false);
+
+        List<HLocale> result =
+                testLocaleServiceImpl.getSupportedLanguageByProjectIteration(
+                        projectSlug, versionSlug);
+
+        assertThat(result, Matchers.hasSize(3));
+
+        assertThat(result.get(0).getLocaleId(), Matchers.equalTo(LocaleId.FR));
+        assertThat(result.get(1).getLocaleId(), Matchers.equalTo(LocaleId.EN));
+        assertThat(result.get(2).getLocaleId(), Matchers.equalTo(LocaleId.DE));
+    }
+
 }
--- zanata-war/src/test/java/org/zanata/webtrans/server/rpc/GetLocaleListHandlerTest.java
+++ zanata-war/src/test/java/org/zanata/webtrans/server/rpc/GetLocaleListHandlerTest.java
@@ -1,15 +1,6 @@
 package org.zanata.webtrans.server.rpc;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.util.List;
-import java.util.Set;
-
-import lombok.extern.slf4j.Slf4j;
 
 import org.hamcrest.Matchers;
 import org.mockito.Mock;
@@ -17,24 +8,22 @@ import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.zanata.common.LocaleId;
-import org.zanata.common.ProjectType;
-import org.zanata.dao.DocumentDAO;
-import org.zanata.dao.LocaleDAO;
-import org.zanata.dao.ProjectDAO;
-import org.zanata.dao.ProjectIterationDAO;
 import org.zanata.model.HLocale;
-import org.zanata.model.HProject;
-import org.zanata.model.HProjectIteration;
 import org.zanata.model.TestFixture;
 import org.zanata.seam.SeamAutowire;
 import org.zanata.security.ZanataIdentity;
-import org.zanata.webtrans.shared.model.DocumentInfo;
-import org.zanata.webtrans.shared.model.Locale;
+import org.zanata.service.impl.LocaleServiceImpl;
 import org.zanata.webtrans.shared.model.WorkspaceId;
 import org.zanata.webtrans.shared.rpc.GetLocaleList;
 import org.zanata.webtrans.shared.rpc.GetLocaleListResult;
+import com.google.common.collect.Lists;
 
-@Test(groups = {"jpa-tests"})
+import lombok.extern.slf4j.Slf4j;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@Test(groups = { "jpa-tests" })
 @Slf4j
 public class GetLocaleListHandlerTest {
     private GetLocaleListHandler handler;
@@ -43,19 +32,7 @@ public class GetLocaleListHandlerTest {
     private ZanataIdentity identity;
 
     @Mock
-    private LocaleDAO localeDAO;
-
-    @Mock
-    private ProjectIterationDAO projectIterationDAO;
-
-    @Mock
-    private ProjectDAO projectDAO;
-
-    @Mock
-    private HProjectIteration hProjectIteration;
-
-    @Mock
-    private HProject hProject;
+    private LocaleServiceImpl localeServiceImpl;
 
     private GetLocaleList action;
 
@@ -66,9 +43,7 @@ public class GetLocaleListHandlerTest {
         handler = SeamAutowire.instance()
                 .reset()
                 .use("identity", identity)
-                .use("projectIterationDAO", projectIterationDAO)
-                .use("projectDAO", projectDAO)
-                .use("localeDAO", localeDAO)
+                .use("localeServiceImpl", localeServiceImpl)
                 .ignoreNonResolvable()
                 .autowire(GetLocaleListHandler.class);
         // @formatter:on
@@ -76,74 +51,30 @@ public class GetLocaleListHandlerTest {
         WorkspaceId workspaceId = TestFixture.workspaceId();
         action = new GetLocaleList();
         action.setWorkspaceId(workspaceId);
-
-        Set<HLocale> iterationLocales = new HashSet<HLocale>();
-        iterationLocales.add(new HLocale(LocaleId.EN_US));
-        iterationLocales.add(new HLocale(LocaleId.DE));
-        when(hProjectIteration.getId()).thenReturn(1L);
-        when(hProjectIteration.getCustomizedLocales())
-                .thenReturn(iterationLocales);
-        when(projectIterationDAO.getBySlug("project", "master"))
-                .thenReturn(hProjectIteration);
-
-        Set<HLocale> projectLocales = new HashSet<HLocale>();
-        projectLocales.add(new HLocale(LocaleId.ES));
-        when(hProject.getId()).thenReturn(1L);
-        when(hProject.getCustomizedLocales()).thenReturn(projectLocales);
-        when(projectDAO.getBySlug("project")).thenReturn(hProject);
-
-        List<HLocale> defaultLocales = new ArrayList<HLocale>();
-        defaultLocales.add(new HLocale(LocaleId.FR));
-        defaultLocales.add(new HLocale(LocaleId.EN));
-        defaultLocales.add(new HLocale(LocaleId.DE));
-        when(localeDAO.findAllActiveAndEnabledByDefault())
-                .thenReturn(defaultLocales);
+        when(
+                localeServiceImpl.getSupportedLanguageByProjectIteration(
+                        "project", "master")).thenReturn(getHLocaleList());
     }
 
     @Test
     public void testExecute() throws Exception {
-        when(hProjectIteration.isOverrideLocales()).thenReturn(false);
-        when(hProject.isOverrideLocales()).thenReturn(false);
-
         GetLocaleListResult result = handler.execute(action, null);
         verify(identity).checkLoggedIn();
-        assertThat(result.getLocales(), Matchers.hasSize(3));
-
-        assertThat(result.getLocales().get(0).getId().getLocaleId(),
-                Matchers.equalTo(LocaleId.FR));
-        assertThat(result.getLocales().get(1).getId().getLocaleId(),
-                Matchers.equalTo(LocaleId.EN));
-        assertThat(result.getLocales().get(2).getId().getLocaleId(),
-                Matchers.equalTo(LocaleId.DE));
-    }
-
-    @Test
-    public void testExecuteWithOverriddenProjectLocales() throws Exception {
-        when(hProject.isOverrideLocales()).thenReturn(true);
-
-        GetLocaleListResult result = handler.execute(action, null);
-        verify(identity).checkLoggedIn();
-        assertThat(result.getLocales(), Matchers.hasSize(1));
-        assertThat(result.getLocales().get(0).getId().getLocaleId(),
-                Matchers.equalTo(LocaleId.ES));
-    }
-
-    @Test
-    public void testExecuteWithOverriddenIterationLocales() throws Exception {
-        when(hProject.isOverrideLocales()).thenReturn(true);
-        when(hProjectIteration.isOverrideLocales()).thenReturn(true);
-
-        GetLocaleListResult result = handler.execute(action, null);
-        verify(identity).checkLoggedIn();
-        assertThat(result.getLocales(), Matchers.hasSize(2));
-        assertThat(result.getLocales().get(0).getId().getLocaleId(),
-                Matchers.equalTo(LocaleId.DE));
-        assertThat(result.getLocales().get(1).getId().getLocaleId(),
-                Matchers.equalTo(LocaleId.EN_US));
+        assertThat(result.getLocales(), Matchers.hasSize(5));
     }
 
     @Test
     public void testRollback() throws Exception {
         handler.rollback(null, null, null);
+    }
+
+    private List<HLocale> getHLocaleList() {
+        List<HLocale> iterationLocales = Lists.newArrayList();
+        iterationLocales.add(new HLocale(LocaleId.EN_US));
+        iterationLocales.add(new HLocale(LocaleId.DE));
+        iterationLocales.add(new HLocale(LocaleId.FR));
+        iterationLocales.add(new HLocale(LocaleId.EN));
+        iterationLocales.add(new HLocale(LocaleId.DE));
+        return iterationLocales;
     }
 }
