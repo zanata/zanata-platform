7cd98d15239fecd2c1f4fff65b7c94c47f348a61
Alex Eng <aeng@redhat.com>
2014-06-04 09:12:30 +1000

    Remove unused images and seam component

--- zanata-war/src/main/java/org/zanata/webtrans/client/resources/Resources.java
+++ zanata-war/src/main/java/org/zanata/webtrans/client/resources/Resources.java
@@ -27,9 +27,6 @@ import com.google.gwt.resources.client.ImageResource;
  * Resources used by the entire application.
  */
 public interface Resources extends ClientBundle {
-    @Source("images/crystal_project/_16x16/filesystems/file_doc.png")
-    ImageResource documentImage();
-
     @Source("images/loading.gif")
     ImageResource spinner();
 
