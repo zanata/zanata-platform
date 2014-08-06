1c97e18b139ab1eb3df8216b0cb37360fa2e2257
David Mason <dr.d.mason@gmail.com>
2014-08-06 15:04:51 +1000

    add function to get supported source types for a ProjectType

--- zanata-common-api/src/main/java/org/zanata/common/ProjectType.java
+++ zanata-common-api/src/main/java/org/zanata/common/ProjectType.java
@@ -2,6 +2,11 @@ package org.zanata.common;
 
 import javax.xml.bind.annotation.XmlEnum;
 import javax.xml.bind.annotation.XmlType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.zanata.common.DocumentType.*;
 
 @XmlType(name = "projectTypeType")
 @XmlEnum(String.class)
@@ -28,5 +33,32 @@ public enum ProjectType {
                     + "' no longer supported, use 'File' instead");
         }
         throw new Exception("Project type '" + projectType + "' not supported");
+    }
+
+    public static List<String> getSupportedSourceFileTypes(ProjectType type) {
+        if (type != null) {
+            switch (type) {
+                case Gettext:
+                case Podir:
+                    return GETTEXT_PORTABLE_OBJECT_TEMPLATE.getExtensions();
+                case File:
+                    return fileProjectSourceFileTypes();
+                default:
+                    break;
+            }
+        }
+        return Arrays.asList();
+    }
+
+    private static List<String> fileProjectSourceFileTypes() {
+        List<DocumentType> supported = Arrays.asList(XML_DOCUMENT_TYPE_DEFINITION,
+                PLAIN_TEXT, IDML, HTML, OPEN_DOCUMENT_TEXT, OPEN_DOCUMENT_PRESENTATION,
+                OPEN_DOCUMENT_GRAPHICS, SUBTITLE);
+
+        List<String> extensions = new ArrayList<String>();
+        for (DocumentType type : supported) {
+            extensions.addAll(type.getExtensions());
+        }
+        return  extensions;
     }
 }
