5a91201d231a6091dc64d3d100fbcb87b12c7087
Damian Jansen <djansen@redhat.com>
2015-03-06 13:10:13 +1000

    Add test for setting a project language alias

--- functional-test/src/main/java/org/zanata/util/LanguageList.java
+++ functional-test/src/main/java/org/zanata/util/LanguageList.java
@@ -23,6 +23,7 @@ package org.zanata.util;
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
@@ -36,7 +37,12 @@ public class LanguageList {
     private LanguageList(){}
 
     private static By localeId = By.className("js-locale-id");
+    private static By localeAlias = By.className("js-locale-alias");
     private static By languageActions = By.className("button--group");
+    private static By dropdown = By.className("i--arrow-down");
+    private static By aliasInput = By.className("form--inline__input");
+    private static By setAlias = By.className("form--inline__addon");
+    private static By addAliasLink = By.linkText("Add alias");
 
     public static List<String> getListedLocales(WebElement localeList) {
         return Lists.transform(getListElements(localeList),
@@ -61,18 +67,51 @@ public class LanguageList {
                 .findElements(By.tagName("button")).get(0).click();
     }
 
-    public static boolean toggleLanguageInList(WebElement localeList, String localeId) {
-        boolean clickedLocale = false;
+    public static boolean toggleLanguageInList(WebElement localeList,
+            String locale) {
+        WebElement localeItem = getLocaleEntry(localeList, locale);
+        try {
+            LanguageList.toggleEnabled(localeItem);
+        } catch (NoSuchElementException nsee) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void clickActionsDropdown(WebElement list, String locale) {
+        getLocaleEntry(list, locale).findElement(dropdown).click();
+    }
+
+    public static void clickAddAlias(WebElement list, String locale) {
+        getLocaleEntry(list, locale).findElement(addAliasLink).click();
+    }
+
+    public static void enterAlias(WebElement localeList, String locale,
+            String alias) {
+        getLocaleEntry(localeList, locale).findElement(aliasInput)
+                .sendKeys(alias);
+    }
+
+    public static void setAlias(WebElement localeList, String locale) {
+        getLocaleEntry(localeList, locale).findElement(setAlias).click();
+    }
+
+    public static String getAliasForLocale(WebElement list, String locale) {
+        return getLocaleEntry(list, locale).findElement(localeAlias)
+                .getText();
+    }
+
+    private static WebElement getLocaleEntry(WebElement list, String locale) {
         List<WebElement> listElements = LanguageList
-                .getListElements(localeList);
+                .getListElements(list);
         for (WebElement localeLi : listElements) {
-            if (LanguageList.getLocaleID(localeLi).equals(localeId)) {
-                LanguageList.toggleEnabled(localeLi);
-                clickedLocale = true;
-                break;
+            if (LanguageList.getLocaleID(localeLi).equals(locale)) {
+                return localeLi;
             }
         }
-        return clickedLocale;
+        throw new NoSuchElementException("Unable to find locale " + locale);
+
     }
+
 }
 
