f11fcc5a8e9c4983b96ff4e120b46ac6a230070d
Patrick Huang <pahuang@redhat.com>
2015-06-17 10:38:58 +1000

    add @JsonIgnore to properties where they have @XmlTrancient and add DTO test

--- zanata-common-api/src/test/java/org/zanata/rest/dto/JsonSchemaTest.java
+++ zanata-common-api/src/test/java/org/zanata/rest/dto/JsonSchemaTest.java
@@ -1,8 +1,11 @@
 package org.zanata.rest.dto;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 
+import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.introspect.JacksonAnnotationIntrospector;
@@ -13,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.zanata.common.TransUnitCount;
 import org.zanata.rest.dto.extensions.comment.SimpleComment;
 import org.zanata.rest.dto.extensions.gettext.HeaderEntry;
 import org.zanata.rest.dto.extensions.gettext.PoHeader;
@@ -23,6 +27,8 @@ import org.zanata.rest.dto.resource.ResourceMeta;
 import org.zanata.rest.dto.resource.TextFlow;
 import org.zanata.rest.dto.resource.TextFlowTarget;
 import org.zanata.rest.dto.resource.TranslationsResource;
+import org.zanata.rest.dto.stats.ContainerTranslationStatistics;
+import org.zanata.rest.dto.stats.TranslationStatistics;
 import org.zanata.rest.dto.stats.contribution.ContributionStatistics;
 import org.zanata.rest.dto.stats.contribution.LocaleStatistics;
 
@@ -79,12 +85,10 @@ public class JsonSchemaTest {
                 { TranslationsResource.class },
                 { ContributionStatistics.class },
                 { LocaleStatistics.class },
-                // this one cuases stack overflow in schema generation
+                // this one cuases stack overflow in schema generation. Will
+                // cover it in DTOSampleMarshallingTest
                 // { ContainerTranslationStatistics.class },
-                // This one has @XmlTransient on some of the getters and jackson
-                // will pick them up if JAXB annotation is used. There is no way
-                // we can be backward compatible (except turning jaxb off).
-                // { TranslationStatistics.class },
+                { TranslationStatistics.class },
                 { Account.class },
                 { CopyTransStatus.class },
                 { Glossary.class },
