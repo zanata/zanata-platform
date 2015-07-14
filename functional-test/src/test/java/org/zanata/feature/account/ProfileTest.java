f2a354f8cbb04449903bbf5dffa0f4e0d065e5ab
Damian Jansen <djansen@redhat.com>
2015-07-14 13:51:05 +1000

    Ignore poorly executing API key test

--- functional-test/src/test/java/org/zanata/feature/account/ProfileTest.java
+++ functional-test/src/test/java/org/zanata/feature/account/ProfileTest.java
@@ -80,6 +80,7 @@ public class ProfileTest extends ZanataTestCase {
     @Feature(summary = "The user can change their API key",
             tcmsTestPlanIds = 5316, tcmsTestCaseIds = 0)
     @Test(timeout = ZanataTestCase.MAX_SHORT_TEST_DURATION)
+    @Ignore("Procedure call tracking appears to be flaky in this test")
     public void changeUsersApiKey() throws Exception {
         DashboardClientTab dashboardClientTab = new LoginWorkFlow()
                 .signIn("translator", "translator")
