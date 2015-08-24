5ccb3b73bf9bc4aff10036554dbd673922aa96c0
Alex Eng <aeng@redhat.com>
2015-08-24 12:25:13 +1000

    fix(version language): Fix concurrent map modification
    https://zanata.atlassian.net/browse/ZNTA-643

--- zanata-war/src/main/java/org/zanata/action/VersionHome.java
+++ zanata-war/src/main/java/org/zanata/action/VersionHome.java
@@ -722,7 +722,7 @@ public class VersionHome extends SlugHome<HProjectIteration> implements
                     msgs.get("jsf.LocaleAlias.NoAliasesToRemove"));
         } else if (removed.size() == 1) {
             facesMessages.addGlobal(FacesMessage.SEVERITY_INFO,
-                    msgs.format("jsf.LocaleAlias.AliasRemoved", removed.get(0)));
+                msgs.format("jsf.LocaleAlias.AliasRemoved", removed.get(0)));
         } else {
             facesMessages.addGlobal(FacesMessage.SEVERITY_INFO,
                     msgs.format("jsf.LocaleAlias.AliasesRemoved",
@@ -768,7 +768,7 @@ public class VersionHome extends SlugHome<HProjectIteration> implements
                         msgs.format("jsf.LocaleAlias.AliasRemoved", localeId));
             } else {
                 facesMessages.addGlobal(FacesMessage.SEVERITY_INFO,
-                        msgs.format("jsf.LocaleAlias.NoAliasToRemove", localeId));
+                    msgs.format("jsf.LocaleAlias.NoAliasToRemove", localeId));
             }
         } else {
             facesMessages.addGlobal(FacesMessage.SEVERITY_INFO,
@@ -847,14 +847,19 @@ public class VersionHome extends SlugHome<HProjectIteration> implements
 
     @Restrict("#{s:hasPermission(versionHome.instance, 'update')}")
     public void disableSelectedLocales() {
-        List<LocaleId> removed = new ArrayList<>();
+        List<LocaleId> toRemove = Lists.newArrayList();
         for (Map.Entry<LocaleId, Boolean> entry :
             getSelectedEnabledLocales().entrySet()) {
             if (entry.getValue()) {
-                boolean wasEnabled = disableLocaleSilently(entry.getKey());
-                if (wasEnabled) {
-                    removed.add(entry.getKey());
-                }
+                toRemove.add(entry.getKey());
+            }
+        }
+
+        List<LocaleId> removed = Lists.newArrayList();
+        for(LocaleId localeId: toRemove) {
+            boolean wasEnabled = disableLocaleSilently(localeId);
+            if (wasEnabled) {
+                removed.add(localeId);
             }
         }
 
