b43f28cc7df9c5f2406775c5e8588ed86cec2eb2
Carlos A. Munoz <camunoz@redhat.com>
2015-08-25 20:28:25 +1000

    Add triggers to postgresql baseline.
    Also remove unnecessary data migration script.

--- zanata-war/src/main/resources/db/postgresql/postgresql_baseline.sql
+++ zanata-war/src/main/resources/db/postgresql/postgresql_baseline.sql
@@ -713,3 +713,15 @@ CREATE INDEX FK_TransUnitVariant_TransUnit ON TransMemoryUnitVariant (trans_unit
 ALTER TABLE TransMemory_Metadata ADD FOREIGN KEY (trans_memory_id) REFERENCES TransMemory (id);
 ALTER TABLE WebHook ADD FOREIGN KEY (projectId) REFERENCES HProject (id);
 CREATE INDEX FK_WebHook_HProject ON WebHook (projectId);
+
+CREATE FUNCTION add_document_history() RETURNS trigger AS $add_document_history$
+   BEGIN
+      IF NEW.revision != OLD.revision THEN
+         INSERT INTO HDocumentHistory(document_id,revision,contentType,docId,locale,name,path,lastChanged,last_modified_by_id,obsolete)
+            VALUES (OLD.id,OLD.revision,OLD.contentType,OLD.docId,OLD.locale,OLD.name,OLD.path,OLD.lastChanged,OLD.last_modified_by_id,OLD.obsolete);
+      END IF;
+   END;
+$add_document_history$ LANGUAGE plpgsql;
+
+CREATE TRIGGER HDocument_Update BEFORE UPDATE ON HDocument
+   FOR EACH ROW EXECUTE PROCEDURE add_document_history();
