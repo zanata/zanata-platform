5ba943c6ca5c3e1266c4b55beee1a222a67c69c6
Carlos A. Munoz <camunoz@redhat.com>
2015-09-24 11:08:23 +1000

    fix: add loader to user profile page
    Squashed commit of the following:
    
    commit f643afe0006984a08cb604662a61fa880a304562
    Author: Carlos A. Munoz <camunoz@redhat.com>
    Date:   Thu Sep 24 09:08:09 2015 +1000
    
        fix: Loader not updating properly after data load
    
    commit 2c1290db1b1a1b944feb71ac609a55a53ae83494
    Author: Carlos A. Munoz <camunoz@redhat.com>
    Date:   Mon Sep 21 14:49:31 2015 +1000
    
        fix: add loader to user profile page
    
        Loader is added when the page is loading data so as to not show a blank page.

--- frontend/src/main/web/user-profile-page/lib/components/RecentContributions.jsx
+++ frontend/src/main/web/user-profile-page/lib/components/RecentContributions.jsx
@@ -31,6 +31,27 @@ var RecentContributions = React.createClass(
     render: function() {
       var dateRange = this.state.dateRange;
 
+      var loader = (
+        <a href="#" className="loader--large is-active">
+          <span className="loader__spinner">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </a>);
+      var chart = this.state.loading ? loader : (
+        <ContributionChart wordCountForEachDay={this.state.matrixForAllDays}
+          dateRangeOption={this.state.dateRangeOption} />);
+      var matrix = this.state.loading ? undefined : (
+        <FilterableMatrixTable
+          wordCountForSelectedDay={this.state.wordCountsForSelectedDayFilteredByContentState}
+          wordCountForEachDay={this.state.wordCountsForEachDayFilteredByContentState}
+          fromDate={dateRange['fromDate']} toDate={dateRange['toDate']}
+          dateRangeOption={this.state.dateRangeOption}
+          selectedContentState={this.state.contentStateOption}
+          selectedDay={this.state.selectedDay}
+          />);
+
       return (
         <div className="l__wrapper">
           <div className="l--push-bottom-1">
@@ -40,16 +61,9 @@ var RecentContributions = React.createClass(
             <h2 className='delta txt--uppercase'>Recent Contributions</h2>
           </div>
           <div className="l--push-bottom-1">
-            <ContributionChart wordCountForEachDay={this.state.matrixForAllDays} dateRangeOption={this.state.dateRangeOption} />
+            {chart}
           </div>
-          <FilterableMatrixTable
-            wordCountForSelectedDay={this.state.wordCountsForSelectedDayFilteredByContentState}
-            wordCountForEachDay={this.state.wordCountsForEachDayFilteredByContentState}
-            fromDate={dateRange['fromDate']} toDate={dateRange['toDate']}
-            dateRangeOption={this.state.dateRangeOption}
-            selectedContentState={this.state.contentStateOption}
-            selectedDay={this.state.selectedDay}
-          />
+          {matrix}
         </div>
       )
     }
