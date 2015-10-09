33d6a723f211549039d67a0c1027a1d48f628635
Carlos A. Munoz <camunoz@redhat.com>
2015-10-09 12:14:11 +1000

    modify transaction util for CDI compatibility

--- zanata-war/src/main/java/org/zanata/transaction/TransactionUtil.java
+++ zanata-war/src/main/java/org/zanata/transaction/TransactionUtil.java
@@ -20,19 +20,13 @@
  */
 package org.zanata.transaction;
 
-import static org.jboss.seam.util.EJB.APPLICATION_EXCEPTION;
-import static org.jboss.seam.util.EJB.rollback;
-
 import java.util.concurrent.Callable;
-
-import javax.transaction.Status;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.jboss.seam.annotations.ApplicationException;
-import org.jboss.seam.transaction.Transaction;
-import org.jboss.seam.transaction.UserTransaction;
-import org.jboss.seam.util.JSF;
+import org.apache.deltaspike.jpa.api.transaction.Transactional;
+import org.zanata.util.BeanHolder;
+import org.zanata.util.ServiceLocator;
 
 /**
  * Utility class to help with transactions.
@@ -56,62 +50,11 @@ public class TransactionUtil {
      *             Exception (if any) thrown by the given function.
      */
     public static <R> R runInTransaction(Callable<R> function) throws Exception {
-        UserTransaction transaction = null;
-        boolean transactionActive = false;
-        boolean newTransactionRequired = false;
-        UserTransaction userTransaction = null;
-
-        try {
-            transaction = Transaction.instance();
-
-            transactionActive = transaction.isActiveOrMarkedRollback()
-                    || transaction.isRolledBack(); // TODO: temp workaround,
-                                                   // what should we really do
-                                                   // in this case??
-            newTransactionRequired =
-                    isNewTransactionRequired(transactionActive);
-            userTransaction = newTransactionRequired ? transaction : null;
-        } catch (IllegalStateException e) {
-            // for shutdown case, when we can't get the tx object because the
-            // event context is gone
-            // but we should still check if a tx is required and fail
-            // accordingly if it is
-            newTransactionRequired = isNewTransactionRequired(false);
-            if (newTransactionRequired) {
-                throw e;
-            }
-        }
-
-        try {
-            if (newTransactionRequired) {
-                log.debug("beginning transaction");
-                userTransaction.begin();
-            }
-
-            R result = function.call();
-            if (newTransactionRequired) {
-                if (transaction.isMarkedRollback()) {
-                    log.debug("rolling back transaction");
-                    userTransaction.rollback();
-                } else {
-                    log.debug("committing transaction");
-                    userTransaction.commit();
-                }
-            }
-            return result;
-        } catch (Exception e) {
-            if (newTransactionRequired
-                    && userTransaction.getStatus() != Status.STATUS_NO_TRANSACTION) {
-                if (isRollbackRequired(e, true)) {
-                    log.debug("rolling back transaction");
-                    userTransaction.rollback();
-                } else {
-                    log.debug("committing transaction after ApplicationException(rollback=false):"
-                            + e.getMessage());
-                    userTransaction.commit();
-                }
-            }
-            throw e;
+        try (BeanHolder<TransactionalExecutor> txExecutor =
+                ServiceLocator.instance()
+                        .getDependent(TransactionalExecutor.class)) {
+            return txExecutor.get()
+                    .runInTransaction(function);
         }
     }
 
@@ -132,31 +75,10 @@ public class TransactionUtil {
         });
     }
 
-    protected static boolean
-            isNewTransactionRequired(boolean transactionActive) {
-        return !transactionActive;
-    }
-
-    public static boolean isRollbackRequired(Exception e, boolean isJavaBean) {
-        Class<? extends Exception> clazz = e.getClass();
-        return (isSystemException(e, isJavaBean, clazz))
-                ||
-                (isJavaBean && clazz.isAnnotationPresent(APPLICATION_EXCEPTION) && rollback(clazz
-                        .getAnnotation(APPLICATION_EXCEPTION)))
-                ||
-                (clazz.isAnnotationPresent(ApplicationException.class) && clazz
-                        .getAnnotation(ApplicationException.class).rollback());
-    }
-
-    private static boolean isSystemException(Exception e, boolean isJavaBean,
-            Class<? extends Exception> clazz) {
-        return isJavaBean &&
-                (e instanceof RuntimeException) &&
-                !clazz.isAnnotationPresent(APPLICATION_EXCEPTION) &&
-                !clazz.isAnnotationPresent(ApplicationException.class) &&
-                // TODO: this is hackish, maybe just turn off RollackInterceptor
-                // for @Converter/@Validator components
-                !JSF.VALIDATOR_EXCEPTION.isInstance(e) &&
-                !JSF.CONVERTER_EXCEPTION.isInstance(e);
+    public static class TransactionalExecutor {
+        @Transactional
+        public <R> R runInTransaction(Callable<R> function) throws Exception {
+            return function.call();
+        }
     }
 }
