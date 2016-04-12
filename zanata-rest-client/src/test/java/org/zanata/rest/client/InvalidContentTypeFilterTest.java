d4c8843321d5436b2f3872b9c1b1ce1376c0c443
Sean Flanigan <sflaniga@redhat.com>
2016-04-12 14:29:23 +1000

    Handle charset as part of Content-Type headers (#110)

--- zanata-rest-client/src/test/java/org/zanata/rest/client/InvalidContentTypeFilterTest.java
+++ zanata-rest-client/src/test/java/org/zanata/rest/client/InvalidContentTypeFilterTest.java
@@ -9,10 +9,12 @@ import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.*;
 import static org.zanata.rest.client.InvalidContentTypeFilter.isContentTypeCompatible;
 
 public class InvalidContentTypeFilterTest {
+    // WildFly 8 and 9 have been known to add ";charset=ISO-8859-1" to
+    // Content-Type headers.
+    private static final String LATIN_1 = "ISO-8859-1";
 
     private String sampleText =
             "<!DOCTYPE composition PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
@@ -61,8 +63,23 @@ public class InvalidContentTypeFilterTest {
         MatcherAssert.assertThat(
                 isContentTypeCompatible(MediaType.WILDCARD_TYPE),
                 equalTo(true));
+
+        MediaType zanataXmlType = new MediaType(
+                "application", "vnd.zanata+xml");
+        MatcherAssert.assertThat(isContentTypeCompatible(zanataXmlType),
+                equalTo(true));
         MatcherAssert.assertThat(isContentTypeCompatible(
-                new MediaType("application", "vnd.zanata+xml")), equalTo(true));
+                zanataXmlType.withCharset(LATIN_1)),
+                equalTo(true));
+
+        MediaType zanataVersionXmlType = new MediaType(
+                "application", "vnd.zanata.version+xml");
+        MatcherAssert.assertThat(isContentTypeCompatible(zanataVersionXmlType),
+                equalTo(true));
+        MatcherAssert.assertThat(isContentTypeCompatible(
+                zanataVersionXmlType.withCharset(LATIN_1)),
+                equalTo(true));
+
         MatcherAssert.assertThat(
                 isContentTypeCompatible(MediaType.TEXT_PLAIN_TYPE),
                 equalTo(true));
