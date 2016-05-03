7c5635e7eab50e06498e816dcb252492691b6f0e
Sean Flanigan <sflaniga@redhat.com>
2016-05-03 15:43:19 +1000

    Control CORS with system property

--- zanata-war/src/main/java/org/zanata/rest/ZanataRestResponseInterceptor.java
+++ zanata-war/src/main/java/org/zanata/rest/ZanataRestResponseInterceptor.java
@@ -29,34 +29,85 @@ import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
+import com.google.common.collect.ImmutableList;
 import org.jboss.resteasy.annotations.interception.HeaderDecoratorPrecedence;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 
+import static org.apache.commons.lang.StringUtils.isEmpty;
+
 /**
- * This Interceptor is a POST process for Zanata REST API request.
- * It support Cross-Origin resource sharing (CORS) by enabling Access-Control header in
- * response header. CORS is needed for request from different domain.
- *
+ * This Interceptor is a filter for Zanata REST API requests.
+ * It supports <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS">
+ * Cross-Origin resource sharing (CORS)</a> by adding {@code Access-Control}
+ * headers to REST responses. CORS is needed for requests from different
+ * domains, eg when testing alternative front-ends to Zanata.
+ * <p>
+ * By default, cross origin support is disabled, but the server administrator
+ * can enable it by setting the system property zanata.originWhitelist. For
+ * instance, in standalone.xml:
+ * <pre>
+ *   {@code <property name="zanata.originWhitelist" value="http://localhost:8000" />}
+ * </pre>
+ * </p>
  * @author Alex Eng <a href="mailto:aeng@redhat.com">aeng@redhat.com</a>
+ * @author Sean Flanigan <a
+ *         href="mailto:sflaniga@redhat.com">sflaniga@redhat.com</a>
  */
 @Provider
 @HeaderDecoratorPrecedence
+//
+//
+//
+// WARNING: Misuse of CORS headers can expose Zanata to CSRF attacks.
+// Please read up on CORS and CSRF before changing this file!
+//
+//
+//
 public class ZanataRestResponseInterceptor implements ContainerResponseFilter {
+    private static final String ZANATA_ORIGIN_WHITELIST =
+            "zanata.originWhitelist";
+    private static final String ALLOW_METHODS =
+            "PUT, POST, DELETE, GET, OPTIONS";
+    private static final ImmutableList<String> originWhitelist;
+
+    static {
+        String whitelist = System.getProperty(ZANATA_ORIGIN_WHITELIST, "");
+        originWhitelist = ImmutableList.copyOf(whitelist.split(" +"));
+    }
+
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
         throws IOException {
+        MultivaluedMap<String, String> requestHeaders =
+                requestContext.getHeaders();
 
-        List<String> requestHeaders = requestContext.getHeaders().get("Access-Control-Request-Headers");
-        if(requestHeaders == null) {
-            requestHeaders = Lists.newArrayList();
+        // Client will use these headers for the next request (assuming this is
+        // a pre-flight request).
+        List<String> nextRequestHeaders = requestHeaders.get("Access-Control-Request-Headers");
+        if (nextRequestHeaders == null) {
+            nextRequestHeaders = Lists.newArrayList();
         }
-        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        MultivaluedMap<String, Object> responseHeaders = responseContext.getHeaders();
 
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Methods", requestContext.getMethod());
-        headers.add("Access-Control-Allow-Headers",
+        // Allow the specified Origin, but only if it is whitelisted.
+        String origin = requestContext.getHeaderString("Origin");
+        if (!isEmpty(origin) && originWhitelist.contains(origin)) {
+            responseHeaders.add("Access-Control-Allow-Origin", origin);
+            responseHeaders.add("Vary", "Origin");
+        }
+
+        // Allow standard HTTP methods.
+        responseHeaders.add("Access-Control-Allow-Methods", ALLOW_METHODS);
+
+        // Allow credentials in requests (eg session cookie).
+        // This is potentially very dangerous, so check your Origin!
+        responseHeaders.add("Access-Control-Allow-Credentials", true);
+
+        // Allow any requested headers. Again, check your Origin!
+        responseHeaders.add("Access-Control-Allow-Headers",
             "X-Requested-With, Content-Type, Accept, " + Joiner.on(",").join(
-                requestHeaders));
+                nextRequestHeaders));
     }
+
 }
