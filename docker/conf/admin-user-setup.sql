87db45584fa0be5fb06bb5f39ecc02fa8856c3ab
Sean Flanigan <sflaniga@redhat.com>
2016-07-18 11:10:11 +1000

    docker: add api key for admin user

--- docker/conf/admin-user-setup.sql
+++ docker/conf/admin-user-setup.sql
@@ -1,7 +1,7 @@
 -- Username: admin
 -- Password: admin1234
-INSERT INTO HAccount (id, creationDate, lastChanged, versionNum, enabled, passwordHash, username)
-VALUES (1, now(), now(), 1, 1, 'UZMf4PIqtTBGAo9wWKuTpg==', 'admin');
+INSERT INTO HAccount (id, creationDate, lastChanged, versionNum, apiKey, enabled, passwordHash, username)
+VALUES (1, now(), now(), 1, 'b6d7044e9ee3b2447c28fb7c50d86d98', 1, 'UZMf4PIqtTBGAo9wWKuTpg==', 'admin');
 
 INSERT INTO HPerson (id, creationDate, lastChanged, versionNum, email, `name`, accountId)
 VALUES (1, now(), now(), 1, 'admin@noreply.org', 'Administrator', 1);
