1ef6d9da83a436a2d0332d77597fb0d7e1e3ed47
kathryngo <kgough@redhat.com>
2016-09-07 11:29:17 +1000

    added button and table cells

--- zanata-frontend/src/frontend/app/containers/ManageSearch/index.js
+++ zanata-frontend/src/frontend/app/containers/ManageSearch/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Button, Panel} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
 import Helmet from 'react-helmet'
 import {Page, ScrollView, View} from 'zanata-ui'
 
@@ -8,16 +8,6 @@ const contentViewContainerTheme = {
     w: 'W(100%)'
   }
 }
-const title = (
-  <h3>Panel title</h3>
-)
-const panelsInstance = (
-  <div>
-    <Panel header={title}>
-      <div className='panel-body'>Panel content</div>
-    </Panel>
-  </div>
-)
 
 class ManageSearch extends Component {
 
@@ -32,98 +22,68 @@ class ManageSearch extends Component {
                 <div className='contentx clearfix center-block'>
                   <h2>Manage Search
                   </h2>
-                  <panelsInstance>
-                    <Panel bsStyle='primary' header={title}>
-                        Panel content
-                    </Panel>
-                  </panelsInstance>
+                  <div className='panel panel-primary'>
+                    <div className='panel-heading'>
+                      <h3 className='panel-title'>Current progress</h3>
+                    </div>
+                    <div className='panel-body'>
+                      No operations are running
+                    </div>
+                  </div>
                   <div className='left-form tablepadding col-xs-12'>
                     <table className='table'>
                       <thead>
-                        <tr className='hidden'>
-                          <th>Username</th>
-                          <th>&nbsp;</th>
+                        <tr>
+                          <th>Table</th>
+                          <th>Purge index</th>
+                          <th>Reindex</th>
+                          <th>Optimise</th>
                         </tr>
                       </thead>
                       <tbody>
                         <tr>
-                          <td>
-                            <a href=''>admin
-                            </a>
-                            <br />
-                            <span className='langcode'>
-                            [user, glossary-admin]</span>
-                          </td>
-                          <td>
-                            <Button bsSize='small'>
-                              <i className='fa fa-times'></i>
-                            Delete</Button>
-                          </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
                         </tr>
                         <tr>
-                          <td>
-                            <a href=''>glossarist</a>
-                          </td>
-                          <td>
-                            <Button bsSize='small'>
-                              <i className='fa fa-times'></i>
-                            Delete</Button>
-                          </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
                         </tr>
                         <tr>
-                          <td>
-                            <a href=''>glossary-admin</a>
-                            <br />
-                            <span className='langcode'>
-                            [glossarist]</span>
-                          </td>
-                          <td>
-                            <Button bsSize='small'>
-                              <i className='fa fa-times'></i>
-                            Delete</Button>
-                          </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
                         </tr>
                         <tr>
-                          <td>
-                            <a href=''>project-creator</a>
-                          </td>
-                          <td>
-                            <Button bsSize='small'>
-                              <i className='fa fa-times'></i>
-                            Delete</Button>
-                          </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
                         </tr>
                         <tr>
-                          <td>
-                            <a href=''>translator</a>
-                            <br />
-                            <span className='langcode'>
-                            ar</span>
-                          </td>
-                          <td>
-                            <Button bsSize='small'>
-                              <i className='fa fa-times'></i>
-                            Delete</Button>
-                          </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
                         </tr>
                         <tr>
-                          <td>
-                            <a href=''>user</a>
-                            <br />
-                            <span className='langcode'>
-                            [project-creator]</span>
-                          </td>
-                          <td>
-                            <Button bsSize='small'>
-                              <i className='fa fa-times'></i>
-                            Delete</Button>
-                          </td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
                         </tr>
                       </tbody>
                     </table>
                   </div>
                 </div>
               </div>
+              <Button bsStyle='primary'>
+              Perform selected actions</Button>
             </div>
           </View>
         </ScrollView>
