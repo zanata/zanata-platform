8fb29b012ad36c6c01eb9c268da694c7964db6ca
David Mason <dr.d.mason@gmail.com>
2016-10-04 14:21:08 +1000

    WIP: serve frontend app from /profile/view/{username}
    Client-side loads the profile properly at that URL.
     - yet to fix all the links to that path
     - changed hashHistory to browserHistory
     - all the hash URLs need to be updated to non-hash URLs

--- server/zanata-frontend/src/frontend/app/containers/Root.js
+++ server/zanata-frontend/src/frontend/app/containers/Root.js
@@ -23,7 +23,7 @@ export default class Root extends Component {
               <Route path='explore' component={Explore} />
               <Route path='glossary' component={Glossary} />
               <Route path='languages' component={Languages} />
-              <Route path='profile/:username' component={UserProfile} />
+              <Route path='profile/view/:username' component={UserProfile} />
               <Route path='project/:projectSlug/glossary'
                 component={Glossary} />
               <Redirect from='profile' to={`profile/${username}`} />
