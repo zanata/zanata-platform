57079baf73b8fabd2b41e4dd5ae59c853656dc22
David Mason <dr.d.mason@gmail.com>
2016-10-05 14:46:20 +1000

    WIP: refactor: delete temp comments

--- zanata-frontend/src/frontend/app/history.js
+++ zanata-frontend/src/frontend/app/history.js
@@ -27,21 +27,6 @@ import { createHistory } from 'history'
  * This is created in a separate module so that other modules can hook in and
  * extend it.
  */
-
-// DONE change to browserHistory
-// DONEset basename for the history to the serving location
-// DONE make sure /profile/view/{username} routes to the right place
-// DONE make sure Profile link on left points to /profile/view/{username}
-// DONE change the default route to go to /profile/view/{username} instead of
-// /profile/{username}
-// TODO fix the explore link/page to work with non-hash history
-// TODO fix the explore components that use hash
-//      (search for user definitely works in master, both logged in and not)
-// TODO fix the profile link in UserTeaser component and make sure it works
-//      (on explore page)
-// TODO make all the other hash history links go to the non-hash places
-//      this includes rewriting to the app URL, and the client-side part
-
 export const history = useRouterHistory(createHistory)({
   basename: window.config.baseUrl
 })
