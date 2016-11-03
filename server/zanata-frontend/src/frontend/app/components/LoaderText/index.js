7431884dcc810add04e283f7a75eaa453147dd47
kgough <kgough@redhat.com>
2016-11-03 12:04:37 +1000

    WIP: loader and loader-text to bootstrap

---
+++ server/zanata-frontend/src/frontend/app/components/LoaderText/index.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react'
+import Loader from '../../components'
+
+const LoaderText = ({
+  children,
+  loading,
+  loadingText,
+  ...props
+}) => {
+  return (
+    <span tagName='span' className='loader-text'
+          componentName='LoaderText'
+      {loading
+        ? <span>
+        {loadingText} <Loader />
+      </span>
+        : children
+      }
+    </span>
+  )
+}
+
+LoaderText.propTypes = {
+  /**
+   * The text/element that is display when there **is no** loading
+   */
+  children: PropTypes.node,
+  /**
+   * The text that is displayed when there **is** loading
+   */
+  loadingText: PropTypes.string,
+  /**
+   * If the component is loading or not
+   */
+  loading: PropTypes.bool,
+}
+
+LoaderText.defaultProps = {
+  loading: false,
+  loadingText: 'Loading'
+}
+
+export default LoaderText
