e2a5a3d77b9fb7f180626ced5c8958932eacb9f9
kgough <kgough@redhat.com>
2016-11-08 14:59:03 +1000

    WIP: test page for components and modal fixes

--- server/zanata-frontend/src/frontend/app/containers/TestComp/index.js
+++ server/zanata-frontend/src/frontend/app/containers/TestComp/index.js
@@ -5,10 +5,10 @@ import { Icon } from '../../components'
 
 class TestComp extends Component {
   open () {
-    this.setState({ show: true })
+    this.setState({ showModal: true })
   }
   close () {
-    this.setState({ show: false })
+    this.setState({ showModal: false })
   }
   render () {
     const tooltip = (
@@ -27,31 +27,26 @@ class TestComp extends Component {
             <Button bsStyle='default'>Test tooltip</Button>
           </OverlayTrigger>
         </ButtonToolbar>
-        <span className='modal-container' style={{height: 200}}>
-          <Button
-            bsStyle='primary'
-            bsSize='large'
-            onClick={::this.open}
-          >
-           Launch demo modal
-          </Button>
+        <Button
+          bsStyle='primary'
+          bsSize='large'
+          onClick={open}
+        >
+          Launch demo modal
+        </Button>
 
-          <Modal show={show} onHide={close}
-            container={this}
-            aria-labelledby='contained-modal-title'
-          >
-            <Modal.Header closeButton>
-              <Modal.Title id='contained-modal-title'>
-              Contained Modal</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
+        <Modal show={show} onHide={close}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+            Contained Modal</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
             Elit est explicabo ipsum eaque dolorem blanditiis doloribus
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={close}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-        </span>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     )
   }
