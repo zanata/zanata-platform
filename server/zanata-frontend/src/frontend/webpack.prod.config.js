122c45366f1577e252ea29e5c9dfe4b39268125c
Alex Eng <aeng@redhat.com>
2016-11-24 10:48:37 +1000

    fix(lint, dependency, generated file)
    - Enable linting
    - Update dependency and shrinkwrap
    - Exclude generated file

--- server/zanata-frontend/src/frontend/webpack.prod.config.js
+++ server/zanata-frontend/src/frontend/webpack.prod.config.js
@@ -18,13 +18,13 @@ module.exports = _.merge({}, defaultConfig, {
     filename: 'frontend.bundle.min.js'
   },
   module: {
-    /* preLoaders: [
+    preLoaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'eslint'
       }
-    ], */
+    ],
     loaders: [
       {
         test: /\.jsx?$/,
