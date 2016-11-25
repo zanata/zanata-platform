85c68a4101eaaf69dec5743f7670ca8707e40f93
kgough <kgough@redhat.com>
2016-11-25 11:18:38 +1000

    WIP: removed unnecessary divs, fixed linting issues

--- server/zanata-frontend/src/frontend/app/containers/Languages/index.js
+++ server/zanata-frontend/src/frontend/app/containers/Languages/index.js
@@ -1,13 +1,15 @@
 import React, {PropTypes, Component} from 'react'
-import { connect } from 'react-redux'
-import { Button, InputGroup, FormGroup, FormControl,
-  Badge, Pagination }
-    from 'react-bootstrap'
+import {connect} from 'react-redux'
+import {
+  Button, InputGroup, FormGroup, FormControl,
+  Badge, Pagination
+}
+  from 'react-bootstrap'
 import Helmet from 'react-helmet'
-import { debounce, find } from 'lodash'
+import {debounce, find} from 'lodash'
 import Entry from './Entry'
 import NewLanguageModal from './NewLanguageModal'
-import { Icon, Notification, LoaderText } from '../../components'
+import {Icon, Notification, LoaderText} from '../../components'
 
 import {
   initialLoad,
@@ -60,112 +62,106 @@ class Languages extends Component {
         )
         }
         <Helmet title='Languages' />
-        <div className='scrollViewTheme'>
-          <div className='contentViewContainer'>
-            <div className=''>
-              <div className=''>
-                <div className='content clearfix center-block'
-                  id='languages-form'>
-                  <h2>
-                    Languages {!loading &&
-                      <Badge className='default'>{totalCount}</Badge>}
-                  </h2>
-                  {permission.canAddLocale &&
-                    <div>
-                      <Button className='primary'
-                        id='btn-language-add-new'
-                        onClick={handleOnDisplayNewLanguage}>
-                        <Icon name='plus'
-                          title='plus' /> Add new language
-                      </Button>
-                      <NewLanguageModal />
-                    </div>
-                  }
-                  <div className='left-form toolbar col-xs-12'>
-                    <div className='search-box col-xs-6 col-sm-8 col-md-6
-                         col-lg-7'>
-                      <FormGroup>
-                        <InputGroup>
-                          <FormControl type='text'
-                            defaultValue={searchText}
-                            onChange={handleOnUpdateSearch} />
-                          <InputGroup.Button>
-                            <Button>
-                              <Icon name='search'
-                                className='s1'
-                                title='search' />&nbsp;
-                            </Button>
-                          </InputGroup.Button>
-                        </InputGroup>
-                      </FormGroup>
-                    </div>
-
-                  {loading
-                      ? <div className='contentViewContainer'>
-                        <LoaderText theme={{ base: { fz: 'Fz(ms1)' } }}
-                          size='2' loading />
-                      </div>
-                      : (<div>
-                        <div className='sort-items col-xs-6 col-sm-4 col-md-4
-                          col-lg-3'>
-                          <FormControl componentClass='select'
-                            className='pull-right' id='sort-options'
-                            onChange={handleOnUpdateSort} value={sort.value}>
-                            {sortOption.map(function (sort, i) {
-                              return <option key={i} value={sort.value}>
-                                {sort.display}</option>
-                            })}
-                          </FormControl>
-                        </div>
-                        <div className='show-items col-xs-5 col-sm-3 col-md-2
-                          col-lg-2'>
-                          <span>Show</span>
-                          <FormControl inline componentClass='select'
-                            onChange={handleOnUpdatePageSize} value={size}
-                            id='page-size-options'>
-                            {pageSizeOption.map(function (value, i) {
-                              return <option key={i} value={value}>
-                                {value}</option>
-                            })}
-                          </FormControl>
-                        </div>
-                        <div className='page-count pull-right col-xs-7 col-sm-8
-                          col-md-12'>
-                          <Pagination
-                            prev
-                            next
-                            bsSize='medium'
-                            items={totalPage}
-                            activePage={page}
-                            onSelect={handlePageChanged} />
-                        </div>
-                      </div>)
-                  }
-                  </div>
-                  {!loading &&
-                    <div className='left-form tablepadding col-xs-12'>
-                      <table className='table' id='languages-table'>
-                        <thead>
-                          <tr className='hidden'>
-                            <th>Language</th>
-                            <th>&nbsp;</th>
-                            <th>&nbsp;</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        {results.map(function (value, i) {
-                          return <Entry key={i} locale={value}
-                            userLanguageTeams={user.languageTeams}
-                            permission={permission}
-                            handleDelete={handleDelete}
-                            isDeleting={deleting} />
-                        })}
-                        </tbody>
-                      </table>
-                    </div>}
-                </div>
+        <div className='scrollViewTheme contentViewContainer'>
+          <div className='content clearfix center-block'
+            id='languages-form'>
+            <h2>
+              Languages {!loading &&
+                <Badge className='default'>{totalCount}</Badge>}
+            </h2>
+            {permission.canAddLocale &&
+              <div>
+                <Button className='primary'
+                  id='btn-language-add-new'
+                  onClick={handleOnDisplayNewLanguage}>
+                  <Icon name='plus'
+                    title='plus' /> Add new language
+                </Button>
+                <NewLanguageModal />
               </div>
+            }
+            <div className='left-form toolbar col-xs-12'>
+              <div className='search-box col-xs-6 col-sm-8 col-md-6
+                         col-lg-7'>
+                <FormGroup>
+                  <InputGroup>
+                    <FormControl type='text'
+                      defaultValue={searchText}
+                      onChange={handleOnUpdateSearch} />
+                    <InputGroup.Button>
+                      <Button>
+                        <Icon name='search'
+                          className='s1'
+                          title='search' />&nbsp;
+                      </Button>
+                    </InputGroup.Button>
+                  </InputGroup>
+                </FormGroup>
+              </div>
+
+              {loading
+                ? <div className='contentViewContainer'>
+                  <LoaderText theme={{base: {fz: 'Fz(ms1)'}}}
+                    size='2' loading />
+                </div>
+                : (<div>
+                  <div className='sort-items col-xs-6 col-sm-4 col-md-4
+                          col-lg-3'>
+                    <FormControl componentClass='select'
+                      className='pull-right' id='sort-options'
+                      onChange={handleOnUpdateSort} value={sort.value}>
+                    {sortOption.map(function (sort, i) {
+                      return <option key={i} value={sort.value}>
+                        {sort.display}</option>
+                    })}
+                    </FormControl>
+                  </div>
+                  <div className='show-items col-xs-5 col-sm-3 col-md-2
+                          col-lg-2'>
+                    <span>Show</span>
+                    <FormControl inline componentClass='select'
+                      onChange={handleOnUpdatePageSize} value={size}
+                      id='page-size-options'>
+                    {pageSizeOption.map(function (value, i) {
+                      return <option key={i} value={value}>
+                        {value}</option>
+                    })}
+                    </FormControl>
+                  </div>
+                  <div className='page-count pull-right col-xs-7 col-sm-8
+                    col-md-12'>
+                    <Pagination
+                      prev
+                      next
+                      bsSize='medium'
+                      items={totalPage}
+                      activePage={page}
+                      onSelect={handlePageChanged} />
+                  </div>
+                </div>)
+              }
             </div>
+            {!loading &&
+              <div className='left-form tablepadding col-xs-12'>
+                <table className='table' id='languages-table'>
+                  <thead>
+                    <tr className='hidden'>
+                      <th>Language</th>
+                      <th>&nbsp;</th>
+                      <th>&nbsp;</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {results.map(function (value, i) {
+                      return <Entry key={i} locale={value}
+                        userLanguageTeams={user.languageTeams}
+                        permission={permission}
+                        handleDelete={handleDelete}
+                        isDeleting={deleting} />
+                    })}
+                  </tbody>
+                </table>
+              </div>}
           </div>
         </div>
       </div>
