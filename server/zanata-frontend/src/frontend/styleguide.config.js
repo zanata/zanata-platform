748c40d0221630349f91b6bccddb4d06cff84576
Alex Eng <aeng@redhat.com>
2016-11-28 11:31:43 +1000

    fix styleguide

--- server/zanata-frontend/src/frontend/styleguide.config.js
+++ server/zanata-frontend/src/frontend/styleguide.config.js
@@ -3,14 +3,13 @@ var path = require('path')
 
 module.exports = {
   title: 'Zanata Style Guide',
-  components: './src/components/**/index.jsx',
+  components: './app/components/**/index.jsx',
   template: './styleguide.html',
   updateWebpackConfig: function (webpackConfig, env) {
-    webpackConfig.entry.push(path.join(__dirname, 'src/styles/base.css'))
-    webpackConfig.entry.push(path.join(__dirname, 'src/styles/atomic.css'))
-    webpackConfig.entry.push(path.join(__dirname, 'src/styles/animations.css'))
-    webpackConfig.entry.push(path.join(__dirname, 'src/styles/extras.css'))
-    webpackConfig.entry.push(path.join(__dirname, 'src/styles/styleguide.css'))
+    webpackConfig.entry.push(path.join(__dirname, 'app/styles/atomic.css'))
+    webpackConfig.entry.push(path.join(__dirname, 'app/styles/style.less'))
+    webpackConfig.entry.push(path.join(__dirname, 'app/styles/variables.less'))
+    webpackConfig.entry.push(path.join(__dirname, 'app/styles/styleguide.css'))
     webpackConfig.module.loaders =
       webpackConfig.module.loaders
         .concat(defaultWebpackConfig.module.loaders)
