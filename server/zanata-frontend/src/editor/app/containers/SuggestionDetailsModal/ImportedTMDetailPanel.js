10af44eb50f059a226c5f329188da2de7a754a88
David Mason <drdmason@gmail.com>
2016-12-05 19:43:27 +1000

    feat(ZNTA-61): add suggestion details modal

---
+++ server/zanata-frontend/src/editor/app/containers/SuggestionDetailsModal/ImportedTMDetailPanel.js
@@ -0,0 +1,94 @@
+/**
+ * A react-bootstrap Panel that displays details for an IMPORTED_TM suggestion
+ * match.
+ */
+
+import React, { Component, PropTypes } from 'react'
+import { Icon, Row } from 'zanata-ui'
+import { Panel, ListGroup, ListGroupItem, Label } from 'react-bootstrap'
+import SuggestionUpdateMessage from '../../components/SuggestionUpdateMessage'
+
+class ImportedTMDetailPanel extends Component {
+
+  matchHeader (matchDetail) {
+    const { transMemorySlug } = matchDetail
+
+    // FIXME use standard styles when they are available, instead of hard-coded
+
+    return (
+      <div className="TransUnit-details">
+        <ul className="u-listInline u-sMB-1-4">
+          <li>
+            <Row>
+              <Icon name="import" size="n1" /> {transMemorySlug}
+            </Row>
+          </li>
+          <Label style={{'backgroundColor': '#20718a'}}>Imported</Label>
+        </ul>
+      </div>
+    )
+  }
+
+  render () {
+    const {
+      source,
+      target,
+      matchDetail,
+      /* The collected ...props are used to make sure the returned Panel will
+       * work properly with the enclosing PanelGroup/Accordion. If the props
+       * are not passed through, panels do not work properly in a group. */
+      ...props } = this.props
+    const { lastChanged, transUnitId } = matchDetail
+    const header = this.matchHeader(matchDetail)
+
+    const lastChangedDate = new Date(lastChanged)
+
+    return (
+      <Panel
+        header={header}
+        {...props}
+        bsStyle="info">
+        {/* this stuff is really similar to the header, but includes a
+         non-truncated document id display. Just using the same header for
+         now as a shortcut. */}
+        {header}
+        <ListGroup>
+          <ListGroupItem className="small" header="Source">
+            <h3>{source}</h3>
+          </ListGroupItem>
+          <ListGroupItem className="small" header="Target">
+            <h3>{target}</h3>
+            <SuggestionUpdateMessage
+              lastChanged={lastChangedDate}
+              matchType={'imported'}
+            />
+          </ListGroupItem>
+          <ListGroupItem className="small" header="Properties">
+            <ul>
+              <li>Id: {transUnitId}</li>
+            </ul>
+          </ListGroupItem>
+        </ListGroup>
+      </Panel>
+    )
+  }
+}
+
+ImportedTMDetailPanel.propTypes = {
+  /* Key of this panel within the group, used to expand/collapse */
+  eventKey: PropTypes.number.isRequired,
+  /* Source text for the match */
+  source: PropTypes.string.isRequired,
+  /* Translated text for the match */
+  target: PropTypes.string.isRequired,
+  /* Detailed descriptive and context information for the match. */
+  matchDetail: PropTypes.shape({
+    // This only renders things from local projects
+    type: PropTypes.oneOf(['IMPORTED_TM']).isRequired,
+    transMemorySlug: PropTypes.string.isRequired,
+    lastChanged: PropTypes.string.isRequired,
+    transUnitId: PropTypes.string.isRequired
+  }).isRequired
+}
+
+export default ImportedTMDetailPanel
---
+++ server/zanata-frontend/src/editor/app/containers/SuggestionDetailsModal/index.js
@@ -0,0 +1,145 @@
+/**
+ * Modal to display the details for a group of suggestion matches.
+ */
+
+import React, { Component } from 'react'
+import { Modal } from 'zanata-ui'
+import { Button, PanelGroup } from 'react-bootstrap'
+import LocalProjectDetailPanel from './LocalProjectDetailPanel'
+import ImportedTMDetailPanel from './ImportedTMDetailPanel'
+
+const sampleProps = {
+  source: 'The Source Text',
+  target: 'The Translated Text'
+}
+
+const sampleData = [
+  {
+    'type': 'LOCAL_PROJECT',
+    'textFlowId': 6,
+    'contentState': 'Translated',
+    'projectId': 'test',
+    'projectName': 'Test Projacked',
+    'version': '1',
+    'documentName': 'document.txt',
+    'documentPath': 'path/to/file/',
+    'resId': '9cef980fa92eaf64699cf1a07a279313',
+    'lastModifiedDate': '2016-11-10T00:51:21.000Z',
+    'lastModifiedBy': 'admin'
+  },
+  {
+    'type': 'LOCAL_PROJECT',
+    'textFlowId': 1,
+    'contentState': 'Approved',
+    'projectId': 'test',
+    'projectName': 'Test Projacked',
+    'version': '1',
+    'documentName': 'document.txt',
+    'documentPath': '',
+    'resId': '9cef980fa92eaf64699cf1a07a279313',
+    'lastModifiedDate': '2016-11-23T23:55:19.000Z',
+    'lastModifiedBy': 'admin'
+  },
+  {
+    'type': 'LOCAL_PROJECT',
+    'textFlowId': 7,
+    'contentState': 'Translated',
+    'projectId': 'test',
+    'projectName': 'Test Projacked',
+    'version': '1',
+    'documentName': 'document.txt',
+    'documentPath':
+      'really/long/path/to/file/that/should/be/shortened/in/the/heading',
+    'resId': '9cef980fa92eaf64699cf1a07a279313',
+    'lastModifiedDate': '2016-11-23T23:55:19.000Z',
+    'lastModifiedBy': 'admin',
+    'sourceComment': 'This is a source comment right here.',
+    'targetComment': 'This one is a target comment.'
+  },
+  {
+    'type': 'IMPORTED_TM',
+    'transMemoryUnitId': 1,
+    'transMemorySlug': 'some-generic-tm',
+    'transUnitId': 'test:1:document.txt:9cef980fa92eaf64699cf1a07a279313',
+    'lastChanged': '2016-12-05T04:01:08.000Z'
+  },
+  {
+    'type': 'IMPORTED_TM',
+    'transMemoryUnitId': 2,
+    'transMemorySlug': 'some-generic-tm',
+    'transUnitId':
+      'test:1:path/to/file/document.txt:9cef980fa92eaf64699cf1a07a279313',
+    'lastChanged': '2016-12-05T04:01:08.000Z'
+  }
+]
+
+class SuggestionDetailsModal extends Component {
+  constructor () {
+    super()
+    this.state = {
+      // FIXME make it false
+      // FIXME use props instead
+      show: true
+    }
+  }
+
+  hideModal () {
+    this.setState({show: false})
+  }
+  showModal () {
+    this.setState({show: true})
+  }
+
+  render () {
+    // FIXME use real data
+    const { source, target } = sampleProps
+    const matchDetails = sampleData
+
+    // FIXME better variable name
+    const panels = matchDetails.map((matchDetail, index) => {
+      const props = {
+        source, target, matchDetail, key: index, eventKey: index
+      }
+      switch (matchDetail.type) {
+        case 'LOCAL_PROJECT':
+          return <LocalProjectDetailPanel {...props} />
+        case 'IMPORTED_TM':
+          return <ImportedTMDetailPanel {...props} />
+        default:
+          console.error('Unrecognised suggestion match type', matchDetail.type)
+      }
+    })
+
+    return (
+      <div>
+        {/* FIXME use the "X more" match detail link to do this instead
+                  or maybe just use a click on any of those detail items */}
+        <Button bsStyle="default"
+          onClick={::this.showModal}>Launch Modal</Button>
+        <Modal
+          show={this.state.show}
+          onHide={::this.hideModal}>
+          <Modal.Header>
+            <Modal.Title><small><span className="pull-left">
+            Translation Memory Details</span></small></Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h3>Locations</h3>
+            <PanelGroup defaultActiveKey={0} accordion>
+              {panels}
+            </PanelGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            {/*
+            <p>Last modified on 30/04/16 13:15 by <a href="">sdickers</a></p>
+            */}
+          </Modal.Footer>
+        </Modal>
+      </div>)
+  }
+}
+
+// TODO make this a connected component, it can pull out the right suggestion
+//      detail based on an on/off state and the appropriate id.
+
+export default SuggestionDetailsModal
