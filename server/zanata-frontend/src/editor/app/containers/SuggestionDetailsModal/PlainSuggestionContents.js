be214ed57f559f616f931073521ca321cb015887
David Mason <drdmason@gmail.com>
2016-12-12 14:05:52 +1000

    fix(ZNTA-61): display status colour for selected detail item

--- server/zanata-frontend/src/editor/app/containers/SuggestionDetailsModal/PlainSuggestionContents.js
+++ server/zanata-frontend/src/editor/app/containers/SuggestionDetailsModal/PlainSuggestionContents.js
@@ -4,12 +4,24 @@
 
 import React, { Component, PropTypes } from 'react'
 import SuggestionContents from '../../components/SuggestionContents'
+import cx from 'classnames'
 
 class PlainSuggestionContents extends Component {
+
+  matchTypeClass (matchType) {
+    return ({
+      imported: 'TransUnit--secondary',
+      translated: 'TransUnit--success',
+      approved: 'TransUnit--highlight'
+    })[matchType]
+  }
+
   render () {
     const { sourceContents, targetContents } = this.props.suggestion
+    const className = cx('TransUnit TransUnit--suggestion u-bgHigh u-sMB-1',
+      this.matchTypeClass(this.props.matchType))
     return (
-      <div className="TransUnit TransUnit--suggestion u-bgHigh u-sMB-1">
+      <div className={className}>
         <div className="TransUnit-status" />
         <div className="TransUnit-panel TransUnit-source">
           <SuggestionContents
@@ -27,6 +39,8 @@ class PlainSuggestionContents extends Component {
 }
 
 PlainSuggestionContents.propTypes = {
+  /* Optional match type colour to display on the status bar. */
+  matchType: PropTypes.string,
   suggestion: PropTypes.shape({
     sourceContents: PropTypes.arrayOf(PropTypes.string).isRequired,
     targetContents: PropTypes.arrayOf(PropTypes.string).isRequired
