e7a73f17a732aa23e3bdcb99602454a0e4bfff65
Alex Eng <aeng@redhat.com>
2016-12-15 16:30:43 +1000

    fix profile page day button

--- server/zanata-frontend/src/frontend/app/containers/UserProfile/DayMatrix.jsx
+++ server/zanata-frontend/src/frontend/app/containers/UserProfile/DayMatrix.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import { ContentStates } from '../../constants/Options'
 import dateUtil from '../../utils/DateHelper'
 import {
   Base
@@ -8,31 +9,41 @@ import { Button } from 'react-bootstrap'
 /**
  * Clickable date and word count component for daily statistics
  */
+const cssClass = {
+    total: 'btn-primary',
+    approved: 'btn-primary',
+    translated: 'btn-success',
+    needswork: 'btn-warning'
+}
 
 const DayMatrix = ({
   dateLabel,
   date,
   wordCount,
   selectedDay,
+  selectedContentState,
   handleSelectedDayChanged,
   ...props
 }) => {
+  let btnClass = date === selectedDay ? 'Bgc(t) C(#fff)' : ''
+  btnClass += selectedContentState ? cssClass[selectedContentState.toLowerCase().replace(' ', '')] : 'btn-primary'
   const dateIsInFuture = date ? dateUtil.isInFuture(date) : false
   /* eslint-disable react/jsx-no-bind */
   return (
-    <Base tagName='td' className='activity-graph'>
+    <td>
       {date
         ? <Button bsStyle='primary'
           onClick={() => handleSelectedDayChanged(date)}
+          className={btnClass}
           disabled={dateIsInFuture || !date}
           title={wordCount + ' words'}>
-          <Base className='calDate'>{date ? dateLabel : '\u00a0'}</Base>
-          <Base className='calInfo'>
+          <div className='calDate'>{date ? dateLabel : '\u00a0'}</div>
+          <div className='calInfo'>
             {dateIsInFuture ? '\u00a0' : wordCount}
-          </Base>
+          </div>
         </Button>
         : <Base atomic={{bgc: 'Bgc(#fff.85)', stretchedBox: 'StretchedBox'}} />}
-    </Base>
+    </td>
   )
   /* eslint-enable react/jsx-no-bind */
 }
@@ -42,6 +53,7 @@ DayMatrix.propTypes = {
   date: PropTypes.string,
   wordCount: PropTypes.number,
   selectedDay: PropTypes.string,
+  selectedContentState: PropTypes.oneOf(ContentStates),
   handleSelectedDayChanged: PropTypes.func.isRequired
 }
 
