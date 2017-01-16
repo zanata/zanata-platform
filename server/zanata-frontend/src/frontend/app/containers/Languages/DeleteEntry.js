158f33060cb2a433f912d8e401a5548024b2393c
Alex Eng <aeng@redhat.com>
2017-01-17 08:36:27 +1000

    Fix overlay for deleteentry

--- server/zanata-frontend/src/frontend/app/containers/Languages/DeleteEntry.js
+++ server/zanata-frontend/src/frontend/app/containers/Languages/DeleteEntry.js
@@ -1,5 +1,9 @@
 import React, { Component, PropTypes } from 'react'
-import { Button, ButtonToolbar, OverlayTrigger, Popover } from 'react-bootstrap'
+import {
+  Button,
+  Popover,
+  Overlay
+} from 'react-bootstrap'
 
 class DeleteEntry extends Component {
 
@@ -13,43 +17,37 @@ class DeleteEntry extends Component {
   render () {
     const {
       locale,
+      show,
       handleDeleteEntryDisplay,
       handleDeleteEntry
     } = this.props
     /* eslint-disable react/jsx-no-bind */
-    const popoverTop =
-      (<ButtonToolbar>
-        <Popover id='popover-positioned-top'>
-          <p>Are you sure you want to delete&nbsp;
-            <strong>{locale.displayName}</strong>?&nbsp;
-          </p>
-          <span className='button-spacing'>
-            <Button bsStyle='default'
-              onClick={() =>
-                handleDeleteEntryDisplay(false)
-              }>
-              Cancel
-            </Button>
-            <Button bsStyle='danger' type='button'
-              onClick={() => {
-                handleDeleteEntry(locale.localeId)
-                handleDeleteEntryDisplay(false)
-              }}>
-              Delete
-            </Button>
-          </span>
-        </Popover>
-      </ButtonToolbar>)
-
     return (
       <div className='D(ib)'>
-        <OverlayTrigger rootClose trigger='click' placement='top'
-          overlay={popoverTop} >
-          <Button bsSize='small'
-            onClick={() => handleDeleteEntryDisplay(true)}>
-            <i className='fa fa-times Mend(ee)'></i>Delete
-          </Button>
-        </OverlayTrigger>
+        <Button bsSize='small'
+          onClick={() => handleDeleteEntryDisplay(true)}>
+          <i className='fa fa-times Mend(ee)'></i>Delete
+        </Button>
+        <Overlay show={show} container={this} placement='right'>
+          <Popover id='popover-positioned-top'>
+            <p>Are you sure you want to delete&nbsp;
+              <strong>{locale.displayName}</strong>?&nbsp;
+            </p>
+            <span className='button-spacing'>
+              <Button bsStyle='default'
+                onClick={() => handleDeleteEntryDisplay(false)}>
+                Cancel
+              </Button>
+              <Button bsStyle='danger' type='button'
+                onClick={() => {
+                  handleDeleteEntry(locale.localeId)
+                  handleDeleteEntryDisplay(false)
+                }}>
+                Delete
+              </Button>
+            </span>
+          </Popover>
+        </Overlay>
       </div>
     )
     /* eslint-enable react/jsx-no-bind */
@@ -57,7 +55,8 @@ class DeleteEntry extends Component {
 }
 
 DeleteEntry.propTypes = {
-  locale: React.PropTypes.object,
+  locale: PropTypes.object,
+  show: PropTypes.bool,
   handleDeleteEntryDisplay: PropTypes.func.isRequired,
   handleDeleteEntry: React.PropTypes.func.isRequired
 }
