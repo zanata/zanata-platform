5c672d13d9ad29de92fb7990e664b98c63229b9c
Damian Jansen <djansen@redhat.com>
2017-01-16 13:53:49 +1000

    Make mvn put-user work for the updated PutUser options
    Without inadvertently inverting the target user disabled state.

--- client/zanata-client-commands/src/main/java/org/zanata/client/commands/PutUserCommand.java
+++ client/zanata-client-commands/src/main/java/org/zanata/client/commands/PutUserCommand.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zanata.rest.dto.Account;
 import com.google.common.base.Splitter;
+
+import java.util.Arrays;
+
 import static com.google.common.collect.Sets.newHashSet;
 import static org.apache.commons.lang3.ObjectUtils.firstNonNull;
 import static org.zanata.client.commands.ConsoleInteractor.DisplayMode.Question;
@@ -29,10 +32,15 @@ public class PutUserCommand extends ConfigurableCommand<PutUserOptions> {
         String userUsername = getOpts().getUserUsername();
         String passwordHash = getOpts().getUserPasswordHash();
         String userEmail = getOpts().getUserEmail();
+        String isEnabled = getOpts().isUserEnabled();
+        final String[] enabledOptions = {"auto", "true", "false"};
+
+        if (!Arrays.asList(enabledOptions).contains(isEnabled.toLowerCase())) {
+            throw new RuntimeException("userEnabled requires true, false or auto");
+        }
 
         Account account = getClientFactory().getAccountClient().get(userUsername);
         boolean newAccount = account == null;
-
         if (newAccount) {
             log.info("Creating new account {}", userUsername);
             account = new Account();
@@ -42,15 +50,19 @@ public class PutUserCommand extends ConfigurableCommand<PutUserOptions> {
                 log.warn("No passwordHash set, user will need to use the Forgot Password feature");
             }
             account.setUsername(userUsername);
+
+            account.setEnabled(isEnabled.equals("auto") || isEnabled.equals("true"));
         } else {
             log.info("Updating account {}", account.getUsername());
+            if (!isEnabled.equals("auto")) {
+                account.setEnabled(Boolean.parseBoolean(isEnabled));
+            }
         }
 
         account.setEmail(firstNonNull(userEmail, account.getEmail()));
         account.setName(firstNonNull(userName, account.getName()));
         account.setApiKey(firstNonNull(getOpts().getUserKey(), account.getApiKey()));
         account.setPasswordHash(firstNonNull(passwordHash, account.getPasswordHash()));
-        account.setEnabled(firstNonNull(getOpts().isUserEnabled(), account.isEnabled()));
 
         Splitter splitter = Splitter.on(",").trimResults().omitEmptyStrings();
         if (getOpts().getUserRoles() != null) {
@@ -63,11 +75,11 @@ public class PutUserCommand extends ConfigurableCommand<PutUserOptions> {
         if (getOpts().isInteractiveMode()) {
             log.info("User: {}", account.getUsername());
             log.info("Name: {}", account.getName());
-            log.info("Email: {}", userEmail);
+            log.info("Email: {}", account.getEmail());
             log.info("PasswordHash: {}", firstNonNull(passwordHash, "(unchanged)"));
             log.info("API Key: {}", firstNonNull(getOpts().getUserKey(), "(unchanged)"));
-            log.info("Roles: {}", firstNonNull(account.getRoles(), "None"));
-            log.info("Languages: {}", firstNonNull(account.getTribes(), "None"));
+            log.info("Roles: {}", account.getRoles());
+            log.info("Languages: {}", account.getTribes());
             log.info("Enabled: {}", account.isEnabled());
 
             ConsoleInteractor console = new ConsoleInteractorImpl(getOpts());
@@ -80,5 +92,4 @@ public class PutUserCommand extends ConfigurableCommand<PutUserOptions> {
                 account.getUsername(), account);
         log.info("Done.");
     }
-
 }
