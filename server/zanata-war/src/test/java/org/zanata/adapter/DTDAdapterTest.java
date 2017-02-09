a6f6a3650e93171f676cce0656a1ca895d24c514
Sean Flanigan <sflaniga@redhat.com>
2017-02-09 18:34:57 +1000

    test(ZNTA-1456): add unit tests for file adapters, instead of GUI tests (#175)
    * test(ZNTA-1456): add unit tests for file adapters, instead of GUI tests
    * test(ZNTA-1456): remove refs to deleted tests
    * test(ZNTA-1456): Replace subtitle functional test with unit test
    Also fixes a minor error where a texflow would contain a
    superfluous newline character if it were last in the document
    * test(ZNTA-1456): Remove SubtitleDocumentTypeTest from testplan
    * test(ZNTA-1456): Replace DTD functional tests with unit test
    Also removes remaining file format upload tests and replaces with
    one dialog queue test for all formats.
    * test(ZNTA-1456): Improve subtitle adapter test
    * test(ZNTA-1456): Clean up adapter tests

---
+++ server/zanata-war/src/test/java/org/zanata/adapter/DTDAdapterTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.zanata.adapter;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableList;
+import org.junit.Before;
+import org.junit.Test;
+import org.zanata.common.LocaleId;
+import org.zanata.rest.dto.resource.Resource;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author djansen <a href="mailto:djansen@redhat.com">djansen@redhat.com</a>
+ */
+public class DTDAdapterTest {
+
+    private DTDAdapter adapter;
+
+    @Before
+    public void setup() {
+        adapter = new DTDAdapter();
+    }
+
+    @Test
+    public void parseBasicDTDFile() {
+        File testFile = new File("src/test/resources/org/zanata/adapter/basicdtd.dtd");
+        assert testFile.exists();
+        Resource resource = adapter.parseDocumentFile(testFile.toURI(),
+                LocaleId.EN, Optional.absent());
+        assertThat(resource.getTextFlows()).hasSize(1);
+        assertThat(resource.getTextFlows().get(0).getContents()).isEqualTo(
+                ImmutableList.of("Test text 1"));
+    }
+}
---
+++ server/zanata-war/src/test/java/org/zanata/adapter/OpenOfficeAdapterTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2017, Red Hat, Inc. and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.zanata.adapter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.zanata.common.LocaleId;
+import org.zanata.rest.dto.resource.Resource;
+
+import com.google.common.base.Optional;
+
+/**
+ * @author Sean Flanigan
+ *         <a href="mailto:sflaniga@redhat.com">sflaniga@redhat.com</a>
+ */
+// @Feature(summary = "The user can translate OpenOffice files",
+// tcmsTestPlanIds = 5316, tcmsTestCaseIds = 0)
+// TODO test writeTranslatedFile
+public class OpenOfficeAdapterTest {
+
+    private OpenOfficeAdapter adapter;
+
+    @Before
+    public void setup() {
+        adapter = new OpenOfficeAdapter();
+    }
+
+    @Test
+    public void parseODG() {
+        Resource resource =
+                parseFile("test-odg.odg");
+        check3TextFlows(resource, true);
+    }
+
+    @Test
+    public void parseODP() {
+        Resource resource =
+                parseFile("test-odp.odp");
+        // TODO we are getting extra junk at the end
+        check3TextFlows(resource, false);
+    }
+
+    @Test
+    public void parseODT() {
+        Resource resource =
+                parseFile("test-odt.odt");
+        check3TextFlows(resource, true);
+    }
+
+    // @Feature(summary = "The user can translate an OpenOffice spreadsheet
+    // file",
+    // tcmsTestPlanIds = 5316, tcmsTestCaseIds = 0)
+    @Test
+    public void parseODS() {
+        Resource resource =
+                parseFile("test-ods.ods");
+        checkTextFlowsForODS(resource);
+    }
+
+    private void checkTextFlowsForODS(Resource resource) {
+        // TODO we are getting extra junk at the end
+        // assertThat(resource.getTextFlows()).hasSize(5);
+        // TODO test IDs
+
+        assertThat(resource.getTextFlows().get(0).getContents().get(0))
+                .isEqualTo("TestODS")
+                .as("Item 1 shows TestODS (the sheet name)");
+        assertThat(resource.getTextFlows().get(1).getContents().get(0))
+                .isEqualTo("First").as("Item 2 shows First (the page name)");
+        assertThat(resource.getTextFlows().get(2).getContents().get(0))
+                .isEqualTo("Line One").as("Item 3 shows Line One");
+        assertThat(resource.getTextFlows().get(3).getContents().get(0))
+                .isEqualTo("Line Two").as("Item 4 shows Line Two");
+        assertThat(resource.getTextFlows().get(4).getContents().get(0))
+                .isEqualTo("Line Three").as("Item 5 shows Line Three");
+    }
+
+    private Resource parseFile(String testFile) {
+        File file = new File("src/test/resources/org/zanata/adapter/" + testFile);
+        assert file.exists();
+        Resource resource = adapter.parseDocumentFile(file.toURI(), LocaleId.EN,
+                Optional.absent());
+        // System.out.println(DTOUtil.toXML(resource));
+        return resource;
+    }
+
+    private void check3TextFlows(Resource resource, boolean checkSize) {
+        if (checkSize) assertThat(resource.getTextFlows()).hasSize(3);
+        // TODO test IDs
+
+        assertThat(resource.getTextFlows().get(0).getContents().get(0))
+                .isEqualTo("Line One").as("Item 0 shows Line One");
+        assertThat(resource.getTextFlows().get(1).getContents().get(0))
+                .isEqualTo("Line Two").as("Item 1 shows Line Two");
+        assertThat(resource.getTextFlows().get(2).getContents().get(0))
+                .isEqualTo("Line Three").as("Item 2 shows Line Three");
+    }
+
+}
