3c95f77b9fadabfdce01f70b190283466ea8f7bb
Damian Jansen <djansen@redhat.com>
2017-02-17 14:02:41 +1000

    refactor(test): Get TSAdapter keys from resource

--- server/zanata-war/src/test/java/org/zanata/adapter/AbstractAdapterTest.java
+++ server/zanata-war/src/test/java/org/zanata/adapter/AbstractAdapterTest.java
@@ -1,6 +1,7 @@
 package org.zanata.adapter;
 
 import com.google.common.base.Optional;
+import org.fedorahosted.openprops.Properties;
 import org.zanata.common.ContentState;
 import org.zanata.common.LocaleId;
 import org.zanata.rest.dto.resource.Resource;
@@ -8,6 +9,14 @@ import org.zanata.rest.dto.resource.TextFlowTarget;
 import org.zanata.rest.dto.resource.TranslationsResource;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Common variables and functions for adapter tests
@@ -17,7 +26,7 @@ import java.io.File;
 abstract class AbstractAdapterTest {
 
     FileFormatAdapter adapter;
-    private String resourcePath = "src/test/resources/org/zanata/adapter/";
+    String resourcePath = "src/test/resources/org/zanata/adapter/";
 
     Resource parseTestFile(String fileName) {
         File testFile = new File(resourcePath.concat(fileName));
@@ -30,6 +39,9 @@ abstract class AbstractAdapterTest {
         return new File(resourcePath.concat(fileName));
     }
 
+    /*
+     * Add a translation to a TranslationsResource
+     */
     void addTranslation(TranslationsResource tRes,
                                 String resId,
                                 String content,
@@ -39,5 +51,43 @@ abstract class AbstractAdapterTest {
         textFlowTarget.setContents(content);
         textFlowTarget.setState(state);
         tRes.getTextFlowTargets().add(textFlowTarget);
+    }
+
+    /*
+     * Add a translation to a Map based translations resource
+     */
+    void addTranslation(Map<String, TextFlowTarget> map,
+                        String resId,
+                        String content,
+                        ContentState state) {
+        TextFlowTarget textFlowTarget = new TextFlowTarget();
+        textFlowTarget.setContents(content);
+        textFlowTarget.setState(state);
+        map.put(resId, textFlowTarget);
+    }
+
+    /*
+     * Create a .properties file in temp using the given character set
+     */
+    File createTempPropertiesFile(Charset charset) throws Exception {
+        File testFile = File.createTempFile("test-properties-temp-" + charset, ".properties");
+        assertThat(testFile.exists());
+        Map<String, String> entries = new LinkedHashMap<>();
+        if (charset == StandardCharsets.ISO_8859_1) {
+            entries.put("line1", "ÀLine One");
+            entries.put("line2", "ÀLine Two");
+            entries.put("line3", "ÀLine Three");
+        } else if (charset == StandardCharsets.UTF_8) {
+            entries.put("line1", "¥Line One");
+            entries.put("line2", "¥Line Two");
+            entries.put("line3", "¥Line Three");
+        }
+        Properties resource = new Properties();
+        for (Map.Entry<String, String> entry : entries.entrySet()) {
+            resource.setProperty(entry.getKey(), entry.getValue());
+        }
+        resource.store(new OutputStreamWriter(new FileOutputStream(testFile),
+                charset), null);
+        return testFile;
     }
 }
