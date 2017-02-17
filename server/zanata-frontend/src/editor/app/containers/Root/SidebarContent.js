2828d89224fcca70fab0f9c5b46477951adb6d20
kgough <kgough@redhat.com>
2017-02-17 16:37:24 +1000

    WIP: details section design for editor sidebar (ZNTA-1732)

--- server/zanata-frontend/src/editor/app/containers/Root/SidebarContent.js
+++ server/zanata-frontend/src/editor/app/containers/Root/SidebarContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Tabs, Tab } from 'react-bootstrap'
+import { Tabs, Tab, FormGroup, InputGroup,
+  FormControl } from 'react-bootstrap'
 import Icon from '../../components/Icon'
 
 const activityTitle = 'Activity'
@@ -12,21 +13,37 @@ const SidebarContent = React.createClass({
   render () {
     return (
       <div>
-        <h1><Icon name="info" className="s1" /> Details</h1>
-        <ul>
-          <li>Resource ID</li>
-          <li>Message Context</li>
-          <li>Reference</li>
-          <li>Flags</li>
-          <li>Source Comment</li>
-          <li>Last Modified</li>
-        </ul>
+        <h1 className="sidebar-heading">
+          <Icon name="info" className="s1" /> Details
+        </h1>
+        <div className="sidebar-wrapper">
+          <ul className="sidebar-details">
+            <li><span>Resource ID</span></li>
+            <li><span>Message Context</span></li>
+            <li><span>Reference</span></li>
+            <li><span>Flags</span></li>
+            <li><span>Source Comment</span></li>
+            <li><span>Last Modified</span></li>
+          </ul>
+          <FormGroup className="trans-link">
+            <InputGroup>
+              <InputGroup.Addon><Icon name="copy"
+                className="s1" />
+              </InputGroup.Addon>
+              <FormControl type="text" />
+            </InputGroup>
+          </FormGroup>
+        </div>
         <Tabs defaultActiveKey={1}>
           <Tab eventKey={1} title={activityTitle}>
-            Tab 1 content
+            <div className="sidebar-wrapper" id="tab1">
+              Tab 1 content
+            </div>
           </Tab>
           <Tab eventKey={2} title={glossaryTitle}>
-            Tab 2 content
+            <div className="sidebar-wrapper" id="tab2">
+              Tab 2 content
+            </div>
           </Tab>
         </Tabs>
       </div>
