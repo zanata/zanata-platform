1e565d887ee32ba610277f54e86e3cbd9599116b
kgough <kgough@redhat.com>
2017-03-17 13:17:17 +1000

    feat(ZNTA-1737): add advanced search panel styling to editor

--- server/zanata-frontend/src/frontend/app/editor/components/EditorSearchInput/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/EditorSearchInput/index.js
@@ -129,66 +129,83 @@ const EditorSearchInput = React.createClass({
           <ul>
             <li className="inline-search-list">
               text:
-              <input ref="text"
-                type="text"
-                placeholder="source and target text"
-                className="InputGroup-input" />
+              <div className="InputGroup--outlined
+                InputGroup--wide InputGroup--rounded">
+                <input ref="text"
+                  type="text"
+                  placeholder="source and target text"
+                  className="InputGroup-input" />
+              </div>
             </li>
             <li className="inline-search-list">
               resource-id:
-              <input ref="resid"
-                type="text"
-                placeholder="exact Resource ID for a string"
-                className="InputGroup-input" />
+              <div className="InputGroup--outlined
+                InputGroup--wide InputGroup--rounded">
+                <input ref="resid"
+                  type="text"
+                  placeholder="exact Resource ID for a string"
+                  className="InputGroup-input" />
+              </div>
             </li>
             <li className="inline-search-list">
               last-modified-by:
-              <input ref="modby"
-                type="text"
-                placeholder="username"
-                className="InputGroup-input" />
-            </li>
-            <li className="inline-search-list">
-              last-modified-by:
-              <input ref="modby"
-                type="text"
-                placeholder="username"
-                className="InputGroup-input" />
+              <div className="InputGroup--outlined
+                InputGroup--wide InputGroup--rounded">
+                <input ref="modby"
+                  type="text"
+                  placeholder="username"
+                  className="InputGroup-input" />
+              </div>
             </li>
             <li className="inline-search-list">
               last-modified-before:
-              <input ref="modbefore"
-                type="text"
-                placeholder="date in format yyyy/mm/dd"
-                className="InputGroup-input" />
+              <div className="InputGroup--outlined
+                InputGroup--wide InputGroup--rounded">
+                <input ref="modbefore"
+                  type="text"
+                  placeholder="date in format yyyy/mm/dd"
+                  className="InputGroup-input" />
+              </div>
             </li>
             <li className="inline-search-list">
               last-modified-after:
-              <input ref="modafter"
-                type="text"
-                placeholder="date in format yyyy/mm/dd"
-                className="InputGroup-input" />
+              <div className="InputGroup--outlined
+                InputGroup--wide InputGroup--rounded">
+                <input ref="modafter"
+                  type="text"
+                  placeholder="date in format yyyy/mm/dd"
+                  className="InputGroup-input" />
+              </div>
             </li>
             <li className="inline-search-list">
               source-comment:
-              <input ref="sourcecomm"
-                type="text"
-                placeholder="source comment text"
-                className="InputGroup-input" />
+              <div className="InputGroup--outlined
+                InputGroup--wide InputGroup--rounded">
+                <input ref="sourcecomm"
+                  type="text"
+                  placeholder="source comment text"
+                  className="InputGroup-input" />
+              </div>
             </li>
             <li className="inline-search-list">
               translation-comment:
-              <input ref="transcomm"
-                type="text"
-                placeholder="translation comment text"
-                className="InputGroup-input" />
+              <div className="InputGroup--outlined
+                InputGroup--wide InputGroup--rounded">
+                <input ref="transcomm"
+                  type="text"
+                  placeholder="translation comment text"
+                  className="InputGroup-input" />
+              </div>
             </li>
             <li className="inline-search-list">
               msgctxt:
-              <input ref="msgctxt"
-                type="text"
-                placeholder="exact Message Context for a string"
-                className="InputGroup-input" />
+              <div className="InputGroup--outlined
+                InputGroup--wide InputGroup--rounded">
+                <input ref="msgctxt"
+                  type="text"
+                  placeholder="exact Message Context for a string"
+                  className="InputGroup-input" />
+              </div>
             </li>
           </ul>
         </Panel>
