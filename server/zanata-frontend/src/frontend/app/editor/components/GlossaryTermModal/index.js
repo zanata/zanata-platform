52946bb18019235f430849ad71fb590bc7309ffe
David Mason <drdmason@gmail.com>
2017-04-06 09:03:39 +1000

    feat(ZNTA-1735): use zanata-ui Modal for glossary details

--- server/zanata-frontend/src/frontend/app/editor/components/GlossaryTermModal/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/GlossaryTermModal/index.js
@@ -1,101 +1,33 @@
-import React, { PropTypes } from 'react'
-import { Modal, Panel, Row, Table } from 'react-bootstrap'
-import { FormattedDate, FormattedTime } from 'react-intl'
-import { Icon } from '../../../components'
+import { connect } from 'react-redux'
+import GlossaryTermModal from './disconnected'
+import { map } from 'lodash'
+import { showGlossaryDetails } from '../../actions/glossary'
 
-/**
- * Modal to show detail for a single glossary term
- */
-const GlossaryTermModal = React.createClass({
-  propTypes: {
-    show: PropTypes.bool.isRequired,
-    close: PropTypes.func.isRequired,
-    sourceLocale: PropTypes.string.isRequired,
-    term: PropTypes.shape({
-      source: PropTypes.string.isRequired,
-      target: PropTypes.string.isRequired
-    }).isRequired,
-    details: PropTypes.arrayOf(
-      PropTypes.shape({
-        description: PropTypes.string,
-        lastModifiedTime: PropTypes.instanceOf(Date).isRequired,
-        pos: PropTypes.string,
-        transComment: PropTypes.string
-      })
-    ).isRequired
-  },
+function mapStateToProps (state) {
+  const { context, glossary, headerData } = state
+  const { details, results } = glossary
+  const { byId, resultIndex, show } = details
 
-  render: function () {
-    const { close, details, sourceLocale, targetLocale, term } = this.props
+  const term = results[resultIndex]
 
-    const selectedDetail = 0;
-    const detail = details[selectedDetail]
+  // undefined items are kept, that just indicates the detail has not been
+  // returned from the API yet.
+  const detailItems = term && term.sourceIdList
+    ? map(term.sourceIdList, id => byId[id]) : []
 
-    const { description, pos, transComment, lastModifiedTime } = detail
-
-    const detailsDisplay = details.map(
-      ({description, lastModifiedTime, pos, transComment}, index) => (
-        <tr key={index}>
-          {/* Do we need this column?
-          <td>#{index}</td>
-          */}
-          <td>{description}</td>
-          <td>{pos}</td>
-          <td>
-            <Icon name="comment" className="comment-icon n1"/> {transComment}
-          </td>
-        </tr>
-      ))
-
-    return (
-      <Modal show
-        onHide={close}>
-        <Modal.Header closeButton>
-          <Modal.Title>Glossary details</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Panel className="split-panel">
-            <h3>Source Term [{sourceLocale}]:</h3>
-            <span className="modal-term">{term.source}</span>
-          </Panel>
-          <Panel className="split-panel">
-            <h3>Target Term [{targetLocale}]:</h3>
-            <span className="modal-term">{term.target}</span>
-          </Panel>
-          <br />
-          <Panel className="gloss-details-panel">
-            <Table className="gloss-details-table">
-              <thead>
-                <tr>
-            {/* Do we need this column?
-                  <th></th>
-            */}
-                  <th>Description</th>
-                  <th>Part of speech</th>
-                  <th>Target comment</th>
-                </tr>
-              </thead>
-              <tbody>
-                {detailsDisplay}
-              </tbody>
-            </Table>
-          </Panel>
-
-          <span className="pull-right u-textMeta">
-            <Row>
-              <Icon name="history" className="s0 history-icon" />
-              <span className="u-sML-1-4">
-                Last modified on&nbsp;
-                <FormattedDate value={lastModifiedTime} format="medium" />&nbsp;
-                <Icon name="clock" className="s0 history-icon" />&nbsp;
-                <FormattedTime value={lastModifiedTime} />
-              </span>
-            </Row>
-          </span>
-        </Modal.Body>
-      </Modal>
-    )
+  return {
+    show,
+    sourceLocale: context.sourceLocale.localeId,
+    targetLocale: headerData.context.selectedLocale,
+    term,
+    details: detailItems
   }
-})
+}
 
-export default GlossaryTermModal
+function mapDispatchToProps (dispatch) {
+  return {
+    close: () => dispatch(showGlossaryDetails(false))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GlossaryTermModal)
--- server/zanata-frontend/src/frontend/app/editor/components/GlossaryTermModal/story.js
+++ server/zanata-frontend/src/frontend/app/editor/components/GlossaryTermModal/story.js
@@ -3,15 +3,24 @@ import { storiesOf } from '@kadira/storybook'
 import { action, decorateAction } from '@kadira/storybook-addon-actions'
 import { Modal, Button, Panel, Row, Table } from 'react-bootstrap'
 import { Icon } from '../../../components'
-import GlossaryTermModal from '.'
+import Lorem from 'react-lorem-component'
+import GlossaryTermModal from './disconnected'
 
 /*
 * See .storybook/README.md for info on the component storybook.
 */
 storiesOf('GlossaryTermModal', module)
   .addDecorator((story) => (
-    <div className="static-modal">
-      {story()}
+    <div>
+      <h1>Lorem Ipsum</h1>
+      <Lorem count={1} />
+      <Lorem mode="list" />
+      <h2>Dolor Sit Amet</h2>
+      <Lorem />
+      <Lorem mode="list" />
+      <div className="static-modal">
+        {story()}
+      </div>
     </div>
   ))
   .add('with 1 detail item', () => (
@@ -28,8 +37,8 @@ storiesOf('GlossaryTermModal', module)
         {
           description: 'Take a turn at batting in a game of sportsball.',
           pos: 'Verb',
-          transComment: 'It sounds a bit like "slugger" like someone might call a junior-league base kid ball.',
-          lastModifiedTime: new Date(1490687578793)
+          targetComment: 'It sounds a bit like "slugger" like someone might call a junior-league base kid ball.',
+          lastModifiedDate: 1490687578793
         }
       ]}
     />
@@ -48,68 +57,19 @@ storiesOf('GlossaryTermModal', module)
         {
           description: 'To hit something with a bat.',
           pos: 'Verb',
-          transComment: "So I schlagged 'im.",
-          lastModifiedTime: new Date(1490687563741)
+          targetComment: "So I schlagged 'im.",
+          lastModifiedDate: 1490687563741
         },{
           description: 'Take a turn at batting in a game of sportsball.',
           pos: 'Verb',
-          transComment: 'It sounds a bit like "slugger" like someone might call a junior-league base kid ball.',
-          lastModifiedTime: new Date(1490687578793)
+          targetComment: 'It sounds a bit like "slugger" like someone might call a junior-league base kid ball.',
+          lastModifiedDate: 1490687578793
         },{
           description: 'For one who is a bat, to hit something with themself.',
           pos: 'Norb',
-          transComment: 'I did not just make it up, it is a real thing.',
-          lastModifiedTime: new Date(409872360000)
+          targetComment: 'I did not just make it up, it is a real thing.',
+          lastModifiedDate: 409872360000
         }
       ]}
     />
   ))
-.add('hard-coded story', () => (
-    <Modal show
-    onHide={action('close')}>
-      <Modal.Header closeButton>
-        <Modal.Title>Glossary details</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Panel className="split-panel">
-          <h3>Source Term [en-US]:</h3>
-          <span className="modal-term">cat</span>
-        </Panel>
-        <Panel className="split-panel">
-          <h3>Target Term [ar-BH]:</h3>
-          <span className="modal-term">caaat</span>
-        </Panel>
-        <br />
-        <Panel className="gloss-details-panel">
-          <Table className="gloss-details-table">
-            <thead>
-              <tr>
-                <th></th>
-                <th>Description</th>
-                <th>Part of speech</th>
-                <th>Target comment</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>#1</td>
-                <td>The animal equivalent to the apathetic housemate</td>
-                <td>Noun</td>
-                <td>
-                  <Icon name="comment" className="comment-icon n1"/>
-                This is a comment that was left on the target term
-                </td>
-              </tr>
-            </tbody>
-          </Table>
-        </Panel>
-        <span className="pull-right u-textMeta">
-          <Row>
-            <Icon name="history" className="s0 history-icon" />
-            <span className="u-sML-1-4">Last modified on DATEY TIMEY</span>
-          </Row>
-        </span>
-      </Modal.Body>
-     </Modal>
-  )
-)
