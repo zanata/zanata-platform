661ae46f8b52158c9e884ddc540fe08043fba816
kgough <kgough@redhat.com>
2017-04-19 10:48:49 +1000

    removed unnecessary lines from Button story in frontend

--- server/zanata-frontend/src/frontend/app/components/Button/story.js
+++ server/zanata-frontend/src/frontend/app/components/Button/story.js
@@ -8,36 +8,26 @@ storiesOf('Button', module)
           Unstyled button
         </Button>
     ))
-
-storiesOf('Button', module)
     .add('primary', () => (
         <Button bsStyle='primary' onClick={action('onClick')}>
           Primary button
         </Button>
     ))
-
-storiesOf('Button', module)
     .add('info', () => (
         <Button bsStyle='info' onClick={action('onClick')}>
           Info button
         </Button>
     ))
-
-storiesOf('Button', module)
     .add('warning', () => (
         <Button bsStyle='warning' onClick={action('onClick')}>
           Warning button
         </Button>
     ))
-
-storiesOf('Button', module)
     .add('danger', () => (
         <Button bsStyle='danger' onClick={action('onClick')}>
           Danger button
         </Button>
     ))
-
-storiesOf('Button', module)
     .add('success', () => (
         <Button bsStyle='success' onClick={action('onClick')}>
           Success button
