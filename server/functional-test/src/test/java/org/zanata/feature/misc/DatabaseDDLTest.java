e9a997349514480939aaf25fefcfd62d993e0d06
Patrick Huang <pahuang@redhat.com>
2017-04-20 16:01:42 +1000

    move DatabaseDDLTest to misc package

--- server/functional-test/src/test/java/org/zanata/feature/misc/DatabaseDDLTest.java
+++ server/functional-test/src/test/java/org/zanata/feature/misc/DatabaseDDLTest.java
@@ -18,7 +18,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.zanata;
+package org.zanata.feature.misc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
