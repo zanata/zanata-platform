3d42f1d89013667abd44f39d6b997aa537d9b59c
kgough <kgough@redhat.com>
2017-04-24 09:44:11 +1000

    revision of Icon story

--- server/zanata-frontend/src/frontend/app/components/Icon/story.js
+++ server/zanata-frontend/src/frontend/app/components/Icon/story.js
@@ -2,85 +2,40 @@ import React from 'react'
 import { storiesOf, action } from '@kadira/storybook'
 import { Icon } from '../'
 
+const allIconNames = ['admin','all','assign','attach','block','chevron-down-double','chevron-down','chevron-left','chevron-right','chevron-up-double','chevron-up','circle','clock','code','comment','copy','cross-circle','cross','dashboard','document','dot','download','edit','ellipsis','export','external-link','filter','folder','glossary','help','history','import','inbox','info','keyboard','language','link','location','locked','logout','mail','maintain','menu','minus','next','notification','piestats','plus','previous','project','refresh','review','search','servmon','settings','star-outline','star','statistics','suggestions','tick-circle','tick','tm','translate','trash','undo','unlocked','upload','user','users','version','warning','zanata']
+
+function renderIcon (name, size) {
+  return <span title={name}> <Icon name={name} className={size} /> </span>
+}
+
+const sizen2Icons = allIconNames.map((name) => renderIcon(name, 'n2'))
+const sizen1Icons = allIconNames.map((name) => renderIcon(name, 'n1'))
+const size0Icons = allIconNames.map((name) => renderIcon(name, 's0'))
+const size1Icons = allIconNames.map((name) => renderIcon(name, 's1'))
+const size2Icons = allIconNames.map((name) => renderIcon(name, 's2'))
+const size3Icons = allIconNames.map((name) => renderIcon(name, 's3'))
+const size4Icons = allIconNames.map((name) => renderIcon(name, 's4'))
+
 storiesOf('Icon', module)
     .add('default', () => (
-<span>
-  <p><Icon name='admin' className='s1' />admin</p>
-  <p><Icon name='all' className='s1' />all</p>
-  <p><Icon name='assign' className='s1' />assign</p>
-  <p><Icon name='attach' className='s1' />attach</p>
-  <p><Icon name='block' className='s1' />block</p>
-  <p><Icon name='chevron-down-double'
-            className='s0' />chevron-double-down</p>
-  <p><Icon name='chevron-down' className='s1' />chevron-down</p>
-  <p><Icon name='chevron-left' className='s1' />chevron-left</p>
-  <p><Icon name='chevron-right' className='s1' />
-    chevron-right</p>
-  <p><Icon name='chevron-up-double'
-            className='s0' />chevron-up-double</p>
-  <p><Icon name='chevron-up' className='s1' />chevron-up</p>
-  <p><Icon name='circle' className='s1' />circle</p>
-  <p><Icon name='clock' className='s1' />clock</p>
-  <p><Icon name='code' className='s1' />code</p>
-  <p><Icon name='comment' className='s1' />comment</p>
-  <p><Icon name='copy' className='s1' />copy</p>
-  <p><Icon name='cross-circle' className='s1' />cross-circle</p>
-  <p><Icon name='cross' className='s1' />cross</p>
-  <p><Icon name='dashboard' className='s1' />dashboard</p>
-  <p><Icon name='document' className='s1' />document</p>
-  <p><Icon name='dot' className='s1' />dot</p>
-  <p><Icon name='download' className='s1' />download</p>
-  <p><Icon name='edit' className='s1' />edit</p>
-  <p><Icon name='ellipsis' className='s1' />ellipsis</p>
-  <p><Icon name='export' className='s1' />export</p>
-  <p><Icon name='external-link'
-            className='s1' />external-link</p>
-  <p><Icon name='filter' className='s1' />filter</p>
-  <p><Icon name='folder' className='s1' />folder</p>
-  <p><Icon name='glossary' className='s1' />glossary</p>
-  <p><Icon name='help' className='s1' />help</p>
-  <p><Icon name='history' className='s1' />history</p>
-  <p><Icon name='import' className='s1' />import</p>
-  <p><Icon name='inbox' className='s1' />inbox</p>
-  <p><Icon name='info' className='s1' />info</p>
-  <p><Icon name='keyboard' className='s1' />keyboard</p>
-  <p><Icon name='language' className='s1' />language</p>
-  <p><Icon name='link' className='s1' />link</p>
-  <p><Icon name='location' className='s1' />location</p>
-  <p><Icon name='locked' className='s1' />locked</p>
-  <p><Icon name='logout' className='s1' />logout</p>
-  <p><Icon name='mail' className='s1' />mail</p>
-  <p><Icon name='maintain' className='s1' />maintain</p>
-  <p><Icon name='menu' className='s1' />menu</p>
-  <p><Icon name='minus' className='s1' />minus</p>
-  <p><Icon name='next' className='s1' />next</p>
-  <p><Icon name='notification' className='s1' />
-    notification</p>
-  <p><Icon name='piestats' className='s1' />piestats</p>
-  <p><Icon name='plus' className='s1' />plus</p>
-  <p><Icon name='previous' className='s1' />previous</p>
-  <p><Icon name='project' className='s1' />project</p>
-  <p><Icon name='refresh' className='s1' />refresh</p>
-  <p><Icon name='review' className='s1' />review</p>
-  <p><Icon name='search' className='s1' />search</p>
-  <p><Icon name='servmon' className='s1' />servmon</p>
-  <p><Icon name='settings' className='s1' />settings</p>
-  <p><Icon name='star-outline' className='s1' />star-outline</p>
-  <p><Icon name='star' className='s1' />star</p>
-  <p><Icon name='statistics' className='s1' />statistics</p>
-  <p><Icon name='suggestions' className='s1' />suggestions</p>
-  <p><Icon name='tick-circle' className='s1' />tick-circle</p>
-  <p><Icon name='tick' className='s1' />tick</p>
-  <p><Icon name='tm' className='s1' />tm</p>
-  <p><Icon name='translate' className='s1' />translate</p>
-  <p><Icon name='trash' className='s1' />trash</p>
-  <p><Icon name='undo' className='s1' />undo</p>
-  <p><Icon name='unlocked' className='s1' />unlocked</p>
-  <p><Icon name='upload' className='s1' />upload</p>
-  <p><Icon name='user' className='s1' />user</p>
-  <p><Icon name='users' className='s1' />users</p>
-  <p><Icon name='version' className='s1' />version</p>
-  <p><Icon name='warning' className='s1' />warning</p>
-  <p><Icon name='zanata' className='s1' />zanata</p>
-</span>
-    ))
+    <div>
+      <h2>Size n2</h2>
+      {sizen2Icons}
+      <h2>Size n1</h2>
+      {sizen1Icons}
+      <h2>Size 0</h2>
+      {size0Icons}
+      <h2>Size 1</h2>
+      {size1Icons}
+      <h2>Size 2</h2>
+      {size2Icons}
+      <h2>Size 3</h2>
+      {size3Icons}
+      <h2>Size 4</h2>
+      {size4Icons}
+    </div>
+))
+
+
+
+
