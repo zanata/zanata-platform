365926b318604d9cbc0fabf85638e5453bd86ec2
David Mason <drdmason@gmail.com>
2017-05-01 18:04:48 +1000

    Create advanced search input component (ZNTA-1737) (#290)
    * feat(ZNTA-1737): add advanced search to react editor
    
    * feat(ZNTA-1737): editor advanced search positioning and styling
    
    Rebased with conflicts in Root/index.css, and index.js.
    
    * feat(ZNTA-1737): styling for advanced search in editor
    
    * feat(ZNTA-1737): move EditorSearchInput component to editor directory
    
    * feat(ZNTA-1737): adding search modal
    
    Rebased with conflicts in ControlsHeader.js
    
    * feat(ZNTA-1737): panel dropdown for editor search
    
    * feat(ZNTA-1737): add advanced search panel to editor
    
    Rebased with conflicts in index.css
    May have inadvertantly added a css rule where it was ambiguous.
    
    * feat(ZNTA-1737): add advanced search panel styling to editor
    
    * feat(ZNTA-1737): remove outline from button on focus in editor
    
    Bootstrap override
    
    * feat(ZNTA-1737): remove search modal references
    
    * feat(ZNTA-1737): remove references to redundant container
    
    * feat(ZNTA-1737): set absolute positioning for advanced search panel
    
    * feat(ZNTA-1737): make advanced search appear on top of header menu in mobile view
    
    * feat(ZNTA-1737): rename functions and action types for advanced search
    
    * feat(ZNTA-1737): remove unused props
    
    * feat(ZNTA-1737): fix prop reference in ControlsHeader
    
    The action was nested in this.props.actions but was being used as though
    it was on this.props
    
    * feat(ZNTA-1737): fix some React render warnings
    
    * feat(ZNTA-1737): set up storybook for EditorSearchInput component
    
    Includes some initial tweaking of the component to work in the storybook.
    Component is probably still broken in the application since this change
    does not attempt to fix it there.
    
    * feat(ZNTA-1737): restructure search input component a little
    
    * feat(ZNTA-1737): remove redundant search text field
    
    * feat(ZNTA-1737): remove search box from live application
    
    Temporarily removing this since it needs more complex hooking-up to
    work in the app.
    
    * feat(ZNTA-1737): minor css fixes to advanced search
    
    * feat(ZNTA-1737): move css import to appropriate section
    
    * feat(ZNTA-1737): prevent recreating static data each render
    
    * fix: remove unused import
    
    * feat(ZNTA-1737): allow show/hide of advanced search fields
    
    * feat(ZNTA-1737): add clear-all button to EditorSearchInput
    
    * feat(ZNTA-1737): styling of advanced search buttons
    
    * feat(ZNTA-1737): remove unnecessary component state
    
    * fix(css): remove less variable from css file
    
    * test(ZNTA-1737): add test for EditorSearchInput

---
+++ server/zanata-frontend/src/frontend/app/editor/components/EditorSearchInput/story.js
@@ -0,0 +1,108 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import { action, decorateAction } from '@kadira/storybook-addon-actions'
+import RealEditorSearchInput from '.'
+
+// Simple wrapper component to handle text updates for the story
+// Uses the component name so it shows accurately how to use it
+class EditorSearchInput extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      search: props.search,
+      advanced: props.advanced
+    }
+    this.toggleAdvanced = ::this.toggleAdvanced
+    this.updateSearch = ::this.updateSearch
+  }
+
+  toggleAdvanced () {
+    // ensure the action is visible in the logger
+    this.props.toggleAdvanced()
+    this.setState({
+      advanced: !this.state.advanced
+    })
+  }
+
+  updateSearch (search) {
+    // ensure the action is visible in the logger
+    this.props.updateSearch(search)
+    this.setState({
+      search: {
+        ...this.state.search,
+        ...search
+      }
+    })
+  }
+
+  render () {
+    return (
+      <RealEditorSearchInput
+        advanced={this.state.advanced}
+        search={this.state.search}
+        updateSearch={this.updateSearch}
+        toggleAdvanced={this.toggleAdvanced}
+      />
+    )
+  }
+}
+
+const blankSearch = {
+  text: '',
+  resourceId: '',
+  lastModifiedBy: '',
+  lastModifiedBefore: '',
+  lastModifiedAfter: '',
+  sourceComment: '',
+  translationComment: '',
+  msgctxt: ''
+}
+
+storiesOf('EditorSearchInput', module)
+  .add('empty', () => (
+    <EditorSearchInput
+      search={blankSearch}
+      updateSearch={action('updateSearch')}
+      advanced={false}
+      toggleAdvanced={action('toggleAdvanced')}
+    />
+  ))
+  .add('simple search', () => (
+    <EditorSearchInput
+      search={{
+        ...blankSearch,
+        text: 'it was the'
+      }}
+      updateSearch={action('updateSearch')}
+      advanced={false}
+      toggleAdvanced={action('toggleAdvanced')}
+    />
+  ))
+  .add('advanced panel open', () => (
+    <EditorSearchInput
+      search={{
+        ...blankSearch,
+        text: 'it was the worst'
+      }}
+      updateSearch={action('updateSearch')}
+      advanced={true}
+      toggleAdvanced={action('toggleAdvanced')}
+    />
+  ))
+  .add('advanced search', () => (
+    <EditorSearchInput
+      search={{
+        text: 'it was the worst of',
+        resourceId: 'para-0001',
+        lastModifiedBy: 'cdickens',
+        lastModifiedBefore: '1859-12-31',
+        lastModifiedAfter: '1859-01-01',
+        sourceComment: 'England and France',
+        translationComment: 'blurst of times?! You stupid monkey!',
+        msgctxt: 'chapter01.txt'
+      }}
+      updateSearch={action('updateSearch')}
+      advanced={true}
+      toggleAdvanced={action('toggleAdvanced')}
+    />
+  ))
