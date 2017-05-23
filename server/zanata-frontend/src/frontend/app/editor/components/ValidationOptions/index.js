adb9cb8ca8ae80497d5a59d7bfa8ee4649bd8779
kgough <kgough@redhat.com>
2017-05-23 19:08:14 +1000

    feat(ZNTA-1736): created ValidationOptions component. Linting errors need fixing

---
+++ server/zanata-frontend/src/frontend/app/editor/components/ValidationOptions/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {Button, Checkbox} from 'react-bootstrap'
+
+const ValidationOptions = React.createClass({
+
+  render () {
+    return (
+        <div>
+          <h2 className="validation">Validation options</h2>
+          <ul>
+            <li>
+              <Checkbox checked>
+                HTML/XML tags
+              </Checkbox>
+            </li>
+            <li>
+              <Checkbox checked>
+                Java variables
+              </Checkbox>
+            </li>
+            <li>
+              <Checkbox checked>
+                Leading/trailing newline (\n)
+              </Checkbox>
+            </li>
+            <li>
+              <Checkbox>
+                Positional printf (XSI extension)
+              </Checkbox>
+            </li>
+            <li>
+              <Checkbox>
+                Printf variables
+              </Checkbox>
+            </li>
+            <li>
+              <Checkbox checked>
+                Tab characters (\t)
+              </Checkbox>
+            </li>
+            <li>
+              <Checkbox checked>
+                XML entity reference
+              </Checkbox>
+            </li>
+          </ul>
+        </div>
+    )
+  })
+
+export default ValidationOptions
+
--- server/zanata-frontend/src/frontend/app/editor/containers/SidebarSettings.js
+++ server/zanata-frontend/src/frontend/app/editor/containers/SidebarSettings.js
@@ -1,68 +1,18 @@
 import React, {PropTypes} from 'react'
-import {Button, Checkbox} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
 import Icon from '../../components/Icon'
+import ValidationOptions from '../components/ValidationOptions'
 
 const SidebarSettings = React.createClass({
 
-  propTypes: {
+  static propTypes: {
     /* close the sidebar */
     close: PropTypes.func.isRequired
   },
 
   sidebarDetails () {
     return (
-      <div>
-        <h2 className="validation">Validation options</h2>
-        <ul>
-          <li>
-            <Checkbox checked>
-              HTML/XML tags
-            </Checkbox>
-          </li>
-          <li>
-            <Checkbox checked>
-              Java variables
-            </Checkbox>
-          </li>
-          <li>
-            <Checkbox checked>
-              Leading/trailing newline (\n)
-            </Checkbox>
-          </li>
-          <li>
-            <Checkbox>
-              Positional printf (XSI extension)
-            </Checkbox>
-          </li>
-          <li>
-            <Checkbox>
-              Printf variables
-            </Checkbox>
-          </li>
-          <li>
-            <Checkbox checked>
-              Tab characters (\t)
-            </Checkbox>
-          </li>
-          <li>
-            <Checkbox checked>
-              XML entity reference
-            </Checkbox>
-          </li>
-        </ul>
-        <Button title="Save"
-          className="Button Button--spacing u-rounded Button--primary">
-          Save
-        </Button>
-        <Button title="Load"
-          className="Button Button--spacing u-rounded Button--primary">
-          Load
-        </Button>
-        <Button title="Restore defaults"
-          className="Button u-rounded Button--secondary">
-          Restore defaults
-        </Button>
-      </div>
+        {ValidationOptions}
     )
   },
 
