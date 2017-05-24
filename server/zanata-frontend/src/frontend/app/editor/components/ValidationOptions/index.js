e9904092a070f904b1921050a756f26a3fab1576
kgough <kgough@redhat.com>
2017-05-24 12:15:07 +1000

    feat(ZNTA-1736): refactoring of sidebar css (wip)

--- server/zanata-frontend/src/frontend/app/editor/components/ValidationOptions/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/ValidationOptions/index.js
@@ -2,52 +2,27 @@ import React from 'react'
 import { Checkbox } from 'react-bootstrap'
 
 const ValidationOptions = React.createClass({
-
   render: function () {
+    const validations = ['HTML/XML tags', 'Java variables', 'Leading/trailing' +
+    ' newline (n)', 'Positional printf (XSI extension)', 'Printf variables',
+    'Tab characters (t)', 'XML entity reference']
+    const checkboxes = validations.map((validation, index) => (
+      <li key={index}>
+        <Checkbox checked>
+          {validation}
+        </Checkbox>
+      </li>
+    ))
     return (
-        <div>
-          <h2 className="validation">Validation options</h2>
-          <ul>
-            <li>
-              <Checkbox checked>
-                HTML/XML tags
-              </Checkbox>
-            </li>
-            <li>
-              <Checkbox checked>
-                Java variables
-              </Checkbox>
-            </li>
-            <li>
-              <Checkbox checked>
-                Leading/trailing newline (\n)
-              </Checkbox>
-            </li>
-            <li>
-              <Checkbox>
-                Positional printf (XSI extension)
-              </Checkbox>
-            </li>
-            <li>
-              <Checkbox>
-                Printf variables
-              </Checkbox>
-            </li>
-            <li>
-              <Checkbox checked>
-                Tab characters (\t)
-              </Checkbox>
-            </li>
-            <li>
-              <Checkbox checked>
-                XML entity reference
-              </Checkbox>
-            </li>
-          </ul>
-        </div>
+      <div>
+        <h2 className="validation">Validation options</h2>
+        <ul>
+          {checkboxes}
+        </ul>
+      </div>
     )
   }
-  })
+})
 
 export default ValidationOptions
 
