e7e5049818394614f634f0ab6bda8bcc09476cd0
David Mason <drdmason@gmail.com>
2017-05-26 11:31:16 +1000

    wip: add logging around intl polyfill loading

--- server/zanata-frontend/src/frontend/app/editor/config/index.js
+++ server/zanata-frontend/src/frontend/app/editor/config/index.js
@@ -5,6 +5,6 @@ const config = window.config || {}
 
 /* The part of the path that is just the server deployment path. e.g. if the
  * server is deployed at example.com/zanata then this will be /zanata */
-config.baseUrl = config.baseUrl || ''
+export const baseUrl = config.baseUrl || ''
 
 export default config
--- server/zanata-frontend/src/frontend/app/editor/index.js
+++ server/zanata-frontend/src/frontend/app/editor/index.js
@@ -119,8 +119,11 @@ if (window.Intl) {
   runApp()
 } else {
   // Intl not present, so polyfill it.
-  require.ensure([], () => {
+  require.ensure([], (require) => {
     // This is 'require' on purpose, do not change to 'import'
+    console.error('Trying to load intl module. Expected path:')
+    console.dir(__webpack_public_path__) // eslint-disable-line
+    console.error('expected filename: intl-polyfill.min.js')
     require('intl')
     runApp()
   }, 'intl-polyfill')
