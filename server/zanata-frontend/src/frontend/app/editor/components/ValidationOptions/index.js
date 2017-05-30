fae87bfa73e9a0211a1493edd82173d8119061bd
kgough <kgough@redhat.com>
2017-05-30 12:16:05 +1000

    feat(ZNTA-1736) changes to ValidationOptions

--- server/zanata-frontend/src/frontend/app/editor/components/ValidationOptions/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/ValidationOptions/index.js
@@ -1,28 +1,27 @@
 import React from 'react'
 import { Checkbox } from 'react-bootstrap'
 
-const ValidationOptions = React.createClass({
-  render: function () {
-    const validations = ['HTML/XML tags', 'Java variables', 'Leading/trailing' +
-    ' newline (n)', 'Positional printf (XSI extension)', 'Printf variables',
-    'Tab characters (t)', 'XML entity reference']
-    const checkboxes = validations.map((validation, index) => (
-      <li key={index}>
-        <Checkbox checked>
-          {validation}
-        </Checkbox>
-      </li>
-    ))
-    return (
-      <div>
-        <h2 className="validation">Validation options</h2>
-        <ul>
-          {checkboxes}
-        </ul>
-      </div>
-    )
-  }
-})
+const validations = ['HTML/XML tags', 'Java variables', 'Leading/trailing' +
+' newline (n)', 'Positional printf (XSI extension)', 'Printf variables',
+  'Tab characters (t)', 'XML entity reference']
+
+const ValidationOptions = () => {
+  const checkboxes = validations.map((validation, index) => (
+    <li key={index}>
+      <Checkbox checked>
+        {validation}
+      </Checkbox>
+    </li>
+  ))
+  return (
+    <div>
+      <h2 className="validation">Validation options</h2>
+      <ul>
+        {checkboxes}
+      </ul>
+    </div>
+  )
+}
 
 export default ValidationOptions
 
