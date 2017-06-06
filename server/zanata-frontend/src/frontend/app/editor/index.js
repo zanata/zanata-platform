19697e04bcb7156bb0975a557e05d5037a2db8ec
David Mason <drdmason@gmail.com>
2017-06-06 10:44:59 +1000

    feat(tablet-support): add polyfills for some tablet browsers (#341)
    * feat(ZNTA-1988): include babel polyfill in editor
    * feat(ZNTA-1989): conditionally load Intl polyfill
    * refactor(editor): move config-loading to a self-contained module
      This will allow us better error-checking, adding defaults and mocking
      for the editor config.
    * feat(ZNTA-1989): request intl-polyfill from the correct path
    * fix(editor-routes): use correct basename for editor when server is deployed to non-root context
    * feat(ZNTA-1989): add fallback path for webpack public path

--- server/zanata-frontend/src/frontend/app/editor/index.js
+++ server/zanata-frontend/src/frontend/app/editor/index.js
@@ -1,5 +1,7 @@
+import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { baseUrl } from './config'
 import { locale, formats } from './config/intl'
 import { addLocaleData, IntlProvider } from 'react-intl'
 import { createStore, applyMiddleware } from 'redux'
@@ -19,6 +21,10 @@ import rootReducer from './reducers'
 import Root from './containers/Root'
 import NeedSlugMessage from './containers/NeedSlugMessage'
 
+// Set the path that webpack will try to load extra chunks from
+// This is needed to load intl-polyfill
+__webpack_public_path__ = baseUrl || '/' // eslint-disable-line
+
 import './index.css'
 
 /**
@@ -32,77 +38,90 @@ import './index.css'
  *  - rendering the React component tree to the page
  *  - ensuring the required css for the React component tree is available
  */
+function runApp () {
+  // TODO add all the relevant locale data
+  // Something like:
+  //  import en from './react-intl/locale-data/en'
+  //  import de from './react-intl/locale-data/de'
+  //    ... then just addLocaleData(en) etc.
+  // See https://github.com/yahoo/react-intl/blob/master/UPGRADE.md
+  // if ('ReactIntlLocaleData' in window) {
+  //   Object.keys(window.ReactIntlLocaleData).forEach(lang => {
+  //     addLocaleData(window.ReactIntlLocaleData[lang])
+  //   })
+  // }
+  addLocaleData({
+    locale: 'en-US'
+  })
 
-// TODO add all the relevant locale data
-// Something like:
-//  import en from './react-intl/locale-data/en'
-//  import de from './react-intl/locale-data/de'
-//    ... then just addLocaleData(en) etc.
-// See https://github.com/yahoo/react-intl/blob/master/UPGRADE.md
-// if ('ReactIntlLocaleData' in window) {
-//   Object.keys(window.ReactIntlLocaleData).forEach(lang => {
-//     addLocaleData(window.ReactIntlLocaleData[lang])
-//   })
-// }
-addLocaleData({
-  locale: 'en-US'
-})
+  // example uses createHistory, but the latest bundles history with
+  // react-router and has some defaults, so now I am just using one of those.
+  // const history = createHistory()
+  const history = browserHistory
+  history.basename = baseUrl
 
-// example uses createHistory, but the latest bundles history with react-router
-// and has some defaults, so now I am just using one of those.
-// const history = createHistory()
-const history = browserHistory
-
-const loggerMiddleware = createLogger({
-  predicate: (getState, action) =>
-    process.env && (process.env.NODE_ENV === 'development'),
-  actionTransformer: (action) => {
-    if (typeof action.type !== 'symbol') {
-      console.warn('You should use a Symbol for this action type: ' +
-        String(action.type))
+  const loggerMiddleware = createLogger({
+    predicate: (getState, action) =>
+      process.env && (process.env.NODE_ENV === 'development'),
+    actionTransformer: (action) => {
+      if (typeof action.type !== 'symbol') {
+        console.warn('You should use a Symbol for this action type: ' +
+          String(action.type))
+      }
+      return {
+        ...action,
+        // allow symbol action type to be printed properly in logs
+        type: String(action.type)
+      }
     }
-    return {
-      ...action,
-      // allow symbol action type to be printed properly in logs
-      type: String(action.type)
-    }
-  }
-})
+  })
 
-const reduxRouterMiddleware = syncHistory(history)
-const createStoreWithMiddleware =
-  applyMiddleware(
-    titleUpdateMiddleware,
-    newContextFetchMiddleware,
-    searchSelectedPhraseMiddleware,
-    reduxRouterMiddleware,
-    thunk,
-    apiMiddleware,
-    // must run after thunk because it fails with thunks
-    getStateInActions,
-    loggerMiddleware
-  )(createStore)
+  const reduxRouterMiddleware = syncHistory(history)
+  const createStoreWithMiddleware =
+    applyMiddleware(
+      titleUpdateMiddleware,
+      newContextFetchMiddleware,
+      searchSelectedPhraseMiddleware,
+      reduxRouterMiddleware,
+      thunk,
+      apiMiddleware,
+      // must run after thunk because it fails with thunks
+      getStateInActions,
+      loggerMiddleware
+    )(createStore)
 
-const store = createStoreWithMiddleware(rootReducer)
-reduxRouterMiddleware.listenForReplays(store)
+  const store = createStoreWithMiddleware(rootReducer)
+  reduxRouterMiddleware.listenForReplays(store)
 
-const rootElement = document.getElementById('appRoot')
+  const rootElement = document.getElementById('appRoot')
 
-// FIXME current (old) behaviour when not enough params are specified is to
-//       reset to blank app and not even keep the project/version part of the
-//       URL. As soon as it has the /translate part of the URL it grabs the
-//       first doc and language in the list and goes ahead.
-//   Should be able to do better than that.
+  // FIXME current (old) behaviour when not enough params are specified is to
+  //       reset to blank app and not even keep the project/version part of the
+  //       URL. As soon as it has the /translate part of the URL it grabs the
+  //       first doc and language in the list and goes ahead.
+  //   Should be able to do better than that.
 
-ReactDOM.render(
-  <IntlProvider locale={locale} formats={formats}>
-    <Provider store={store}>
-      <Router history={history}>
-        {/* The ** is docId, captured as params.splat by react-router. */}
-        <Route
-          path="/project/translate/:projectSlug/v/:versionSlug/**"
-          component={Root} />
-        <Route path="/*" component={NeedSlugMessage} />
-      </Router>
-    </Provider>
-  </IntlProvider>, rootElement)
+  ReactDOM.render(
+    <IntlProvider locale={locale} formats={formats}>
+      <Provider store={store}>
+        <Router history={history}>
+          {/* The ** is docId, captured as params.splat by react-router. */}
+          <Route
+            path="/project/translate/:projectSlug/v/:versionSlug/**"
+            component={Root} />
+          <Route path="/*" component={NeedSlugMessage} />
+        </Router>
+      </Provider>
+    </IntlProvider>, rootElement)
+}
+
+if (window.Intl) {
+  runApp()
+} else {
+  // Intl not present, so polyfill it.
+  require.ensure([], (require) => {
+    // This is 'require' on purpose, do not change to 'import'
+    require('intl')
+    runApp()
+  }, 'intl-polyfill')
+}
