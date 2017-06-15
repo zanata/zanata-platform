8895f8267b6afaa997d7311d35130f27d941fa61
David Mason <drdmason@gmail.com>
2017-06-15 13:36:17 +1000

    chore(ZNTA-1922): replace react-chartjs with react-chartjs-2

--- server/zanata-frontend/src/frontend/app/containers/UserProfile/ContributionChart.jsx
+++ server/zanata-frontend/src/frontend/app/containers/UserProfile/ContributionChart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import utilsDate from '../../utils/DateHelper'
-import { Line as LineChart } from 'react-chartjs'
+import { Line as LineChart } from 'react-chartjs-2'
 
 var defaultChartOptions = {
   animationEasing: 'easeOutQuint',
@@ -145,8 +145,8 @@ class ContributionChart extends React.Component {
       <LineChart
         data={chartData}
         options={chartOptions}
-        width='800'
-        height='250' />
+        width={800}
+        height={250} />
     )
   }
 }
