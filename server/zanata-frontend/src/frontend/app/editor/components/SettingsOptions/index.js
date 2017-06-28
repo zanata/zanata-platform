5af2c3cc2757ad3601f14adf8aa2007e45d0151c
kgough <kgough@redhat.com>
2017-06-28 15:10:42 +1000

    feat(ZNTA-2070) WIP SettingsOptions component

--- server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/index.js
@@ -6,10 +6,6 @@ const settings =
     ['Enter key saves immediately',
       'Syntax highlighting']
 
-const layouts =
-    ['Suggestions diff',
-      'Panel layout']
-
 const SettingsOptions = ({states, updateSettingsOption}) => {
   const checkboxes = settings.map((setting, index) => (
     <li key={index}>
@@ -19,24 +15,11 @@ const SettingsOptions = ({states, updateSettingsOption}) => {
           onChange={updateSettingsOption} />
     </li>
   ))
-  const defaults = layouts.map((layout, index) => (
-      <li key={index}>
-        <LayoutCheckbox
-            layout={layout}
-            checked={states[layout]}
-            onChange={updateSettingsOption} />
-      </li>
-  ))
+
   return (
     <div className="settings-options">
-      <h2>Editor options</h2>
       <ul>
         {checkboxes}
-      </ul>
-      <br />
-      <p>Set current layout as default:</p>
-      <ul>
-      {defaults}
       </ul>
     </div>
   )
@@ -70,28 +53,6 @@ class SettingsCheckbox extends React.Component {
         <Checkbox checked={checked}
            onChange={this.onChange}>
           {setting}
-        </Checkbox>
-    )
-  }
-}
-
-class LayoutCheckbox extends React.Component {
-  static propTypes = {
-    layout: PropTypes.string.isRequired,
-    checked: PropTypes.bool.isRequired,
-    /* Will be called with (validation, newValue) */
-    onChange: PropTypes.func.isRequired
-  }
-
-  onChange = (event) => {
-    this.props.onChange(this.props.layout, event.target.checked)
-  }
-
-  render () {
-    const { layout, checked } = this.props
-    return (
-        <Checkbox checked={checked} onChange={this.onChange}>
-          {layout}
         </Checkbox>
     )
   }
