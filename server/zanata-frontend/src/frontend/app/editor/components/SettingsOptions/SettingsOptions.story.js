9c62880a09221fff5d0e86b686d14b419307d8f3
kgough <kgough@redhat.com>
2017-06-29 20:34:39 +1000

    feat(ZNTA-2070) WIP: default props for SettingsOptions component

--- server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/SettingsOptions.story.js
+++ server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/SettingsOptions.story.js
@@ -5,29 +5,34 @@ import RealSettingsOptions from '.'
 
 class SettingsOptions extends React.Component {
   static propTypes = {
-    settings: PropTypes.shape.isRequired,
-    updateSettingsOption: PropTypes.func.isRequired
+    settings: PropTypes.shape({
+      id: PropTypes.any.isRequired, // I will update this to whatever I use when I wire it up
+      label: PropTypes.string.isRequired,
+      active: PropTypes.bool.isRequired
+    }).isRequired,
+    /* arguments: (any: settingId, bool: active) */
+    updateSetting: PropTypes.func.isRequired
   }
   constructor (props) {
     super(props)
     this.state = props.state
   }
-  updateSettingsOption = (settings, checked) => {
+  updateSettingOption = (settings, checked) => {
     // record the check state in the wrapper
     this.setState({ [settings]: checked })
     // call the real one that was passed in
-    this.props.updateSettingsOption(settings, checked)
+    this.props.updateSettingOption(settings, checked)
   }
   render () {
     return (
         <RealSettingsOptions
-            updateSettingsOption={this.updateSettingsOption}
+            updateSettingOption={this.updateSettingOption}
             states={this.state} />
     )
   }
 }
 
-const updateAction = action('updateSettingsOption')
+const updateAction = action('updateSettingOption')
 const settings =
     ['HTML/XML tags',
       'Java variables',
@@ -41,6 +46,6 @@ storiesOf('SettingsOptions', module)
     .add('default', () => (
         <SettingsOptions
             settings={settings}
-            updateSettingsOption={action(updateAction)} />
+            updateSettingOption={action(updateAction)} />
     ))
 
--- server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/index.js
@@ -2,16 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import SettingOption from '../SettingOption'
 
-const settings =
-    ['HTML/XML tags',
-      'Java variables',
-      'Leading/trailing newline (n)',
-      'Positional printf (XSI extension)',
-      'Printf variables',
-      'Tab characters (t)',
-      'XML entity reference']
-
 const SettingsOptions = ({states, updateSettingOption}) => {
+  const settings =
+      ['HTML/XML tags',
+        'Java variables',
+        'Leading/trailing newline (n)',
+        'Positional printf (XSI extension)',
+        'Printf variables',
+        'Tab characters (t)',
+        'XML entity reference']
   const checkboxes = settings.map((settings, index) => (
       <li key={index}>
         <SettingOption
@@ -26,6 +25,16 @@ const SettingsOptions = ({states, updateSettingOption}) => {
         </ul>
       </div>
   )
+}
+
+SettingsOptions.propTypes = {
+  settings: PropTypes.shape({
+    id: PropTypes.any.isRequired, // I will update this to whatever I use when I wire it up
+    label: PropTypes.string.isRequired,
+    active: PropTypes.bool.isRequired
+  }).isRequired,
+  /* arguments: (any: settingId, bool: active) */
+  updateSettingOption: PropTypes.func.isRequired
 }
 
 export default SettingsOptions
