75d9b3c5ccb4b8976b1f549b9ed84f1f4fdee093
kgough <kgough@redhat.com>
2017-06-29 18:21:01 +1000

    feat(ZNTA-2070) SettingOption component created

--- server/zanata-frontend/src/frontend/app/editor/components/SettingOption/SettingOption.story.js
+++ server/zanata-frontend/src/frontend/app/editor/components/SettingOption/SettingOption.story.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { storiesOf, action } from '@kadira/storybook'
-import SettingOption from '.'
+import RealSettingOption from '.'
+
+class SettingOption extends React.Component {
+  static propTypes = {
+    setting: PropTypes.shape.isRequired,
+    updateSetting: PropTypes.func.isRequired
+  }
+  constructor (props) {
+    super(props)
+    this.state = props.state
+  }
+  updateSetting = (setting, checked) => {
+    // record the check state in the wrapper
+    this.setState({ [setting]: checked })
+    // call the real one that was passed in
+    this.props.updateSetting(setting, checked)
+  }
+  render () {
+    return (
+        <RealSettingOption
+            updateSetting={this.updateSetting}
+            states={this.state} />
+    )
+  }
+}
+
+const updateAction = action('updateSetting')
 
 storiesOf('SettingOption', module)
   .add('default', () => (
@@ -11,5 +37,5 @@ storiesOf('SettingOption', module)
           label: 'Krankenwagen',
           active: {true}
      }}
-      updateSetting={action(updateSetting)} />
+      updateSetting={action(updateAction)} />
   ))
