f57c8c86fb581e7bf5fa6b23727cf1cba8732a0b
kgough <kgough@redhat.com>
2017-06-29 18:45:10 +1000

    feat(ZNTA-2070) added working checkboxes to SettingOptoin and SettingsOptions stories - still WIP

--- server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/SettingsOptions.story.js
+++ server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/SettingsOptions.story.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { storiesOf, action } from '@kadira/storybook'
-import SettingsOptions from '.'
+import RealSettingsOptions from '.'
+
+class SettingsOptions extends React.Component {
+  static propTypes = {
+    settings: PropTypes.shape.isRequired,
+    updateSettingsOption: PropTypes.func.isRequired
+  }
+  constructor (props) {
+    super(props)
+    this.state = props.state
+  }
+  updateSettingsOption = (settings, checked) => {
+    // record the check state in the wrapper
+    this.setState({ [settings]: checked })
+    // call the real one that was passed in
+    this.props.updateSettingsOption(settings, checked)
+  }
+  render () {
+    return (
+        <RealSettingsOptions
+            updateSettingsOption={this.updateSettingsOption}
+            states={this.state} />
+    )
+  }
+}
+
+const updateAction = action('updateSettingsOption')
 
 storiesOf('SettingsOptions', module)
     .add('default', () => (
@@ -11,5 +37,6 @@ storiesOf('SettingsOptions', module)
               label: 'Krankenwagen',
               active: {true}
             }}
-            updateSetting={action(updateSetting)} />
+            updateSettingsOption={action(updateAction)} />
     ))
+
--- server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/index.js
@@ -2,15 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import SettingOption from '../SettingOption'
 
-const SettingsOptions = ({states, updateSettingsOption}) => {
-  const checkboxes = settings.map((setting, index) => (
-    <li key={index}>
+class SettingsOptions extends React.Component {
+  static propTypes = {
+    settings: PropTypes.shape({
+      id: PropTypes.any.isRequired, // I will update this to whatever I use when I wire it up
+      label: PropTypes.string.isRequired,
+      active: PropTypes.bool.isRequired
+    }).isRequired,
+    /* arguments: (any: settingId, bool: active) */
+    updateSettingsOption: PropTypes.func.isRequired
+  }
+
+  onChange = (event) => {
+    this.props.onChange(this.props.setting, event.target.checked)
+  }
+
+  render () {
+  const { setting, checked } = this.props
+  const checkboxes = (
+    <li>
       <SettingOption
           setting={setting}
-          checked={states[setting]}
-          onChange={updateSettingsOption} />
+          checked={true}
+          onChange={this.onChange} />
     </li>
-  ))
+  )
 
   return (
     <div className="settings-options">
@@ -19,16 +35,7 @@ const SettingsOptions = ({states, updateSettingsOption}) => {
       </ul>
     </div>
   )
-}
-
-SettingsOptions.propTypes = {
-    settings: PropTypes.shape({
-      id: PropTypes.any.isRequired, // I will update this to whatever I use when I wire it up
-      label: PropTypes.string.isRequired,
-      active: PropTypes.bool.isRequired
-    }).isRequired,
-    /* arguments: (any: settingId, bool: active) */
-    updateSettingsOption: PropTypes.func.isRequired
+  }
 }
 
 export default SettingsOptions
