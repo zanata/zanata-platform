da9717f910b8073fb3d1c228558c11c90db5677b
Patrick Huang <pahuang@redhat.com>
2017-06-29 10:46:03 +1000

    add new option to use an ephemeral database container

--- server/docker/rundb.sh
+++ server/docker/rundb.sh
@@ -16,7 +16,10 @@ VOLUME_DIR=$HOME/docker-volumes/zanata-mariadb
 
 mkdir -p $VOLUME_DIR
 
-while getopts ":n:h" opt; do
+CONTAINER_NAME=zanatadb
+VOLUME_OPT="-v $VOLUME_DIR:/var/lib/mysql:Z"
+EPHEMERAL=false
+while getopts ":n:eh" opt; do
   case ${opt} in
     n)
       echo "===== set docker network to $OPTARG ====="
@@ -28,6 +31,17 @@ while getopts ":n:h" opt; do
       echo "-h                 : display help"
       exit
       ;;
+    e)
+      echo "===== starting an ephemeral database container ===="
+      if [ ! -z  $(docker ps --all --quiet --filter name=$CONTAINER_NAME) ]; then
+           echo "removing $CONTAINER_NAME container"
+           docker rm -f $CONTAINER_NAME
+      fi
+      # instead of giving the container volume mapping,
+      # we tell it to remove itself once stopped.
+      VOLUME_OPT=" --rm "
+      EPHEMERAL=true
+      ;;
     \?)
       echo "Invalid option: -${OPTARG}. Use -h for help" >&2
       exit 1
@@ -37,13 +51,22 @@ done
 
 ensure_docker_network
 
-docker run --name zanatadb \
+docker run --name $CONTAINER_NAME \
   -e MYSQL_USER=$DB_USERNAME -e MYSQL_PASSWORD=$DB_PASSWORD \
   -e MYSQL_DATABASE=$DB_SCHEMA -e MYSQL_ROOT_PASSWORD=$DB_ROOT_PASSWORD \
   -P --net=${DOCKER_NETWORK} \
-  -v $VOLUME_DIR:/var/lib/mysql:Z \
+  ${VOLUME_OPT} \
   -d mariadb:10.1 \
   --character-set-server=utf8 --collation-server=utf8_general_ci
 
 echo ''
-echo 'Please use the command "docker logs zanatadb" to check that MariaDB starts correctly.'
+echo "Please use the command 'docker logs $CONTAINER_NAME' to check that MariaDB starts correctly."
+
+if [ "$EPHEMERAL" == 'true' ]
+then
+    echo "====================================================================="
+    echo "Once MariaDB and Zanata are running, execute below commands to create an admin user"
+    echo "docker cp $DIR/conf/admin-user-setup.sql $CONTAINER_NAME:/tmp/"
+    echo "docker exec $CONTAINER_NAME /bin/sh -c 'mysql -u$DB_USERNAME -p$DB_PASSWORD $DB_SCHEMA </tmp/admin-user-setup.sql'"
+    echo "====================================================================="
+fi
