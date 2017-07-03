ea955d671c2af8caff62f2c1ebe702027a184d17
kgough <kgough@redhat.com>
2017-07-03 14:35:28 +1000

    feat(ZNTA-2070) removed wrapper class from SettingOption and SettingsOptions stories

--- server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/SettingsOptions.story.js
+++ server/zanata-frontend/src/frontend/app/editor/components/SettingsOptions/SettingsOptions.story.js
@@ -1,46 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { storiesOf, action } from '@kadira/storybook'
-import RealSettingsOptions from '.'
-
-class SettingsOptions extends React.Component {
-  static propTypes = {
-    settings: PropTypes.shape({
-      id: PropTypes.any.isRequired,
-      label: PropTypes.string.isRequired,
-      active: PropTypes.bool.isRequired
-    }).isRequired,
-    updateSetting: PropTypes.func.isRequired
-  }
-  constructor (props) {
-    super(props)
-    this.state = { settings: props.settings }
-  }
-  updateSetting = (id, active) => {
-    // record the check state in the wrapper
-    this.setState(newState => ({
-      settings: newState.settings.map(setting => {
-        if (setting.id === id) {
-          return {
-            ...setting,
-            active
-          }
-        } else {
-          return setting
-        }
-      })
-    }))
-    // call the real one that was passed in
-    this.props.updateSetting(id, active)
-  }
-  render () {
-    return (
-      <RealSettingsOptions
-        settings={ this.state.settings }
-        updateSetting={ this.updateSetting } />
-    )
-  }
-}
+import SettingsOptions from '.'
 
 const updateSetting = action('updateSetting')
 const listUnchecked =
