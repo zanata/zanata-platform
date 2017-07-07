fee736847dc59d8095dff9cf6a3dc0d00f409d08
David Mason <drdmason@gmail.com>
2017-07-07 13:04:42 +1000

    refactor(phrase filter): use flux standard actions for status filter
    All the actions will be updated to be flux standard actions (FSAs), and
    I have edited these actions, so this is an appropriate time to update
    them.
    This also removes the warning for using a non-symbol as an action type,
    since all action types will be changed to strings so that they are
    serializable (required for advanced development tools such as time-travel
    debugging).

--- server/zanata-frontend/src/frontend/app/editor/actions/phrases-filter-actions.js
+++ server/zanata-frontend/src/frontend/app/editor/actions/phrases-filter-actions.js
@@ -1,16 +1,15 @@
+import { createAction } from 'redux-actions'
 import {
   RESET_STATUS_FILTERS,
   UPDATE_STATUS_FILTER
 } from './phrases-action-types'
 import { clampPage } from './controls-header-actions'
 
-export function resetStatusFilter () {
-  return {type: RESET_STATUS_FILTERS}
-}
+export const resetStatusFilter = createAction(RESET_STATUS_FILTERS)
 
 export function updateStatusFilter (status) {
   return (dispatch) => {
-    dispatch({type: UPDATE_STATUS_FILTER, status})
+    dispatch(createAction(UPDATE_STATUS_FILTER)(status))
     // needed in case there is a different page count after filtering
     dispatch(clampPage())
   }
