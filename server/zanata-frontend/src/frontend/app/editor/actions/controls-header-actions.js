f57e8d9b2a787d72074cc959742e76becd204b0b
David Mason <drdmason@gmail.com>
2017-07-23 16:34:42 +1000

    feat(ZNTA-975): WIP fixing all phrase list refs for filter, fixed paging
    Paging was using old location, changed to use selectors to it will adapt
    when selectors change next time.

--- server/zanata-frontend/src/frontend/app/editor/actions/controls-header-actions.js
+++ server/zanata-frontend/src/frontend/app/editor/actions/controls-header-actions.js
@@ -1,6 +1,4 @@
-import {
-  calculateMaxPageIndexFromState
-} from '../utils/filter-paging-util'
+import { getLocation, getPageIndex, getMaxPageIndex } from '../selectors'
 import { replaceRouteQuery } from '../utils/RoutingHelpers'
 
 /* Adjust the page number so it is in the valid range.
@@ -16,31 +14,31 @@ export const UPDATE_PAGE = Symbol('UPDATE_PAGE')
 export function firstPage () {
   return (dispatch, getState) => {
     const pageIndex = 0
-    updatePage(dispatch, getState().routing.location, pageIndex)
+    updatePage(dispatch, getLocation(getState()), pageIndex)
   }
 }
 
 export function nextPage () {
   return (dispatch, getState) => {
-    const currentPageIndex = getState().phrases.paging.pageIndex
+    const currentPageIndex = getPageIndex(getState())
     const pageIndex = Math.min(currentPageIndex + 1,
-      calculateMaxPageIndexFromState(getState()))
-    updatePage(dispatch, getState().routing.location, pageIndex)
+      getMaxPageIndex(getState()))
+    updatePage(dispatch, getLocation(getState()), pageIndex)
   }
 }
 
 export function previousPage () {
   return (dispatch, getState) => {
-    const currentPageIndex = getState().phrases.paging.pageIndex
+    const currentPageIndex = getPageIndex(getState())
     const pageIndex = Math.max(currentPageIndex - 1, 0)
-    updatePage(dispatch, getState().routing.location, pageIndex)
+    updatePage(dispatch, getLocation(getState()), pageIndex)
   }
 }
 
 export function lastPage () {
   return (dispatch, getState) => {
-    const pageIndex = calculateMaxPageIndexFromState(getState())
-    updatePage(dispatch, getState().routing.location, pageIndex)
+    const pageIndex = getMaxPageIndex(getState())
+    updatePage(dispatch, getLocation(getState()), pageIndex)
   }
 }
 
