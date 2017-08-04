09a66c058ffa5190ee6a2c9cd5f7175c67d0da1f
Sean Flanigan <sflaniga@redhat.com>
2017-08-04 18:13:28 +1000

    test: rename *Tests -> *Test
    This helps to avoid problems with detection of test classes.

--- server/gwt-shared/src/test/java/org/zanata/webtrans/shared/validation/HtmlXmlTagValidationTest.java
+++ server/gwt-shared/src/test/java/org/zanata/webtrans/shared/validation/HtmlXmlTagValidationTest.java
@@ -39,7 +39,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Alex Eng <a href="mailto:aeng@redhat.com">aeng@redhat.com</a>
  *
  **/
-public class HtmlXmlTagValidationTests {
+public class HtmlXmlTagValidationTest {
     private HtmlXmlTagValidation htmlXmlTagValidation;
 
     private ValidationMessages messages;
