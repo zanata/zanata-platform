2223da6dbe74f6ed0c0ea6c6ce5fd17866d5bd4c
David Mason <drdmason@gmail.com>
2017-08-04 15:11:10 +1000

    chore(ZNTA-2119): add storybook info addon
    Includes example usage on editor <Button> component.

--- server/zanata-frontend/src/frontend/app/editor/components/Button/Button.story.js
+++ server/zanata-frontend/src/frontend/app/editor/components/Button/Button.story.js
@@ -1,62 +1,63 @@
 import React from 'react'
 import { storiesOf, action } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
 import Button from '.'
 
 /*
  * See .storybook/README.md for info on the component storybook.
  */
 storiesOf('Button', module)
-  .add('plain', () => (
+  .add('plain', withInfo()(() => (
     <Button title="Click should trigger onClick action"
-            onClick={action('onClick')}>
+      onClick={action('onClick')}>
       Unstyled button. Pretty plain. Should be used with some styles.
     </Button>
-  ))
-  .add('plain (disabled)', () => (
+  )))
+  .add('plain (disabled)', withInfo()(() => (
     <Button title="Should not dispatch onClick action"
-            onClick={action('onClick')}
-            disabled>
+      onClick={action('onClick')}
+      disabled>
       Plain disabled button. Click event should <strong>not</strong> be fired.
     </Button>
-  ))
-  .add('colour styles', () => (
+  )))
+  .add('colour styles', withInfo()(() => (
     <ul>
       <li>
         <Button title="Click should trigger onClick action"
-                onClick={action('onClick')}
-                className="Button Button--primary">
+          onClick={action('onClick')}
+          className="Button Button--primary">
           Button Button--primary
         </Button>
       </li>
       <li>
         <Button title="Click should trigger onClick action"
-                onClick={action('onClick')}
-                className="Button Button--success">
+          onClick={action('onClick')}
+          className="Button Button--success">
           Button Button--success
         </Button>
       </li>
       <li>
         <Button title="Click should trigger onClick action"
-                onClick={action('onClick')}
-                className="Button Button--unsure">
+          onClick={action('onClick')}
+          className="Button Button--unsure">
           Button Button--unsure
         </Button>
       </li>
       <li>
         <Button title="Click should trigger onClick action"
-                onClick={action('onClick')}
-                className="Button Button--neutral">
+          onClick={action('onClick')}
+          className="Button Button--neutral">
           Button Button--neutral
         </Button>
       </li>
     </ul>
-  ))
-  .add('rounded and styled', () => (
+  )))
+  .add('rounded and styled', withInfo()(() => (
     <Button title="Styles from suggestion panel button"
-            onClick={action('onClick')}
-            className="Button Button--small u-rounded Button--primary">
+      onClick={action('onClick')}
+      className="Button Button--small u-rounded Button--primary">
       Button Button--small u-rounded Button--primary
     </Button>
-  ))
+  )))
 
 // TODO add more variety of styles. See if there is a stylesheet to compare to
