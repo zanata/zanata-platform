30d84bdcd60e85d85c00ad98fe22ed1e546767a2
David Mason <drdmason@gmail.com>
2017-08-09 15:17:36 +1000

    feat(ZNTA-2156): make dropdown open and close

--- server/zanata-frontend/src/frontend/app/editor/components/RejectTranslationModal/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/RejectTranslationModal/index.js
@@ -1,58 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Dropdown from '../Dropdown'
 import { Modal } from '../../../components'
 /**
  * TODO add a concise description of this component
  */
-const toggleDropdown = () => {}
-const RejectTranslationModal = ({
-  show,
-  key,
-  className,
-  onHide,
-  isOpen
-}) => {
-  return (
-    <Modal show={show}
-      onHide={close}
-      key="reject-translation-modal"
-      className="suggestions-modal">
-      <Modal.Header>
-        <Modal.Title><small><span className="pull-left">
-        Reject translation</span></small></Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        Reason:
-        <Dropdown enabled isOpen
-          onToggle={toggleDropdown()}
-          className="Dropdown--right u-sMV-1-2">
-          <Dropdown.Button>
-            <a className="Dropdown-item">
-              Dropdown button
-            </a>
-          </Dropdown.Button>
-          <Dropdown.Content>
-            <ul>
-              <li>Cat</li>
-              <li>Dog</li>
-              <li>Honey Badger</li>
-              <li>Walrus</li>
-            </ul>
-          </Dropdown.Content>
-        </Dropdown>
-      </Modal.Body>
-    </Modal>
-  )
-}
+export class RejectTranslationModal extends Component {
+  static propTypes = {
+    show: PropTypes.bool,
+    className: PropTypes.string,
+    key: PropTypes.string,
+    onHide: PropTypes.func,
+    toggleDropdown: PropTypes.func.isRequired,
+    isOpen: PropTypes.bool.isRequired
+  }
 
-RejectTranslationModal.propTypes = {
-  show: PropTypes.bool,
-  className: PropTypes.string,
-  key: PropTypes.string,
-  onHide: PropTypes.func,
-  toggleDropdown: PropTypes.func.isRequired,
-  isOpen: PropTypes.bool.isRequired
+  constructor (props) {
+    super(props)
+    this.state = {
+      dropdownOpen: false
+    }
+  }
+
+  toggleDropdown = () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }))
+  }
+
+  render () {
+    const {
+      show,
+      key,
+      className,
+      onHide,
+      isOpen
+    } = this.props
+    return (
+      <Modal show={show}
+        onHide={close}
+        key="reject-translation-modal"
+        className="suggestions-modal">
+        <Modal.Header>
+          <Modal.Title><small><span className="pull-left">
+          Reject translation</span></small></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Reason:
+          <Dropdown enabled isOpen={this.state.dropdownOpen}
+            onToggle={this.toggleDropdown}
+            className="Dropdown--right u-sMV-1-2">
+            <Dropdown.Button>
+              <a className="Dropdown-item">
+                Dropdown button
+              </a>
+            </Dropdown.Button>
+            <Dropdown.Content>
+              <ul>
+                <li onClick={this.toggleDropdown}>Cat</li>
+                <li onClick={this.toggleDropdown}>Dog</li>
+                <li onClick={this.toggleDropdown}>Honey Badger</li>
+                <li onClick={this.toggleDropdown}>Walrus</li>
+              </ul>
+            </Dropdown.Content>
+          </Dropdown>
+        </Modal.Body>
+      </Modal>
+    )
+  }
 }
 
 export default RejectTranslationModal
