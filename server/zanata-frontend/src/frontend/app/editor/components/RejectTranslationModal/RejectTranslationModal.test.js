bee8090426c11f8bd627bcf43196195dab2ff403
kgough <kgough@redhat.com>
2017-08-21 10:31:03 +1000

    updated snapshots for RejectTranslationModal

--- server/zanata-frontend/src/frontend/app/editor/components/RejectTranslationModal/RejectTranslationModal.test.js
+++ server/zanata-frontend/src/frontend/app/editor/components/RejectTranslationModal/RejectTranslationModal.test.js
@@ -7,9 +7,19 @@ import ReactDOMServer from 'react-dom/server'
 import RejectTranslationModal from '.'
 
 describe('RejectTranslationModal Test', () => {
-  it('can render', () => {
+  it('critical priority', () => {
     <RejectTranslationModal show isOpen={true}
       criteria="test"
       priority="Critical"  textState="u-textDanger" />
+  })
+  it('major priority', () => {
+    <RejectTranslationModal show isOpen={true}
+      criteria="test"
+      priority="Major"  textState="u-textWarning" />
+  })
+  it('minor priority', () => {
+    <RejectTranslationModal show isOpen={true}
+      criteria="test"
+      priority="Minor" />
   })
 })
