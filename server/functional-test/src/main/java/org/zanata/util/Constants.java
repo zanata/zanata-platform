4434b62da2bbee64c2bc598552b89e7cf780493a
Patrick Huang <pahuang@redhat.com>
2017-08-25 16:31:34 +1000

    remove unneeded enum value

--- server/functional-test/src/main/java/org/zanata/util/Constants.java
+++ server/functional-test/src/main/java/org/zanata/util/Constants.java
@@ -28,7 +28,7 @@ public enum Constants {
     projectsLink("Projects"), webDriverType("webdriver.type"), chrome, firefox,
     htmlUnit, sampleProjects("zanata.sample.projects.basedir"), zanataApiKey(
             "zanata.apikey"), zanataTranslatorKey("zanata.translatorkey"),
-    zanataAdminUser("zanata.admin.username"), webDriverWait("webdriver.wait");
+    webDriverWait("webdriver.wait");
 
     public static final int FIFTY_SEC = 50000;
     private String value;
