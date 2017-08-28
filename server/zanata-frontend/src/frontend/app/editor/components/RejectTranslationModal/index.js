71da7e1702b13a20b76ecc3783a5cff5a1bca808
kgough <kgough@redhat.com>
2017-08-28 11:51:50 +1000

    fix(ZNTA-2186) refactoring and other changes for PR

--- server/zanata-frontend/src/frontend/app/editor/components/RejectTranslationModal/index.js
+++ server/zanata-frontend/src/frontend/app/editor/components/RejectTranslationModal/index.js
@@ -74,7 +74,7 @@ export class RejectTranslationModal extends Component {
       <Modal show={show}
         onHide={close}
         key="reject-translation-modal"
-        id="rejection-modal">
+        id="RejectTranslationModal">
         <Modal.Header>
           <Modal.Title>Reject translation</Modal.Title>
         </Modal.Header>
--- server/zanata-frontend/src/frontend/app/editor/containers/SuggestionDetailsModal/index.js
+++ server/zanata-frontend/src/frontend/app/editor/containers/SuggestionDetailsModal/index.js
@@ -49,7 +49,7 @@ class SuggestionDetailsModal extends Component {
       <Modal
         show
         onHide={this.props.onClose}
-        id="suggestions-modal">
+        id="SuggestionDetailsModal">
         <Modal.Header>
           <Modal.Title><small><span className="pull-left">
           Suggestion Details</span></small></Modal.Title>
