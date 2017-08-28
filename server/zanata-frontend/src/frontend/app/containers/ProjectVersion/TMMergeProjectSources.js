47ebdf66c82548307109cebbfd6ab278695f5097
Patrick Huang <pahuang@redhat.com>
2017-08-28 14:14:45 +1000

    ZNTA-2093 - change according to new design

--- server/zanata-frontend/src/frontend/app/containers/ProjectVersion/TMMergeProjectSources.js
+++ server/zanata-frontend/src/frontend/app/containers/ProjectVersion/TMMergeProjectSources.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {Panel, InputGroup, Col, FormControl,
-  ToggleButtonGroup, ToggleButton} from 'react-bootstrap'
+import {InputGroup, Col, FormControl, OverlayTrigger, Radio,
+  Tooltip, Button} from 'react-bootstrap'
 import {
   Icon, LoaderText, DraggableVersionPanels
 } from '../../components'
 import ProjectVersionPanels from './ProjectVersionPanels'
+import Toggle from 'react-toggle'
 
 import {
   ProjectType, FromProjectVersionType
@@ -15,6 +16,13 @@ const DO_NOT_RENDER = undefined
 const ALL = 'ALL'
 const SAME = 'SAME'
 const OTHER = 'OTHER'
+
+const tooltip1 = (
+  <Tooltip id='from-project-source' title='From project source'>
+    Exact text matches from projects are used before exact matches in imported
+    TM. Fuzzy text matches from projects are used before fuzzy matches in
+    imported TM.
+  </Tooltip>)
 
 /*
  * Component to display TM merge from project sources
@@ -42,7 +50,7 @@ class TMMergeProjectSources extends Component {
     super(props)
     this.state = this.defaultState
   }
-  onFromProjectSelectionChange = (value) => {
+  onFromProjectSelectionChange = (value) => () => {
     this.setState((prevState, props) => ({
       fromProjectSelection: value
     }))
@@ -66,7 +74,7 @@ class TMMergeProjectSources extends Component {
     const fromVersionsPanel = this.state.fromProjectSelection !== OTHER
       ? DO_NOT_RENDER
       : (
-      <div>
+      <span>
         <Col xs={12}>
           <InputGroup>
             <InputGroup.Addon>
@@ -101,36 +109,42 @@ class TMMergeProjectSources extends Component {
             onDraggableMoveEnd={onDragMoveEnd}
             removeVersion={removeProjectVersion} />
         </Col>
-      </div>
+      </span>
       )
     return (
-      <div>
-        <Col xs={12} className='vmerge-boxes'>
-          <Panel>
-            <Col xs={12}>
-              <div className='vmerge-title'>
-                <span>From</span>
-                <span className="text-info"> Project Source</span>
-              </div>
-            </Col>
-            <Col xs={12} className='vmerge-searchbox'>
-              Search TM from
-              <ToggleButtonGroup
-                type="radio" name="button"
-                value={this.state.fromProjectSelection}
-                onChange={this.onFromProjectSelectionChange}>
-                <ToggleButton value={SAME}> this project
-                </ToggleButton>
-                <ToggleButton value={ALL}> all projects
-                </ToggleButton>
-                <ToggleButton value={OTHER}> other projects that I choose
-                </ToggleButton>
-              </ToggleButtonGroup>
-            </Col>
-            {fromVersionsPanel}
-          </Panel>
+      <span>
+        <Col xs={12}>
+          <div className='vmerge-title vmerge-title-flex'>
+            <span>
+              <Toggle icons={false} defaultChecked />
+            </span>
+            <span>From </span>
+            <span className="panel-name">Project Source</span>
+            <OverlayTrigger placement='right' overlay={tooltip1}>
+              <Button bsStyle="link" className="tooltip-btn">
+                <Icon name="info" className="s0 info-icon" />
+              </Button>
+            </OverlayTrigger>
+          </div>
         </Col>
-      </div>
+        <Col xs={12} className='vmerge-searchbox'>
+          Search TM from
+          <Radio name="fromProjectSelection" inline
+            checked={this.state.fromProjectSelection === SAME}
+            onChange={this.onFromProjectSelectionChange(SAME)}> this project
+          </Radio>
+          <Radio name="fromProjectSelection" inline
+            checked={this.state.fromProjectSelection === ALL}
+            onChange={this.onFromProjectSelectionChange(ALL)}> all projects
+          </Radio>
+          <Radio name="fromProjectSelection" inline
+            checked={this.state.fromProjectSelection === OTHER}
+            onChange={this.onFromProjectSelectionChange(OTHER)}> some
+            projects
+          </Radio>
+        </Col>
+        {fromVersionsPanel}
+      </span>
     )
   }
 }
