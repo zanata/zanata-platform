f56e5bcaf867d6bd48434acaf808ca06dffc8c72
David Mason <drdmason@gmail.com>
2017-09-25 12:42:16 +1000

    refactor(actions): make PHRASE_SUGGESTION_FINISHED_COPYING an FSA

--- server/zanata-frontend/src/frontend/app/editor/actions/suggestions-actions.js
+++ server/zanata-frontend/src/frontend/app/editor/actions/suggestions-actions.js
@@ -117,6 +117,9 @@ const textSuggestionFinishedCopying =
 
 const phraseSuggestionStartedCopying = createAction(
   PHRASE_SUGGESTION_STARTED_COPYING, (phraseId, index) => ({ phraseId, index }))
+const phraseSuggestionFinishedCopying = createAction(
+  PHRASE_SUGGESTION_FINISHED_COPYING,
+  (phraseId, index) => ({ phraseId, index }))
 
 function copyTextSuggestionN (index) {
   return (dispatch, getState) => {
@@ -143,10 +146,6 @@ function copyPhraseSuggestionN (phraseId, index) {
         500)
     }
   }
-}
-
-function phraseSuggestionFinishedCopying (phraseId, index) {
-  return { type: PHRASE_SUGGESTION_FINISHED_COPYING, phraseId, index }
 }
 
 export function textSuggestionsUpdated (
