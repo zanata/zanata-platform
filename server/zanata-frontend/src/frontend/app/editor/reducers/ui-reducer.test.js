233bd77758fbe0e388bd6915d2bdbbae98d64a52
David Mason <drdmason@gmail.com>
2017-09-25 13:23:51 +1000

    refactor(actions): make SUGGESTION_PANEL_HEIGHT_CHANGE an FSA

--- server/zanata-frontend/src/frontend/app/editor/reducers/ui-reducer.test.js
+++ server/zanata-frontend/src/frontend/app/editor/reducers/ui-reducer.test.js
@@ -139,7 +139,7 @@ describe('ui-reducer test', () => {
   it('can record suggestion panel height change', () => {
     const changedHeight = uiReducer(undefined, {
       type: SUGGESTION_PANEL_HEIGHT_CHANGE,
-      percentageHeight: 0.4
+      payload: 0.4
     })
     expect(changedHeight.panels.suggestions.heightPercent).toEqual(0.4)
   })
