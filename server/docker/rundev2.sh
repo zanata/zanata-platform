b0a08b1efa5460e16b0c0e0638f49c0445b7e5fd
Sean Flanigan <sflaniga@redhat.com>
2017-10-04 12:57:41 +1000

    Allow to use rundev2.sh from any dir

--- server/docker/rundev2.sh
+++ server/docker/rundev2.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# determine directory containing this script
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(realpath "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+
 docker build --tag wildfly-zanata-base .
 
 # First transform flattens everything
