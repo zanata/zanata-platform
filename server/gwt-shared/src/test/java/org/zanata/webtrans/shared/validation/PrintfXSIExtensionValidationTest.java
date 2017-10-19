eed3ac96ee91a00546890ce788ab72b8bd55a4b6
Alex Eng <aeng@redhat.com>
2017-10-19 12:37:34 +1000

    fix: printf validation (#586)
    https://zanata.atlassian.net/browse/ZNTA-2238

--- server/gwt-shared/src/test/java/org/zanata/webtrans/shared/validation/PrintfXSIExtensionValidationTest.java
+++ server/gwt-shared/src/test/java/org/zanata/webtrans/shared/validation/PrintfXSIExtensionValidationTest.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *         href="mailto:pahuang@redhat.com">pahuang@redhat.com</a>
  */
 public class PrintfXSIExtensionValidationTest {
-    private PrintfXSIExtensionValidation printfVariablesValidation;
+    private PrintfXSIExtensionValidation printfXSIExtensionValidation;
 
     private ValidationMessages messages;
 
@@ -28,26 +28,36 @@ public class PrintfXSIExtensionValidationTest {
         MockitoAnnotations.initMocks(this);
         messages = fakeValidationMessages();
 
-        printfVariablesValidation =
+        printfXSIExtensionValidation =
                 new PrintfXSIExtensionValidation(
                         ValidationId.PRINTF_XSI_EXTENSION, messages);
-        printfVariablesValidation.getRules().setEnabled(true);
+        printfXSIExtensionValidation.getRules().setEnabled(true);
     }
 
     @Test
     public void idAndDescriptionAreSet() {
-        assertThat(printfVariablesValidation.getId())
+        assertThat(printfXSIExtensionValidation.getId())
                 .isEqualTo(ValidationId.PRINTF_XSI_EXTENSION);
     }
 
     @Test
-    public void validPositionalVariables() {
+    public void validImplicitPositionalVariables() {
         String source = "%s: Read error at byte %s, while reading %lu byte";
         String target = "%1$s：Read error while reading %3$lu bytes，at %2$s";
         List<String> errorList =
-                printfVariablesValidation.validate(source, target);
+                printfXSIExtensionValidation.validate(source, target);
 
-        assertThat(errorList.size()).isEqualTo(0);
+        assertThat(errorList).isEmpty();
+    }
+
+    @Test
+    public void validExplicitPositionalVariables() {
+        String source = "%1$s: Read error at byte %2$s, while reading %3$lu byte";
+        String target = "%1$s：Read error while reading %3$lu bytes，at %2$s";
+        List<String> errorList =
+                printfXSIExtensionValidation.validate(source, target);
+
+        assertThat(errorList).isEmpty();
     }
 
     @Test
@@ -55,7 +65,7 @@ public class PrintfXSIExtensionValidationTest {
         String source = "%s: Read error at byte %s, while reading %lu byte";
         String target = "%1$s：Read error while reading %lu bytes，at %2$s";
         List<String> errorList =
-                printfVariablesValidation.validate(source, target);
+                printfXSIExtensionValidation.validate(source, target);
 
         assertThat(errorList.size()).isEqualTo(3);
 
@@ -70,7 +80,7 @@ public class PrintfXSIExtensionValidationTest {
         String source = "%s: Read error at byte %s, while reading %lu byte";
         String target = "%3$s：Read error while reading %99$lu bytes，at %2$s";
         List<String> errorList =
-                printfVariablesValidation.validate(source, target);
+                printfXSIExtensionValidation.validate(source, target);
 
         assertThat(errorList.size()).isEqualTo(3);
 
@@ -86,7 +96,7 @@ public class PrintfXSIExtensionValidationTest {
         String target = "%3$s：Read error while reading %3$lu bytes, at %2$s";
 
         List<String> errorList =
-                printfVariablesValidation.validate(source, target);
+                printfXSIExtensionValidation.validate(source, target);
 
         assertThat(errorList.size()).isEqualTo(3);
         assertThat(errorList).contains(
@@ -101,7 +111,7 @@ public class PrintfXSIExtensionValidationTest {
         String source = "%s of %d and %lu";
         String target = "%2$d %2$s %9$lu %z";
         List<String> errorList =
-                printfVariablesValidation.validate(source, target);
+                printfXSIExtensionValidation.validate(source, target);
         assertThat(errorList).contains(
                 messages.varPositionOutOfRange("%9$lu"),
                 messages.mixVarFormats(),
