8721b2d3eb8e195d7e920a8ac81fd29ce5d3c9c7
Alex Eng <aeng@redhat.com>
2017-10-20 10:52:19 +1000

    fix: avoid validation warning when project id has not changed
    https://zanata.atlassian.net/browse/ZNTA-1976

--- server/services/src/main/java/org/zanata/action/ProjectHome.java
+++ server/services/src/main/java/org/zanata/action/ProjectHome.java
@@ -717,6 +717,9 @@ public class ProjectHome extends SlugHome<HProject>
     }
 
     public boolean validateSlug(String slug, String componentId) {
+        if (getInstance().getSlug().equals(slug)) {
+            return true;
+        }
         if (!isSlugAvailable(slug)) {
             facesMessages.addToControl(componentId,
                     "This Project ID is not available");
