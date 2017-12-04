ff8ea75a19f63bc23291ab2c6f60a4a853ebdb43
Kathryn Gough <kgough@redhat.com>
2017-12-04 12:44:48 +1000

    WIP: concurrent editing stories (transunit items)

--- server/zanata-frontend/src/frontend/app/editor/components/ConcurrentEditing/Concurrent.story.js
+++ server/zanata-frontend/src/frontend/app/editor/components/ConcurrentEditing/Concurrent.story.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { Alert } from 'react-bootstrap'
+import { Alert, Button, Image } from 'react-bootstrap'
 import Icon from '../../../components/Icon'
-
+import {action} from "@storybook/react/dist/client/index";
 /*
  * See .storybook/README.md for info on the component storybook.
  */
@@ -16,4 +16,20 @@ storiesOf('Concurrent editing', module)
           <a href="">Resolve merge conflicts</a> before continuing.
         </span>
         </Alert>
+    ))
+    .add('transunit items', () => (
+        <span>
+          <h1>TransUnit items</h1>
+          <Image src="https://i.imgur.com/yQWlJaH.png" responsive />
+          <br />
+          <h2>Concurrent user notice</h2>
+          <br />
+          <h2>Resolve merge conflict button</h2>
+          <br />
+           <Button title="Click should trigger onClick action"
+                   onClick={action('onClick')}
+                   className="EditorButton Button--secondary u-rounded">
+          Resolve conflict
+        </Button>
+        </span>
     ))
