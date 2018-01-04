a06e9dac9a67a609cd15ca34545367a73c3d172e
kgough <kgough@redhat.com>
2018-01-04 10:32:46 +1000

    feat(ZNTA-2308) add details to Modal story in frontend

--- server/zanata-frontend/src/frontend/app/components/Modal/Modal.story.js
+++ server/zanata-frontend/src/frontend/app/components/Modal/Modal.story.js
@@ -7,10 +7,10 @@ import {
   Table,
   OverlayTrigger,
   Tooltip,
-  Badge
+  Badge,
+  Well
 } from 'react-bootstrap'
 import {Icon, Modal} from '../../components'
-import Lorem from 'react-lorem-component'
 
 const tooltipAssam = (
   <Tooltip id='tooltipassam'>
@@ -65,14 +65,18 @@ storiesOf('Modal', module)
     </div>
   ))
   .add('default', () => (
-    <Modal
+        <Modal
       show
       onHide={action('onHide')}>
       <Modal.Header>
         <Modal.Title>Modal heading</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Lorem />
+        <h2><img src="https://react-bootstrap.github.io/assets/logo.png" width="42px" />Modals</h2>
+        <Well>See the <a href="https://react-bootstrap.github.io/components.html#modals-props-modal">react-boostrap documentation on Modals</a> for props.</Well>
+
+        <p>A modal with header, body, and set of actions in the footer. Use <code>Modal</code> in combination with other components to show or hide your Modal. The <code>Modal</code> Component comes with a few convenient "sub components": <code>Modal.Header</code>, <code>Modal.Title</code>, <code>Modal.Body</code>, and <code>Modal.Footer</code>, which you can use to build the Modal content.
+      </p>
       </Modal.Body>
       <Modal.Footer>
         <span className='bootstrap u-pullRight'>
