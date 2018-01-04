15b991007d60d5c330a64233e38d6f1fa93e485d
kgough <kgough@redhat.com>
2018-01-04 10:47:02 +1000

    feat(ZNTA-2308) add details to Notification story in frontend

--- server/zanata-frontend/src/frontend/app/components/Notification/Notification.story.js
+++ server/zanata-frontend/src/frontend/app/components/Notification/Notification.story.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Notification from './component'
+import { Well, Table } from 'react-bootstrap'
 
 storiesOf('Notification', module)
   .add('Info', () => (
     <Notification severity="info"
       show
       message="This is an information message"
-      details="These are the details of the message" />
+      details="Use when information needs to be given to the user before proceeding" />
   ))
   .add('Warning', () => (
-    <Notification severity="warn"
+      <Notification severity="warn"
       show
       message="This is a warning message"
-      details="These are the details of the message" />
+      details="Use when a warning needs to be given to the user before proceeding" />
   ))
   .add('Error', () => (
     <Notification severity="error"
       show
       message="This is an error message"
-      details="These are the details of the message" />
-  ))
+      details="Use when there is a danger in the user taking a specific action" />
+        ))
