76fdecccfff45daf04a33ef28d1bbf2612e28dc6
efloden <efloden@redhat.com>
2018-01-17 16:07:33 +1000

    refactor: reduce imports for react-sytax-highlighter

--- server/zanata-frontend/src/app/editor/components/TransUnitTranslationPanel.js
+++ server/zanata-frontend/src/app/editor/components/TransUnitTranslationPanel.js
@@ -8,8 +8,13 @@ import { LoaderText } from '../../components'
 import { pick } from 'lodash'
 import { phraseTextSelectionRange } from '../actions/phrases-actions'
 import { getSyntaxHighlighting } from '../reducers'
-import SyntaxHighlighter from 'react-syntax-highlighter'
+import SyntaxHighlighter, { registerLanguage }
+  from 'react-syntax-highlighter/light'
+import xml from 'react-syntax-highlighter/languages/hljs/xml'
 import { atelierLakesideLight } from 'react-syntax-highlighter/styles/hljs'
+
+registerLanguage('xml', xml)
+
 /**
  * Panel to display and edit translations of a phrase.
  */
