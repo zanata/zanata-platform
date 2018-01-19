d0d8769025b7d3132fd71593ea34fae175d23bfe
Sean Flanigan <sflaniga@redhat.com>
2018-01-20 01:08:23 +1000

    Update to Babel 7

--- server/zanata-frontend/src/app/legacy.js
+++ server/zanata-frontend/src/app/legacy.js
@@ -19,7 +19,7 @@
  * site: http://www.fsf.org.
  */
 
-import 'babel-polyfill'
+import '@babel/polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Nav, Icons } from './components'
