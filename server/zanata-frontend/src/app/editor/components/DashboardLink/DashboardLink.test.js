2356506c13e8d347b0e067d3117ec1a155d63d2c
kgough <kgough@redhat.com>
2018-02-16 10:42:59 +1000

    fix: editorl url references

--- server/zanata-frontend/src/app/editor/components/DashboardLink/DashboardLink.test.js
+++ server/zanata-frontend/src/app/editor/components/DashboardLink/DashboardLink.test.js
@@ -6,7 +6,7 @@ import DashboardLink from '.'
 describe('DashboardLinkTest', () => {
   it('DashboardLink markup', () => {
     const profilePicSrc =
-      '//www.emoji-cheat-sheet.com/graphics/emojis/smiling_imp.png'
+      'https://www.webpagefx.com/tools/emoji-cheat-sheet/graphics/emojis/smiling_imp.png'
 
     const actual = ReactDOMServer.renderToStaticMarkup(
       <DashboardLink name="Hades"
