fcd22b3df08276042e903f562f25b5ae045667c2
efloden <efloden@redhat.com>
2018-03-12 12:21:54 +1000

    test(ZNTA-2401): markup for all translatingindicator modes

--- server/zanata-frontend/src/app/editor/components/TranslatingIndicator/TranslatingIndicator.test.js
+++ server/zanata-frontend/src/app/editor/components/TranslatingIndicator/TranslatingIndicator.test.js
@@ -7,7 +7,7 @@ import { IntlProvider } from 'react-intl'
 
 /* global describe expect it */
 describe('TranslatingIndicator', () => {
-  it('renders default markup', () => {
+  it('renders default viewing markup', () => {
     const permissions = {
       reviewer: false,
       translator: false
@@ -21,9 +21,57 @@ describe('TranslatingIndicator', () => {
       /* eslint-disable max-len */
       <button className='Link--neutral u-sPV-1-6 u-floatLeft u-sizeHeight-1_1-2 u-sMR-1-4'>
         <Row>
-          <Icon name='translate' className='s2' /> <span
+          <Icon name='locked' className='s2 u-textDanger' /> <span
             className='u-ltemd-hidden TransIndicator u-sMR-1-4'>
             <span>Viewing</span>
+          </span>
+        </Row>
+      </button>
+      /* eslint-enable max-len */
+    )
+    expect(actual).toEqual(expected)
+  })
+  it('renders reviewer markup', () => {
+    const permissions = {
+      reviewer: true,
+      translator: false
+    }
+    const actual = ReactDOMServer.renderToStaticMarkup(
+      <IntlProvider locale='en'>
+        <TranslatingIndicator permissions={permissions} />
+      </IntlProvider>)
+
+    const expected = ReactDOMServer.renderToStaticMarkup(
+      /* eslint-disable max-len */
+      <button className='Link--neutral u-sPV-1-6 u-floatLeft u-sizeHeight-1_1-2 u-sMR-1-4'>
+        <Row>
+          <Icon name='review' className='s2' /> <span
+            className='u-ltemd-hidden TransIndicator u-sMR-1-4'>
+            <span>Reviewing</span>
+          </span>
+        </Row>
+      </button>
+      /* eslint-enable max-len */
+    )
+    expect(actual).toEqual(expected)
+  })
+  it('renders translator markup', () => {
+    const permissions = {
+      reviewer: false,
+      translator: true
+    }
+    const actual = ReactDOMServer.renderToStaticMarkup(
+      <IntlProvider locale='en'>
+        <TranslatingIndicator permissions={permissions} />
+      </IntlProvider>)
+
+    const expected = ReactDOMServer.renderToStaticMarkup(
+      /* eslint-disable max-len */
+      <button className='Link--neutral u-sPV-1-6 u-floatLeft u-sizeHeight-1_1-2 u-sMR-1-4'>
+        <Row>
+          <Icon name='translate' className='s2' /> <span
+            className='u-ltemd-hidden TransIndicator u-sMR-1-4'>
+            <span>Translating</span>
           </span>
         </Row>
       </button>
