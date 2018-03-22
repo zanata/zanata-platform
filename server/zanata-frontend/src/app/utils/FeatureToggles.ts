1cbbdb8795994cbb59c8a7d92ea90c7dcfd7fc61
efloden <efloden@redhat.com>
2018-03-22 13:21:11 +1000

    fix: mock validationservice in projectversiontest

---
+++ server/zanata-frontend/src/app/utils/FeatureToggles.ts
@@ -0,0 +1,9 @@
+/*
+ * Custom Feature Switch functions for the continuous development of components.
+ * Changing the boolean value of each function toggles the corressponding features.
+ */
+
+/**
+ * Toggles the Validation Options feature in the Settings Panel.
+ */
+export const showValidationOptions = () => false;
