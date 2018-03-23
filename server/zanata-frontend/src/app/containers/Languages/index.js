f8d531bb966e67b23491e0abda1a3ccbf6ebeb08
kgough <kgough@redhat.com>
2018-03-23 11:20:15 +1000

    refactor(ZNTA-2407) change ID of languages page so it doesn't conflict with ids used in selenium tests

--- server/zanata-frontend/src/app/containers/Languages/index.js
+++ server/zanata-frontend/src/app/containers/Languages/index.js
@@ -105,7 +105,7 @@ class Languages extends Component {
 
     /* eslint-disable max-len, react/jsx-no-bind */
     return (
-      <div className='wideView bstrapReact' id='languages'>
+      <div className='wideView bstrapReact' id='languagesPage'>
         {notification &&
         (<Notification severity={notification.severity}
           message={notification.message}
