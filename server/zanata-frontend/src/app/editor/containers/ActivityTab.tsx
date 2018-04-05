a946e3fdfd1663ee9d5165f22ad152f49fe430d4
efloden <efloden@redhat.com>
2018-04-05 14:21:01 +1000

    refactor: create interfaces, abstract filter functions in activitytab

--- server/zanata-frontend/src/app/editor/containers/ActivityTab.tsx
+++ server/zanata-frontend/src/app/editor/containers/ActivityTab.tsx
@@ -5,11 +5,9 @@ import ActivityFeedItem from '../components/ActivityFeedItem'
 import {
   ActivityFilter, filterActivityTypes
 } from '../utils/activity-util'
-// ActivityItemList, activityTypes,
-// import { statuses } from '../utils/phrase'
 import { transUnitStatusToPhraseStatus } from '../utils/status-util'
 import { ALL, COMMENTS, UPDATES } from '../utils/activity-util'
-import { isEmpty, orderBy } from 'lodash'
+import { isUndefined, isEmpty, orderBy } from 'lodash'
 import React from 'react'
 import * as PropTypes from 'prop-types'
 import { commentTextLimit } from './RejectTranslation'
@@ -23,6 +21,54 @@ interface ActivityTabProps {
   selectedActivites?: ActivityFilter
 }
 
+// interface FilterItem {
+//   type: string,
+//   content: string,
+//   lastModifiedTime: any,
+//   user: any
+// }
+
+type Filter = (transHistory: any) => any
+
+const commentFilter: Filter = ({reviewComments}) => reviewComments.map((value) => {
+  return {
+    type: 'comment',
+    content: value.comment,
+    lastModifiedTime: (new Date(value.creationDate)),
+    user: {
+      name: value.commenterName,
+      username: value.username
+    }
+  }
+})
+
+const historyFilter: Filter = ({historyItems, latest}) => ({...(historyItems.map((historyItem) => {
+  const lastModified = new Date(historyItem.modifiedDate)
+  return {
+    type: 'revision',
+    content: historyItem.contents[0],
+    commentText: historyItem.revisionComment,
+    lastModifiedTime: lastModified,
+    status: transUnitStatusToPhraseStatus(historyItem.status),
+    user: {
+      name: historyItem.modifiedByPersonName,
+      username: historyItem.modifiedBy
+    }
+  }
+})), latest: latestHistoryAsItem(latest)})
+
+const latestHistoryAsItem = (latest) => ({
+  type: 'revision',
+  content: latest.contents[0],
+  commentText: latest.revisionComment,
+  lastModifiedTime: (new Date(latest.modifiedDate)),
+  status: transUnitStatusToPhraseStatus(latest.status),
+  user: {
+    name: latest.modifiedByPersonName,
+    username: latest.modifiedBy
+  }
+})
+
 /*
  * ActivityTab for Sidebar, displays TransUnit History, CommentBox
  * for entering new TransUnit comments.
@@ -34,76 +80,38 @@ const ActivityTab: React.SFC<ActivityTabProps> = ({
   selectActivityTypeFilter,
   selectedActivites
 }) => {
-  const {historyItems, reviewComments, latest} = transHistory
-  // Format a latestHistoryActivityItem from transHistory.latestHistoryItem
-  const latestHistoryActivityItem = (latest) => ({
-    type: 'revision',
-    content: latest.contents[0],
-    commentText: latest.revisionComment,
-    lastModifiedTime: (new Date(latest.modifiedDate)),
-    status: transUnitStatusToPhraseStatus(latest.status),
-    user: {
-      name: latest.modifiedByPersonName,
-      username: latest.modifiedBy
-    }
-  })
-  // Format a reviewCommentsList from reviewComments
-  const reviewCommentsList = (reviewComments) => reviewComments.map((value) => {
-    return {
-      type: 'comment',
-      content: value.comment,
-      lastModifiedTime: (new Date(value.creationDate)),
-      user: {
-        name: value.commenterName,
-        username: value.username
-      }
-    }
-  })
-  // Format a historyItemsList from historyItems
-  const historyActivityItems = (historyItems) => historyItems.map((historyItem) => {
-    const lastModified = new Date(historyItem.modifiedDate)
-    return {
-      type: 'revision',
-      content: historyItem.contents[0],
-      commentText: historyItem.revisionComment,
-      lastModifiedTime: lastModified,
-      status: transUnitStatusToPhraseStatus(historyItem.status),
-      user: {
-        name: historyItem.modifiedByPersonName,
-        username: historyItem.modifiedBy
-      }
-    }
-  })
+  const {reviewComments, latest} = transHistory
   /* Returns Activity Items list filtered by comments and updates */
   const filterActivityItems = (activityFilterType) => {
     if (isEmpty(reviewComments) && isEmpty(latest)) {
         return DO_NOT_RENDER
     }
-    const latestItem = latestHistoryActivityItem(latest)
-    const historyItemsList = {
-      ...historyActivityItems(historyItems), latestItem}
+    let results: any[]
     switch (activityFilterType) {
       case ALL:
-        return orderBy({...reviewCommentsList(reviewComments), ...historyItemsList},
-          ['lastModifiedTime'], ['desc'])
+        results = {...historyFilter(transHistory), ...commentFilter(transHistory)}
+        break
       case COMMENTS:
-        return orderBy(reviewCommentsList(reviewComments), ['lastModifiedTime'], ['desc'])
+        results = commentFilter(transHistory)
+        break
       case UPDATES:
-        return orderBy(historyItemsList, ['lastModifiedTime'], ['desc'])
+        results = historyFilter(transHistory)
+        break
       default:
-        return undefined
+        throw new Error()
     }
+    return orderBy(results, ['lastModifiedTime'], ['desc'])
   }
   const filteredActivityItems = filterActivityItems(selectedActivites)
-  const ActivityItems = filteredActivityItems.map((item, index) => (
+  const ActivityItems = (isUndefined(filteredActivityItems))
+    ? DO_NOT_RENDER
+    : filteredActivityItems.map((item, index) => (
       <ActivityFeedItem key={index} {...item} />))
   return (
     <div>
       <div className='SidebarEditor-wrapper' id='SidebarEditorTabs-pane2'>
         <ActivitySelectList selectItem={selectActivityTypeFilter}
           selected={selectedActivites} />
-        {/* <LanguageSelectList selectItem={selectLanguageFilter}
-          *  selected={selectedLanguages} /> */}
       </div>
       <div className='SidebarActivity'>
         <CommentBox postComment={postComment} maxLength={commentTextLimit} />
