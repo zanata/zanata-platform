826daa3d91e8fa5c9658c9ce0ce2761d456aac0a
efloden <efloden@redhat.com>
2018-05-02 12:15:52 +1000

    fix: antd modal storybook

--- server/zanata-frontend/src/app/components/Modal/Modal.story.js
+++ server/zanata-frontend/src/app/components/Modal/Modal.story.js
@@ -1,24 +1,20 @@
-// // @ts-nocheck
-// import React from 'react'
-// import { storiesOf } from '@storybook/react'
-// import { action } from '@storybook/addon-actions'
-// import Modal from 'antd/lib/modal'
-// // import 'antd/lib/modal/style/index.less'
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Modal from 'antd/lib/modal'
+// import 'antd/lib/modal/style/index.less'
 // import Button from 'antd/lib/button'
-// // import 'antd/lib/button/style/index.less'
-//
-// storiesOf('Modal', module)
-//   .add('default', () => (
-//     <div>
-//       <Button type="primary" onClick={action('onClick')}>Open</Button>
-//       <Modal
-//         title="Basic Modal"
-//         visible
-//         onOk={action('onClick')}
-//         onCancel={action('onClick')}
-//       >
-//         <p>A modal with header, body, and set of actions in the footer.</p>
-//         <a href='https://ant.design/components/modal'>ant.design Modal</a>
-//       </Modal>
-//     </div>
-//   ))
+// import 'antd/lib/button/style/index.less'
+
+storiesOf('Modal', module)
+  .add('default', () => (
+    <Modal
+      title="Basic Modal"
+      visible
+      onOk={action('onClick')}
+      onCancel={action('onClick')}
+    >
+      <p>A modal with header, body, and set of actions in the footer.</p>
+      <a href='https://ant.design/components/modal'>ant.design Modal</a>
+    </Modal>
+  ))
