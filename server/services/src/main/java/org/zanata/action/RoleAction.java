425a0b588760ca8178132eee3603e014ba356d57
Damian Jansen <djansen@redhat.com>
2018-05-10 13:51:54 +1000

    fix(ZNTA-1993): Fix the role manager validation
    Prevent empty role name or greater that 255 characters
    Prevent role name changes
    Prevent overwriting an existing role
    Remove users from role before deleting it
    Add test

--- server/services/src/main/java/org/zanata/action/RoleAction.java
+++ server/services/src/main/java/org/zanata/action/RoleAction.java
@@ -5,14 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.inject.Model;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ValueChangeEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.deltaspike.jpa.api.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.zanata.seam.security.IdentityManager;
 import org.zanata.security.ZanataIdentity;
 import org.zanata.security.annotations.CheckLoggedIn;
+import org.zanata.ui.faces.FacesMessages;
 
 /**
  * @author Patrick Huang
@@ -24,10 +30,12 @@ import org.zanata.security.annotations.CheckLoggedIn;
 @Transactional
 @CheckLoggedIn
 public class RoleAction implements Serializable {
+    private final static Logger log = LoggerFactory.getLogger(RoleAction.class);
     private static final long serialVersionUID = -3830647911484729768L;
     private String originalRole;
     private String role;
     private List<String> groups;
+    private final int MAX_NAME_SIZE = 255;
 
     @Inject
     IdentityManager identityManager;
@@ -35,8 +43,11 @@ public class RoleAction implements Serializable {
     @Inject
     ZanataIdentity identity;
 
+    @Inject
+    FacesMessages facesMessages;
+
     public void loadRole() {
-        if (role == null) {
+        if (StringUtils.isBlank(role)) {
             // creating new role
             groups = new ArrayList<>();
         } else {
@@ -45,13 +56,54 @@ public class RoleAction implements Serializable {
         }
     }
 
+    @SuppressWarnings("unused")
+    public boolean validateRoleName(ValueChangeEvent event) {
+        String roleName = (String) event.getNewValue();
+        String componentId = event.getComponent().getId();
+
+        if (roleName.length() > MAX_NAME_SIZE) {
+            facesMessages.addToControl(componentId, "Role name too long");
+            return false;
+        }
+        if (identityManager.roleExists(roleName)){
+            facesMessages.addToControl(componentId, "Role name not available");
+            return false;
+        }
+        if (!StringUtils.isBlank(originalRole) && !roleName.equals(originalRole)) {
+            facesMessages.addToControl(componentId,
+                    "Role name change not allowed");
+            return false;
+        }
+        return true;
+    }
+
     public String save() {
-        if (role != null && originalRole != null &&
-                !role.equals(originalRole)) {
-            identityManager.deleteRole(originalRole);
+        boolean roleExists = role != null && identityManager.roleExists(role);
+
+        if (StringUtils.isBlank(role)) {
+            facesMessages.addGlobal(FacesMessage.SEVERITY_ERROR,
+                    "Empty role name");
+            return "failure";
+        } else if (role.length() > MAX_NAME_SIZE) {
+            log.warn("User {} attempted to enter an excessive role name",
+                    identity.getAccountUsername());
+            facesMessages.addGlobal(FacesMessage.SEVERITY_ERROR,
+                    "Role name exceeds " + String.valueOf(MAX_NAME_SIZE) +
+                            " characters");
+            setRole(originalRole);
+            return "failure";
         }
 
-        if (identityManager.roleExists(role)) {
+        if (roleExists && !role.equals(originalRole)) {
+            log.warn("User {} attempting to overwrite another role",
+                identity.getAccountUsername());
+            facesMessages.addGlobal(FacesMessage.SEVERITY_ERROR,
+                    "Attempted to overwrite another role");
+            setRole(originalRole);
+            return "failure";
+        }
+
+        if (roleExists) {
             return saveExistingRole();
         } else {
             return saveNewRole();
