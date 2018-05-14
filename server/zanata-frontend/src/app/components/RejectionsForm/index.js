933d868afe5b233b10998b06b452b959e6ed81ab
efloden <efloden@redhat.com>
2018-05-14 11:40:41 +1000

    refactor: RejectionsForm bootstap components to ant design

--- server/zanata-frontend/src/app/components/RejectionsForm/index.js
+++ server/zanata-frontend/src/app/components/RejectionsForm/index.js
@@ -2,11 +2,23 @@
 import React from 'react'
 import { Component } from 'react'
 import * as PropTypes from 'prop-types'
-import { Form, FormGroup, ControlLabel, Button, OverlayTrigger, Tooltip }
-  from 'react-bootstrap'
-import { Icon, TextInput, SelectableDropdown } from '../../components'
-import Toggle from 'react-toggle'
+import { Icon } from '../../components'
 import { isEmpty } from 'lodash'
+
+import Button from 'antd/lib/button'
+import 'antd/lib/button/style/index.less'
+import Form from 'antd/lib/form'
+import 'antd/lib/form/style/index.less'
+import Input from 'antd/lib/input'
+import 'antd/lib/input/style/index.less'
+import Select from 'antd/lib/select'
+import 'antd/lib/select/style/index.less'
+import Switch from 'antd/lib/switch'
+import 'antd/lib/switch/style/index.less'
+import Tooltip from 'antd/lib/tooltip'
+import 'antd/lib/tooltip/style/index.less'
+
+const Option = Select.Option
 
 /**
  * Reject Translations Administration panel
@@ -15,10 +27,6 @@ export const MINOR = 'Minor'
 export const MAJOR = 'Major'
 export const CRITICAL = 'Critical'
 const DO_NOT_RENDER = undefined
-
-const tooltipSave = (<Tooltip id='tooltip'>Save criteria</Tooltip>)
-
-const tooltipDelete = (<Tooltip id='tooltip'>Delete criteria</Tooltip>)
 
 function priorityToTextState (priority) {
   switch (priority) {
@@ -49,6 +57,7 @@ class RejectionsForm extends Component {
     commentRequired: PropTypes.bool,
     // if it's in admin mode, we will allow user to update
     isAdminMode: PropTypes.bool.isRequired,
+    key: PropTypes.number,
     // whether delete button shoud be displayed
     displayDelete: PropTypes.bool.isRequired,
     className: PropTypes.string,
@@ -106,63 +115,68 @@ class RejectionsForm extends Component {
       commentRequired,
       className,
       isAdminMode,
+      key,
       displayDelete,
-      criteriaPlaceholder,
-      criterionId
+      criteriaPlaceholder
     } = this.props
-    const textState = priorityToTextState(this.state.priority)
     const error = isEmpty(this.state.description)
-    const title = <span className={textState}>{this.state.priority}</span>
     const priorityDisabled = !isAdminMode && !commentRequired
     const deleteBtn = displayDelete
       ? (
-      <OverlayTrigger placement='top' overlay={tooltipDelete}>
-        <Button bsStyle='danger' className={className} onClick={this.onDelete}>
+      <Tooltip title='Delete criteria'>
+        <Button type='danger' className={className} onClick={this.onDelete}>
           <Icon name='trash' className='s0 iconEdit' />
         </Button>
-      </OverlayTrigger>
+      </Tooltip>
       ) : DO_NOT_RENDER
     const commentToggle = isAdminMode ? (
-      <FormGroup id='toggleComment' controlId='formInlineEditable'>
-        <ControlLabel>Comment required</ControlLabel><br />
-        <Toggle icons={false} onChange={this.onEditableChange}
-          checked={this.state.isCommentRequired} />
-      </FormGroup>
+      <Form.Item label='Comment required'>
+        <Switch
+          checked={this.state.isCommentRequired}
+          onChange={this.onEditableChange} />
+      </Form.Item>
       )
       : DO_NOT_RENDER
     const formBtn = isAdminMode ? (
-      <FormGroup controlId='formInlineButtonEdit'>
-        <ControlLabel>&nbsp;</ControlLabel><br />
-        <OverlayTrigger placement='top' overlay={tooltipSave}>
-          <Button bsStyle='primary' className={className} onClick={this.onSave}
+      <Form.Item>
+        <Tooltip title='Save criteria'>
+          <Button type='primary' className={className} onClick={this.onSave}
             disabled={error}>
             <Icon name='tick' className='s0 iconEdit' />
           </Button>
-        </OverlayTrigger>
+        </Tooltip>
         {deleteBtn}
-      </FormGroup>
+      </Form.Item>
     ) : DO_NOT_RENDER
     return (
-      <Form id='rejectionsForm' inline>
-        <FormGroup className='u-flexGrow1' controlId='formInlineCriteria'>
-          <ControlLabel>Criteria</ControlLabel><br />
-          <TextInput multiline editable={isAdminMode || commentRequired}
-            type='text' numberOfLines={2} onChange={this.onTextChange}
-            placeholder={criteriaPlaceholder} maxLength={255}
+      <Form key={key} layout='inline'>
+        <Form.Item label='Criteria'>
+          { /* TODO: Fix layout style={{ width: '500px' }} */ }
+          <Input.TextArea
+            disabled={!isAdminMode}
+            maxLength={255}
+            onChange={this.onTextChange}
+            placeholder={criteriaPlaceholder}
+            rows={2}
             value={this.state.description} />
-        </FormGroup>
-        <FormGroup controlId='formInlinePriority'>
-          <ControlLabel>Priority</ControlLabel><br />
-          <SelectableDropdown
-            id={criterionId + 'review-criteria-dropdown-basic'}
-            onSelectDropdownItem={this.onPriorityChange}
-            selectedValue={this.state.priority}
-            title={title}
-            valueToDisplay={priorityToDisplay}
-            values={[MINOR, MAJOR, CRITICAL]}
+        </Form.Item>
+        <Form.Item label='Priority'>
+          <Select
+            defaultValue={this.state.priority}
             disabled={priorityDisabled}
-          />
-        </FormGroup>
+            onChange={this.onPriorityChange}
+            label={this.state.priority} >
+            <Option key={0} value={MINOR}>
+              {priorityToDisplay(MINOR)}
+            </Option>
+            <Option key={1} value={MAJOR}>
+              {priorityToDisplay(MAJOR)}
+            </Option>
+            <Option key={2} value={CRITICAL}>
+              {priorityToDisplay(CRITICAL)}
+            </Option>
+          </Select>
+        </Form.Item>
         {commentToggle}
         {formBtn}
       </Form>
