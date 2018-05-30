ed6c2a456743fd1d5f4baf8b1d677cd8e9389d8f
Earl Floden <efloden@redhat.com>
2018-05-30 16:23:44 +1000

    fix(ZNTA-2587): mock ant design buttons, re-enable storyshot tests (#821)
    * fix: mock ant design buttons, re-enable storyshot tests
    
    * fix: move ant button mock to storyshots-editor and storyshots-frontend test files

--- server/zanata-frontend/src/app/components/Button/Button.story.js
+++ server/zanata-frontend/src/app/components/Button/Button.story.js
@@ -5,8 +5,8 @@ import Button from 'antd/lib/button'
 import Layout from 'antd/lib/layout'
 
 storiesOf('Button', module)
-    .add('default (no test)', () => (
-        <Layout>
-          <Button type='primary'>Primary style</Button>
-        </Layout>
+    .add('default', () => (
+      <Layout>
+        <Button type='primary'>Primary style</Button>
+      </Layout>
     ))
