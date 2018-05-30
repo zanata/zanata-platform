4c3b9a357aece92d15c408fc4fac3eabc8c10c74
Damian Jansen <djansen@redhat.com>
2018-05-30 16:10:23 +1000

    fix(ZNTA-1083): indicate the experimental nature of raw upload

--- server/services/src/test/java/org/zanata/service/impl/TranslationFileServiceImplTest.java
+++ server/services/src/test/java/org/zanata/service/impl/TranslationFileServiceImplTest.java
@@ -20,6 +20,7 @@
  */
 package org.zanata.service.impl;
 
+import com.google.common.base.Optional;
 import org.hibernate.Session;
 import org.jglue.cdiunit.InRequestScope;
 import org.junit.Test;
@@ -27,18 +28,43 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.zanata.ZanataTest;
 import org.zanata.common.DocumentType;
+import org.zanata.common.LocaleId;
+import org.zanata.common.ProjectType;
+import org.zanata.dao.DocumentDAO;
+import org.zanata.dao.ProjectIterationDAO;
+import org.zanata.exception.ZanataServiceException;
+import org.zanata.model.HDocument;
+import org.zanata.model.HLocale;
+import org.zanata.model.HProject;
+import org.zanata.model.HProjectIteration;
+import org.zanata.rest.dto.resource.TranslationsResource;
 import org.zanata.test.CdiUnitRunner;
 
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.Mockito.when;
 
 @RunWith(CdiUnitRunner.class)
 public class TranslationFileServiceImplTest extends ZanataTest {
 
     @Inject
     TranslationFileServiceImpl transFileService;
+
+    @Mock
+    @Produces
+    private ProjectIterationDAO projectIterationDAO;
+
+    @Mock
+    @Produces
+    private DocumentDAO documentDAO;
 
     @Produces @Mock Session session;
 
@@ -47,4 +73,100 @@ public class TranslationFileServiceImplTest extends ZanataTest {
     public void hasPlainTextAdapter() {
         assertThat(transFileService.hasAdapterFor(DocumentType.PLAIN_TEXT)).isTrue();
     }
+
+    @Test
+    @InRequestScope
+    public void parsePoInFileProject() throws Exception {
+        String project = "test";
+        String version = "master";
+        String docId = "test.pot";
+        String poContent = "msgid \"\"\n" +
+                "msgstr \"\"\n" +
+                "\"Project-Id-Version: test-master\\n\"\n" +
+                "\n" +
+                "#, c-format\n" +
+                "msgid \"Thing 1\"\n" +
+                "msgid_plural \"%d files removed\"\n" +
+                "msgstr[0] \"1 aoeuaouaou\"\n" +
+                "msgstr[1] \"%d aoeuaouao\"\n" +
+                "msgstr[2] \"\"";
+        File tempFile = File.createTempFile("test", ".po");
+        Files.write(tempFile.toPath(), poContent.getBytes());
+        InputStream stream = new FileInputStream(tempFile);
+
+        HProject hProject = new HProject();
+        hProject.setDefaultProjectType(ProjectType.File);
+        HProjectIteration hProjectIteration = new HProjectIteration();
+        hProjectIteration.setProject(hProject);
+        hProjectIteration.setProjectType(ProjectType.File);
+        HDocument hDocument = new HDocument();
+
+        when(projectIterationDAO.getBySlug(project, version))
+                .thenReturn(hProjectIteration);
+        when(documentDAO.getByDocIdAndIteration(hProjectIteration, docId))
+                .thenReturn(hDocument);
+
+        TranslationsResource translationsResource = transFileService
+                .parseTranslationFile(stream, tempFile.getName(),
+                "ru", project, version, docId, Optional.absent());
+        assertThat(translationsResource.getTextFlowTargets().get(0)
+                .getContents().get(0)).isEqualTo("1 aoeuaouaou");
+    }
+
+    @Test
+    @InRequestScope
+    public void parseAdapterFileInFileProject() throws Exception {
+        String project = "test";
+        String version = "master";
+        String docId = "test.properties";
+        String plaintextContent = "first: test message";
+        File tempFile = File.createTempFile("test", ".properties");
+        Files.write(tempFile.toPath(), plaintextContent.getBytes());
+        InputStream stream = new FileInputStream(tempFile);
+
+        HProject hProject = new HProject();
+        hProject.setDefaultProjectType(ProjectType.File);
+        HProjectIteration hProjectIteration = new HProjectIteration();
+        hProjectIteration.setProject(hProject);
+        hProjectIteration.setProjectType(ProjectType.File);
+        HDocument hDocument = new HDocument();
+        hDocument.setLocale(new HLocale(new LocaleId("en")));
+
+        when(projectIterationDAO.getBySlug(project, version))
+                .thenReturn(hProjectIteration);
+        when(documentDAO.getByDocIdAndIteration(hProjectIteration, docId))
+                .thenReturn(hDocument);
+        when(documentDAO.getByProjectIterationAndDocId(project, version, docId))
+                .thenReturn(hDocument);
+
+        TranslationsResource translationsResource = transFileService
+                .parseTranslationFile(stream, tempFile.getName(),
+                        "ru", project, version, docId, Optional.absent());
+        assertThat(translationsResource.getTextFlowTargets().get(0)
+                .getContents().get(0)).isEqualTo("test message");
+    }
+
+    @Test
+    @InRequestScope
+    public void rejectNonPoInGettextProject() throws Exception {
+        String project = "test";
+        String version = "master";
+        String docId = "test.pot";
+        File tempFile = File.createTempFile("test", ".ts");
+        InputStream stream = new FileInputStream(tempFile);
+        HProject hProject = new HProject();
+        hProject.setDefaultProjectType(ProjectType.Gettext);
+        HProjectIteration hProjectIteration = new HProjectIteration();
+        when(projectIterationDAO.getBySlug(project, version))
+                .thenReturn(hProjectIteration);
+
+        try {
+            transFileService.parseTranslationFile(stream, tempFile.getName(),
+                    "ru", project, version, docId, Optional.absent());
+            fail("Expected a ZanataServiceException");
+        } catch (ZanataServiceException zse) {
+            assertThat(zse.getMessage()).contains("Unsupported Translation file");
+        }
+    }
+
 }
