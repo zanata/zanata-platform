a3d3b3ea27d0037a86ecbec1696b6e9366ed073a
efloden <efloden@redhat.com>
2018-06-08 14:37:17 +1000

    fix(ZNTA-2606): validate duplicate glossary entries

--- server/zanata-frontend/src/app/containers/Glossary/NewEntryModal.js
+++ server/zanata-frontend/src/app/containers/Glossary/NewEntryModal.js
@@ -13,6 +13,8 @@ import Input from 'antd/lib/input'
 import 'antd/lib/input/style/'
 import Modal from 'antd/lib/modal'
 import 'antd/lib/modal/style/index.less'
+import notification from 'antd/lib/notification'
+import 'antd/lib/notification/style/css'
 
 import {
   glossaryToggleNewEntryModal,
@@ -25,6 +27,7 @@ class NewEntryModal extends Component {
     entry: PropTypes.object,
     isSaving: PropTypes.bool,
     show: PropTypes.bool,
+    terms: PropTypes.object,
     handleNewEntryDisplay: PropTypes.func,
     handleNewEntryCreate: PropTypes.func
   }
@@ -77,12 +80,33 @@ class NewEntryModal extends Component {
     })
   }
 
+  handleCreate = () => {
+    const { entry } = this.state
+    const { handleNewEntryCreate, terms } = this.props
+    const foundDuplicate = terms && Object.values(terms).some((term) => {
+      return (
+        term.description === entry.description &&
+        term.pos === entry.pos &&
+        term.srcTerm.content === entry.srcTerm.content
+      )
+    })
+    if (foundDuplicate) {
+      notification.error({
+        message: 'Could not create Glossary entry.',
+        description: 'Duplicate entry exists.',
+        duration: null
+      })
+    } else {
+      handleNewEntryCreate(this.state.entry)
+      this.resetFields()
+    }
+  }
+
   render () {
     const {
       show,
       isSaving,
-      handleNewEntryDisplay,
-      handleNewEntryCreate
+      handleNewEntryDisplay
       } = this.props
     const isAllowSave = !isEmpty(this.state.entry.srcTerm.content)
 
@@ -105,11 +129,7 @@ class NewEntryModal extends Component {
             aria-label='button'
             type='primary'
             disabled={!isAllowSave || isSaving}
-            onClick={
-              () => {
-                handleNewEntryCreate(this.state.entry); this.resetFields()
-              }
-            }>
+            onClick={this.handleCreate}>
             <LoaderText loading={isSaving} loadingText='Saving'>
               Save
             </LoaderText>
@@ -144,8 +164,9 @@ class NewEntryModal extends Component {
 }
 
 const mapStateToProps = (state) => {
+  const { terms } = state.glossary
   const { entry, isSaving, show } = state.glossary.newEntry
-  return { entry, isSaving, show }
+  return { entry, isSaving, show, terms }
 }
 
 const mapDispatchToProps = (dispatch) => {
