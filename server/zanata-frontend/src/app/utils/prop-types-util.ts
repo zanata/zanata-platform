90307bdada96bdd2fa56ecc994c5a38a34e594b5
Sean Flanigan <sflaniga@redhat.com>
2018-07-10 10:05:11 +1000

    Add incomplete types for Redux state (#945)
    * Add incomplete types for Redux state
    * Update eslintrc
    * Fix accidental state mutation of state.importFile

--- server/zanata-frontend/src/app/utils/prop-types-util.ts
+++ server/zanata-frontend/src/app/utils/prop-types-util.ts
@@ -1,25 +1,43 @@
 import * as PropTypes from 'prop-types'
-import {processStatusCodes, entityStatuses, EntityStatus} from './EnumValueUtils'
+import {processStatusCodes, entityStatuses, EntityStatus, ProcessStatusCode} from './EnumValueUtils'
 
 export const entityStatusPropType = PropTypes.oneOf(entityStatuses)
 
 export const versionDtoPropType = PropTypes.shape({
   id: PropTypes.string.isRequired,
-  status: entityStatusPropType
+  status: entityStatusPropType.isRequired
 })
 
+// this shape seems to be org.zanata.rest.search.dto.ProjectVersionSearchResult (partial)
 export interface VersionDto {
   id: string,
-  // TODO does entityStatusPropType implicitly have isRequired?
-  status?: EntityStatus,
+  status: EntityStatus,
 }
 
+// this shape seems to be org.zanata.rest.search.dto.ProjectSearchResult (partial)
+export interface ProjectSearchResult {
+  id: string
+  status: string
+  title: string
+  versions: VersionDto[]
+}
+// matches ProjectSearchResult above
 export const ProjectType = PropTypes.shape({
   id: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   versions: PropTypes.arrayOf(versionDtoPropType).isRequired
 })
+
+// in future, it would be good to use a tag type for locale IDs
+// https://github.com/Microsoft/TypeScript/issues/4895
+export type LocaleId = string
+
+export interface Locale {
+  displayName: string
+  localeId: LocaleId
+  nativeName: string
+}
 
 export const LocaleType = PropTypes.shape({
   displayName: PropTypes.string.isRequired,
@@ -44,3 +62,9 @@ export const processStatusType = PropTypes.shape({
   percentageComplete: PropTypes.number.isRequired,
   statusCode: processStatusCodeType.isRequired
 })
+
+export interface ProcessStatus {
+  url: string
+  percentageComplete: number
+  statusCode: ProcessStatusCode
+}
