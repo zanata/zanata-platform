7d32ee9fd2b36f9a0f4b487d29b837dda845a98f
kgough <kgough@redhat.com>
2018-07-18 09:48:45 +1000

    WIP(ZNTA-2526)

--- server/zanata-frontend/src/app/components/Sidebar/Sidebar.story.js
+++ server/zanata-frontend/src/app/components/Sidebar/Sidebar.story.js
@@ -1,55 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Sidebar from '.'
-import AboutPage from '../../containers/ProjectVersion/AboutPage'
-import PeoplePage from '../../containers/ProjectVersion/PeoplePage'
-import GroupsPage from '../../containers/ProjectVersion/GroupsPage'
-import LanguagesPage from '../../containers/ProjectVersion/LanguagesPage'
-import DocumentsPage from '../../containers/ProjectVersion/DocumentsPage'
-
-const aboutText = 'This is one rocking project version. This is the best' +
-    ' project version ever.'
-const url = 'https://www.google.com'
-const linkname = 'Our awesome webpage'
 
 storiesOf('Sidebar', module)
     .add('default', () => (
         <div>
           <Sidebar />
-          <div className='flexTab'>
-            <p>This sidebar example has the active tag applied to both the People
-              and Languages pages to provide examples of how this design handles
-              sidebar links.</p>
-          </div>
       </div>
-    ))
-    .add('AboutPage', () => (
-        <div>
-          <Sidebar />
-          <AboutPage aboutText={aboutText} aboutLink={url} linkName={linkname} />
-        </div>
-    ))
-    .add('PeoplePage', () => (
-        <div>
-          <Sidebar />
-          <PeoplePage />
-        </div>
-    ))
-    .add('GroupsPage', () => (
-        <div>
-          <Sidebar />
-          <GroupsPage />
-        </div>
-    ))
-    .add('LanguagesPage', () => (
-        <div>
-          <Sidebar />
-          <LanguagesPage />
-        </div>
-    ))
-    .add('DocumentsPage', () => (
-        <div>
-          <Sidebar />
-          <DocumentsPage />
-        </div>
     ))
--- server/zanata-frontend/src/app/components/Sidebar/index.jsx
+++ server/zanata-frontend/src/app/components/Sidebar/index.jsx
@@ -1,13 +1,56 @@
 import React from 'react'
 import { Component } from 'react'
+import Layout from 'antd/lib/layout/'
+import 'antd/lib/layout/style/css'
+import Icon from '../../components/Icon'
+import Menu from 'antd/lib/menu'
+import 'antd/lib/menu/style/css'
+import Select from 'antd/lib/select'
+import 'antd/lib/select/style/css'
+
+const { Content, Sider } = Layout
+const Option = Select.Option
 
 class Sidebar extends Component {
   /* eslint-disable react/jsx-no-bind, no-return-assign */
   render () {
+    const projectTitle = 'Zanata Server'
     return (
-      <div id='pvSidebar'>
-
-      </div>
+      <Layout>
+        <Sider breakpoint='sm'
+          defaultCollapsed={false}
+          width='300'
+          className='pvSidebar'
+          collapsedWidth='0'>
+          <h1 className='di'><Icon name='project' className='s4' />{projectTitle}</h1>
+          <Menu>
+            <Menu.Item key='1'>
+              <Icon name='users' className='s1' />
+              <span className='nav-text'>People</span>
+            </Menu.Item>
+            <Menu.Item key='2'>
+              <Icon name='glossary' className='s1' />
+              <span className='nav-text'>Glossary</span>
+            </Menu.Item>
+            <Menu.Item key='3'>
+              <Icon name='info' className='s1' />
+              <span className='nav-text'>About</span>
+            </Menu.Item>
+            <Menu.Item key='4'>
+              <Icon name='settings' className='s1' />
+              <span className='nav-text'>Settings</span>
+            </Menu.Item>
+          </Menu>
+          <div className='mt3 mb3'>
+            <Select placeholder='Options' style={{ width: 120 }}>
+              <Option value='1'>Option 1</Option>
+              <Option value='2'>Option 2</Option>
+              <Option value='3'>Option 3</Option>
+            </Select>
+          </div>
+        </Sider>
+        <Content>Content</Content>
+      </Layout>
     )
     /* eslint-enable react/jsx-no-bind, no-return-assign */
   }
