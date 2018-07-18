6b3f3668603f9b6a32fe5ebf7135e7975e2073a2
Damian Jansen <djansen@redhat.com>
2018-07-18 11:50:05 +1000

    fix(ZNTA-2634): replace language property in translated TS (#925)
    * fix(ZNTA-2634): replace language property in translated TS
    * fix(ZNTA-2634): improve the regex and add tests
    * fix(ZNTA-2634): add bad locale check and test
    * fix(ZNTA-2634): remove unnecessary check
    * fix(ZNTA-2634): convert test to Kotlin
    * fix(ZNTA-2634): clean up adapter and test
    * fix(ZNTA-2634): preserve case on target locale
    * fix(ZNTA-2634): ignore test, cannot support user parts in locale
    * fix(ZNTA-2634): use empty string instead of .EMPTY

--- server/services/src/test/resources/org/zanata/adapter/test-ts-invalid.ts
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE TS []>
-<TS version="2.1" sourcelanguage="en" language="sv">
-  <!-- Contains a double bracket here -->
-  <<context>
-  <name>testContext1</name>
-  <message>
-    <source>First source</source>
-  </message>
-</TS>
