158ab6b9f3b733d2538976059a9e655a6845a83a
Damian Jansen <djansen@redhat.com>
2018-07-24 14:36:20 +1000

    chore(ZNTA-2694): remove erroneous import

--- server/gwt-editor/src/test/java/org/zanata/webtrans/client/service/TransUnitSaveServiceTest.java
+++ server/gwt-editor/src/test/java/org/zanata/webtrans/client/service/TransUnitSaveServiceTest.java
@@ -1,7 +1,6 @@
 package org.zanata.webtrans.client.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
