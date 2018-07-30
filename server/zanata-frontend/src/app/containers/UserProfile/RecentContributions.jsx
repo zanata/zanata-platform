ee17c080062acf54667e2edf26f1ec3680ab4faf
kgough <kgough@redhat.com>
2018-07-30 12:38:39 +1000

    refactor(ZNTA-2715) WIP: remove react-date-range component and replace with ant design DatePicker

--- server/zanata-frontend/src/app/containers/UserProfile/RecentContributions.jsx
+++ server/zanata-frontend/src/app/containers/UserProfile/RecentContributions.jsx
@@ -1,74 +1,15 @@
 // @ts-nocheck
 import React from 'react'
-import * as PropTypes from 'prop-types'
-import ContributionChart from './ContributionChart'
-import FilterableMatrixTable from './FilterableMatrixTable'
-import { DateRange } from 'react-date-range'
-import utilsDate from '../../utils/DateHelper'
-import { TextInput } from '../../components'
-import Button from 'antd/lib/button'
-import 'antd/lib/button/style/css'
-import Modal from 'antd/lib/modal'
-import 'antd/lib/modal/style/css'
-
-const STATS_MAX_DAYS = 365
+import DatePicker from 'antd/lib/date-picker'
+import 'antd/lib/date-picker/style/css'
 
 /**
  * User profile statistics root page
  */
 class RecentContributions extends React.Component {
-  static propTypes = {
-    matrixForAllDays: PropTypes.array,
-    dateRange: PropTypes.object,
-    wordCountsForSelectedDayFilteredByContentState: PropTypes.array,
-    wordCountsForEachDayFilteredByContentState: PropTypes.array,
-    contentStateOption: PropTypes.string,
-    selectedDay: PropTypes.string,
-    handleDateRangeChanged: PropTypes.func,
-    handleFilterChanged: PropTypes.func,
-    handleSelectedDayChanged: PropTypes.func.isRequired
-  }
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      dateRange: props.dateRange,
-      showDateRange: false
-    }
-  }
-
-  onToggleShowDateRange = () => {
-    this.setState(prevState => ({
-      showDateRange: !prevState.showDateRange
-    }))
-  }
-
-  onDateRangeChanged = (dateRange) => {
-    // adjust dateRange to be in STATS_MAX_DAYS
-    const adjustedDateRange =
-      utilsDate.keepDateInRange(
-        dateRange.startDate, dateRange.endDate, STATS_MAX_DAYS)
-    this.setState({
-      dateRange: adjustedDateRange
-    })
-  }
 
   render () {
-    const {
-      dateRange,
-      matrixForAllDays,
-      wordCountsForSelectedDayFilteredByContentState,
-      wordCountsForEachDayFilteredByContentState,
-      contentStateOption,
-      selectedDay,
-      handleFilterChanged,
-      handleSelectedDayChanged,
-      handleDateRangeChanged
-    } = this.props
-
-    const displayDateRange =
-      utilsDate.shortDate(this.state.dateRange.startDate) +
-      ' to ' + utilsDate.shortDate(this.state.dateRange.endDate)
+    const { RangePicker } = DatePicker
 
     /* eslint-disable react/jsx-no-bind */
     return (
@@ -77,57 +18,22 @@ class RecentContributions extends React.Component {
           <h2 className='userProfile-recentContributions'>
           Recent Contributions</h2>
           <div className='dateRange-container'>
-            <Button className='btn-link fr pa0' aria-label='button'
-              onClick={() => this.onToggleShowDateRange()}>
-              <span>
-                <TextInput editable={false} value={displayDateRange} />
-              </span>
-            </Button>
-
-            {this.state.showDateRange &&
-              <Modal
-                id='profile'
-                title={'Date range selection'}
-                width={'54rem'}
-                visible={this.state.showDateRange}
-                onCancel={() => this.onToggleShowDateRange()}
-                footer={[
-                  <Button key='back'
-                    aria-label='button'
-                    onClick={() => this.onToggleShowDateRange()}>
-                    Cancel
-                  </Button>,
-                  <Button key='ok' type='primary'
-                    aria-label='button'
-                    onClick={
-                    () => handleDateRangeChanged(this.state.dateRange)}>
-                    Apply
-                  </Button>]} >
-                <span className='txt-muted'>(Maximum 365 days)</span>
-                <DateRange
-                  startDate={this.state.dateRange.startDate}
-                  endDate={this.state.dateRange.endDate}
-                  ranges={utilsDate.getDefaultDateRange()}
-                  className='dateRange-calendar'
-                  onChange={this.onDateRangeChanged} />
-              </Modal>
-            }
+            <RangePicker
+              dateRender={(current) => {
+                const style = {}
+                if (current.date() === 1) {
+                  style.border = '1px solid #1890ff'
+                  style.borderRadius = '50%'
+                }
+                return (
+                  <div className="ant-calendar-date" style={style}>
+                    {current.date()}
+                  </div>
+                )
+              }}
+            />
           </div>
         </div>
-        <div className='flexChart-container'>
-          <ContributionChart
-            wordCountForEachDay={matrixForAllDays}
-            dateRange={dateRange} />
-        </div>
-        <FilterableMatrixTable
-          wordCountForSelectedDay={wordCountsForSelectedDayFilteredByContentState} // eslint-disable-line max-len
-          wordCountForEachDay={wordCountsForEachDayFilteredByContentState}
-          dateRange={dateRange}
-          selectedContentState={contentStateOption}
-          selectedDay={selectedDay}
-          handleFilterChanged={handleFilterChanged}
-          handleSelectedDayChanged={handleSelectedDayChanged}
-        />
       </div>
     )
     /* eslint-enable react/jsx-no-bind */
