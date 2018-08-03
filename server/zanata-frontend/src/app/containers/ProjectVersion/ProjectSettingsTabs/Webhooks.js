f855be814c2a18e6c3a8c33982a0b4c1100ef2d6
kgough <kgough@redhat.com>
2018-08-03 16:02:16 +1000

    feat(ZNTA-2526) update webhook settings in storybook for new project version pages

--- server/zanata-frontend/src/app/containers/ProjectVersion/ProjectSettingsTabs/Webhooks.js
+++ server/zanata-frontend/src/app/containers/ProjectVersion/ProjectSettingsTabs/Webhooks.js
@@ -13,43 +13,77 @@ import Row from 'antd/lib/row'
 import 'antd/lib/row/style/css'
 import Select from 'antd/lib/select'
 import 'antd/lib/select/style/css'
+import Collapse from 'antd/lib/collapse'
+import 'antd/lib/collapse/style/css'
+import Checkbox from 'antd/lib/checkbox'
+import 'antd/lib/checkbox/style/css'
 
 const FormItem = Form.Item
 const Option = Select.Option
+const Panel = Collapse.Panel
 
 class Webhooks extends Component {
 
   render() {
-
+    const addWebhookContent = (
+      <Form layout='horizontal'>
+        <Row>
+          <FormItem label='Payload URL'>
+            <Input />
+          </FormItem>
+        </Row>
+        <Row>
+          <FormItem label='Webhook name'>
+            <Input />
+          </FormItem>
+        </Row>
+        <Row>
+          <FormItem label='Secret'>
+            <Input />
+          </FormItem>
+        </Row>
+        <h3>Type</h3>
+        <p>
+          <Checkbox>Translation milestone</Checkbox><br />
+          <span className='f6'>A document is 100% translated or approved</span>
+        </p>
+        <p>
+          <Checkbox>Translation update</Checkbox><br />
+          <span className='f6'>Translations are updated (singly or in a batch)</span>
+        </p>
+        <p>
+          <Checkbox>Project version</Checkbox><br />
+          <span className='f6'>Project version is created or removed</span>
+        </p>
+        <p>
+          <Checkbox>Project maintainer update</Checkbox><br />
+          <span className='f6'>Project maintainer is added or removed</span>
+        </p>
+        <p>
+          <Checkbox>Document</Checkbox><br />
+          <span className='f6'>Source document is added or removed</span>
+        </p>
+        <p>
+          <Checkbox>Manual event</Checkbox><br />
+          <span className='f6'>An event that can be triggered manually</span>
+        </p>
+      </Form>
+    )
     return (
       /* eslint-disable max-len */
       <div className='flexTab wideView permissions'>
         <h1 className='txt-info'><Icon name='code' className='s5 v-sub mr2' />
           <span className='fw4'>Webhooks</span></h1>
-        <h2>Add webhook</h2>
-        <Form layout='horizontal'>
-          <Row>
-            <FormItem label='Payload URL'>
-              <Input />
-            </FormItem>
-          </Row>
-          <Row>
-              <FormItem label='Type'>
-                <Select defaultValue="1">
-                  <Option value="1">DocumentMilestoneEvent - Trigger when a document is 100% translated or approved</Option>
-                  <Option value="2">DocumentStatsEvent - Trigger when translations are updated (singly or in a batch)</Option>
-                </Select>
-              </FormItem>
-            </Row>
+        <Collapse defaultActiveKey={'1'}>
+          <Panel header="Add webhook" key="1">
+            <h2>New webhook</h2>
+            {addWebhookContent}
             <Row>
-              <FormItem label='Secret'>
-                <Input />
-              </FormItem>
-          </Row>
-        </Form>
-        <Row>
-          <Button type='primary'>Save webhook</Button>
-        </Row>
+              <Button disabled className='mr3'>Test webhook</Button>
+              <Button type='primary'>Save webhook</Button>
+            </Row>
+          </Panel>
+        </Collapse>
       </div>
       /* eslint-enable max-len */
     )
