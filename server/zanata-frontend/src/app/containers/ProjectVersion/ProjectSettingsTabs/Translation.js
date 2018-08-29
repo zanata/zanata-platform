88694257b9a8b6a4493326d525eaad2e4980582c
kgough <kgough@redhat.com>
2018-08-30 09:14:22 +1000

    feat(ZNTA-2526) add code examples to project settings - translations panel

--- server/zanata-frontend/src/app/containers/ProjectVersion/ProjectSettingsTabs/Translation.js
+++ server/zanata-frontend/src/app/containers/ProjectVersion/ProjectSettingsTabs/Translation.js
@@ -45,71 +45,181 @@ class Translation extends Component {
         <p>Use these validations to keep translations consistent with the
           source text.</p>
         <h3>HTML/XML tags</h3>
-        <p className='w-100'>Check that XML/HTML tags are consistent.
-          <Collapse bordered={false}>
+        <p className='w-100'>Check that XML/HTML tags are consistent.</p>
+        <Radio.Group>
+          <Radio.Button>Off</Radio.Button>
+          <Radio.Button className='btn-warn'>Warning</Radio.Button>
+          <Radio.Button className='btn-danger'>Error</Radio.Button>
+        </Radio.Group>
+        <Collapse bordered={false}>
           <Panel showArrow={false}  header="Show example..." key="1">
             <Row>
-              <Col span={12}>
+              <Col span={10}>
                 SOURCE
                 <Highlight innerHTML={false}>
                   {'<p><strong>Hello world</strong></p>'}
                 </Highlight>
               </Col>
-              <Col span={12}>
+              <Col span={14}>
                 TARGET
+                <Highlight innerHTML={false}>
+                  {'<p><strong>Hello world</stong></p>'}
+                </Highlight>
               </Col>
             </Row>
           </Panel>
-        </Collapse></p>
-        <Radio.Group>
-          <Radio.Button>Off</Radio.Button>
-          <Radio.Button className='btn-warn'>Warning</Radio.Button>
-          <Radio.Button className='btn-danger'>Error</Radio.Button>
-        </Radio.Group>
+        </Collapse>
         <h3>Java variables</h3>
-        <p>Check that java style variables are consistent. More…</p>
+        <p>Check that java style variables are consistent.</p>
         <Radio.Group>
           <Radio.Button>Off</Radio.Button>
           <Radio.Button className='btn-warn'>Warning</Radio.Button>
           <Radio.Button className='btn-danger'>Error</Radio.Button>
         </Radio.Group>
+        <Collapse bordered={false}>
+          <Panel showArrow={false}  header="Show example..." key="2">
+            <Row>
+              <Col span={10}>
+                SOURCE
+                <Highlight innerHTML={false}>
+                  {'value must be between {0} and {1}'}
+                </Highlight>
+              </Col>
+              <Col span={14}>
+                TARGET
+                <Highlight innerHTML={false}>
+                  {'value must be between {0} and {2}'}
+                </Highlight>
+              </Col>
+            </Row>
+          </Panel>
+        </Collapse>
         <h3>Leading/trailing newline</h3>
-        <p>Check for consistent leading and trailing newline. More…</p>
+        <p>Check for consistent leading and trailing newline.</p>
         <Radio.Group>
           <Radio.Button>Off</Radio.Button>
           <Radio.Button className='btn-warn'>Warning</Radio.Button>
           <Radio.Button className='btn-danger'>Error</Radio.Button>
         </Radio.Group>
+        <Collapse bordered={false}>
+          <Panel showArrow={false}  header="Show example..." key="3">
+            <Row>
+              <Col span={10}>
+                SOURCE
+                <Highlight innerHTML={false}>
+                  {'\\n hello world with lead new line'}
+                </Highlight>
+              </Col>
+              <Col span={14}>
+                TARGET
+                <Highlight innerHTML={false}>
+                  {'missing \\n hello world with lead new line'}
+                </Highlight>
+              </Col>
+            </Row>
+          </Panel>
+        </Collapse>
         <h3>Positional printf (XSI extension)</h3>
-        <p>Check that positional printf style (%n$x) variables are consistent.
-          More…</p>
+        <p>Check that positional printf style (%n$x) variables are consistent.</p>
         <Radio.Group>
           <Radio.Button>Off</Radio.Button>
           <Radio.Button className='btn-warn'>Warning</Radio.Button>
           <Radio.Button className='btn-danger'>Error</Radio.Button>
         </Radio.Group>
+        <Collapse bordered={false}>
+          <Panel showArrow={false}  header="Show example..." key="4">
+            <Row>
+              <Col span={10}>
+                SOURCE
+                <Highlight innerHTML={false}>
+                  {'value must be between %x$1 and %y$2'}
+                </Highlight>
+              </Col>
+              <Col span={14}>
+                TARGET
+                <Highlight innerHTML={false}>
+                  {'value must be between %x$1 and %y$3'}
+                </Highlight>
+              </Col>
+            </Row>
+          </Panel>
+        </Collapse>
         <h3>Printf variables</h3>
-        <p>Check that printf style (%x) variables are consistent. More…</p>
+        <p>Check that printf style (%x) variables are consistent.</p>
         <Radio.Group>
           <Radio.Button>Off</Radio.Button>
           <Radio.Button className='btn-warn'>Warning</Radio.Button>
           <Radio.Button className='btn-danger'>Error</Radio.Button>
         </Radio.Group>
+        <Collapse bordered={false}>
+          <Panel showArrow={false}  header="Show example..." key="5">
+            <Row>
+              <Col span={10}>
+                SOURCE
+                <Highlight innerHTML={false}>
+                  {'value must be between %x and %y'}
+                </Highlight>
+              </Col>
+              <Col span={14}>
+                TARGET
+                <Highlight innerHTML={false}>
+                  {'value must be between %x and %z'}
+                </Highlight>
+              </Col>
+            </Row>
+          </Panel>
+        </Collapse>
         <h3>Tab characters</h3>
-        <p>Check whether source and target have the same number of tabs. More …
+        <p>Check whether source and target have the same number of tabs.
         </p>
         <Radio.Group>
           <Radio.Button>Off</Radio.Button>
           <Radio.Button className='btn-warn'>Warning</Radio.Button>
           <Radio.Button className='btn-danger'>Error</Radio.Button>
         </Radio.Group>
+        <Collapse bordered={false}>
+          <Panel showArrow={false}  header="Show example..." key="6">
+            <Row>
+              <Col span={10}>
+                SOURCE
+                <Highlight innerHTML={false}>
+                  {'\\t hello world'}
+                </Highlight>
+              </Col>
+              <Col span={14}>
+                TARGET
+                <Highlight innerHTML={false}>
+                  {'missing tab char (\\t) hello world'}
+                </Highlight>
+              </Col>
+            </Row>
+          </Panel>
+        </Collapse>
         <h3>XML entity reference</h3>
-        <p>Check that XML entities are complete. More…</p>
+        <p>Check that XML entities are complete.</p>
         <Radio.Group>
           <Radio.Button>Off</Radio.Button>
           <Radio.Button className='btn-warn'>Warning</Radio.Button>
           <Radio.Button className='btn-danger'>Error</Radio.Button>
         </Radio.Group>
+        <Collapse bordered={false}>
+          <Panel showArrow={false}  header="Show example..." key="7">
+            <Row>
+              <Col span={10}>
+                SOURCE
+                <Highlight innerHTML={false}>
+                  {'Pepper &amp; salt'}
+                </Highlight>
+              </Col>
+              <Col span={14}>
+                TARGET
+                <Highlight innerHTML={false}>
+                  {'Pepper amp incomplete entity, missing \'& and ;\' salt'}
+                </Highlight>
+              </Col>
+            </Row>
+          </Panel>
+        </Collapse>
         <h2>Copy translations</h2>
         <p>Copy Translations" attempts to reuse translations that have been
           entered in Zanata by matching them with untranslated strings in your
